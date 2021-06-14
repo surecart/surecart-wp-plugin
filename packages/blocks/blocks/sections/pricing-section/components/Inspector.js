923d07f02320deafdddfa36024595a087467f257
Andre <andre@themewich.com>
2021-06-14 15:24:32 -0500

    Pricing section inspector.

---
+++ packages/blocks/blocks/sections/pricing-section/components/Inspector.js
@@ -0,0 +1,50 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { InspectorControls } from '@wordpress/editor';
+
+import {
+	RangeControl,
+	PanelBody,
+	PanelRow,
+	RadioControl,
+} from '@wordpress/components';
+
+export default ( { attributes, setAttributes } ) => {
+	const { columns, type } = attributes;
+
+	return (
+		<InspectorControls>
+			<PanelBody title={ __( 'Attributes', 'checkout_engine' ) }>
+				<PanelRow>
+					<RadioControl
+						label={ __( 'Type', 'checkout_engine' ) }
+						help="The type of product selection"
+						selected={ type }
+						options={ [
+							{
+								label: __( 'Choose one', 'checkout_egine' ),
+								value: 'radio',
+							},
+							{
+								label: __( 'Choose many', 'checkout_engine' ),
+								value: 'checkbox',
+							},
+						] }
+						onChange={ ( type ) => setAttributes( { type } ) }
+					/>
+				</PanelRow>
+				<PanelRow>
+					<RangeControl
+						label={ __( 'Columns', 'checkout_engine' ) }
+						value={ columns }
+						onChange={ ( columns ) => setAttributes( { columns } ) }
+						min={ 1 }
+						max={ 5 }
+					/>
+				</PanelRow>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
--- packages/blocks/blocks/sections/pricing-section/edit.js
+++ packages/blocks/blocks/sections/pricing-section/edit.js
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { RichText } from '@wordpress/block-editor';
 import InnerBlocks from '../../../components/InnerBlocks';
+import Inspector from './components/Inspector';
 
 /**
  * Component Dependencies
@@ -14,6 +15,7 @@ export default ( { className, attributes, setAttributes, isSelected } ) => {
 	const {
 		default: defaultChoice,
 		products,
+		columns,
 		type,
 		label,
 		description,
@@ -25,7 +27,10 @@ export default ( { className, attributes, setAttributes, isSelected } ) => {
 		// }
 
 		return (
-			<CeChoices className={ className }>
+			<CeChoices
+				className={ className }
+				style={ { '--columns': columns } }
+			>
 				<CeChoice name="plan" type={ type } required checked>
 					Gold Plan
 					<span slot="description">$9.99, then $49.99 per month</span>
@@ -39,31 +44,37 @@ export default ( { className, attributes, setAttributes, isSelected } ) => {
 	};
 
 	return (
-		<CeFormSection>
-			<RichText
-				slot="label"
-				aria-label={ __( 'Label' ) }
-				placeholder={ __( 'Add a title' ) }
-				value={ label }
-				onChange={ ( value ) => setAttributes( { label: value } ) }
-				withoutInteractiveFormatting
-				allowedFormats={ [ 'core/bold', 'core/italic' ] }
+		<div>
+			<Inspector
+				attributes={ attributes }
+				setAttributes={ setAttributes }
 			/>
-			<RichText
-				slot="description"
-				aria-label={ __( 'Description' ) }
-				placeholder={ __( 'Add a description' ) }
-				value={ description }
-				onChange={ ( value ) =>
-					setAttributes( { description: value } )
-				}
-				withoutInteractiveFormatting
-				allowedFormats={ [ 'core/bold', 'core/italic' ] }
-			/>
+			<CeFormSection>
+				<RichText
+					slot="label"
+					aria-label={ __( 'Label' ) }
+					placeholder={ __( 'Add a title' ) }
+					value={ label }
+					onChange={ ( value ) => setAttributes( { label: value } ) }
+					withoutInteractiveFormatting
+					allowedFormats={ [ 'core/bold', 'core/italic' ] }
+				/>
+				<RichText
+					slot="description"
+					aria-label={ __( 'Description' ) }
+					placeholder={ __( 'Add a description' ) }
+					value={ description }
+					onChange={ ( value ) =>
+						setAttributes( { description: value } )
+					}
+					withoutInteractiveFormatting
+					allowedFormats={ [ 'core/bold', 'core/italic' ] }
+				/>
 
-			{ productsSelector() }
+				{ productsSelector() }
 
-			<InnerBlocks templateLock={ false } />
-		</CeFormSection>
+				<InnerBlocks templateLock={ false } />
+			</CeFormSection>
+		</div>
 	);
 };
