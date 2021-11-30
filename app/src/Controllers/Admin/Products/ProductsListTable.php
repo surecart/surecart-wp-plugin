6c66bc5889958500e9a4b0636d141b328af9ba7f
Andre Gagnon <ajgagnon@uwalumni.com>
2021-11-30 15:56:48 -0600

    Product images on list table.

--- app/src/Controllers/Admin/Products/ProductsListTable.php
+++ app/src/Controllers/Admin/Products/ProductsListTable.php
@@ -231,6 +231,19 @@ class ProductsListTable extends ListTable {
 	public function column_name( $product ) {
 		ob_start();
 		?>
+
+		<div class="ce-product-name">
+		<?php if ( $product->image_url ) { ?>
+			<img src="<?php echo esc_url( $product->image_url ); ?>" class="ce-product-image-preview" />
+		<?php } else { ?>
+		<div class="ce-product-image-preview">
+			<svg xmlns="http://www.w3.org/2000/svg" style="width: 18px; height: 18px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+			  </svg>
+		</div>
+	  <?php } ?>
+
+	  <div>
 		<a class="row-title" aria-label="<?php echo esc_attr( 'Edit Product', 'checkout_engine' ); ?>" href="<?php echo esc_url( \CheckoutEngine::getUrl()->edit( 'product', $product->id ) ); ?>">
 			<?php echo esc_html_e( $product->name ); ?>
 		</a>
@@ -243,7 +256,9 @@ class ProductsListTable extends ListTable {
 			],
 		);
 		?>
+		</div>
 
+		</div>
 		<?php
 		return ob_get_clean();
 	}
