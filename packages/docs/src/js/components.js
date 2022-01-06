ec755a109b07f492487bd63fdb54da3626e340bd
Andre Gagnon <ajgagnon@uwalumni.com>
2022-01-06 16:37:58 -0600

    window is undefined

--- packages/docs/src/js/components.js
+++ packages/docs/src/js/components.js
@@ -2,5 +2,3 @@ if ( typeof window !== 'undefined' ) {
 	const { defineCustomElements } = require( '@checkout-engine/components' );
 	defineCustomElements();
 }
-// @ts-ignore
-window.Buffer = window.Buffer || require( 'buffer' ).Buffer;
