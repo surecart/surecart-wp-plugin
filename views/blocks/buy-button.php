651e6832c2fb09fc5ce8d2a314d1615188745e98
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-22 17:36:32 -0500

    Add style to buy button.

--- views/blocks/buy-button.php
+++ views/blocks/buy-button.php
@@ -1,3 +1,3 @@
-<sc-button href="<?php echo esc_url( $href ); ?>" type="<?php echo esc_attr( $type ); ?> size="<?php echo esc_attr( $size ); ?>>
+<sc-button href="<?php echo esc_url( $href ); ?>" style="<?php echo esc_attr( $style ); ?>" type="<?php echo esc_attr( $type ); ?> size="<?php echo esc_attr( $size ); ?>>
 	<?php echo esc_html( $label ); ?>
 </sc-button>
