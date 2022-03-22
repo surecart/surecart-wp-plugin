9f2a78271fcbc852ab436aab695d1aef80a909b6
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-22 17:36:14 -0500

    Fix user profile view.

--- views/admin/user-profile.php
+++ views/admin/user-profile.php
@@ -2,15 +2,28 @@
 
 <table class="form-table">
 	<tr>
-		<th><?php esc_html_e( 'Customer', 'presto-player-pro' ); ?></th>
-		<td>
-			<?php if ( $customer ) : ?>
-				<a href="<?php echo esc_url( $edit_link ); ?>">
-					<?php echo wp_kses_post( $customer->name ?? $customer->email ); ?>
+		<th><?php esc_html_e( 'Customer', 'surecart' ); ?></th>
+
+		<?php if ( ! empty( $test_customer ) ) : ?>
+			<td>
+				<a href="<?php echo esc_url( $edit_test_link ); ?>">
+					<?php echo wp_kses_post( $test_customer->name ?? $test_customer->email ); ?>
 				</a>
-			<?php else : ?>
-				<?php esc_html_e( 'This user is not a customer.', 'surecart' ); ?>
-			<?php endif; ?>
-		</td>
+
+				(<?php esc_html_e( 'Test', 'surecart' ); ?>)
+			</td>
+		<?php endif; ?>
+
+		<?php if ( ! empty( $live_customer ) ) : ?>
+			<td>
+				<a href="<?php echo esc_url( $edit_live_link ); ?>">
+					<?php echo wp_kses_post( $live_customer->name ?? $live_customer->email ); ?>
+				</a>
+			</td>
+		<?php endif; ?>
+
+		<?php if ( empty( $live_customer ) && empty( $test_customer ) ) : ?>
+			<?php esc_html_e( 'This user is not a customer.', 'surecart' ); ?>
+		<?php endif; ?>
 	</tr>
 </table>
