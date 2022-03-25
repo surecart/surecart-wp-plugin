11c8b0eb58a64c63a1dbca2fe481a5a0dea78b02
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-25 15:10:59 -0500

    Show alert for now if session expires.

--- packages/components/src/functions/fetch.ts
+++ packages/components/src/functions/fetch.ts
@@ -8,13 +8,15 @@ fetchImplementation.use((options, next) => {
   result.catch(e => {
     if (e?.code === 'rest_cookie_invalid_nonce') {
       nonceRefreshCount++;
-      if (nonceRefreshCount > 3) {
+      if (nonceRefreshCount === 3) {
+        alert('Your session expired. Please reload the page and try again.');
         throw {
           code: 'refresh_page',
           message: 'Your session expired. Please reload the page.',
         };
       }
     }
+    throw e;
   });
   return result;
 });
