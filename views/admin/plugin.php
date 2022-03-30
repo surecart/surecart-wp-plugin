3bedbe7722d7703eaf5822edf86b95f56e4ed120
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-29 21:44:46 -0500

    Change position of "find my api token"

--- views/admin/plugin.php
+++ views/admin/plugin.php
@@ -80,13 +80,15 @@
 					<sc-text style="margin-bottom: 1em; --line-height:1; --color: var(--sc-color-gray-500)"><?php esc_html_e( 'Update your api token to change or update the connection to SureCart.', 'surecart' ); ?></sc-text>
 					<sc-card>
 						<sc-input label="<?php echo esc_attr_e( 'Api Token', 'surecart' ); ?>" type="password" value="<?php echo esc_attr( $api_token ); ?>" name="api_token" placeholder="<?php echo esc_attr_e( 'Enter your api token.', 'surecart' ); ?>"></sc-input>
-						<?php if ( defined( 'SURECART_APP_URL' ) ) : ?>
-						<sc-button href="<?php echo esc_url( SURECART_APP_URL ); ?>" target="_blank">
-							<?php esc_html_e( 'Find My Api Token', 'surecart' ); ?>
-							<sc-icon name="arrow-right" slot="suffix"></sc-icon>
-						</sc-button>
-						<?php endif; ?>
 					</sc-card>
+					<?php if ( defined( 'SURECART_APP_URL' ) ) : ?>
+						<sc-flex justify-content="center">
+							<sc-button href="<?php echo esc_url( SURECART_APP_URL ); ?>" type="link" target="_blank">
+								<?php esc_html_e( 'Find My Api Token', 'surecart' ); ?>
+								<sc-icon name="arrow-right" slot="suffix"></sc-icon>
+							</sc-button>
+						</sc-flex>
+						<?php endif; ?>
 				</sc-flex>
 
 				<sc-flex flex-direction="column">
