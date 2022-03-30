c298e0103d3c4986a1e003cefaf9dab9084090a3
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-29 20:48:07 -0500

    Use built-in fetch for components for now.

--- packages/components/src/functions/fetch.ts
+++ packages/components/src/functions/fetch.ts
@@ -1,24 +1,35 @@
 import apiFetch from '@wordpress/api-fetch';
 
-const fetchImplementation = window?.wp?.apiFetch ? window.wp.apiFetch : apiFetch;
-let nonceRefreshCount = 0;
+apiFetch.use(apiFetch.createRootURLMiddleware(window?.scData?.root_url));
+// @ts-ignore
+apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware(window?.scData?.nonce);
+// @ts-ignore
+apiFetch.use(apiFetch.nonceMiddleware);
+// @ts-ignore
+apiFetch.nonceEndpoint = window?.scData?.nonce_endpoint;
 
-fetchImplementation.use((options, next) => {
-  const result = next(options);
-  result.catch(e => {
-    if (e?.code === 'rest_cookie_invalid_nonce') {
-      nonceRefreshCount++;
-      if (nonceRefreshCount === 3) {
-        alert('Your session expired. Please reload the page and try again.');
-        throw {
-          code: 'refresh_page',
-          message: 'Your session expired. Please reload the page.',
-        };
-      }
-    }
-    throw e;
-  });
-  return result;
-});
+export default apiFetch;
+// let nonceRefreshCount = 0;
 
-export default fetchImplementation;
+// console.log(window?.scData?.root_url);
+// fetchImplementation.use(fetchImplementation.createRootURLMiddleware(window?.scData?.root_url));
+
+// fetchImplementation.use((options, next) => {
+//   const result = next(options);
+//   result.catch(e => {
+//     if (e?.code === 'rest_cookie_invalid_nonce') {
+//       nonceRefreshCount++;
+//       if (nonceRefreshCount === 3) {
+//         alert('Your session expired. Please reload the page and try again.');
+//         throw {
+//           code: 'refresh_page',
+//           message: 'Your session expired. Please reload the page.',
+//         };
+//       }
+//     }
+//     throw e;
+//   });
+//   return result;
+// });
+
+// export default fetchImplementation;
