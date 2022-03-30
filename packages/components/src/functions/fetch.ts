9eb1cdad445faf559d3448738b4783c99022e605
Andre Gagnon <ajgagnon@uwalumni.com>
2022-03-29 20:50:12 -0500

    Remove comments.

--- packages/components/src/functions/fetch.ts
+++ packages/components/src/functions/fetch.ts
@@ -9,27 +9,3 @@ apiFetch.use(apiFetch.nonceMiddleware);
 apiFetch.nonceEndpoint = window?.scData?.nonce_endpoint;
 
 export default apiFetch;
-// let nonceRefreshCount = 0;
-
-// console.log(window?.scData?.root_url);
-// fetchImplementation.use(fetchImplementation.createRootURLMiddleware(window?.scData?.root_url));
-
-// fetchImplementation.use((options, next) => {
-//   const result = next(options);
-//   result.catch(e => {
-//     if (e?.code === 'rest_cookie_invalid_nonce') {
-//       nonceRefreshCount++;
-//       if (nonceRefreshCount === 3) {
-//         alert('Your session expired. Please reload the page and try again.');
-//         throw {
-//           code: 'refresh_page',
-//           message: 'Your session expired. Please reload the page.',
-//         };
-//       }
-//     }
-//     throw e;
-//   });
-//   return result;
-// });
-
-// export default fetchImplementation;
