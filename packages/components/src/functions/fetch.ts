4e985565a861ebd4cf59f30a67393016946fe2ef
Andre Gagnon <ajgagnon@uwalumni.com>
2022-04-12 12:02:44 -0500

    Conditionally set middleware for fetch.

--- packages/components/src/functions/fetch.ts
+++ packages/components/src/functions/fetch.ts
@@ -1,11 +1,13 @@
 import apiFetch from '@wordpress/api-fetch';
 
-apiFetch.use(apiFetch.createRootURLMiddleware(window?.scData?.root_url));
-// @ts-ignore
-apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware(window?.scData?.nonce);
-// @ts-ignore
-apiFetch.use(apiFetch.nonceMiddleware);
-// @ts-ignore
-apiFetch.nonceEndpoint = window?.scData?.nonce_endpoint;
+if (window?.scData) {
+  apiFetch.use(apiFetch.createRootURLMiddleware(window?.scData?.root_url));
+  // @ts-ignore
+  apiFetch.nonceMiddleware = apiFetch.createNonceMiddleware(window?.scData?.nonce);
+  // @ts-ignore
+  apiFetch.use(apiFetch.nonceMiddleware);
+  // @ts-ignore
+  apiFetch.nonceEndpoint = window?.scData?.nonce_endpoint;
+}
 
 export default apiFetch;
