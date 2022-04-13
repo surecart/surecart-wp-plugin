f4105bdd8185f14ce614b9a9001c11f4764c6fd7
Andre Gagnon <ajgagnon@uwalumni.com>
2022-04-13 15:36:48 -0500

    Maybe load initial styles for when not hydrated yet.

--- app/src/WordPress/Assets/AssetsServiceProvider.php
+++ app/src/WordPress/Assets/AssetsServiceProvider.php
@@ -121,13 +121,13 @@ class AssetsServiceProvider implements ServiceProviderInterface {
 	 * @return void
 	 */
 	public function registerComponentScripts() {
-		wp_register_script(
-			'surecart-components',
-			trailingslashit( \SureCart::core()->assets()->getUrl() ) . 'dist/components/surecart/surecart.esm.js',
-			[ 'wp-api-fetch' ],
-			filemtime( trailingslashit( $this->container[ SURECART_CONFIG_KEY ]['app_core']['path'] ) . 'dist/components/surecart/surecart.esm.js' ),
-			false
-		);
+		// wp_register_script(
+		// 	'surecart-components',
+		// 	trailingslashit( \SureCart::core()->assets()->getUrl() ) . 'dist/components/surecart/surecart.esm.js',
+		// 	[ 'wp-api-fetch' ],
+		// 	filemtime( trailingslashit( $this->container[ SURECART_CONFIG_KEY ]['app_core']['path'] ) . 'dist/components/surecart/surecart.esm.js' ),
+		// 	false
+		// );
 
 		wp_set_script_translations( 'surecart-components', 'surecart', WP_LANG_DIR . '/plugins/' );
 
@@ -205,16 +205,30 @@ class AssetsServiceProvider implements ServiceProviderInterface {
 		wp_enqueue_style( 'surecart-themes-default' );
 
 		$brand  = \SureCart::account()->brand;
-		$style  = 'sc-form, sc-checkout {
-			visibility: hidden;
-			opacity: 0;
-			transition: opacity 0.1s ease;
-		}
-		sc-checkout.hydrated,
-		sc-form.hydrated {
-			visibility: visible;
-			opacity: 1;
+		$style  = '
+		sc-form {
+			display: block;
+		  }
+
+		  sc-form form > *:not(:last-child) {
+			margin-bottom: var(--sc-form-row-spacing, 0.75em);
+
+			&.wp-block-spacer {
+			  margin-bottom: 0;
+			}
+		  }
+
+		  sc-form .wp-block-columns .wp-block-column {
+			margin: 50px;
+		  }
+
+		sc-input:not(.hydrated), sc-customer-name:not(.hydrated), sc-customer-email:not(.hydrated) {
+			height: 50px;
+			width: 100%;
+			display: block;
+			background: var(--sc-color-gray-500);
 		}';
+
 		$style .= ':root {';
 		$style .= '--sc-color-primary-500: #' . ( $brand->color ?? '000' ) . ';';
 		$style .= '--sc-focus-ring-color-primary: #' . ( $brand->color ?? '000' ) . ';';
