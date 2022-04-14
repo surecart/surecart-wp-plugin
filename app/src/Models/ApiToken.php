1995310ff6cb94a7d03673df61587b66a988aaba
Andre Gagnon <ajgagnon@uwalumni.com>
2022-04-14 08:40:20 -0500

    No longer store a salt ref since it's not needed.

--- app/src/Models/ApiToken.php
+++ app/src/Models/ApiToken.php
@@ -17,24 +17,13 @@ class ApiToken {
 	protected $key = 'sc_api_token';
 
 	/**
-	 * Reference for the salt.
-	 *
-	 * @var string
-	 */
-	protected $salt_ref_key = 'sc_salt_ref';
-
-	/**
 	 * Save and encrypt the API token.
 	 *
 	 * @param string $value The API token.
 	 * @return bool True if the value was updated, false otherwise.
 	 */
 	protected function save( $value ) {
-		$updated = update_option( $this->key, Encryption::encrypt( $value ) );
-		// save the encryption salt reference when the token is updated.
-		if ( $updated ) {
-			$this->saveSaltEnd();
-		}
+		return update_option( $this->key, Encryption::encrypt( $value ) );
 	}
 
 	/**
@@ -44,25 +33,6 @@ class ApiToken {
 	 */
 	protected function get() {
 		return Encryption::decrypt( get_option( $this->key, '' ) );
-	}
-
-	/**
-	 * Save just the last 6 characters of the salt used to notify
-	 * the user if it's been changed.
-	 */
-	protected function saveSaltEnd() {
-		$key      = Encryption::getDefaultKey();
-		$just_end = substr( $key, -6 );
-		return update_option( $this->salt_ref_key, $just_end );
-	}
-
-	/**
-	 * Get a refernce to the salt used to encrypt the API token.
-	 *
-	 * @return string The salt end.
-	 */
-	protected function getSaltRef() {
-		return get_option( $this->salt_ref_key, '' );
 	}
 
 	/**
