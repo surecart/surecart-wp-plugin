f196fa814d585b67421009df608a0ca9582398f9
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-02 13:52:06 -0500

    Remove version as it's not in this branch.

--- app/src/Request/RequestService.php
+++ app/src/Request/RequestService.php
@@ -96,9 +96,6 @@ class RequestService {
 				$args['headers']['Authorization'] = "Bearer $this->token";
 			}
 
-			// add version header.
-			$args['headers']['X-SURECART-WP-PLUGIN-VERSION'] = \SureCart::plugin()->version();
-
 			// parse args.
 			$args = wp_parse_args(
 				$args,
