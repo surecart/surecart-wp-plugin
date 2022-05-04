2fb459235872185e2e3554c4b939745056e8904d
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-04 16:40:50 -0500

    Remove redirect response.
    # Conflicts:
    #	app/src/Middleware/ComponentAssetsMiddleware.php

---
+++ app/src/Middleware/ComponentAssetsMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SureCart\Middleware;
+
+use Closure;
+use SureCartCore\Requests\RequestInterface;
+
+/**
+ * Middleware for handling model archiving.
+ */
+class ComponentAssetsMiddleware {
+	/**
+	 * Enqueue component assets.
+	 *
+	 * @param RequestInterface $request Request.
+	 * @param Closure          $next Next.
+	 * @param string           $model_name Model name.
+	 * @return function
+	 */
+	public function handle( RequestInterface $request, Closure $next ) {
+		\SureCart::assets()->enqueueComponents();
+		return $next( $request );
+	}
+}
