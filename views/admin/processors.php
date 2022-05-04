cfbc4dc30e503f97ca0dc29ea9e01b71734652a9
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-04 14:35:35 -0500

    Check if processors are empty to prevent errors with unconnected processors.

--- views/admin/processors.php
+++ views/admin/processors.php
@@ -59,16 +59,16 @@
 							</sc-text>
 
 							<div>
-								<?php if ( $processors->stripe_live && $processors->stripe_test ) : ?>
+								<?php if ( ! empty( $processors->stripe_live ) && ! empty( $processors->stripe_test ) ) : ?>
 									<sc-tag type="success"><?php esc_html_e( 'Connected', 'surecart' ); ?></sc-tag>
 								<?php else : ?>
-									<?php if ( $processors->stripe_live ) : ?>
-										<sc-tag type="success"><?php esc_html_e( 'Live Connected', 'surecart' ); ?></sc-tag>
-										<sc-tag type="default"><?php esc_html_e( 'Test Not Connected', 'surecart' ); ?></sc-tag>
+									<?php if ( ! empty( $processors->stripe_live ) ) : ?>
+										<sc-tag type="success"><?php esc_html_e( 'Live Payments Allowed', 'surecart' ); ?></sc-tag>
+										<sc-tag type="warning"><?php esc_html_e( 'Test Payment Not Allowed', 'surecart' ); ?></sc-tag>
 									<?php endif; ?>
-									<?php if ( $processors->stripe_test ) : ?>
-										<sc-tag type="info"><?php esc_html_e( 'Test Connected', 'surecart' ); ?></sc-tag>
-										<sc-tag type="default"><?php esc_html_e( 'Live Not Connected', 'surecart' ); ?></sc-tag>
+									<?php if ( ! empty( $processors->stripe_test ) ) : ?>
+										<sc-tag type="info"><?php esc_html_e( 'Test Payments Allowed', 'surecart' ); ?></sc-tag>
+										<sc-tag type="warning"><?php esc_html_e( 'Live Payments Not Allowed', 'surecart' ); ?></sc-tag>
 									<?php endif; ?>
 								<?php endif; ?>
 							</div>
@@ -137,7 +137,7 @@
 								</sc-text>
 
 								<div>
-									<?php if ( $processors->paypal_live ) : ?>
+									<?php if ( ! empty( $processors->paypal_live ) ) : ?>
 										<sc-tag type="success"><?php esc_html_e( 'Connected', 'surecart' ); ?></sc-tag>
 									<?php else : ?>
 										<sc-tag type="default"><?php esc_html_e( 'Not Connected', 'surecart' ); ?></sc-tag>
@@ -211,7 +211,7 @@
 								<?php esc_html_e( 'Connect with a PayPal sandbox account to test your integration before going live.', 'surecart' ); ?>
 							</sc-text>
 							<div>
-								<?php if ( $processors->paypal_test ) : ?>
+								<?php if ( ! empty( $processors->paypal_test ) ) : ?>
 									<sc-tag type="success"><?php esc_html_e( 'Connected', 'surecart' ); ?></sc-tag>
 								<?php else : ?>
 									<sc-tag type="default"><?php esc_html_e( 'Not Connected', 'surecart' ); ?></sc-tag>
