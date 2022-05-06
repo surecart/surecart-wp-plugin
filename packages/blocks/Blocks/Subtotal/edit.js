5a0b431bd5d959323d3fa2eae83cb1a70362b761
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-06 08:33:43 -0500

    Add label and Label to subtotal block.

--- packages/blocks/Blocks/Subtotal/edit.js
+++ packages/blocks/Blocks/Subtotal/edit.js
@@ -1,8 +1,28 @@
-export default ({ attributes }) => {
+import { __ } from '@wordpress/i18n';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, PanelRow, TextControl } from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
+
+export default ({ attributes, setAttributes }) => {
 	const { text } = attributes;
+
 	return (
-		<sc-line-item-total total="subtotal">
-			<span slot="description">{text}</span>
-		</sc-line-item-total>
+		<Fragment>
+			<InspectorControls>
+				<PanelBody title={__('Attributes', 'surecart')}>
+					<PanelRow>
+						<TextControl
+							label={__('Label', 'surecart')}
+							value={text}
+							onChange={(text) => setAttributes({ text })}
+						/>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+
+			<sc-line-item-total total="subtotal">
+				<span slot="description">{text}</span>
+			</sc-line-item-total>
+		</Fragment>
 	);
 };
