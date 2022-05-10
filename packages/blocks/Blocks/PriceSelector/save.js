9a2cc67faa123d074c6bcd335675a736bb44a4aa
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-10 11:34:30 -0500

    Remove unused attribute.

--- packages/blocks/Blocks/PriceSelector/save.js
+++ packages/blocks/Blocks/PriceSelector/save.js
@@ -4,7 +4,7 @@
 import { InnerBlocks } from '@wordpress/block-editor';
 
 export default ({ attributes }) => {
-	const { label, type, columns, required } = attributes;
+	const { label, type, columns } = attributes;
 	return (
 		<sc-price-choices label={label} type={type} columns={columns}>
 			<div>
