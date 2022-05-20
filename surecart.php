8d266f96dd1bcc5f1f91ac2566137efaed878205
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-20 16:22:39 -0500

    Beta version.

--- surecart.php
+++ surecart.php
@@ -3,7 +3,7 @@
  * Plugin Name: SureCart
  * Plugin URI: https://surecart.com/
  * Description:
- * Version: 0.13.0
+ * Version: 0.13.0-beta1
  * Requires at least: 5.9
  * Requires PHP: 7.4
  * Author: SureCart
