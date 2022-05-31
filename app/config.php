6d616ac8273664112a941bc07e8c8500f2166bf2
Andre Gagnon <ajgagnon@uwalumni.com>
2022-05-31 09:33:33 -0500

    TutorLMS listing

--- app/config.php
+++ app/config.php
@@ -67,6 +67,7 @@ return [
 		// integrations.
 		\SureCart\Integrations\DiviServiceProvider::class,
 		\SureCart\Integrations\LearnDash\LearnDashServiceProvider::class,
+		\SureCart\Integrations\TutorLMS\TutorLMSServiceProvider::class,
 		\SureCart\Integrations\User\UserServiceProvider::class,
 	],
 
