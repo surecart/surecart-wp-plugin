299c8c8d65f7af69c633fd22ea48a551f990a417
Andre Gagnon <ajgagnon@uwalumni.com>
2022-06-08 13:21:17 -0500

    Change to 60 SECONDS :/

--- app/src/Account/AccountService.php
+++ app/src/Account/AccountService.php
@@ -26,7 +26,7 @@ class AccountService {
 		$this->account = get_transient( 'surecart_account' );
 		if ( false === $this->account ) {
 			$this->account = Account::with( [ 'brand', 'portal_protocol', 'tax_protocol' ] )->find();
-			set_transient( 'surecart_account', $this->account, 60 * MINUTE_IN_SECONDS );
+			set_transient( 'surecart_account', $this->account, 60 );
 		}
 	}
 
