a8d478f26b2c3737e0e31f63e941e4958aa6b818
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-09 11:14:23 -0500

    Don't look at plugin version for general migration.

--- app/src/Database/GeneralMigration.php
+++ app/src/Database/GeneralMigration.php
@@ -49,13 +49,8 @@ abstract class GeneralMigration {
 	 * @return boolean
 	 */
 	public function shouldMigrate() {
-		// we've already done this migration.
-		if ( $this->version < $this->getLastMigrationVersion() ) {
-			return false;
-		}
-
-		// we will run it if the migrated version is less than the current version of the plugin.
-		return version_compare( $this->getLastMigrationVersion(), \SureCart::plugin()->version(), '<' );
+		// check if we already have done this migration.
+		return version_compare( $this->version, $this->getLastMigrationVersion(), '>=' );
 	}
 
 	/**
