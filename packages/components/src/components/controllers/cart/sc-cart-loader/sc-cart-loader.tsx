4d6a287b849889ab549b55a81e241875451f65c0
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-09 11:16:45 -0500

    z-index for cart loader.

--- packages/components/src/components/controllers/cart/sc-cart-loader/sc-cart-loader.tsx
+++ packages/components/src/components/controllers/cart/sc-cart-loader/sc-cart-loader.tsx
@@ -1,34 +1,30 @@
-import { Component, h, Prop, State } from '@stencil/core';
-import { Order } from '../../../../types';
+import { Component, h, Prop } from '@stencil/core';
 import { getOrder } from '../../../../store/checkouts';
 import uiStore from '../../../../store/ui';
 
 @Component({
   tag: 'sc-cart-loader',
+  styleUrl: 'sc-cart-loader.scss',
   shadow: true,
 })
 export class ScCartLoader {
   /** The form id to use for the cart. */
   @Prop({ reflect: true }) formId: string;
 
-  @Prop() template: string;
-
+  /** The mode for the form. */
   @Prop() mode: 'live' | 'test' = 'live';
 
-  @State() order: Order;
-
-  @State() loaded: boolean;
-
-  /**
-   * Search for the sc-checkout component on a page to make sure
-   * we don't show the cart on a checkout page.
-   */
-  pageHasForm() {
-    return !!document.querySelector('sc-checkout');
-  }
+  /** The cart template to inject when opened. */
+  @Prop() template: string;
 
   render() {
-    if (this.pageHasForm()) return null;
+    /**
+     * Search for the sc-checkout component on a page to make sure
+     * we don't show the cart on a checkout page.
+     */
+    if (!!document.querySelector('sc-checkout')) return null;
+
+    // return the loader.
     return <div innerHTML={getOrder(this.formId, this.mode)?.line_items?.pagination?.count || uiStore?.state?.cart?.open ? this.template : ''}></div>;
   }
 }
