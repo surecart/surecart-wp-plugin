116bed78b9aa146d22de4127b08e97134e67b20c
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-26 13:37:26 -0500

    Add slide out cart disabled setting

--- app/src/Controllers/Rest/SettingsController.php
+++ app/src/Controllers/Rest/SettingsController.php
@@ -19,11 +19,12 @@ class SettingsController {
 	public function find( \WP_REST_Request $request ) {
 		return rest_ensure_response(
 			[
-				'object'                 => 'settings',
-				'api_token'              => ApiToken::get(),
-				'uninstall'              => (bool) get_option( 'sc_uninstall', false ),
-				'stripe_payment_element' => (bool) get_option( 'sc_stripe_payment_element', false ),
-				'use_esm_loader'         => (bool) get_option( 'surecart_use_esm_loader', false ),
+				'object'                  => 'settings',
+				'api_token'               => ApiToken::get(),
+				'uninstall'               => (bool) get_option( 'sc_uninstall', false ),
+				'stripe_payment_element'  => (bool) get_option( 'sc_stripe_payment_element', false ),
+				'use_esm_loader'          => (bool) get_option( 'surecart_use_esm_loader', false ),
+				'slide_out_cart_disabled' => (bool) get_option( 'sc_slide_out_cart_disabled', false ),
 			]
 		);
 	}
@@ -62,6 +63,11 @@ class SettingsController {
 		// update performance option.
 		if ( isset( $request['use_esm_loader'] ) ) {
 			update_option( 'surecart_use_esm_loader', $request->get_param( 'use_esm_loader' ) );
+		}
+
+		// update slide out cart option.
+		if ( isset( $request['slide_out_cart_disabled'] ) ) {
+			update_option( 'sc_slide_out_cart_disabled', (bool) $request->get_param( 'slide_out_cart_disabled' ) );
 		}
 
 		return rest_ensure_response( $this->find( $request ) );
