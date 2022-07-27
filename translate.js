cdf431c4db20b69e454b6526b2bff486f0f2f201
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-27 09:48:03 -0500

    Add separate translation build file

---
+++ translate.js
@@ -0,0 +1,22 @@
+var shell = require('shelljs');
+
+// app directory
+shell.exec(
+	'./vendor/bin/wp i18n make-pot ./app languages/surecart.pot --ignore-domain --skip-audit'
+);
+// views directory
+shell.exec(
+	'./vendor/bin/wp i18n make-pot ./views languages/surecart.pot --ignore-domain --merge --skip-audit'
+);
+// admin directory
+shell.exec(
+	'./vendor/bin/wp i18n make-pot ./packages/admin languages/surecart.pot --ignore-domain --merge --skip-audit'
+);
+// blocks directory
+shell.exec(
+	'./vendor/bin/wp i18n make-pot ./packages/blocks --exclude=node_modules,dist languages/surecart.pot --ignore-domain --merge --skip-audit'
+);
+// components directory
+shell.exec(
+	'./vendor/bin/wp i18n make-pot ./dist/components/surecart languages/surecart.pot --ignore-domain --merge --skip-audit'
+);
