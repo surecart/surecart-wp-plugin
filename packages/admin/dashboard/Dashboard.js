539cdd0b51424cc16ddbdacd01373b3dbbf312c4
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-29 12:42:27 -0500

    Add spacing css variable to dashboard

--- packages/admin/dashboard/Dashboard.js
+++ packages/admin/dashboard/Dashboard.js
@@ -14,43 +14,43 @@ import LearnMore from './components/LearnMore';
 import Overview from './components/overview/Overview';
 
 import {
-    ScFlex,
-    ScDivider,
-    ScBreadcrumbs,
-    ScBreadcrumb,
+	ScFlex,
+	ScDivider,
+	ScBreadcrumbs,
+	ScBreadcrumb,
 } from '@surecart/components-react';
 
 export default () => {
-    return (
-        <DashboardModel
-            title={
-                <div
-                    css={css`
-                        display: flex;
-                        align-items: center;
-                        gap: 1em;
-                    `}
-                >
-                    <ScBreadcrumbs>
-                        <ScBreadcrumb>
-                            <Logo display="block" />
-                        </ScBreadcrumb>
-                        <ScBreadcrumb href="admin.php?page=sc-dashboard">
-                            {__('Dashboard', 'surecart')}
-                        </ScBreadcrumb>
-                    </ScBreadcrumbs>
-                </div>
-            }
-        >
-            <Fragment>
-                <GetStarted />
-                <Overview />
-                <ScDivider style={{"--spacing": "1em"}} />
-                <ScFlex columnGap="2em">
-                    <RecentOrders />
-                    <LearnMore />
-                </ScFlex>
-            </Fragment>
-        </DashboardModel>
-    );
+	return (
+		<DashboardModel
+			title={
+				<div
+					css={css`
+						display: flex;
+						align-items: center;
+						gap: 1em;
+					`}
+				>
+					<ScBreadcrumbs>
+						<ScBreadcrumb>
+							<Logo display="block" />
+						</ScBreadcrumb>
+						<ScBreadcrumb href="admin.php?page=sc-dashboard">
+							{__('Dashboard', 'surecart')}
+						</ScBreadcrumb>
+					</ScBreadcrumbs>
+				</div>
+			}
+		>
+			<Fragment>
+				<GetStarted />
+				<Overview />
+				<ScDivider style={{ '--spacing': '1em' }} />
+				<ScFlex style={{ '--sc-flex-column-gap': '2em' }}>
+					<RecentOrders />
+					<LearnMore />
+				</ScFlex>
+			</Fragment>
+		</DashboardModel>
+	);
 };
