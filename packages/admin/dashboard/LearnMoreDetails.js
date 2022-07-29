d53dbd541276cf10db69bcf5ecda2a37ff285d0c
Andre Gagnon <ajgagnon@uwalumni.com>
2022-07-29 12:42:41 -0500

    Refactor learn more details component

--- packages/admin/dashboard/LearnMoreDetails.js
+++ packages/admin/dashboard/LearnMoreDetails.js
@@ -1,12 +1,53 @@
-export default ({title, descriptions}) => {
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { ScFlex, ScIcon, ScStackedListRow } from '@surecart/components-react';
+
+export default ({ icon, href = '#', title, descriptions }) => {
 	return (
-		<div className="sc-learn-details">
-            <p className='sc-learn-details-title'>
-                {title}
-            </p>
-            <p className='sc-learn-details-desc'>
-                {descriptions}
-            </p>
-        </div>  
+		<ScStackedListRow
+			href={href}
+			style={{
+				'--columns': '1',
+				overflow: 'hidden',
+			}}
+		>
+			<ScFlex
+				justify-content="flex-start"
+				alignItems="center"
+				style={{ '--sc-flex-column-gap': '1em' }}
+			>
+				<ScIcon
+					name={icon}
+					style={{
+						fontSize: '60px',
+						color: 'var(--sc-color-brand-primary)',
+					}}
+				/>
+				<div
+					css={css`
+						.sc-learn-details-title {
+							font-weight: 700;
+							font-size: 16px;
+							line-height: 28px;
+							color: #334155;
+							margin: 0.4em 0;
+						}
+						.sc-learn-details-desc {
+							font-weight: 400;
+							font-size: 16px;
+							line-height: 28px;
+							color: #334155;
+							margin: 0.4em 0;
+						}
+						.sc-learn-more-title {
+							line-height: 32px;
+						}
+					`}
+				>
+					<p className="sc-learn-details-title">{title}</p>
+					<p className="sc-learn-details-desc">{descriptions}</p>
+				</div>
+			</ScFlex>
+		</ScStackedListRow>
 	);
 };
--- packages/admin/dashboard/components/LearnMore.js
+++ packages/admin/dashboard/components/LearnMore.js
@@ -4,79 +4,153 @@ import { __ } from '@wordpress/i18n';
 import LearnMoreDetails from '../LearnMoreDetails';
 
 import {
-    ScCard, 
-    ScDashboardModule, 
-    ScStackedList, 
-    ScStackedListRow,
-    ScIcon,
-    ScFlex,
+	ScCard,
+	ScDashboardModule,
+	ScStackedList,
 } from '@surecart/components-react';
 
 export default () => {
-    return (
-        <ScDashboardModule 
-            css={css`
-                width: 33%;
-                @media screen and (max-width: 782px) {
-                    width: 100%;
-                }
-                .sc-learn-details{
-                    width: 75%;
-                }
-                .sc-learn-details-title {
-                    font-weight: 700;
-                    font-size: 16px;
-                    line-height: 28px;
-                    color: #334155; 
-                    margin: 0.4em 0;  
-                }
-                .sc-learn-details-desc {
-                    font-weight: 400;
-                    font-size: 16px;
-                    line-height: 28px;
-                    color: #334155; 
-                    margin: 0.4em 0;  
-                }
-                .sc-learn-more-title {
-                    line-height: 32px;
-                }
-            `}
-        >
-            <span className='sc-learn-more-title' slot="heading">{__('Learn More', 'surecart')}</span>
-            <ScCard noPadding>
-                <ScStackedList>
-                    <ScStackedListRow href="#" style={{ '--columns': '2', '--sc-list-row-background-color': 'transparent' }}>
-                        <ScFlex justifyContent="center" alignItems="center">
-                            <ScIcon name="film" style={{fontSize: "60px", 'margin-right':'15px', color: "#08BA4F"}} />
-                            <LearnMoreDetails
-                                title={__('Tutorial Videos', 'surecart')} 
-                                descriptions={__('Learn more about SureCart', 'surecart')} 
-                            />  
-                            <ScIcon name="chevron-right" style={{fontSize: "20px", color: "#334155"}} />
-                        </ScFlex>
-                    </ScStackedListRow>
-                    <ScStackedListRow href="#" style={{ '--columns': '2', '--sc-list-row-background-color': 'transparent' }}>
-                        <ScFlex justifyContent="center" alignItems="center">
-                            <ScIcon name="users" style={{fontSize: "60px", 'margin-right':'15px', color: "#08BA4F"}} />
-                            <LearnMoreDetails
-                                title={__('Join Our Community', 'surecart')} 
-                                descriptions={__('Connect with others in on Facebook', 'surecart')} 
-                            />  
-                            <ScIcon name="chevron-right" style={{fontSize: "20px", color: "#334155"}} />
-                        </ScFlex>
-                    </ScStackedListRow>
-                    <ScStackedListRow href="#" style={{ '--columns': '2', '--sc-list-row-background-color': 'transparent' }}>
-                        <ScFlex justifyContent="center" alignItems="center">
-                            <ScIcon name="life-buoy" style={{fontSize: "60px", 'margin-right':'15px', color: "#08BA4F"}} />
-                            <LearnMoreDetails
-                                title={__('Get Help', 'surecart')} 
-                                descriptions={__('Contact our support for additional help', 'surecart')} 
-                            />  
-                            <ScIcon name="chevron-right" style={{fontSize: "20px", color: "#334155"}} />
-                        </ScFlex>
-                    </ScStackedListRow>
-                </ScStackedList>
-            </ScCard>
-        </ScDashboardModule>
-    );
+	return (
+		<ScDashboardModule
+			css={css`
+				width: 33%;
+				@media screen and (max-width: 782px) {
+					width: 100%;
+				}
+				.sc-learn-details {
+					width: 75%;
+				}
+				.sc-learn-more-title {
+					line-height: 32px;
+				}
+			`}
+		>
+			<span className="sc-learn-more-title" slot="heading">
+				{__('Learn More', 'surecart')}
+			</span>
+			<ScCard noPadding>
+				<ScStackedList>
+					<LearnMoreDetails
+						icon="film"
+						href="#"
+						title={__('Tutorial Videos', 'surecart')}
+						descriptions={__(
+							'Learn more about SureCart',
+							'surecart'
+						)}
+					/>
+					<LearnMoreDetails
+						icon="film"
+						href="#"
+						title={__('Tutorial Videos', 'surecart')}
+						descriptions={__(
+							'Learn more about SureCart',
+							'surecart'
+						)}
+					/>
+					<LearnMoreDetails
+						icon="film"
+						href="#"
+						title={__('Tutorial Videos', 'surecart')}
+						descriptions={__(
+							'Learn more about SureCart',
+							'surecart'
+						)}
+					/>
+					{/* <ScStackedListRow
+						href="#"
+						style={{
+							'--columns': '1',
+							overflow: 'hidden',
+						}}
+					>
+						<ScFlex
+							justify-content="flex-start"
+							alignItems="center"
+							style={{ '--sc-flex-column-gap': '1em' }}
+						>
+							<ScIcon
+								name="film"
+								style={{
+									fontSize: '60px',
+									color: 'var(--sc-color-brand-primary)',
+								}}
+							/>
+							<LearnMoreDetails
+								title={__('Tutorial Videos', 'surecart')}
+								descriptions={__(
+									'Learn more about SureCart',
+									'surecart'
+								)}
+							/>
+						</ScFlex>
+
+						<ScIcon
+							name="chevron-right"
+							slot="suffix"
+							style={{ fontSize: '20px', color: '#334155' }}
+						/>
+					</ScStackedListRow>
+					<ScStackedListRow
+						href="#"
+						style={{
+							'--columns': '2',
+							'--sc-list-row-background-color': 'transparent',
+						}}
+					>
+						<ScFlex justifyContent="center" alignItems="center">
+							<ScIcon
+								name="users"
+								style={{
+									fontSize: '60px',
+									'margin-right': '15px',
+									color: '#08BA4F',
+								}}
+							/>
+							<LearnMoreDetails
+								title={__('Join Our Community', 'surecart')}
+								descriptions={__(
+									'Connect with others in on Facebook',
+									'surecart'
+								)}
+							/>
+							<ScIcon
+								name="chevron-right"
+								style={{ fontSize: '20px', color: '#334155' }}
+							/>
+						</ScFlex>
+					</ScStackedListRow>
+					<ScStackedListRow
+						href="#"
+						style={{
+							'--columns': '2',
+							'--sc-list-row-background-color': 'transparent',
+						}}
+					>
+						<ScFlex justifyContent="center" alignItems="center">
+							<ScIcon
+								name="life-buoy"
+								style={{
+									fontSize: '60px',
+									'margin-right': '15px',
+									color: '#08BA4F',
+								}}
+							/>
+							<LearnMoreDetails
+								title={__('Get Help', 'surecart')}
+								descriptions={__(
+									'Contact our support for additional help',
+									'surecart'
+								)}
+							/>
+							<ScIcon
+								name="chevron-right"
+								style={{ fontSize: '20px', color: '#334155' }}
+							/>
+						</ScFlex>
+					</ScStackedListRow> */}
+				</ScStackedList>
+			</ScCard>
+		</ScDashboardModule>
+	);
 };
