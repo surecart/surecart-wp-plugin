36ea857146a410dbf1eb07dc15943e1daf16eb54
Andre Gagnon <ajgagnon@uwalumni.com>
2022-08-05 09:59:31 -0500

    Start dark mode theme and story

---
+++ packages/components/src/components/themes.stories.ts
@@ -0,0 +1,36 @@
+export default {
+  title: 'Components/Themes',
+};
+
+const Template = ({ theme }) => {
+  localStorage.clear();
+  return `<div data-theme="${theme}" style="background: var(--sc-color-gray-900); height: 100%; padding: 40px;">
+  <sc-form>
+    <sc-input label="Test" help="Help text" placeholder="placeholder"><span slot="prefix">Prefix</span><span slot="suffix">Suffix</span></sc-input>
+    <sc-input label="Test" help="Help text" value="test" placeholder="placeholder" clearable name="name" type="text"><span slot="prefix">Prefix</span><span slot="suffix">Suffix</span></sc-input>
+    <sc-flex justify-content="flex-start">
+      <sc-button>Button</sc-button>
+      <sc-button type="primary">Button</sc-button>
+      <sc-button type="success">Button</sc-button>
+      <sc-button type="info">Button</sc-button>
+      <sc-button type="warning">Button</sc-button>
+      <sc-button type="danger">Button</sc-button>
+
+      <sc-button outline>Button</sc-button>
+      <sc-button type="primary" outline>Button</sc-button>
+      <sc-button type="success" outline>Button</sc-button>
+      <sc-button type="info" outline>Button</sc-button>
+      <sc-button type="warning" outline>Button</sc-button>
+      <sc-button type="danger" outline>Button</sc-button>
+
+      <sc-button type="text">Button</sc-button>
+      <sc-button type="link">Button</sc-button>
+    </sc-flex>
+  </sc-form>
+</div>`;
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  theme: 'dark',
+};
