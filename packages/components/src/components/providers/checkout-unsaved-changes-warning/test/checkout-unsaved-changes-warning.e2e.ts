ab1974ed7bf864e506a653f5a669bb3aba8373c9
Andre Gagnon <ajgagnon@uwalumni.com>
2022-08-15 14:45:35 -0500

    Fix unsaved changes warning tests

--- packages/components/src/components/providers/checkout-unsaved-changes-warning/test/checkout-unsaved-changes-warning.e2e.ts
+++ packages/components/src/components/providers/checkout-unsaved-changes-warning/test/checkout-unsaved-changes-warning.e2e.ts
@@ -3,9 +3,9 @@ import { newE2EPage } from '@stencil/core/testing';
 describe('checkout-unsaved-changes-warning', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<checkout-unsaved-changes-warning></checkout-unsaved-changes-warning>');
+    await page.setContent('<sc-checkout-unsaved-changes-warning></sc-checkout-unsaved-changes-warning>');
 
-    const element = await page.find('checkout-unsaved-changes-warning');
+    const element = await page.find('sc-checkout-unsaved-changes-warning');
     expect(element).toHaveClass('hydrated');
   });
 });
--- packages/components/src/components/providers/checkout-unsaved-changes-warning/test/checkout-unsaved-changes-warning.spec.tsx
+++ packages/components/src/components/providers/checkout-unsaved-changes-warning/test/checkout-unsaved-changes-warning.spec.tsx
@@ -1,11 +1,11 @@
-import { CheckoutUnsavedChangesWarning } from '../sc-checkout-unsaved-changes-warning';
+import { ScCheckoutUnsavedChangesWarning } from '../sc-checkout-unsaved-changes-warning';
 import { newSpecPage } from '@stencil/core/testing';
 
 describe('checkout-unsaved-changes-warning', () => {
   it('renders', async () => {
     const page = await newSpecPage({
-      components: [CheckoutUnsavedChangesWarning],
-      html: `<checkout-unsaved-changes-warning></checkout-unsaved-changes-warning>`,
+      components: [ScCheckoutUnsavedChangesWarning],
+      html: `<sc-checkout-unsaved-changes-warning></sc-checkout-unsaved-changes-warning>`,
     });
     expect(page.root).toMatchSnapshot();
   });
