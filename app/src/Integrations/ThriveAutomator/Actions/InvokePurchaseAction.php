b92b6e1e4a75e0115039bdf90ca3f85f7c8c8685
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-08-17 12:59:12 +0600

    Add Invoke Purchase Action class
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

---
+++ app/src/Integrations/ThriveAutomator/Actions/InvokePurchaseAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SureCart\Integrations\ThriveAutomator\Actions;
+
+use SureCart\Integrations\ThriveAutomator\ThriveAutomatorApp;
+use Thrive\Automator\Items\Action;
+
+class InvokePurchaseAction extends Action {
+	public static function get_id() {
+		return 'surecart_invoke_purchase_action';
+	}
+
+	public static function get_name() {
+		return 'Invoke Purchase';
+	}
+
+	public static function get_description() {
+		return 'Invoke SureCart Purchase';
+	}
+
+	public static function get_image() {
+		return esc_url( trailingslashit( plugin_dir_url( SURECART_PLUGIN_FILE ) ) . 'app/src/Integrations/ThriveAutomator/images/icon.svg' );
+	}
+
+	public static function get_app_id() {
+		return ThriveAutomatorApp::get_id();
+	}
+
+	public static function get_required_action_fields() {
+		return array( 'surecart_products' );
+	}
+
+	public static function get_required_data_objects() {
+		return array( 'surecart_product_data' );
+	}
+
+	public function do_action( $data ) {
+		return false;
+	}
+}
