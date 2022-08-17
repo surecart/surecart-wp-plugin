31de7f9336fba29174b86325db02c56a5ba9cd2a
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-08-17 18:07:20 +0600

    Add code comment
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- app/src/Integrations/ThriveAutomator/Actions/InvokePurchaseAction.php
+++ app/src/Integrations/ThriveAutomator/Actions/InvokePurchaseAction.php
@@ -6,30 +6,65 @@ use SureCart\Integrations\ThriveAutomator\ThriveAutomatorApp;
 use Thrive\Automator\Items\Action;
 
 class InvokePurchaseAction extends Action {
+	/**
+	 * Get the action identifier
+	 *
+	 * @return string
+	 */
 	public static function get_id() {
 		return 'surecart_invoke_purchase_action';
 	}
 
+	/**
+	 * Get the action name/label
+	 *
+	 * @return string
+	 */
 	public static function get_name() {
 		return 'Invoke Purchase';
 	}
 
+	/**
+	 * Get the action description
+	 *
+	 * @return string
+	 */
 	public static function get_description() {
 		return 'Invoke SureCart Purchase';
 	}
 
+	/**
+	 * Get the action logo
+	 *
+	 * @return string
+	 */
 	public static function get_image() {
 		return esc_url( trailingslashit( plugin_dir_url( SURECART_PLUGIN_FILE ) ) . 'app/src/Integrations/ThriveAutomator/images/icon.svg' );
 	}
 
+	/**
+	 * Get the name of app to which action belongs
+	 *
+	 * @return string
+	 */
 	public static function get_app_id() {
 		return ThriveAutomatorApp::get_id();
 	}
 
+	/**
+	 * Array of action-field keys, required for the action to be setup
+	 *
+	 * @return array
+	 */
 	public static function get_required_action_fields() {
 		return array( 'surecart_products' );
 	}
 
+	/**
+	 * Get an array of keys with the required data-objects
+	 *
+	 * @return array
+	 */
 	public static function get_required_data_objects() {
 		return array( 'surecart_product_data' );
 	}
