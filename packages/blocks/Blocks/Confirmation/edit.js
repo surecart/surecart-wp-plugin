2fb91da0ab450dbbfb7f3fd7a99e47eff9d04eee
Andre Gagnon <ajgagnon@uwalumni.com>
2022-08-19 09:45:37 -0500

    Add default template to order confirmation block.

--- packages/blocks/Blocks/Confirmation/edit.js
+++ packages/blocks/Blocks/Confirmation/edit.js
@@ -4,6 +4,21 @@ import { __ } from '@wordpress/i18n';
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import { ScOrderConfirmation } from '@surecart/components-react';
 
+const DEFAULT_TEMPLATE = [
+	[
+		'core/paragraph',
+		{
+			content:
+				'Thank you for your purchase! Please check your inbox for additional information.',
+		},
+	],
+	['surecart/order-confirmation-line-items', {}],
+	[
+		'surecart/customer-dashboard-button',
+		{ label: 'Manage Orders', full: true },
+	],
+];
+
 export default () => {
 	const blockProps = useBlockProps();
 
@@ -288,7 +303,10 @@ export default () => {
 						}
 					`}
 				>
-					<InnerBlocks templateLock={false} />
+					<InnerBlocks
+						templateLock={false}
+						template={DEFAULT_TEMPLATE}
+					/>
 				</div>
 			</ScOrderConfirmation>
 		</div>
