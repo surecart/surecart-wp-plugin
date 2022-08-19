c03cd94f568539dec45cf9b550dc3911d901805f
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-08-19 22:58:11 +0600

    Refactor ProductDataField class
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- app/src/Integrations/ThriveAutomator/Fields/ProductDataField.php
+++ app/src/Integrations/ThriveAutomator/Fields/ProductDataField.php
@@ -10,66 +10,104 @@ use Thrive\Automator\Items\Data_Field;
 use SureCart\Models\Product;
 
 /**
- * Class User_Role_Field
+ * Class ProductDataField
  */
 class ProductDataField extends Data_Field {
 	/**
-	 * Field name
+	 * Get the data field identifier
+	 *
+	 * @return string
+	 */
+	public static function get_id() {
+		return 'surecart_product';
+	}
+
+	/**
+	 * Get the data field name
+	 *
+	 * @return string
 	 */
 	public static function get_name() {
 		return 'SureCart Product';
 	}
 
 	/**
-	 * Field description
+	 * Get the data field description
+	 *
+	 * @return string
 	 */
 	public static function get_description() {
-		return 'Show all SureCart Product';
+		return 'Show all SureCart Products';
 	}
 
 	/**
-	 * Field input placeholder
+	 * Get the data field input placeholder
+	 *
+	 * @return string
 	 */
 	public static function get_placeholder() {
 		return 'Select SureCart Product';
 	}
 
-	public static function get_id() {
-		return 'surecart_product';
-	}
-
+	/**
+	 * Get the data field supported filter
+	 *
+	 * @return array
+	 */
 	public static function get_supported_filters() {
 		return [ 'dropdown' ];
 	}
 
 	/**
 	 * For multiple option inputs, name of the callback function called through ajax to get the options
+	 *
+	 * @return array
 	 */
 	public static function get_options_callback() {
-		$roles = [];
+		$options = [];
 		$products = Product::get();
 		foreach ($products as $product) {
-			$roles[ $product->id ] = [
+			$options[ $product->id ] = [
 				'label' => $product->name,
 				'id'    => $product->id,
 			];
 		}
 
-		return $roles;
+		return $options;
 	}
 
+	/**
+	 * Determine if the values are direct or an ajax request
+	 *
+	 * @return boolean
+	 */
 	public static function is_ajax_field() {
 		return true;
 	}
 
+	/**
+	 * Get data field value type
+	 *
+	 * @return string
+	 */
 	public static function get_field_value_type() {
 		return static::TYPE_ARRAY;
 	}
 
+	/**
+	 * Get dummy value
+	 *
+	 * @return string
+	 */
 	public static function get_dummy_value() {
 		return 'subscriber';
 	}
 
+	/**
+	 * Get validators
+	 *
+	 * @return array
+	 */
 	public static function get_validators() {
 		return [ 'required' ];
 	}
