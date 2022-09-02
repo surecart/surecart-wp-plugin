9e24d7e645f430fe2d7461aa4d8bbc8ab5a5503a
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-02 14:28:26 -0500

    Fix classname support leading to block deprecation error.

--- packages/blocks/Blocks/Address/deprecated.js
+++ packages/blocks/Blocks/Address/deprecated.js
@@ -12,6 +12,9 @@ export default [
 				type: 'boolean',
 			},
 		},
+		supports: {
+			className: false,
+		},
 		save({ attributes, className }) {
 			const { label, full, show_name } = attributes;
 			return (
