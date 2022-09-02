b59c1c5e545e9b26b987cb20f1dc60111693aead
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-02 15:57:43 -0500

    Remove settings and theme declaration from hooks.php

--- app/hooks.php
+++ app/hooks.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Declare any actions and filters here.
+ * Declare any actions and filters here. USE THIS SPARINGLY.
+ *
  * In most cases you should use a service provider, but in cases where you
  * just need to add an action/filter and forget about it you can add it here.
  *
@@ -30,26 +31,3 @@ add_filter(
 		return $url;
 	}
 );
-
-add_filter(
-	'body_class',
-	function( $classes ) {
-		$classes[] = 'surecart-theme-' . get_option( 'surecart_theme', 'light' );
-		return $classes;
-	}
-);
-
-function sc_register_settings() {
-	register_setting(
-		'general',
-		'surecart_theme',
-		array(
-			'type'              => 'string',
-			'show_in_rest'      => true,
-			'sanitize_callback' => 'sanitize_text_field',
-			'default'           => 'light',
-		)
-	);
-}
-add_action( 'admin_init', 'sc_register_settings' );
-add_action( 'rest_api_init', 'sc_register_settings' );
