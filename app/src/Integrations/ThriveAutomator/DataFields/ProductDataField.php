c95c121d05ea2e501fd223979068e954109bb7c2
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-12 10:39:42 -0500

    Change "SureCart Product" to "Product"

--- app/src/Integrations/ThriveAutomator/DataFields/ProductDataField.php
+++ app/src/Integrations/ThriveAutomator/DataFields/ProductDataField.php
@@ -3,7 +3,7 @@
 namespace SureCart\Integrations\ThriveAutomator\DataFields;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Silence is golden!
+	exit; // Silence is golden!
 }
 
 use SureCart\Models\Product;
@@ -13,104 +13,106 @@ use Thrive\Automator\Items\Data_Field;
  * Class ProductDataField
  */
 class ProductDataField extends Data_Field {
-    /**
-     * Get the data field identifier
-     *
-     * @return string
-     */
-    public static function get_id() {
-        return 'surecart_product_data_field';
-    }
+	/**
+	 * Get the data field identifier
+	 *
+	 * @return string
+	 */
+	public static function get_id() {
+		return 'surecart_product_data_field';
+	}
 
-    /**
-     * Get the data field name
-     *
-     * @return string
-     */
-    public static function get_name() {
-        return 'SureCart Product';
-    }
+	/**
+	 * Get the data field name
+	 *
+	 * @return string
+	 */
+	public static function get_name() {
+		return 'Product';
+	}
 
-    /**
-     * Get the data field description
-     *
-     * @return string
-     */
-    public static function get_description() {
-        return 'Show all SureCart Products';
-    }
+	/**
+	 * Get the data field description
+	 *
+	 * @return string
+	 */
+	public static function get_description() {
+		return 'Show all SureCart Products';
+	}
 
-    /**
-     * Get the data field input placeholder
-     *
-     * @return string
-     */
-    public static function get_placeholder() {
-        return 'Select SureCart Product';
-    }
+	/**
+	 * Get the data field input placeholder
+	 *
+	 * @return string
+	 */
+	public static function get_placeholder() {
+		return 'Select Product';
+	}
 
-    /**
-     * Get the data field supported filter
-     *
-     * @return array
-     */
-    public static function get_supported_filters() {
-        return ['dropdown'];
-    }
+	/**
+	 * Get the data field supported filter
+	 *
+	 * @return array
+	 */
+	public static function get_supported_filters() {
+		return [ 'dropdown' ];
+	}
 
-    /**
-     * For multiple option inputs, name of the callback function called through ajax to get the options
-     *
-     * @return array
-     */
-    public static function get_options_callback() {
-        $options = [];
-        $products = Product::where( [
-            'archived' => false,
-        ] )->get();
-        foreach ( $products as $product ) {
-            $options[$product->id] = [
-                'label' => $product->name,
-                'id'    => $product->id,
-            ];
-        }
+	/**
+	 * For multiple option inputs, name of the callback function called through ajax to get the options
+	 *
+	 * @return array
+	 */
+	public static function get_options_callback() {
+		$options  = [];
+		$products = Product::where(
+			[
+				'archived' => false,
+			]
+		)->get();
+		foreach ( $products as $product ) {
+			$options[ $product->id ] = [
+				'label' => $product->name,
+				'id'    => $product->id,
+			];
+		}
 
-        return $options;
-    }
+		return $options;
+	}
 
-    /**
-     * Determine if the values are direct or an ajax request
-     *
-     * @return boolean
-     */
-    public static function is_ajax_field() {
-        return true;
-    }
+	/**
+	 * Determine if the values are direct or an ajax request
+	 *
+	 * @return boolean
+	 */
+	public static function is_ajax_field() {
+		return true;
+	}
 
-    /**
-     * Get data field value type
-     *
-     * @return string
-     */
-    public static function get_field_value_type() {
-        return static::TYPE_ARRAY;
-    }
+	/**
+	 * Get data field value type
+	 *
+	 * @return string
+	 */
+	public static function get_field_value_type() {
+		return static::TYPE_ARRAY;
+	}
 
-    /**
-     * Get dummy value
-     *
-     * @return string
-     */
-    public static function get_dummy_value() {
-        return 'subscriber';
-    }
+	/**
+	 * Get dummy value
+	 *
+	 * @return string
+	 */
+	public static function get_dummy_value() {
+		return 'subscriber';
+	}
 
-    /**
-     * Get validators
-     *
-     * @return array
-     */
-    public static function get_validators() {
-        return ['required'];
-    }
+	/**
+	 * Get validators
+	 *
+	 * @return array
+	 */
+	public static function get_validators() {
+		return [ 'required' ];
+	}
 }
