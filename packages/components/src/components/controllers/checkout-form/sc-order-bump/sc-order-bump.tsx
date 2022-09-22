1c41503f8eba4ec534a3b6268eec35e8636a8440
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-22 08:19:14 -0500

    Add order bump component tests.

--- packages/components/src/components/controllers/checkout-form/sc-order-bump/sc-order-bump.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-order-bump/sc-order-bump.tsx
@@ -17,11 +17,8 @@ export class ScOrderBump {
   /** The checkout */
   @Prop() checkout: Checkout;
 
-  /** Should we show the label */
-  @Prop() showLabel: boolean;
-
   /** Should we show the controls */
-  @Prop() showControl: boolean;
+  @Prop({ reflect: true }) showControl: boolean;
 
   /** Add line item event */
   @Event() scAddLineItem: EventEmitter<LineItemData>;
@@ -47,15 +44,17 @@ export class ScOrderBump {
   }
 
   newPrice() {
-    let amount = (this.bump?.price as Price)?.amount || 0;
+    let amount = null;
+    let initialAmount = (this.bump?.price as Price)?.amount || 0;
 
     if (this.bump?.amount_off) {
-      amount = Math.max(0, amount - this.bump?.amount_off);
+      amount = Math.max(0, initialAmount - this.bump?.amount_off);
     }
     if (this.bump?.percent_off) {
-      const off = amount * (this.bump?.percent_off / 100);
-      amount = Math.max(0, amount - off);
+      const off = initialAmount * (this.bump?.percent_off / 100);
+      amount = Math.max(0, initialAmount - off);
     }
+
     return amount;
   }
 
@@ -68,9 +67,8 @@ export class ScOrderBump {
           value={(this.bump?.price as Price)?.amount}
           currency={(this.bump?.price as Price)?.currency}
         ></sc-format-number>{' '}
-        {this.newPrice() === 0 ? (
-          __('Free', 'surecart')
-        ) : (
+        {this.newPrice() === 0 && __('Free', 'surecart')}
+        {this.newPrice() !== null && this.newPrice() > 0 && (
           <Fragment>
             <sc-format-number type="currency" class="bump__new-price" value={this.newPrice()} currency={(this.bump?.price as Price).currency} />
             <span class="bump__interval">{intervalString(this.bump?.price as Price, { labels: { interval: '/', period: __('for', 'surecart') } })}</span>
@@ -100,7 +98,6 @@ export class ScOrderBump {
       <sc-choice
         value={this.bump?.id}
         type="checkbox"
-        showLabel={this.showLabel}
         showControl={this.showControl}
         checked={isBumpInOrder(this.bump, this.checkout)}
         onScChange={e => this.updateLineItem(e.target.checked)}
--- packages/components/src/components/controllers/checkout-form/sc-order-bump/test/sc-order-bump.spec.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-order-bump/test/sc-order-bump.spec.tsx
@@ -1,11 +1,52 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
+import { Bump } from '../../../../../types';
 import { ScOrderBump } from '../sc-order-bump';
 
 describe('sc-order-bump', () => {
-  it('renders', async () => {
+  it('renders empty', async () => {
     const page = await newSpecPage({
       components: [ScOrderBump],
       html: `<sc-order-bump></sc-order-bump>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+  it('renders bump with name', async () => {
+    const page = await newSpecPage({
+      components: [ScOrderBump],
+      template: () => <sc-order-bump bump={{ name: 'Test' } as Bump}></sc-order-bump>,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+  it('renders bump with description, cta and image', async () => {
+    const page = await newSpecPage({
+      components: [ScOrderBump],
+      template: () => (
+        <sc-order-bump
+          bump={{ metadata: { description: 'Description', cta: 'CTA test' }, price: { product: { image_url: 'https://test.com/image.jpg' } } } as Bump}
+        ></sc-order-bump>
+      ),
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+  it('renders bump does not render an image without a description', async () => {
+    const page = await newSpecPage({
+      components: [ScOrderBump],
+      template: () => <sc-order-bump bump={{ metadata: { cta: 'CTA test' }, price: { product: { image_url: 'https://test.com/image.jpg' } } } as Bump}></sc-order-bump>,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+  it('renders bump with percent off', async () => {
+    const page = await newSpecPage({
+      components: [ScOrderBump],
+      template: () => <sc-order-bump bump={{ percent_off: 50, price: { amount: 1000, currency: 'usd' } } as Bump}></sc-order-bump>,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+  it('renders bump with amount off', async () => {
+    const page = await newSpecPage({
+      components: [ScOrderBump],
+      template: () => <sc-order-bump bump={{ amount_off: -500, price: { amount: 1000, currency: 'usd' } } as Bump}></sc-order-bump>,
     });
     expect(page.root).toMatchSnapshot();
   });
