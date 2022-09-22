caf257e8eb93d8713acc64164ad81519eedd5d2d
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-22 10:32:38 -0500

    Fix issue with blank product in price redux.

--- packages/admin/bumps/modules/filters/price/PriceFilter.js
+++ packages/admin/bumps/modules/filters/price/PriceFilter.js
@@ -1,13 +1,35 @@
 import { ScFormatNumber } from '@surecart/components-react';
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
 import useEntity from '../../../../hooks/useEntity';
 import { intervalString } from '../../../../util/translations';
 import FilterItem from '../FilterItem';
 
 export default ({ id, onRemove }) => {
-	const { price, hasLoadedPrice } = useEntity('price', id, {
-		expand: ['product'],
-	});
+	const { price, hasLoadedPrice } = useSelect(
+		(select) => {
+			const entityData = [
+				'surecart',
+				'price',
+				id,
+				{
+					expand: ['product'],
+					t: '1', // clear any cache to fetch fresh.
+				},
+			];
+			return {
+				price: select(coreStore)?.getEditedEntityRecord?.(
+					...entityData
+				),
+				hasLoadedPrice: select(coreStore)?.hasFinishedResolution?.(
+					'getEditedEntityRecord',
+					[...entityData]
+				),
+			};
+		},
+		[id]
+	);
 
 	return (
 		<FilterItem
