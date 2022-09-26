b6b289e327e27c6867123ea1ca2d63b81cdaaf5d
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-26 08:54:53 -0500

    Prettier. Translations. ScInput instead of ScChange.

--- packages/blocks/Blocks/DonationAmount/edit.js
+++ packages/blocks/Blocks/DonationAmount/edit.js
@@ -1,12 +1,8 @@
-import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress/element';
+import { ScChoice, ScFormatNumber, ScPriceInput } from '@surecart/components-react';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { PanelBody, PanelRow, TextControl } from '@wordpress/components';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import {
-	ScChoice,
-	ScFormatNumber,
-	ScPriceInput,
-} from '@surecart/components-react';
+import { Fragment } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 export default ({ attributes, setAttributes }) => {
 	const { label, amount, currency } = attributes;
@@ -17,24 +13,24 @@ export default ({ attributes, setAttributes }) => {
 		<Fragment>
 			<InspectorControls>
 				<PanelBody title={__('Attributes', 'surecart')}>
-          <PanelRow>
-            <TextControl
-              label={__('Label for donation', 'surecart')}
-              placeholder="Buy me coffee!"
-              value={label}
-              onChange={(value) => {
+					<PanelRow>
+						<TextControl
+							label={__('Label for donation', 'surecart')}
+							placeholder={__('Buy me coffee!', 'surecart')}
+							value={label}
+							onChange={(value) => {
 								setAttributes({
 									label: value,
-								})
+								});
 							}}
-            ></TextControl>
-          </PanelRow>
+						></TextControl>
+					</PanelRow>
 					<PanelRow>
 						<ScPriceInput
 							label={__('Amount', 'surecart')}
 							currencyCode={currency}
 							value={amount}
-							onScChange={(e) =>
+							onScInput={(e) =>
 								setAttributes({
 									amount: parseInt(e.target.value),
 								})
@@ -49,12 +45,16 @@ export default ({ attributes, setAttributes }) => {
 				value={amount}
 				{...blockProps}
 			>
-				{!!label ? label : <ScFormatNumber
-					type="currency"
-					currency={currency || 'USD'}
-					value={amount}
-					minimum-fraction-digits="0"
-				></ScFormatNumber>}
+				{!!label ? (
+					label
+				) : (
+					<ScFormatNumber
+						type="currency"
+						currency={currency || 'USD'}
+						value={amount}
+						minimum-fraction-digits="0"
+					></ScFormatNumber>
+				)}
 			</ScChoice>
 		</Fragment>
 	);
