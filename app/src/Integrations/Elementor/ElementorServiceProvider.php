e1f89d88907ffa023a5b2b3a44f38852bd780b32
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-26 12:22:34 -0500

    Rename ElementorServiceProvider

---
+++ app/src/Integrations/Elementor/ElementorServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace SureCart\Integrations\Elementor;
+
+use SureCartCore\ServiceProviders\ServiceProviderInterface;
+
+/**
+ * Elementor service provider.
+ */
+class ElementorServiceProvider implements ServiceProviderInterface {
+
+	/**
+	 * Register all dependencies in the IoC container.
+	 *
+	 * @param \Pimple\Container $container Service container.
+	 * @return void
+	 */
+	public function register( $container ) {
+		if ( ! class_exists( '\Elementor\Plugin' ) ) {
+			return;
+		}
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'widget' ] );
+		add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'load_scripts' ] );
+	}
+
+	/**
+	 * Elementor widget register
+	 *
+	 * @return void
+	 */
+	public function widget() {
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new ReusableFormWidget() );
+	}
+
+	/**
+	 * Elementor load scripts
+	 *
+	 * @return void
+	 */
+	public function load_scripts() {
+		wp_enqueue_script( 'surecart-elementor-editor', plugins_url( 'assets/editor.js', __FILE__ ), array( 'elementor-editor', 'jquery' ), date( 'Y-m-d' ), true );
+		wp_localize_script(
+			'surecart-elementor-editor',
+			'scElementorData',
+			[
+				'site_url' => site_url(),
+			]
+		);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param  \Pimple\Container $container Service Container.
+	 */
+	public function bootstrap( $container ) {}
+}
