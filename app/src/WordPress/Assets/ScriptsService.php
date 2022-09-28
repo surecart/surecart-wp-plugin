a721f9026330c9483f5a3fa5f28022a143016470
Andre Gagnon <ajgagnon@uwalumni.com>
2022-09-28 13:42:14 -0500

    Fix typo

--- app/src/WordPress/Assets/ScriptsService.php
+++ app/src/WordPress/Assets/ScriptsService.php
@@ -156,9 +156,9 @@ class ScriptsService {
 		wp_register_script( 'surecart-google-recaptcha', 'https://www.google.com/recaptcha/api.js?render=' . \SureCart::settings()->recaptcha()->getSiteKey(), [], \SureCart::plugin()->version(), true );
 
 		if ( get_option( 'surecart_stripe_script_enabled', false ) ) {
-			// regsiter stripe.
+			// register stripe.
 			wp_enqueue_script( 'surecart-stripe-script', 'https://js.stripe.com/v3', [], \SureCart::plugin()->version(), false );
-		}	
+		}
 	}
 
 	/**
