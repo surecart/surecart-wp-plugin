54c15ce0b40ccc3a4ade7e55cecf978013151576
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-10-10 13:49:36 +0600

    Implement SSR for default country with fallback of Tax address or Brandh address
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

---
+++ packages/blocks/Blocks/Address/Block.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace SureCartBlocks\Blocks\Address;
+
+use SureCartBlocks\Blocks\BaseBlock;
+
+/**
+ * Address Block.
+ */
+class Block extends BaseBlock {
+	/**
+	 * Render the block
+	 *
+	 * @param array  $attributes Block attributes.
+	 * @param string $content Post content.
+	 *
+	 * @return string
+	 */
+	public function render( $attributes, $content = '' ) {
+		$default_country = $attributes['default_country'] ?? \SureCart::account()->tax_protocol->address->country ?? \SureCart::account()->brand->address->country ?? null;
+		ob_start(); ?>
+
+		<sc-order-shipping-address
+			label="<?php echo esc_attr( $attributes['label'] ); ?>"
+			full="<?php echo esc_attr( $attributes['full'] ? '1' : null ); ?>"
+			show-name="<?php echo esc_attr( $attributes['show_name'] ? '1' : null ); ?>"
+			default-country="<?php echo esc_attr( $default_country ); ?>"
+		></sc-order-shipping-address>
+
+		<?php
+		return ob_get_clean();
+	}
+}
--- packages/blocks/Blocks/Address/index.js
+++ packages/blocks/Blocks/Address/index.js
@@ -8,7 +8,7 @@ import { pin as icon } from '@wordpress/icons';
  * Internal dependencies
  */
 import edit from './edit';
-import save from './save';
+// import save from './save';
 import deprecated from './deprecated';
 import metadata from './block.json';
 
@@ -19,6 +19,6 @@ export { metadata, name };
 export const settings = {
 	icon,
 	edit,
-	save,
+	// save,
 	deprecated,
 };
