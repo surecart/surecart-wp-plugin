2c26e684413fcff10b57c3e18c55e42c1a093ee7
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-10-13 18:06:22 +0600

    Finish writing e2e test case
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- packages/components/src/components/controllers/checkout-form/order-shipping-address/test/sc-order-shipping-address.e2e.ts
+++ packages/components/src/components/controllers/checkout-form/order-shipping-address/test/sc-order-shipping-address.e2e.ts
@@ -31,19 +31,19 @@ describe('sc-order-shipping-address', () => {
 
   it('renders custom placeholders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<sc-order-shipping-address name-placeholder="test name" country-placeholder="country test" full></sc-order-shipping-address>');
-    page.waitForChanges();
+    await page.setContent('<sc-order-shipping-address name-placeholder="name test" country-placeholder="country test" city-placeholder="city test" line-1-placeholder="line 1 test" line-2-placeholder="line 2 test" postal-code-placeholder="postal code test" state-placeholder="state test" full></sc-order-shipping-address>');
+    await page.waitForChanges();
 
     let element = await page.find('sc-order-shipping-address >>> sc-address');
     let placeholders = await element.getProperty('placeholders');
     expect(placeholders).toEqual({
-      "city": "City",
-      "line_1": "Address",
-      "line_2": "Address Line 2",
-      name: 'test name',
-      'country': 'country test' ,
-      "postal_code": "Postal Code/Zip",
-      "state": "State/Province/Region"
+      name: 'name test',
+      country: 'country test' ,
+      city: "city test",
+      line_1: "line 1 test",
+      line_2: "line 2 test",
+      postal_code: "postal code test",
+      state: "state test"
     });
   });
 });
