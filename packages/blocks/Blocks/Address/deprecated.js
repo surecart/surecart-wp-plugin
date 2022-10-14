449dd034f340b3550ce53a2d6624615d4be33c11
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-10-14 11:18:33 +0600

    Add deprecated code
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- packages/blocks/Blocks/Address/deprecated.js
+++ packages/blocks/Blocks/Address/deprecated.js
@@ -44,4 +44,30 @@ export default [
 			);
 		},
 	},
+  {
+    attributes: {
+      label: {
+        type: "string",
+        default: "Address"
+      },
+      full: {
+        type: "boolean",
+        default: true
+      },
+      show_name: {
+        type: "boolean"
+      }
+    },
+    save({ attributes, className }) {
+      const { label, full, show_name } = attributes;
+      return (
+        <sc-order-shipping-address
+          className={className}
+          label={label}
+          full={full ? '1' : null}
+          show-name={show_name ? '1' : null}
+        ></sc-order-shipping-address>
+      );
+    }
+  }
 ];
