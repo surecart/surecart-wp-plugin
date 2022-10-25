5b9f7931f238ccbdc3756ce9e16a7d52d1f67088
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-10-25 23:38:44 +0600

    Add Box ui components
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- packages/admin/abandoned-checkouts-stats/AbandonedCheckoutStats.js
+++ packages/admin/abandoned-checkouts-stats/AbandonedCheckoutStats.js
@@ -1,3 +1,19 @@
+import { __ } from '@wordpress/i18n';
+import Box from '../ui/Box';
+
 export default () => {
-	return 'working';
+
+	return (
+    <>
+      <Box title={__('Recoverable Orders', 'surecart')}>
+        Test
+      </Box>
+      <Box title={__('Recovered Orders', 'surecart')}>
+        Test
+      </Box>
+      <Box title={__('Recovered Order Recovery Rate', 'surecart')}>
+        Test
+      </Box>
+    </>
+  );
 };
