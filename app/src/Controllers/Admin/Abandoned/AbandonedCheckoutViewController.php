0aec1ea60f5c1a7d57e87f7fe07baffa18a7385a
Alamgir Hossain <alamgircse.bd@gmail.com>
2022-10-26 12:56:48 +0600

    SC-1065 Abandoned cart entitlement integration and CTA

--- app/src/Controllers/Admin/Abandoned/AbandonedCheckoutViewController.php
+++ app/src/Controllers/Admin/Abandoned/AbandonedCheckoutViewController.php
@@ -13,13 +13,22 @@ class AbandonedCheckoutViewController {
 	 * Index.
 	 */
 	public function index() {
-		$table = new AbandonedCheckoutListTable();
-		$table->prepare_items();
-		return \SureCart::view( 'admin/abandoned-orders/index' )->with(
-			[
-				'table' => $table,
-			]
-		);
+		$entitlements = \SureCart::account()->entitlements;
+
+		if ( empty( $entitlements->abandoned_checkouts ) ) {
+			wp_enqueue_style( 'surecart-themes-default' );	
+			wp_enqueue_script('surecart-components');
+
+			return \SureCart::view( 'admin/abandoned-orders/cta-banner' )->toString();
+		} else {
+			$table = new AbandonedCheckoutListTable();
+			$table->prepare_items();
+			return \SureCart::view( 'admin/abandoned-orders/index' )->with(
+				[
+					'table' => $table,
+				]
+			);
+		}
 	}
 
 	/**
