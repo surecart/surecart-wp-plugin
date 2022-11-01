66a53f7817792a5f133fefaf77695081fecff5b5
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-11-01 20:15:34 +0600

    Implement useEffect

--- packages/admin/customers/modules/PaymentMethods.js
+++ packages/admin/customers/modules/PaymentMethods.js
@@ -1,63 +1,83 @@
 import { ScButton, ScTag } from '@surecart/components-react';
 import { store as coreStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
-import { useState } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { __, _n } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
+import apiFetch from '@wordpress/api-fetch';
 import DataTable from '../../components/DataTable';
 
 export default ({ customerId }) => {
+  const [paymentMethods, setPaymentMethods] = useState([]);
+  const [loading, setLoading] = useState(false);
 	const [page, setPage] = useState(1);
 	const [perPage, setPerPage] = useState(10);
 
-	const { paymentMethods, loading } = useSelect(
-		(select) => {
-			const queryArgs = [
-				'surecart',
-				'payment_method',
-				{
-					customer_ids: [customerId],
-					expand: ['card', 'customer', 'billing_agreement', 'paypal_account', 'payment_instrument', 'bank_account'],
-					page,
-					per_page: perPage,
-				},
-			];
-			const paymentMethods = select(coreStore).getEntityRecords(...queryArgs);
-			const loading = select(coreStore).isResolving(
-				'getEntityRecords',
-				queryArgs
-			);
-			return {
-				paymentMethods,
-				loading: loading && page === 1,
-			};
-		},
-		[customerId, page, perPage]
-	);
+	// const { paymentMethods, loading } = useSelect(
+	// 	(select) => {
+	// 		const queryArgs = [
+	// 			'surecart',
+	// 			'payment_method',
+	// 			{
+	// 				customer_ids: [customerId],
+	// 				expand: ['card', 'customer', 'billing_agreement', 'paypal_account', 'payment_instrument', 'bank_account'],
+	// 				page,
+	// 				per_page: perPage,
+	// 			},
+	// 		];
+	// 		const paymentMethods = select(coreStore).getEntityRecords(...queryArgs);
+	// 		const loading = select(coreStore).isResolving(
+	// 			'getEntityRecords',
+	// 			queryArgs
+	// 		);
+	// 		return {
+	// 			paymentMethods,
+	// 			loading: loading && page === 1,
+	// 		};
+	// 	},
+	// 	[customerId, page, perPage]
+	// );
+
+  useEffect(()=>{
+    setLoading(true);
+    const payment = async () => {
+      const data = await apiFetch({
+        path: addQueryArgs(`surecart/v1/payment_methods/`, {
+          expand: ['card', 'customer', 'billing_agreement', 'paypal_account', 'payment_instrument', 'bank_account']
+        }),
+        customer_ids: [customerId]
+      });
+      setPaymentMethods(data);
+      setLoading(false);
+    }
+    payment();
+  }, []);
 
   const setDefault = async (id) => {
     try {
-      loading = true;
+      // loading = true;
+      console.log(paymentMethods);
       await apiFetch({
         path: `surecart/v1/customers/${customerId}`,
         method: 'PATCH',
         data: {
-          default_payment_method: method.id,
+          default_payment_method: id,
         },
       });
-      paymentMethods = (await apiFetch({
-        path: addQueryArgs(`surecart/v1/payment_methods/`, {
-          expand: ['card', 'customer', 'billing_agreement', 'paypal_account', 'payment_instrument', 'bank_account']
-        }),
-      }));
+      // paymentMethods = (await apiFetch({
+      //   path: addQueryArgs(`surecart/v1/payment_methods/`, {
+      //     expand: ['card', 'customer', 'billing_agreement', 'paypal_account', 'payment_instrument', 'bank_account']
+      //   }),
+      // }));
     } catch (e) {
-      alert(e?.messsage || __('Something went wrong', 'surecart'));
+      // alert(e?.messsage || __('Something went wrong', 'surecart'));
+      console.log(e);
     } finally {
-      loading = false;
+      // loading = false;
     }
   }
 
-  console.log(paymentMethods);
+  // console.log(paymentMethods);
 
 	return (
 		<>
