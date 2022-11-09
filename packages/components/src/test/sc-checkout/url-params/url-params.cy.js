fe23ccd8e5f142b941730d887643f5c9e8f946de
Andre Gagnon <ajgagnon@uwalumni.com>
2022-11-09 12:23:28 -0600

    Add test for if form mode does not match.

--- packages/components/src/test/sc-checkout/url-params/url-params.cy.js
+++ packages/components/src/test/sc-checkout/url-params/url-params.cy.js
@@ -8,6 +8,7 @@ beforeEach(() => {
       id: 'test',
       object: 'checkout',
       status: 'draft',
+      live_mode: false,
       line_items: {
         object: 'list',
         pagination: { count: 1, limit: 20, page: 1, url: '/api/v1/line_items?order_ids%5B%5D=5b4cf222-7898-4655-84a2-73b0559deb99' },
@@ -113,6 +114,77 @@ describe('Abandoned Cart', () => {
     cy.visit('/test/sc-checkout/url-params?checkout_id=test2&coupon=TESTCOUPON');
     cy.wait('@createUpdate').its('request.url').should('include', 'checkouts/test2');
   });
+  it.only('Shows an empty cart if form mode does not match', () => {
+    cy.intercept(
+      {
+        path: '**/surecart/v1/checkouts/*',
+      },
+      {
+        id: 'test',
+        object: 'checkout',
+        status: 'draft',
+        live_mode: true,
+        line_items: {
+          object: 'list',
+          pagination: { count: 1, limit: 20, page: 1, url: '/api/v1/line_items?order_ids%5B%5D=5b4cf222-7898-4655-84a2-73b0559deb99' },
+          data: [
+            {
+              id: '5366e5a3-f129-402d-8e25-5b98061142e9',
+              object: 'line_item',
+              ad_hoc_amount: null,
+              discount_amount: 0,
+              quantity: 1,
+              subtotal_amount: 1200,
+              tax_amount: 0,
+              total_amount: 1200,
+              trial: false,
+              price: {
+                id: 'ac7b1d56-0b7d-4e29-b2e5-e01ace8bf4ac',
+                object: 'price',
+                ad_hoc: false,
+                ad_hoc_max_amount: null,
+                ad_hoc_min_amount: 0,
+                amount: 1200,
+                archived: false,
+                archived_at: null,
+                currency: 'usd',
+                current_version: true,
+                metadata: [],
+                recurring_interval: 'month',
+                recurring_interval_count: 1,
+                tax_behavior: 'exclusive',
+                trial_duration_days: null,
+                product: {
+                  id: '36d82beb-2441-4e0d-b7b7-c39f4e46cd5b',
+                  object: 'product',
+                  archived: false,
+                  archived_at: null,
+                  description: null,
+                  image_url: null,
+                  name: 'Product Name',
+                  recurring: true,
+                  tax_enabled: true,
+                  tax_category: null,
+                  product_group: null,
+                  created_at: 1645027950,
+                  updated_at: 1645130871,
+                },
+                created_at: 1645027950,
+                updated_at: 1645130871,
+              },
+              created_at: 1647382132,
+              updated_at: 1647382132,
+              price_id: 'ac7b1d56-0b7d-4e29-b2e5-e01ace8bf4ac',
+            },
+          ]
+        }
+      }
+    ).as('fetchMismatchLiveMode');
+
+    cy.visit('/test/sc-checkout/url-params?checkout_id=test&coupon=TESTCOUPON');
+    cy.wait('@fetchMismatchLiveMode');
+    cy.get('sc-order-summary').shadow().find('p').contains('Your cart is empty');
+  })
 });
 
 describe('Payment Instrument Redirects', () => {
