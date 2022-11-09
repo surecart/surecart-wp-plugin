46cd804913c7d659f89130707fd6d1d7c4fee277
Andre Gagnon <ajgagnon@uwalumni.com>
2022-11-09 14:59:29 -0600

    Add retries

--- packages/components/cypress.config.ts
+++ packages/components/cypress.config.ts
@@ -4,6 +4,14 @@ export default defineConfig({
   chromeWebSecurity: false,
   projectId: 'ae7zwm',
   experimentalWebKitSupport: true,
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  },
   e2e: {
     baseUrl: 'http://localhost:3456',
     specPattern: '**/*.cy.js',
