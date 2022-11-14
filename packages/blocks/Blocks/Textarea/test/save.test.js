00472bd9cc9501d2cd70d5ec55ab1189d295bc19
Alamgir Hossain <alamgircse.bd@gmail.com>
2022-11-14 10:05:02 +0600

    SC-72 TextArea field

---
+++ packages/blocks/Blocks/Textarea/test/save.test.js
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import { registerBlockType, createBlock, serialize } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies.
+ */
+import { name, settings } from '../index';
+
+// Make variables accessible for all tests.
+let block;
+let serializedBlock;
+
+describe('surecart/buttons', () => {
+	beforeAll(() => {
+		// Register the block.
+		registerBlockType(name, { category: 'common', ...settings });
+	});
+
+	beforeEach(() => {
+		// Create the block with the minimum attributes.
+		block = createBlock(name);
+
+		// Reset the reused variables.
+		serializedBlock = '';
+	});
+	it('Should render with defaults', () => {
+		serializedBlock = serialize(block);
+		expect(serializedBlock).toBeDefined();
+		expect(serializedBlock).toContain(`wp:surecart/textarea`);
+	});
+
+	it('should render with custom class name', () => {
+		block.attributes.className = 'my-custom-class';
+		serializedBlock = serialize(block);
+
+		expect(serializedBlock).toBeDefined();
+		expect(serializedBlock).toContain('my-custom-class');
+		expect(serializedBlock).toMatchSnapshot();
+	});
+});
