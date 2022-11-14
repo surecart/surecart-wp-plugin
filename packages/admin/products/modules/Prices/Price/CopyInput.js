ae4b2e2a43f57ff349d19374f595562bfe2b5fb6
Andre Gagnon <ajgagnon@uwalumni.com>
2022-11-14 17:09:54 -0600

    Formatting

--- packages/admin/products/modules/Prices/Price/CopyInput.js
+++ packages/admin/products/modules/Prices/Price/CopyInput.js
@@ -28,12 +28,12 @@ export default ({ label, text }) => {
 			readonly
 			value={text}
 		>
-      { (location.protocol === 'https:') && (
-			<ScButton slot="suffix" size="small" onClick={copy}>
-				<ScIcon name="clipboard" slot="prefix" />
-				{__('Copy', 'surecart')}
-			</ScButton>
-      )}
+      {location.protocol === 'https:' && (
+				<ScButton slot="suffix" size="small" onClick={copy}>
+					<ScIcon name="clipboard" slot="prefix" />
+					{__('Copy', 'surecart')}
+				</ScButton>
+			)}
 		</ScInput>
 	);
 };
