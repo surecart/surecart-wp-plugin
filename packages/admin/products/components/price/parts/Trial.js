dfad5ee74fe267a5154fb56d0e137d30532cf6ea
Andre Gagnon <ajgagnon@uwalumni.com>
2022-11-22 16:42:27 -0600

    Free trial entitlement

--- packages/admin/products/components/price/parts/Trial.js
+++ packages/admin/products/components/price/parts/Trial.js
@@ -1,7 +1,46 @@
 import { __ } from '@wordpress/i18n';
-import { ScInput } from '@surecart/components-react';
+import {
+	ScFormControl,
+	ScInput,
+	ScSwitch,
+	ScTag,
+} from '@surecart/components-react';
+import { useDispatch } from '@wordpress/data';
+import { store as uiStore } from '@surecart/ui-data';
 
 export default ({ className, price, updatePrice }) => {
+	const { setUpgradeModal } = useDispatch(uiStore);
+
+	// must have the
+	if (!scData?.entitlements?.subscription_trials) {
+		return (
+			<ScFormControl label={true} style={{ flex: 1 }}>
+				<span slot="label">
+					{__('Free Trial Days', 'surecart')}{' '}
+					<ScTag type="success" size="small" pill slot="prefix">
+						{__('Upgrade', 'surecart')}
+					</ScTag>
+				</span>
+				<ScInput
+					onClick={() => setUpgradeModal(true)}
+					className={className}
+					type="number"
+					min={1}
+					max={365}
+					value={price?.trial_duration_days}
+					disabled={true}
+					onScInput={(e) =>
+						updatePrice({
+							trial_duration_days: parseInt(e.target.value),
+						})
+					}
+				>
+					<span slot="suffix">{__('Days', 'surecart')}</span>
+				</ScInput>
+			</ScFormControl>
+		);
+	}
+
 	return (
 		<ScInput
 			label={__('Free Trial Days', 'surecart')}
