25f725881f648ed4c47245f831181e21229bc542
Andre Gagnon <ajgagnon@uwalumni.com>
2023-01-02 16:53:55 -0600

    Add route, permissions, controller.

--- app/src/Rest/OrderRestServiceProvider.php
+++ app/src/Rest/OrderRestServiceProvider.php
@@ -32,6 +32,27 @@ class OrderRestServiceProvider extends RestServiceProvider implements RestServic
 	protected $methods = [ 'index', 'find' ];
 
 	/**
+	 * Register Additional REST Routes
+	 *
+	 * @return void
+	 */
+	public function registerRoutes() {
+		register_rest_route(
+			"$this->name/v$this->version",
+			$this->endpoint . '/(?P<id>\S+)/cancel/',
+			[
+				[
+					'methods'             => \WP_REST_Server::EDITABLE,
+					'callback'            => $this->callback( $this->controller, 'cancel' ),
+					'permission_callback' => [ $this, 'cancel_permissions_check' ],
+				],
+				// Register our schema callback.
+				'schema' => [ $this, 'get_item_schema' ],
+			]
+		);
+	}
+
+	/**
 	 * Get our sample schema for a post.
 	 *
 	 * @return array The sample schema for a post
@@ -112,5 +133,15 @@ class OrderRestServiceProvider extends RestServiceProvider implements RestServic
 	 */
 	public function get_items_permissions_check( $request ) {
 		return current_user_can( 'read_sc_orders', $request->get_params() );
+	}
+
+	/**
+	 * Cancelling orders.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 * @return true|\WP_Error True if the request has access to create items, WP_Error object otherwise.
+	 */
+	public function cancel_item_permissions_check( $request ) {
+		return current_user_can( 'edit_sc_orders' );
 	}
 }
