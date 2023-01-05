c54ad923b7e09445d8adbea297979a0c87c9e8d3
Andre Gagnon <ajgagnon@uwalumni.com>
2023-01-05 11:38:43 -0600

    Add blank template file.

---
+++ templates/pages/template-surecart-blank.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: SureCart Blank
+Template Post Type: sc-product
+*/
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="https://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<?php do_action( 'surecart_before_content_wrapper' ); ?>
+
+<?php
+while ( have_posts() ) :
+	the_post();
+	the_content();
+endwhile;
+?>
+
+<?php do_action( 'surecart_after_content_wrapper' ); ?>
+
+</body>
+<?php wp_footer(); ?>
