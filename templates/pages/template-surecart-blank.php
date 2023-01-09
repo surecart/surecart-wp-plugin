08b5afddb6c00ea1a0299c0c5f0fe3871842ff98
Andre Gagnon <ajgagnon@uwalumni.com>
2023-01-09 11:04:48 -0600

    Add page template styles

--- templates/pages/template-surecart-blank.php
+++ templates/pages/template-surecart-blank.php
@@ -10,38 +10,6 @@ Template Post Type: sc-product
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="https://gmpg.org/xfn/11">
-<style>
-	body {
-		background-color: #FBFBFB !important;
-	}
-	#layout_checkout {
-		min-height: 100vh;
-		max-width: 1160px;
-		margin: 0 auto;
-		display: flex;
-		padding-top: 8vh;
-		position: relative;
-	}
-	#layout_checkout::after {
-		content: "";
-		position: fixed;
-		top: 0;
-		right: 0;
-		width: 50%;
-		height: 100vh;
-		background-color: white;
-		box-shadow: 5px 0px 20px rgba(0, 0, 0, 0.12);
-	}
-	#layout_checkout > div {
-		width: 100%;
-		z-index: 99;
-	}
-	@media screen and (max-width: 782px) {
-		#layout_checkout::after {
-			display: none;
-		}
-	}
-</style>
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
