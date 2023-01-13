a4a666516e361145cb868b32734a2353dd1333a8
Andre Gagnon <ajgagnon@uwalumni.com>
2023-01-12 20:56:17 -0600

    Formatting

--- packages/components/src/components/controllers/checkout-form/sc-conditional-form/conditional-functions.ts
+++ packages/components/src/components/controllers/checkout-form/sc-conditional-form/conditional-functions.ts
@@ -5,22 +5,20 @@
  * @param {object} props Data.
  * @returns {boolean}
  */
-export const is_any_rule_group_passed = ( groups, props ) => {
-
+export const is_any_rule_group_passed = (groups, props) => {
   let result = false;
 
-  if( Array.isArray(groups) ) {
-    groups.forEach( element => {
-
-      if ( result ) {
+  if (Array.isArray(groups)) {
+    groups.forEach(element => {
+      if (result) {
         return;
       }
-      result = is_rules_passed( element['rules'], props );
+      result = is_rules_passed(element['rules'], props);
     });
   }
 
   return result;
-}
+};
 
 /**
  * CHeck if all rules are passed or not.
@@ -29,48 +27,47 @@ export const is_any_rule_group_passed = ( groups, props ) => {
  * @param {object} props Data.
  * @returns {boolean}
  */
-export const is_rules_passed = ( rules, props ) => {
+export const is_rules_passed = (rules, props) => {
   const { checkout, processor } = props;
 
   let result = true;
 
-  rules.forEach( rule => {
-
-    if ( false === result ) {
+  rules.forEach(rule => {
+    if (false === result) {
       return;
     }
     let ruleOperator = rule['operator'];
-		let ruleValue    = rule['value'];
+    let ruleValue = rule['value'];
 
     switch (rule['condition']) {
       case 'cart_total':
-        result = compare_number_values( parseFloat( checkout.total_amount ), parseFloat( ruleValue ), ruleOperator );
+        result = compare_number_values(parseFloat(checkout.total_amount), parseFloat(ruleValue), ruleOperator);
         break;
       case 'cart_item':
-        const cart_products = get_cart_products( checkout );
-        result = compare_object_values( cart_products, ruleValue, ruleOperator );
+        const cart_products = get_cart_products(checkout);
+        result = compare_object_values(cart_products, ruleValue, ruleOperator);
         break;
       case 'cart_coupons':
-        const cart_coupons = get_cart_coupons( checkout );
-        result = compare_object_values( cart_coupons, ruleValue, ruleOperator );
+        const cart_coupons = get_cart_coupons(checkout);
+        result = compare_object_values(cart_coupons, ruleValue, ruleOperator);
         break;
       case 'cart_shipping_country':
         const temp_cart_scountry = [{ value: checkout?.shipping_address?.country }];
-        result = compare_object_values( temp_cart_scountry, ruleValue, ruleOperator );
+        result = compare_object_values(temp_cart_scountry, ruleValue, ruleOperator);
         break;
       case 'cart_billing_country':
         const temp_cart_bcountry = [{ value: checkout?.billing_address?.country }];
-        result = compare_object_values( temp_cart_bcountry, ruleValue, ruleOperator );
+        result = compare_object_values(temp_cart_bcountry, ruleValue, ruleOperator);
         break;
       case 'cart_payment_method':
         const temp_cart_processor = [{ value: processor }];
-        result = compare_object_values( temp_cart_processor, ruleValue, ruleOperator );
+        result = compare_object_values(temp_cart_processor, ruleValue, ruleOperator);
         break;
     }
   });
 
   return result;
-}
+};
 
 /**
  * Get array of products from checkout.
@@ -78,20 +75,18 @@ export const is_rules_passed = ( rules, props ) => {
  * @param {object} checkout CHeckout data.
  * @returns {array}
  */
-export const get_cart_products = ( checkout ) => {
+export const get_cart_products = checkout => {
   let products = [];
 
   checkout.line_items.data.forEach(element => {
-    products.push(
-      {
-        label: element.price.product.name,
-        value: element.price.product.id,
-      }
-    );
+    products.push({
+      label: element.price.product.name,
+      value: element.price.product.id,
+    });
   });
 
   return products;
-}
+};
 
 /**
  * Get array of coupons from checkout.
@@ -99,21 +94,18 @@ export const get_cart_products = ( checkout ) => {
  * @param {object} checkout CHeckout data.
  * @returns {array}
  */
-export const get_cart_coupons = ( checkout ) => {
+export const get_cart_coupons = checkout => {
   let coupons = [];
 
   if (checkout?.discount?.coupon) {
-    coupons.push(
-      {
-        label: checkout.discount.coupon.name,
-        value: checkout.discount.coupon.id,
-      }
-    );
-
+    coupons.push({
+      label: checkout.discount.coupon.name,
+      value: checkout.discount.coupon.id,
+    });
   }
 
   return coupons;
-}
+};
 
 /**
  * Compare object values.
@@ -124,33 +116,32 @@ export const get_cart_coupons = ( checkout ) => {
  * @returns {boolean}
  */
 
-export const compare_object_values = ( cart_values, rule_values, operator ) => {
-
+export const compare_object_values = (cart_values, rule_values, operator) => {
   let result = false;
 
-  switch ( operator ) {
+  switch (operator) {
     case 'all':
-        result = rule_values.filter( n1 => cart_values.some( n2 => n1.value == n2.value ) ).length === rule_values.length;
+      result = rule_values.filter(n1 => cart_values.some(n2 => n1.value == n2.value)).length === rule_values.length;
       break;
     case 'any':
-        result = cart_values.filter( n1 => rule_values.some( n2 => n1.value == n2.value ) ).length  >= 1;
+      result = cart_values.filter(n1 => rule_values.some(n2 => n1.value == n2.value)).length >= 1;
       break;
     case 'none':
-        result = cart_values.filter( n1 => rule_values.some( n2 => n1.value == n2.value ) ).length === 0;
+      result = cart_values.filter(n1 => rule_values.some(n2 => n1.value == n2.value)).length === 0;
       break;
     case 'exist':
-        result = cart_values.length >= 1;
+      result = cart_values.length >= 1;
       break;
     case 'not_exist':
-        result = cart_values.length === 0;
+      result = cart_values.length === 0;
       break;
     default:
-        result = false;
+      result = false;
       break;
   }
 
   return result;
-}
+};
 
 /**
  * Compare string values.
@@ -160,11 +151,10 @@ export const compare_object_values = ( cart_values, rule_values, operator ) => {
  * @param string operator Rule operator.
  * @returns {boolean}
  */
-export const compare_number_values = ( number1, number2, operator ) => {
-
+export const compare_number_values = (number1, number2, operator) => {
   let result = false;
 
-  switch ( operator ) {
+  switch (operator) {
     case '==':
       result = number1 === number2;
       break;
@@ -189,4 +179,4 @@ export const compare_number_values = ( number1, number2, operator ) => {
   }
 
   return result;
-}
+};
--- packages/components/src/components/controllers/checkout-form/sc-conditional-form/sc-conditional-form.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-conditional-form/sc-conditional-form.tsx
@@ -11,7 +11,6 @@ import * as logic from './conditional-functions';
   styleUrl: 'sc-conditional-form.css',
   shadow: true,
 })
-
 export class ScConditionalForm {
   /** Selector label */
   @Prop() rule_groups: object = [];
@@ -19,20 +18,18 @@ export class ScConditionalForm {
   /** Checkout Session from sc-checkout. */
   @Prop() checkout: Checkout;
 
-   /** The currently selected processor */
+  /** The currently selected processor */
   @Prop() selectedProcessorId: ProcessorName;
 
-
   render() {
-    let show = logic.is_any_rule_group_passed( this.rule_groups, { checkout: this.checkout, processor: this.selectedProcessorId } );
-    if ( ! show ) return null;
+    let show = logic.is_any_rule_group_passed(this.rule_groups, { checkout: this.checkout, processor: this.selectedProcessorId });
+    if (!show) return null;
     return (
       <Host>
         <slot></slot>
       </Host>
     );
   }
-
 }
 
 openWormhole(ScConditionalForm, ['checkout', 'selectedProcessorId'], false);
