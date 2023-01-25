964a13efa5f8f49f9b6fa46ef3ca94a299087c4d
Andre Gagnon <ajgagnon@uwalumni.com>
2023-01-25 15:47:30 -0600

    Add snapshot test

--- packages/components/src/components/controllers/checkout-form/sc-conditional-form/test/sc-conditional-form.spec.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-conditional-form/test/sc-conditional-form.spec.tsx
@@ -7,12 +7,6 @@ describe('sc-conditional-form', () => {
       components: [ScConditionalForm],
       html: `<sc-conditional-form></sc-conditional-form>`,
     });
-    expect(page.root).toEqualHtml(`
-      <sc-conditional-form>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </sc-conditional-form>
-    `);
+    expect(page.root).toMatchSnapshot();
   });
 });
