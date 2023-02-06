2fb8a25bf66f8f1cc366dfb33cd4a0d157ea3802
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-06 07:53:36 -0600

    Add closing tags

--- templates/pages/template-surecart-blank.php
+++ templates/pages/template-surecart-blank.php
@@ -6,25 +6,21 @@ Template Name: SureCart Blank
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="https://gmpg.org/xfn/11">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
+<?php do_action( 'surecart_template_blank_body_open' ); ?>
 
-<?php do_action( 'surecart_before_content_wrapper' ); ?>
-
-<div>
-	<?php
-	while ( have_posts() ) :
-		the_post();
-		the_content();
+<?php
+while ( have_posts() ) :
+	the_post();
+	the_content();
 	endwhile;
-	?>
-</div>
+?>
 
-<?php do_action( 'surecart_after_content_wrapper' ); ?>
-
-</body>
 <?php wp_footer(); ?>
+</body>
+</html>
