d967840cc443f79e6eb54b9b0177cc966b9c6ee7
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-06 13:56:20 -0600

    Update seeder to use new columns format.

--- templates/forms/checkout.php
+++ templates/forms/checkout.php
@@ -7,10 +7,8 @@ return [
 	'categories' => [],
 	'blockTypes' => [],
 	'content'    => '<!-- wp:surecart/columns {"isFullHeight":true,"isReversedOnMobile":true,"style":{"spacing":{"blockGap":{"top":"0px","left":"0px"}}}} -->
-	<sc-columns is-stacked-on-mobile="true" is-full-height="true" is-reversed-on-mobile="true" class="wp-block-surecart-columns" style="gap:0px 0px"><!-- wp:surecart/column {"style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"},"blockGap":"30px"}},"layout":{"type":"constrained","contentSize":"500px","justifyContent":"right"}} -->
-	<sc-column class="wp-block-surecart-column is-layout-constrained is-horizontally-aligned-right" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:500px;--sc-form-row-spacing:30px"><!-- wp:group {"layout":{"inherit":true}} -->
-	<div class="wp-block-group"><!-- wp:surecart/store-logo {"width":120,"maxHeight":100,"isLinkToHome":false} /--></div>
-	<!-- /wp:group -->
+	<sc-columns is-stacked-on-mobile="1" is-full-height="1" is-reversed-on-mobile="1" class="wp-block-surecart-columns" style="gap:0px 0px"><!-- wp:surecart/column {"style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"},"blockGap":"30px"}},"layout":{"type":"constrained","contentSize":"500px","justifyContent":"right"}} -->
+	<sc-column class="wp-block-surecart-column is-layout-constrained is-horizontally-aligned-right" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:500px;--sc-form-row-spacing:30px"><!-- wp:surecart/store-logo {"width":120,"maxHeight":100,"isLinkToHome":false} /-->
 
 	<!-- wp:surecart/name -->
 	<sc-customer-name label="Name" required class="wp-block-surecart-name"></sc-customer-name>
@@ -30,7 +28,7 @@ return [
 	<!-- /wp:surecart/column -->
 
 	<!-- wp:surecart/column {"width":"","style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"},"blockGap":"30px"},"color":{"background":"#f3f4f6"},"border":{"color":"#dce0e6","width":"1px"}},"layout":{"type":"constrained","contentSize":"400px","justifyContent":"left"}} -->
-	<sc-column class="wp-block-surecart-column is-layout-constrained is-horizontally-aligned-left has-border-color has-background" style="border-color:#dce0e6;border-width:1px;background-color:#f3f4f6;padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:400px;--sc-form-row-spacing:30px"><!-- wp:surecart/totals {"collapsible":true} -->
+	<sc-column class="wp-block-surecart-column is-layout-constrained is-horizontally-aligned-left has-border-color has-background" style="border-color:#dce0e6;border-width:1px;background-color:#f3f4f6;padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:400px;--sc-form-row-spacing:30px"><!-- wp:surecart/totals {"collapsible":true,"collapsedOnMobile":true} -->
 	<sc-order-summary collapsible="1" closed-text="Show Summary" open-text="Summary" collapsed-on-mobile="1" class="wp-block-surecart-totals"><!-- wp:surecart/divider -->
 	<sc-divider></sc-divider>
 	<!-- /wp:surecart/divider -->
