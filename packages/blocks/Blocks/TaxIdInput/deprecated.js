f3c54d5769dfad20f46ae4d6f8222e3f32ceaf28
kiptoo <biikiptoo596@gmail.com>
2023-02-10 17:02:55 +0300

    Remove unused attributes

--- packages/blocks/Blocks/TaxIdInput/deprecated.js
+++ packages/blocks/Blocks/TaxIdInput/deprecated.js
@@ -1,22 +1,6 @@
 export default [
 	{
-		attributes: {
-			other_label: {
-				type: 'string',
-			},
-			ca_gst_label: {
-				type: 'string',
-			},
-			au_abn_label: {
-				type: 'string',
-			},
-			gb_vat_label: {
-				type: 'string',
-			},
-			eu_vat_label: {
-				type: 'string',
-			},
-		},
+		attributes: {},
 		supports: {
 			className: false,
 		},
