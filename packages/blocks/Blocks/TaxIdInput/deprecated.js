508b0b2955c38c342e397b2acb04c0e230f485d7
kiptoo <biikiptoo596@gmail.com>
2023-02-10 16:35:39 +0300

    remove latest version on the deprecated

--- packages/blocks/Blocks/TaxIdInput/deprecated.js
+++ packages/blocks/Blocks/TaxIdInput/deprecated.js
@@ -20,31 +20,6 @@ export default [
 		supports: {
 			className: false,
 		},
-		save({ attributes, className }) {
-			const {
-				other_label,
-				ca_gst_label,
-				au_abn_label,
-				gb_vat_label,
-				eu_vat_label,
-			} = attributes;
-
-			return (
-				<sc-order-tax-id-input
-					other-label={other_label || null}
-					ca-gst-label={ca_gst_label || null}
-					au-abn-label={au_abn_label || null}
-					gb-vat-label={gb_vat_label || null}
-					eu-vat-label={eu_vat_label || null}
-				></sc-order-tax-id-input>
-			);
-		},
-	},
-	{
-		attributes: {},
-		supports: {
-			className: false,
-		},
 		save() {
 			return <sc-order-tax-id-input></sc-order-tax-id-input>;
 		},
