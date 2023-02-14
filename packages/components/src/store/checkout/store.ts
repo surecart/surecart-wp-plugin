424f900c806a277b0047115c5edec1f3d2860239
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-13 20:27:53 -0600

    Refactor checkout

--- packages/components/src/store/checkout/store.ts
+++ packages/components/src/store/checkout/store.ts
@@ -20,22 +20,5 @@ const { state, onChange, on } = createStore<Store>(
   },
 );
 
-export const listenTo = (prop, propKey: string | string[], callback) =>
-  on('set', (key, newValue, oldValue) => {
-    // ignore non-keys
-    if (key !== prop) return;
-    // handle an array, if one has changed, run callback.
-    if (Array.isArray(propKey)) {
-      if (propKey.some(key => JSON.stringify(newValue?.[key]) !== JSON.stringify(oldValue?.[key]))) {
-        return callback(newValue, oldValue);
-      }
-    }
-    // handle string.
-    if (typeof propKey === 'string') {
-      if (JSON.stringify(newValue?.[propKey]) === JSON.stringify(oldValue?.[propKey])) return;
-      return callback(newValue?.[propKey], oldValue?.[propKey]);
-    }
-  });
-
 export default state;
 export { state, onChange, on };
