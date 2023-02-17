e96cc8407f284c40a48d6680e72dcc2335ea4e46
kiptoo <biikiptoo596@gmail.com>
2023-02-17 12:55:30 +0300

    Add advanced abandoned checkout settings to plugin

--- packages/admin/settings/abandoned/AbandonedSettings.js
+++ packages/admin/settings/abandoned/AbandonedSettings.js
@@ -284,6 +284,48 @@ export default () => {
 					/>
 				)}
 			</SettingsBox>
+			<SettingsBox
+				title={__('Advanced Settings', 'surecart')}
+				description={__(
+					'Advanced settings for abandoned checkouts',
+					'surecart'
+				)}
+				loading={!hasLoadedItem}
+			>
+				<ScInput
+					type="number"
+					min="1"
+					required
+					label={__('Grace Period Days', 'surecart')}
+					value={item?.grace_period_days}
+					onScInput={(e) => {
+						if (e.target.value) {
+							editItem({
+								grace_period_days: e.target.value,
+							});
+						}
+					}}
+				>
+					<span slot="suffix">{__('Days', 'surecart')}</span>
+				</ScInput>
+
+				<ScSwitch
+					checked={item?.ignore_purchased_products}
+					onScChange={(e) =>
+						editItem({
+							ignore_purchased_products: e.target.checked ? 1 : 0,
+						})
+					}
+				>
+					{__('Ignore Purchased Products', 'surecart')}
+					<span slot="description">
+						{__(
+							"Don't create abandoned checkout if all products in checkout have been purchased.",
+							'surecart'
+						)}
+					</span>
+				</ScSwitch>
+			</SettingsBox>
 		</SettingsTemplate>
 	);
 };
