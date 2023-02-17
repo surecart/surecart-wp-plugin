9460c78d5c46788fa238e175c65aeb52acdf54e8
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-17 13:59:28 -0600

    Fix stripe tests and artifact uploads.

--- packages/components/src/test/sc-checkout/stripe-payment-element/checkout-stripe-payment-element.cy.js
+++ packages/components/src/test/sc-checkout/stripe-payment-element/checkout-stripe-payment-element.cy.js
@@ -111,9 +111,8 @@ describe('Checkout Stripe', () => {
 
     cy.wait('@confirm');
     cy.get('@confirmPayment').should('have.been.calledOnce');
-    cy.get('sc-dialog').find('sc-text').contains('Thanks for your order').should('be.visible');
+    cy.get('sc-dialog').find('sc-dashboard-module').should('be.visible');
     cy.get('sc-button').contains('Continue').should('be.visible').click()
-    cy.location('pathname').should('contain', 'success');
-    cy.location('search').should('contain', 'order=test');
+    cy.get('sc-button[href]').should('have.attr', 'href').and('contain', 'success').and('contain', 'order=test');
   });
 });
