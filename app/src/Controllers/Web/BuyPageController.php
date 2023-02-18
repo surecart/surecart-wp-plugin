484d2c808634903009c96a810873dd55296a8b04
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-18 10:04:31 -0600

    Update view and controller

--- app/src/Controllers/Web/BuyPageController.php
+++ app/src/Controllers/Web/BuyPageController.php
@@ -1,6 +1,9 @@
 <?php
 namespace SureCart\Controllers\Web;
 
+use SureCart\Models\ManualPaymentMethod;
+use SureCart\Models\Processor;
+
 /**
  * Handles webhooks
  */
@@ -30,9 +33,42 @@ class BuyPageController {
 			return \SureCart::redirect()->to( esc_url_raw( \SureCart::routeUrl( 'product', [ 'id' => $product->slug ] ) ) );
 		}
 
+		$user = wp_get_current_user();
+
+		$processors = Processor::get();
+		if ( is_wp_error( $processors ) ) {
+			$processors = [];
+		}
+		$processors = array_values(
+			array_filter(
+				$processors ?? [],
+				function( $processor ) {
+					return $processor->approved && $processor->enabled;
+				}
+			)
+		);
+
 		return \SureCart::view( 'web/buy' )->with(
 			[
-				'product' => $product,
+				'product'                       => $product,
+				'font_size'                     => 16,
+				'customer'                      => [
+					'email' => $user->user_email,
+					'name'  => $user->display_name,
+				],
+				'honeypot_enabled'              => (bool) get_option( 'surecart_honeypot_enabled', true ),
+				'currency_code'                 => $attributes['currency'] ?? \SureCart::account()->currency,
+				'tax_protocol'                  => \SureCart::account()->tax_protocol,
+				'abandoned_checkout_return_url' => esc_url( trailingslashit( get_site_url() ) . 'surecart/redirect' ),
+				'processors'                    => (array) $processors,
+				'manual_payment_methods'        => (array) ManualPaymentMethod::where( [ 'archived' => false ] )->get() ?? [],
+				'stripe_payment_element'        => (bool) get_option( 'sc_stripe_payment_element', false ),
+				'mode'                          => apply_filters( 'surecart/payments/mode', $attributes['mode'] ?? 'live' ),
+				'form_id'                       => $product->id,
+				'id'                            => 'sc-checkout-' . $product->id,
+				'prices'                        => $product->prices->data ?? [],
+				'loading_text'                  => [],
+				'success_url'                   => ! empty( $attributes['success_url'] ) ? $attributes['success_url'] : \SureCart::pages()->url( 'order-confirmation' ),
 			]
 		);
 	}
--- views/web/buy.php
+++ views/web/buy.php
@@ -5,6 +5,7 @@ Template Name: SureCart
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,16 +13,74 @@ Template Name: SureCart
 </head>
 
 <body <?php body_class(); ?>>
-<?php do_action( 'surecart_buy_page_body_open' ); ?>
+	<?php do_action( 'surecart_buy_page_body_open' ); ?>
 
-<sc-product-buy-page id="product-buy-page"></sc-product-buy-page>
+	<sc-checkout id="checkout-buy-form" style="text-align: left;--sc-form-row-spacing: 25px;">
+		<sc-form>
+			<sc-columns is-stacked-on-mobile="1" is-full-height="1" is-reversed-on-mobile="1" class="wp-block-surecart-columns has-background" style="background-color:#f3f4f6;gap:0px 0px">
+				<sc-column class="wp-block-surecart-column is-layout-constrained is-horizontally-aligned-right has-white-background-color has-background" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:450px;--sc-form-row-spacing:30px">
+					<sc-customer-email label="Email" placeholder="your@email.com" required></sc-customer-email>
 
-<?php
-\SureCart::assets()->addComponentData( 'sc-product-buy-page', '#product-buy-page', [ 'product' => $product ] );
-?>
+					<sc-customer-name label="Name" placeholder="Your Full Name" required></sc-customer-name>
 
-<?php
-wp_footer();
-?>
+					<sc-payment id="checkout-buy-payment">
+						<sc-stripe-payment-element slot="stripe"></sc-stripe-payment-element>
+					</sc-payment>
+
+					<sc-order-submit type="primary" full="true" size="large" icon="lock" show-total="true">Purchase</sc-order-submit>
+				</sc-column>
+
+				<sc-column class="wp-block-surecart-column is-sticky is-layout-constrained is-horizontally-aligned-left" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;--sc-column-content-width:450px;--sc-form-row-spacing:30px">
+					<sc-order-summary collapsible="1" closed-text="" open-text="" collapsed-on-mobile="1" class="wp-block-surecart-totals">
+						<sc-divider></sc-divider>
+
+						<sc-line-items removable="1" editable="1" class="wp-block-surecart-line-items"></sc-line-items>
+
+						<sc-divider></sc-divider>
+
+						<sc-line-item-total total="subtotal" class="wp-block-surecart-subtotal"><span slot="description">Subtotal</span></sc-line-item-total>
+
+						<sc-order-coupon-form></sc-order-coupon-form>
+
+						<sc-line-item-tax class="wp-block-surecart-tax-line-item"></sc-line-item-tax>
+
+						<sc-divider></sc-divider>
+
+						<sc-line-item-total total="total" size="large" show-currency="1" class="wp-block-surecart-total"><span slot="title">Total</span><span slot="subscription-title">Total Due Today</span></sc-line-item-total>
+					</sc-order-summary>
+				</sc-column>
+			</sc-columns>
+		</sc-form>
+	</sc-checkout>
+
+	<?php
+	\SureCart::assets()->addComponentData(
+		'sc-checkout',
+		'#checkout-buy-form',
+		[
+			'product'                    => $product,
+			'prices'                     => $prices,
+			'customer'                   => $customer ?? '',
+			'formId'                     => $form_id ?? '',
+			'currencyCode'               => $currency_code ?? null,
+			'modified'                   => $modified ?? null,
+			'loggedIn'                   => is_user_logged_in(),
+			'mode'                       => $mode ?? 'live',
+			'alignment'                  => $align ?? '',
+			'taxProtocol'                => $tax_protocol ?? [],
+			'loadingText'                => $loading_text ?? [],
+			'stripePaymentElement'       => $stripe_payment_element ?? false,
+			'successUrl'                 => esc_url_raw( $success_url ?? \SureCart::pages()->url( 'order-confirmation' ) ),
+			'processors'                 => $processors,
+			'manualPaymentMethods'       => $manual_payment_methods,
+			'abandonedCheckoutReturnUrl' => $abandoned_checkout_return_url,
+		]
+	);
+	?>
+
+	<?php
+	wp_footer();
+	?>
 </body>
+
 </html>
