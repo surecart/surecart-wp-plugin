1314f2926f883c867bb427e0504dbc6d19881526
Andre Gagnon <ajgagnon@uwalumni.com>
2023-02-21 11:35:43 -0600

    Update permissions

--- app/src/Rest/SubscriptionRestServiceProvider.php
+++ app/src/Rest/SubscriptionRestServiceProvider.php
@@ -121,12 +121,12 @@ class SubscriptionRestServiceProvider extends RestServiceProvider implements Res
 
 		register_rest_route(
 			"$this->name/v$this->version",
-			$this->endpoint. '/(?P<id>\S+)/pay_off/',
+			$this->endpoint . '/(?P<id>\S+)/pay_off/',
 			[
 				[
 					'methods'             => \WP_REST_Server::EDITABLE,
 					'callback'            => $this->callback( $this->controller, 'payOff' ),
-					'permission_callback' => [ $this, 'get_item_permissions_check' ],
+					'permission_callback' => [ $this, 'payoff_item_permissions_check' ],
 				],
 				// Register our schema callback.
 				'schema' => [ $this, 'get_item_schema' ],
@@ -207,6 +207,17 @@ class SubscriptionRestServiceProvider extends RestServiceProvider implements Res
 	 */
 	public function cancel_permissions_check( $request ) {
 		return current_user_can( 'cancel_sc_subscription', $request['id'] );
+	}
+
+	/**
+	 * Only a person who has the ability to edit all subscription
+	 * can pay off a subscription early.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 * @return true|\WP_Error True if the request has access to create items, WP_Error object otherwise.
+	 */
+	public function payoff_item_permissions_check( $request ) {
+		return current_user_can( 'edit_sc_subscriptions' );
 	}
 
 	/**
