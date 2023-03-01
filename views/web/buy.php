d935d0e83a050364c5ecd343097b4535762d86e9
Andre Gagnon <ajgagnon@uwalumni.com>
2023-03-01 16:14:19 -0600

    Use passed prices and filter_block_content honeypot fix.

--- views/web/buy.php
+++ views/web/buy.php
@@ -14,6 +14,12 @@ use SureCartBlocks\Blocks\Form\Block as FormBlock;
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
+	<style>
+	/** block editor theme fix. */
+	body .is-layout-constrained > * + * {
+		margin-top: 0;
+	}
+	</style>
 </head>
 
 <body <?php body_class(); ?>>
@@ -24,9 +30,9 @@ use SureCartBlocks\Blocks\Form\Block as FormBlock;
 	ob_start();
 	require 'buy-template.php';
 	$content = ob_get_clean();
-	$price   = $product->prices->data[0];
+
 	if ( $price ) {
-		echo wp_kses_post(
+		echo filter_block_content(
 			( new FormBlock() )->render(
 				[
 					'prices'  => [
@@ -40,7 +46,7 @@ use SureCartBlocks\Blocks\Form\Block as FormBlock;
 					'mode'    => $mode,
 				],
 				do_blocks( $content )
-			)
+			),
 		);
 	}
 	?>
