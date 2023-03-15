df33ac7a319b8e8622edd848cc56d21567cfd321
Andre Gagnon <ajgagnon@uwalumni.com>
2023-03-15 15:28:43 -0500

    Update abandoned checkout test to use page id and buy_page_product_id

--- packages/components/src/test/sc-checkout/abandoned-checkout/abandoned-checkout.cy.js
+++ packages/components/src/test/sc-checkout/abandoned-checkout/abandoned-checkout.cy.js
@@ -1,19 +1,24 @@
 describe('Abandoned Checkouts', () => {
   beforeEach(() => {
-    cy.intercept({
-			method: 'POST',
-			path: '**/surecart/v1/checkouts/*',
-		}, {
-      id: "test",
-      object: "checkout",
-      status: "draft"
-    }).as('createUpdate');
+    cy.intercept(
+      {
+        method: 'POST',
+        path: '**/surecart/v1/checkouts/*',
+      },
+      {
+        id: 'test',
+        object: 'checkout',
+        status: 'draft',
+      },
+    ).as('createUpdate');
   });
 
   it('Sends abandoned_checkout_return_url with request', async () => {
     cy.visit('/test/sc-checkout/abandoned-checkout');
     cy.wait('@createUpdate').then(({ request }) => {
-      expect(request.body.abandoned_checkout_return_url).to.eq('https://test.com/surecart/redirect');
-    })
+      expect(request.body.metadata.page_url).to.include('/test/sc-checkout/abandoned-checkout');
+      expect(request.body.metadata.page_id).to.eq(1);
+      expect(request.body.metadata.buy_page_product_id).to.eq('testproductid');
+    });
   });
 });
