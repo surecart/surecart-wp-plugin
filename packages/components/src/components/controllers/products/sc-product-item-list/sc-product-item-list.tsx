54ee12de4e16ce377ab1bbaf5a74d49874486b27
iamdebadipti <iamdebadipti@gmail.com>
2023-04-05 01:09:11 +0530

    removed unnecessary

--- packages/components/src/components/controllers/products/sc-product-item-list/sc-product-item-list.tsx
+++ packages/components/src/components/controllers/products/sc-product-item-list/sc-product-item-list.tsx
@@ -1,5 +1,6 @@
-import { Component, h, Host, Prop, State } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 import { addQueryArgs } from '@wordpress/url';
+import { __ } from '@wordpress/i18n';
 
 import { Product } from '../../../../types';
 import apiFetch from '../../../../functions/fetch';
@@ -50,53 +51,51 @@ export class ScProductItemList {
 
   render() {
     return (
-      <Host>
+      <div
+        class={{
+          'product-item-list-wrapper': true,
+        }}
+      >
         <div
           class={{
-            'product-item-list-wrapper': true,
+            'product-item-list': true,
           }}
         >
-          <div
-            class={{
-              'product-item-list': true,
-            }}
-          >
-            {this.loading ? (
-              [...Array(10)].map(() => (
-                <div class="product-item-list__loader">
-                  {this.layoutConfig?.map(layout => {
-                    switch (layout.blockName) {
-                      case 'surecart/product-item-title':
-                        return <sc-skeleton style={{ width: '80%' }}></sc-skeleton>;
-                      case 'surecart/product-item-image':
-                        return (
-                          <sc-skeleton
-                            style={{
-                              'width': '100%',
-                              'minHeight': '90%',
-                              'aspectRatio': layout.attributes?.ratio ?? '1/1.4',
-                              '--sc-border-radius-pill': '12px',
-                            }}
-                          ></sc-skeleton>
-                        );
-                      case 'surecart/product-item-price':
-                        return <sc-skeleton style={{ width: '40%' }}></sc-skeleton>;
-                      default:
-                        return null;
-                    }
-                  })}
-                </div>
-              ))
-            ) : this.products?.length ? (
-              this.products.map(product => {
-                return <sc-product-item product={product} layoutConfig={this.layoutConfig}></sc-product-item>;
-              })
-            ) : (
-              <p>No Product Found.</p>
-            )}
-          </div>
+          {this.loading ? (
+            [...Array(10)].map(() => (
+              <div class="product-item-list__loader">
+                {this.layoutConfig?.map(layout => {
+                  switch (layout.blockName) {
+                    case 'surecart/product-item-title':
+                      return <sc-skeleton style={{ width: '80%' }}></sc-skeleton>;
+                    case 'surecart/product-item-image':
+                      return (
+                        <sc-skeleton
+                          style={{
+                            'width': '100%',
+                            'minHeight': '90%',
+                            'aspectRatio': layout.attributes?.ratio ?? '1/1.4',
+                            '--sc-border-radius-pill': '12px',
+                          }}
+                        ></sc-skeleton>
+                      );
+                    case 'surecart/product-item-price':
+                      return <sc-skeleton style={{ width: '40%' }}></sc-skeleton>;
+                    default:
+                      return null;
+                  }
+                })}
+              </div>
+            ))
+          ) : this.products?.length ? (
+            this.products.map(product => {
+              return <sc-product-item product={product} layoutConfig={this.layoutConfig}></sc-product-item>;
+            })
+          ) : (
+            <p>{__('No Product Found.', 'surecart')}</p>
+          )}
         </div>
-      </Host>
+      </div>
     );
   }
 }
