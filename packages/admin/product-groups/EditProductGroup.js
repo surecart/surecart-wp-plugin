6f5f1e213b8f16b1d935e327a27410a335d82f7c
iamdebadipti <iamdebadipti@gmail.com>
2023-04-06 13:23:45 +0530

    added archive & delete options for product group

--- packages/admin/product-groups/EditProductGroup.js
+++ packages/admin/product-groups/EditProductGroup.js
@@ -1,8 +1,15 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { ScButton } from '@surecart/components-react';
+import {
+	ScButton,
+	ScDropdown,
+	ScIcon,
+	ScMenu,
+	ScMenuItem,
+} from '@surecart/components-react';
 import { Fragment, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { store as noticesStore } from '@wordpress/notices';
 
 import Error from '../components/Error';
 import useEntity from '../hooks/useEntity';
@@ -12,16 +19,21 @@ import Details from './modules/Details';
 import SaveButton from '../templates/SaveButton';
 import useSave from '../settings/UseSave';
 import Products from './modules/Products';
+import { useDispatch } from '@wordpress/data';
 
 export default ({ id }) => {
+	const { createSuccessNotice, createErrorNotice } =
+		useDispatch(noticesStore);
 	const [error, setError] = useState(null);
 	const { save } = useSave();
 	const {
 		item,
 		editItem,
 		itemError,
+		saveItem,
 		saveError,
 		savingItem,
+		deleteItem,
 		deletingItem,
 		hasLoadedItem,
 	} = useEntity('product-group', id);
@@ -35,6 +47,48 @@ export default ({ id }) => {
 			save({ successMessage: __('Product group updated.', 'surecart') });
 		} catch (e) {
 			setError(e);
+		}
+	};
+
+	const onDelete = async () => {
+		const r = confirm(
+			__(
+				'Permanently delete this product group? You cannot undo this action.',
+				'surecart'
+			)
+		);
+		if (!r) return;
+
+		try {
+			await deleteItem({ throwOnError: true });
+			createSuccessNotice(__('Product group deleted.', 'surecart'));
+			window.location.assign('admin.php?page=sc-product-groups');
+		} catch (e) {
+			console.error(e?.message);
+			setError(e);
+		}
+	};
+
+	const toggleArchive = async () => {
+		const r = confirm(
+			item?.archived
+				? __('Un-Archive this product group?', 'surecart')
+				: __('Archive this product group?', 'surecart')
+		);
+
+		if (!r) return;
+
+		try {
+			await saveItem({ archived: !item?.archived });
+			createSuccessNotice(
+				!item?.archived
+					? __('Product group archived.', 'surecart')
+					: __('Product group un-archived.', 'surecart'),
+				{ type: 'snackbar' }
+			);
+		} catch (e) {
+			console.error(e);
+			createErrorNotice(e?.message, { type: 'snackbar' });
 		}
 	};
 
@@ -54,6 +108,21 @@ export default ({ id }) => {
 				gap: 0.5em;
 			`}
 		>
+			<ScDropdown placement="bottom-end">
+				<ScButton type="text" slot="trigger">
+					<ScIcon name="more-horizontal" />
+				</ScButton>
+				<ScMenu>
+					<ScMenuItem onClick={toggleArchive}>
+						{item?.archived
+							? __('Un-Archive', 'surecart')
+							: __('Archive', 'surecart')}
+					</ScMenuItem>
+					<ScMenuItem onClick={onDelete}>
+						{__('Delete', 'surecart')}
+					</ScMenuItem>
+				</ScMenu>
+			</ScDropdown>
 			<SaveButton busy={deletingItem || savingItem}>
 				{__('Save Group', 'surecart')}
 			</SaveButton>
