1a894b68e841f3cbd30f88b84e011ed43cacbe27
iamdebadipti <iamdebadipti@gmail.com>
2023-04-18 16:57:28 +0530

    made onboarding screens full width*height

--- packages/admin/templates/OnboardingModel.js
+++ packages/admin/templates/OnboardingModel.js
@@ -18,10 +18,15 @@ export default ({ children }) => {
 						);
 					}
 					#wpcontent {
+						margin: 0 !important;
 						padding: 0;
 					}
 					#wpbody-content {
 						padding: 0 !important;
+						position: fixed;
+						top: 0;
+						left: 0;
+						z-index: 999999;
 					}
 					#wpfooter {
 						display: none;
@@ -33,10 +38,12 @@ export default ({ children }) => {
 			/>
 			<div
 				css={css`
-					height: calc(100vh - 32px);
+					height: 100vh;
+					width: 100vw;
 					display: flex;
 					flex-direction: column;
 					overflow: hidden;
+					background-color: #f0f0f1;
 				`}
 			>
 				{children}
