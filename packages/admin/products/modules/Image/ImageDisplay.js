5bc22b8e2c7f49fa0ea398bc85ce9a8595999732
Andre Gagnon <ajgagnon@uwalumni.com>
2023-04-19 11:23:32 -0500

    Image display styling

--- packages/admin/products/modules/Image/ImageDisplay.js
+++ packages/admin/products/modules/Image/ImageDisplay.js
@@ -1,42 +1,45 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { ScIcon } from '@surecart/components-react';
+import { ScIcon, ScTag } from '@surecart/components-react';
 import { __ } from '@wordpress/i18n';
 
-export default ({ productMedia, onDeleteImage }) => {
+export default ({ productMedia, onDeleteImage, isFeatured }) => {
 	return (
 		<div
 			css={css`
 				background: #f3f3f3;
 				cursor: move;
 				position: relative;
+				border-radius: var(--sc-border-radius-medium);
+				border: var(--sc-input-border);
+				box-shadow: var(--sc-input-box-shadow);
 
 				.overlay,
 				.delete-icon {
-					display: none;
+					opacity: 0;
+					visibility: hidden;
+					transition: all var(--sc-transition-medium) ease-in-out;
 				}
 
 				:hover .overlay,
 				:hover .delete-icon {
-					display: block;
+					opacity: 1;
+					visibility: visible;
 				}
 			`}
 			media-id={productMedia.id}
 		>
-			{productMedia.position === 0 && (
-				<span
+			{isFeatured && (
+				<ScTag
+					type="info"
 					css={css`
 						position: absolute;
-						top: 0;
-						left: 0;
-						background-color: var(--sc-color-danger-500);
-						color: var(--sc-color-white);
-						padding: 0.3rem 0.4rem;
-						border-bottom-right-radius: 4px;
+						top: 5px;
+						right: 5px;
 					`}
 				>
 					{__('Featured', 'surecart')}
-				</span>
+				</ScTag>
 			)}
 			<ScIcon
 				className="delete-icon"
@@ -74,7 +77,7 @@ export default ({ productMedia, onDeleteImage }) => {
 					max-width: 100%;
 					width: 380px;
 					aspect-ratio: 1/1;
-					object-fit: cover;
+					object-fit: contain;
 					height: auto;
 					display: block;
 					border-radius: var(--sc-border-radius-medium);
