9ef95fdae049ac8699e4367b6be407cd12a0c1f5
Andre Gagnon <ajgagnon@uwalumni.com>
2023-04-27 14:21:19 -0500

    Format cart service

--- app/src/Cart/CartService.php
+++ app/src/Cart/CartService.php
@@ -7,117 +7,116 @@ use SureCart\Models\Form;
 /**
  * The cart service.
  */
-class CartService
-{
-    /**
-     * Bootstrap the cart.
-     *
-     * @return void
-     */
-    public function bootstrap() {
-        // Slide-out is disabled. Do not load scripts.
-        if ((bool) get_option('sc_slide_out_cart_disabled', false)) {
-            return;
-        }
+class CartService {
+	/**
+	 * Bootstrap the cart.
+	 *
+	 * @return void
+	 */
+	public function bootstrap() {
+		// Slide-out is disabled. Do not load scripts.
+		if ( (bool) get_option( 'sc_slide_out_cart_disabled', false ) ) {
+			return;
+		}
 
-        // enqueue scripts needed for slide out cart.
-        add_action(
-            'wp_enqueue_scripts',
-            function () {
-                \SureCart::assets()->enqueueComponents();
-            }
-        );
-        add_action('wp_footer', [$this, 'renderCartComponent']);
-    }
+		// enqueue scripts needed for slide out cart.
+		add_action(
+			'wp_enqueue_scripts',
+			function () {
+				\SureCart::assets()->enqueueComponents();
+			}
+		);
+		add_action( 'wp_footer', [ $this, 'renderCartComponent' ] );
+	}
 
-    /**
-     * Get the cart template.
-     *
-     * @return string
-     */
-    public function cartTemplate() {
-        $form = $this->getForm();
+	/**
+	 * Get the cart template.
+	 *
+	 * @return string
+	 */
+	public function cartTemplate() {
+		$form = $this->getForm();
 
-        if (empty($form->ID)) {
-            return '';
-        }
+		if ( empty( $form->ID ) ) {
+			return '';
+		}
 
-        $cart = \SureCart::cartPost()->get();
+		$cart = \SureCart::cartPost()->get();
 
-        if (empty($cart->post_content)) {
-            return '';
-        }
+		if ( empty( $cart->post_content ) ) {
+			return '';
+		}
 
-        $floating_icon_enabled = $this->isFloatingIconEnabled() ? 'false' : 'true';
+		$floating_icon_enabled = $this->isFloatingIconEnabled() ? 'false' : 'true';
 
-        ob_start();
-        ?>
+		ob_start();
+		?>
 
 		<sc-cart
 			id="sc-cart"
-			header="<?php esc_attr_e('Cart', 'surecart');?>"
-			form-id="<?php echo esc_attr($form->ID); ?>"
-			mode="<?php echo esc_attr(Form::getMode($form->ID)); ?>"
-			checkout-link="<?php echo esc_attr(\SureCart::pages()->url('checkout')); ?>"
+			header="<?php esc_attr_e( 'Cart', 'surecart' ); ?>"
+			form-id="<?php echo esc_attr( $form->ID ); ?>"
+			mode="<?php echo esc_attr( Form::getMode( $form->ID ) ); ?>"
+			checkout-link="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
 			style="font-size: 16px"
 			floating-icon-enabled="<?php echo esc_attr( $floating_icon_enabled ); ?>"
 		>
-			<?php echo wp_kses_post(do_blocks($cart->post_content)); ?>
+			<?php echo wp_kses_post( do_blocks( $cart->post_content ) ); ?>
 		</sc-cart>
 
 		<?php
-        return trim(preg_replace('/\s+/', ' ', ob_get_clean()));
-    }
+		return trim( preg_replace( '/\s+/', ' ', ob_get_clean() ) );
+	}
 
-    /**
-     * Render the cart components.
-     *
-     * @return void
-     */
-    public function renderCartComponent() {
-        $form = $this->getForm();
-        if (empty($form->ID)) {
-            return;
-        }
-        $template = $this->cartTemplate();
-        ?>
+	/**
+	 * Render the cart components.
+	 *
+	 * @return void
+	 */
+	public function renderCartComponent() {
+		$form = $this->getForm();
+		if ( empty( $form->ID ) ) {
+			return;
+		}
+		$template = $this->cartTemplate();
+		?>
 		<sc-cart-loader
-			form-id="<?php echo esc_attr($form->ID); ?>"
-			mode="<?php echo esc_attr(Form::getMode($form->ID)); ?>"
-			template='<?php echo esc_attr($template); ?>'>
+			form-id="<?php echo esc_attr( $form->ID ); ?>"
+			mode="<?php echo esc_attr( Form::getMode( $form->ID ) ); ?>"
+			template='<?php echo esc_attr( $template ); ?>'>
 		</sc-cart-loader>
 		<?php
-    }
+	}
 
-    /**
-     * Get the form
-     *
-     * @return \WP_Post The default form post.
-     */
-    public function getForm() {
-        return \SureCart::forms()->getDefault();
-    }
+	/**
+	 * Get the form
+	 *
+	 * @return \WP_Post The default form post.
+	 */
+	public function getForm() {
+		return \SureCart::forms()->getDefault();
+	}
 
-    /**
-     * Check if floating cart icon is enabled
-     * 
-     * @return string
-     */
-    public function isFloatingIconEnabled() {
-        $cart_icon_type = (string) get_option('surecart_cart_icon_type', null);
-        return $cart_icon_type === 'menu_icon';
-    }
-    
-    /**
-     * Check if menu cart icon is enabled
-     * 
-     * @param integer $term_id Term ID.
-     * @return bool
-     */
-    public function isMenuIconEnabled( $term_id ) {
-        $cart_menu_ids    = (array) get_option( 'surecart_cart_menu_selected_ids', null );
-		$cart_icon_type   = (string) get_option( 'surecart_cart_icon_type', null );
-        
-        return $cart_icon_type === 'floating_icon' || !in_array( $term_id, $cart_menu_ids );
-    }
+	/**
+	 * Check if floating cart icon is enabled
+	 *
+	 * @return string
+	 */
+	public function isFloatingIconEnabled() {
+		$cart_icon_type = (string) get_option( 'surecart_cart_icon_type', null );
+		return $cart_icon_type === 'menu_icon';
+	}
+
+	/**
+	 * Check if menu cart icon is enabled
+	 *
+	 * @param integer $term_id Term ID.
+	 * @return bool
+	 */
+	public function isMenuIconEnabled( $term_id ) {
+		$cart_menu_ids  = (array) get_option( 'surecart_cart_menu_selected_ids', null );
+		$cart_icon_type = (string) get_option( 'surecart_cart_icon_type', null );
+
+		return $cart_icon_type === 'floating_icon' || ! in_array( $term_id, $cart_menu_ids );
+	}
 }
