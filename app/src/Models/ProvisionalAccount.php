07c18028c78fee273b131e3463ca044bea738911
kiptoo <biikiptoo596@gmail.com>
2023-04-28 16:21:54 +0300

    Update the model to make public requests

--- app/src/Models/ProvisionalAccount.php
+++ app/src/Models/ProvisionalAccount.php
@@ -19,4 +19,16 @@ class ProvisionalAccount extends Model {
 	 * @var string
 	 */
 	protected $object_name = 'provisional_account';
+
+	/**
+	 * Make the API request.
+	 *
+	 * @param array  $args Array of arguments.
+	 * @param string $endpoint Optional endpoint override.
+	 *
+	 * @return Model
+	 */
+	protected function makeRequest( $args = [], $endpoint = '' ) {
+		return \SureCart::unAuthorizedRequest( ...$this->prepareRequest( $args, $endpoint ) );
+	}
 }
