f8e366069a76d405ee70fc12d0e5136e8b7cfcba
kiptoo <biikiptoo596@gmail.com>
2023-05-02 22:40:49 +0300

    add shipping to sidebar

---
+++ packages/admin/settings/shipping/ShippingProfileSettings.js
@@ -0,0 +1,7 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { __ } from '@wordpress/i18n';
+
+export default () => {
+	return <div>Hello this is the shipping settings page</div>;
+};
