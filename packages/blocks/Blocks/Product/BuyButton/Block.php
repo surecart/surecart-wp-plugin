ca1acd5c8fca7c2363ec31dd56156ed215b594a9
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-03 15:42:59 -0500

    Start refactor for allowing block edit options for style

--- packages/blocks/Blocks/Product/BuyButton/Block.php
+++ packages/blocks/Blocks/Product/BuyButton/Block.php
@@ -15,6 +15,69 @@ class Block extends BaseBlock {
 	 */
 	public static $instance;
 
+		/**
+		 * Get the style for the block
+		 *
+		 * @param  array $attributes Style variables.
+		 * @return string
+		 */
+	public function getVars( $attr, $prefix ) {
+		$style = '';
+		// padding.
+		if ( ! empty( $attr['style']['spacing']['padding'] ) ) {
+			$padding = $attr['style']['spacing']['padding'];
+			$style  .= $prefix . '-padding-top: ' . $this->getSpacingPresetCssVar( array_key_exists( 'top', $padding ) ? $padding['top'] : '0' ) . ';';
+			$style  .= $prefix . '-padding-bottom: ' . $this->getSpacingPresetCssVar( array_key_exists( 'bottom', $padding ) ? $padding['bottom'] : '0' ) . ';';
+			$style  .= $prefix . '-padding-left: ' . $this->getSpacingPresetCssVar( array_key_exists( 'left', $padding ) ? $padding['left'] : '0' ) . ';';
+			$style  .= $prefix . '-padding-right: ' . $this->getSpacingPresetCssVar( array_key_exists( 'right', $padding ) ? $padding['right'] : '0' ) . ';';
+		}
+		// margin.
+		if ( ! empty( $attr['style']['spacing']['margin'] ) ) {
+			$margin = $attr['style']['spacing']['margin'];
+			$style .= $prefix . '-margin-top: ' . $this->getSpacingPresetCssVar( array_key_exists( 'top', $margin ) ? $margin['top'] : '0' ) . ';';
+			$style .= $prefix . '-margin-bottom: ' . $this->getSpacingPresetCssVar( array_key_exists( 'bottom', $margin ) ? $margin['bottom'] : '0' ) . ';';
+			$style .= $prefix . '-margin-left: ' . $this->getSpacingPresetCssVar( array_key_exists( 'left', $margin ) ? $margin['left'] : '0' ) . ';';
+			$style .= $prefix . '-margin-right: ' . $this->getSpacingPresetCssVar( array_key_exists( 'right', $margin ) ? $margin['right'] : '0' ) . ';';
+		}
+		// aspect ratio.
+		if ( ! empty( $attr['ratio'] ) ) {
+			$style .= $prefix . '-aspect-ratio: ' . $attr['ratio'] . ';';
+		}
+		// border width.
+		if ( ! empty( $attr['style']['border']['width'] ) ) {
+			$style .= $prefix . '-border-width: ' . $attr['style']['border']['width'] . ';';
+		}
+		// border radius.
+		if ( ! empty( $attr['style']['border']['radius'] ) ) {
+			$style .= $prefix . '-border-radius: ' . $attr['style']['border']['radius'] . ';';
+		}
+		// font weight.
+		if ( ! empty( $attr['style']['typography']['fontWeight'] ) ) {
+			$style .= $prefix . '-font-weight: ' . $attr['style']['typography']['fontWeight'] . ';';
+		}
+		// font size.
+		if ( ! empty( $attr['fontSize'] ) || ! empty( $attr['style']['typography']['fontSize'] ) ) {
+			$font_size = ! empty( $attr['fontSize'] ) ? $this->getFontSizePresetCssVar( $attr['fontSize'] ) : $attr['style']['typography']['fontSize'];
+			$style    .= $prefix . '-font-size: ' . $font_size . ';';
+		}
+		// border color.
+		if ( ! empty( $attr['borderColor'] ) || ! empty( $attr['style']['border']['color'] ) ) {
+			$border_color = ! empty( $attr['borderColor'] ) ? $this->getColorPresetCssVar( $attr['borderColor'] ) : $attr['style']['border']['color'];
+			$style       .= $prefix . '-border-color: ' . $border_color . ';';
+		}
+		// text color.
+		if ( ! empty( $attr['textColor'] ) || ! empty( $attr['style']['color']['text'] ) ) {
+			$text_color = ! empty( $attr['textColor'] ) ? $this->getColorPresetCssVar( $attr['textColor'] ) : $attr['style']['color']['text'];
+			$style     .= $prefix . '-text-color: ' . $text_color . ';';
+		}
+		// text align.
+		if ( ! empty( $attr['align'] ) ) {
+			$style .= $prefix . '-align: ' . $attr['align'] . ';';
+		}
+
+		return $style;
+	}
+
 	/**
 	 * Render the block
 	 *
@@ -27,7 +90,7 @@ class Block extends BaseBlock {
 		ob_start();
 		?>
 
-		<sc-product-buy-button add-to-cart href="#" type="primary">
+		<sc-product-buy-button add-to-cart href="#" type="primary" style="<?php echo esc_attr( $this->getVars( $attributes, '--sc-button' ) ); ?>">
 			<?php echo esc_html( $attributes['text'] ); ?>
 		</sc-product-buy-button>
 
