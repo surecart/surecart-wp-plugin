04bf5c897c01e15df597e928def1b507221c31a0
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-08 15:37:13 -0500

    Duplicate imports

--- packages/blocks/Blocks/BuyButton/edit.js
+++ packages/blocks/Blocks/BuyButton/edit.js
@@ -7,8 +7,6 @@ import { __ } from '@wordpress/i18n';
 import {
 	InspectorControls,
 	PanelColorSettings,
-	InspectorControls,
-	PanelColorSettings,
 	RichText,
 } from '@wordpress/block-editor';
 import {
