bf9bddcc89c43f99f0e62e43f4fd5b3e0810b4ab
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-10 07:50:43 -0500

    Store name and email in user store.

--- packages/components/src/store/user/store.ts
+++ packages/components/src/store/user/store.ts
@@ -2,10 +2,14 @@ import { createStore } from '@stencil/store';
 
 interface Store {
   loggedIn: boolean;
+  email: string;
+  name: string;
 }
 
 const { state, onChange, dispose } = createStore<Store>({
   loggedIn: false,
+  email: '',
+  name: '',
 });
 
 export default state;
