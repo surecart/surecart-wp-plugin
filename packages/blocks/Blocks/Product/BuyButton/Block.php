d8d9824f92caea8ad1e7f86d30dab9a23bdf172e
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-16 14:36:28 -0500

    Unavailable if no prices.

--- packages/blocks/Blocks/Product/BuyButton/Block.php
+++ packages/blocks/Blocks/Product/BuyButton/Block.php
@@ -114,7 +114,7 @@ class Block extends BaseBlock {
 				class="wp-block-button <?php echo esc_attr( $width_class ); ?>"
 				button-text="<?php echo esc_attr( $attributes['text'] ); ?>">
 				<a class="wp-block-button__link wp-element-button sc-button <?php echo esc_attr( $this->getClasses( $attributes ) ); ?>" style="<?php echo esc_attr( $this->getStyles( $attributes ) ); ?>">
-					<span data-text><?php echo wp_kses_post( $product->archived ? __( 'Unavailable For Purchase', 'surecart' ) : $attributes['text'] ); ?></span>
+					<span data-text><?php echo wp_kses_post( $product->archived || empty( $product->prices->data ) ? __( 'Unavailable For Purchase', 'surecart' ) : $attributes['text'] ); ?></span>
 					<sc-spinner data-loader></sc-spinner>
 				</a>
 			</sc-product-buy-button>
