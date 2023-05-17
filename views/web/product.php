13f6e2543ac63c051f90c4be8677f88e0bbc1de1
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-17 16:31:29 -0500

    Remove commented code.

--- views/web/product.php
+++ views/web/product.php
@@ -4,7 +4,3 @@ $template = get_query_template(
 	$templates
 );
 require $template;
-// get_header();
-// echo '<style>.is-layout-constrained .alignwide { max-width: ' . (int) ( $content_width ?? 1080 ) . 'px; margin-left: auto; margin-right: auto; }</style>';
-// echo surecart_get_the_block_template_html( $content ); // phpcs:ignore WordPress.Security.EscapeOutput
-// get_footer();
