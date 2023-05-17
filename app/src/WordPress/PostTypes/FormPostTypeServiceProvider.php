8184c4eb7aa0c3cb6b62d26b2c93f1af394df2da
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-17 16:50:14 -0500

    Change template post type to productpageposttypescrivce.

--- app/src/WordPress/PostTypes/FormPostTypeServiceProvider.php
+++ app/src/WordPress/PostTypes/FormPostTypeServiceProvider.php
@@ -14,14 +14,14 @@ class FormPostTypeServiceProvider implements ServiceProviderInterface {
 	 *  @param  \Pimple\Container $container Service Container.
 	 */
 	public function register( $container ) {
-		$container['surecart.forms']         = function ( $container ) {
+		$container['surecart.forms']                   = function ( $container ) {
 			return new FormPostTypeService( $container['surecart.pages'] );
 		};
-		$container['surecart.cart.post']     = function( $container ) {
+		$container['surecart.cart.post']               = function( $container ) {
 			return new CartPostTypeService( $container['surecart.pages'] );
 		};
-		$container['surecart.template.post'] = function( $container ) {
-			return new TemplatePostTypeService( $container['surecart.pages'] );
+		$container['surecart.post_types.product_page'] = function() {
+			return new ProductPagePostTypeService();
 		};
 
 		$app = $container[ SURECART_APPLICATION_KEY ];
@@ -37,6 +37,6 @@ class FormPostTypeServiceProvider implements ServiceProviderInterface {
 	public function bootstrap( $container ) {
 		$container['surecart.forms']->bootstrap();
 		$container['surecart.cart.post']->bootstrap();
-		$container['surecart.template.post']->bootstrap();
+		$container['surecart.post_types.product_page']->bootstrap();
 	}
 }
