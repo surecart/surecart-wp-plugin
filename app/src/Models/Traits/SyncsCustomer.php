b1810f1be3cf05cc3ea725a238cefa5c570b9a77
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-23 11:10:09 -0500

    Don't sync by default so we can whitelist special syncing.

--- app/src/Models/Traits/SyncsCustomer.php
+++ app/src/Models/Traits/SyncsCustomer.php
@@ -9,11 +9,31 @@ use SureCart\Models\Subscription;
  */
 trait SyncsCustomer {
 	/**
+	 * Should we sync?
+	 *
+	 * @var boolean
+	 */
+	protected $should_sync = false;
+
+	/**
 	 * Should we sync the user to a customer?
 	 *
 	 * @return boolean
 	 */
 	protected function shouldSyncCustomer() {
+		if ( ! $this->should_sync ) {
+			return false;
+		}
 		return get_option( 'surecart_auto_sync_user_to_customer', false );
+	}
+
+	/**
+	 * Enable syncing.
+	 *
+	 * @return this
+	 */
+	protected function sync( $sync = true ) {
+		$this->should_sync = $sync;
+		return $this;
 	}
 }
