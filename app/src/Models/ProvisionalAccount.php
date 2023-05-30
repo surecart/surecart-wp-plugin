87a12051dbd764897caf0b67edd688fd235368a9
Andre Gagnon <ajgagnon@uwalumni.com>
2023-05-30 15:56:18 -0500

    Only allow provisional account create if apitoken is not yet added.

--- app/src/Models/ProvisionalAccount.php
+++ app/src/Models/ProvisionalAccount.php
@@ -31,4 +31,20 @@ class ProvisionalAccount extends Model {
 	protected function makeRequest( $args = [], $endpoint = '' ) {
 		return \SureCart::unAuthorizedRequest( ...$this->prepareRequest( $args, $endpoint ) );
 	}
+
+	/**
+	 * Create a new model
+	 *
+	 * @param array $attributes Attributes to create.
+	 *
+	 * @return $this|false
+	 */
+	protected function create( $attributes = [] ) {
+		// we only allow this if the setup is not complete.
+		if ( ! empty( ApiToken::get() ) ) {
+			return new \WP_Error( 'setup_complete', __( 'You have already set up your store.', 'surecart' ) );
+		}
+
+		return parent::create();
+	}
 }
