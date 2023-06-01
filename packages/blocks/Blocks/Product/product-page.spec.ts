30b465dc1878e38c183c901629168619b5096b80
Andre Gagnon <ajgagnon@uwalumni.com>
2023-06-01 10:35:42 -0500

    Change to product page.

--- packages/blocks/Blocks/Product/product-page.spec.ts
+++ packages/blocks/Blocks/Product/product-page.spec.ts
@@ -3,7 +3,7 @@
  */
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
-test.describe('surecart/logout-button', () => {
+test.describe('Product Page', () => {
 	let product;
 
 	test.beforeEach(async ({ requestUtils }) => {
