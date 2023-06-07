adedcfed9220f02dbfd74af600dff1b85c9aae03
Andre Gagnon <ajgagnon@uwalumni.com>
2023-06-07 14:29:00 -0500

    Add name and url to provisional account.

--- app/src/Controllers/Rest/ProvisionalAccountController.php
+++ app/src/Controllers/Rest/ProvisionalAccountController.php
@@ -17,6 +17,21 @@ class ProvisionalAccountController extends RestController {
 	protected $class = ProvisionalAccount::class;
 
 	/**
+	 * Create model.
+	 *
+	 * @param \WP_REST_Request $request Rest Request.
+	 *
+	 * @return \WP_REST_Response|\WP_Error
+	 */
+	public function create( \WP_REST_Request $request ) {
+		// add name and url to provisional account.
+		$request->set_param( 'account_name', get_bloginfo( 'name' ) );
+		$request->set_param( 'account_url', get_bloginfo( 'url' ) );
+
+		return parent::create( $request );
+	}
+
+	/**
 	 * When indexing, fetch the account.
 	 *
 	 * @param \WP_REST_Request $request Rest Request.
