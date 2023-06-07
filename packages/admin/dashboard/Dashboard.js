6ed992749e2828a7d6c76e35d495840542f63784
Andre Gagnon <ajgagnon@uwalumni.com>
2023-06-07 14:12:44 -0500

    Handle claim success message.

--- packages/admin/dashboard/Dashboard.js
+++ packages/admin/dashboard/Dashboard.js
@@ -14,18 +14,37 @@ import {
 	ScBreadcrumb,
 	ScSwitch,
 	ScProvisionalBanner,
+	ScAlert,
 } from '@surecart/components-react';
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { useState } from 'react';
+import { hasQueryArg, removeQueryArgs } from '@wordpress/url';
+import { useEffect } from 'react';
 
 export default () => {
 	const [liveMode, setLiveMode] = useState(true);
+	const showClaimSuccess = hasQueryArg(
+		window.location.href,
+		'account_claimed_success'
+	);
+
+	useEffect(() => {
+		if (showClaimSuccess) {
+			window.history.replaceState(
+				{},
+				document.title,
+				removeQueryArgs(window.location.href, 'account_claimed_success')
+			);
+		}
+	}, [showClaimSuccess]);
+
 	return (
 		<>
 			{!!scData?.claim_url && (
 				<ScProvisionalBanner claimUrl={scData?.claim_url} />
 			)}
+
 			<DashboardModel
 				title={
 					<div
@@ -58,6 +77,20 @@ export default () => {
 				}
 			>
 				<Fragment>
+					{!!showClaimSuccess && (
+						<ScAlert
+							title={__('Setup Complete!', 'surecart')}
+							type="success"
+							open={!!showClaimSuccess}
+							closable
+							style={{ fontSize: '16px' }}
+						>
+							{__(
+								'Your store is now connected to SureCart.',
+								'surecart'
+							)}
+						</ScAlert>
+					)}
 					<GetStarted />
 					<Overview liveMode={liveMode} setLiveMode={setLiveMode} />
 					<ScDivider style={{ '--spacing': '1em' }} />
