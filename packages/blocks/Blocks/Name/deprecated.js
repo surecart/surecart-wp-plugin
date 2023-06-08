785087b363a98665957e315029892ef941019e0c
iamdebadipti <iamdebadipti@gmail.com>
2023-06-08 14:46:27 +0530

    updated name block deprecation

--- packages/blocks/Blocks/Name/deprecated.js
+++ packages/blocks/Blocks/Name/deprecated.js
@@ -45,4 +45,46 @@ export default [
 			);
 		},
 	},
+	{
+		attributes: {
+			required: {
+				type: 'boolean',
+			},
+			label: {
+				type: 'string',
+				default: 'Name',
+			},
+			help: {
+				type: 'string',
+				default: '',
+			},
+			placeholder: {
+				type: 'string',
+			},
+		},
+		save({ attributes, className }) {
+			const {
+				label,
+				help,
+				autofocus,
+				placeholder,
+				showLabel,
+				size,
+				required,
+			} = attributes;
+
+			return (
+				<sc-customer-name
+					class={className || false}
+					label={label || false}
+					help={help || false}
+					autofocus={autofocus || false}
+					placeholder={placeholder || false}
+					showLabel={showLabel || false}
+					size={size || false}
+					required={required ? '1' : '0'}
+				></sc-customer-name>
+			);
+		},
+	},
 ];
