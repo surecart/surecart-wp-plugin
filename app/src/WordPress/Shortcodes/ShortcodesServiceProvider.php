a77ff0c0007672068a6503444740bc96601d8892
Rajkiran Bagal <rajkiranb@bsf.io>
2023-06-20 10:32:43 +0530

    SC-2258 - Fix: Resolve Illegal string offset 'id' PHP Error when Divi Builder is active.

--- app/src/WordPress/Shortcodes/ShortcodesServiceProvider.php
+++ app/src/WordPress/Shortcodes/ShortcodesServiceProvider.php
@@ -199,9 +199,11 @@ class ShortcodesServiceProvider implements ServiceProviderInterface {
 	 *
 	 * @param  array  $atts Shortcode attributes.
 	 * @param  string $content Shortcode content.
+	 * @param  string $content Shortcode tag.
+	 *
 	 * @return string Shortcode output.
 	 */
-	public function formShortcode( $atts ) {
+	public function formShortcode( $atts, $content, $name ) {
 		$atts = shortcode_atts(
 			[
 				'id' => null,
@@ -227,7 +229,7 @@ class ShortcodesServiceProvider implements ServiceProviderInterface {
 			return __( 'This form is not available or has been deleted.', 'surecart' );
 		}
 
-		return apply_filters( 'surecart/shortcode/render', do_blocks( $form->post_content ), $atts, $form );
+		return apply_filters( 'surecart/shortcode/render', do_blocks( $form->post_content ), $atts, $name, $form );
 	}
 
 	/**
