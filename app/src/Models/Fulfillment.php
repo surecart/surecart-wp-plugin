c07b66d9cdb6bd0121821f3b2a6b53de5338ef73
Andre Gagnon <ajgagnon@uwalumni.com>
2023-06-21 16:28:10 -0500

    Add fulfillment, fulfillment item models.

---
+++ app/src/Models/Fulfillment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SureCart\Models;
+
+/**
+ * Fulfillment model.
+ */
+class Fulfillment extends Model {
+	/**
+	 * Rest API endpoint
+	 *
+	 * @var string
+	 */
+	protected $endpoint = 'fulfillments';
+
+	/**
+	 * Object name
+	 *
+	 * @var string
+	 */
+	protected $object_name = 'fulfillment';
+}
