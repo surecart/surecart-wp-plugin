7eb84c7d1bcfe8a200bb60e877a6b8e2ac29f6e3
iamdebadipti <iamdebadipti@gmail.com>
2023-06-30 14:17:33 +0530

    navigate to buy link if add_to_cart disabled

--- packages/components/src/components/controllers/product/sc-product-buy-button/sc-product-buy-button.tsx
+++ packages/components/src/components/controllers/product/sc-product-buy-button/sc-product-buy-button.tsx
@@ -1,7 +1,7 @@
-import { Component, Element, h, Host } from '@stencil/core';
+import { Component, Element, h, Host, Prop } from '@stencil/core';
 import { state } from '@store/product';
 import { __ } from '@wordpress/i18n';
-import { submitCartForm } from '@store/product/mutations';
+import { getProductBuyLink, submitCartForm } from '@store/product/mutations';
 
 @Component({
   tag: 'sc-product-buy-button',
@@ -11,11 +11,23 @@ import { submitCartForm } from '@store/product/mutations';
 export class ScProductBuyButton {
   @Element() el: HTMLScProductBuyButtonElement;
 
+  // Is add to cart enabled
+  @Prop() add_to_cart: boolean;
+
   handleCartClick(e) {
     e.preventDefault();
 
     // already busy, do nothing.
     if (state.busy) return;
+
+    // if add to cart is undefined/false navigate to buy url
+    if (!this.add_to_cart) {
+      const checkoutUrl = window?.scData?.pages?.checkout;
+      if (!checkoutUrl) return;
+      const buyLink = getProductBuyLink(checkoutUrl);
+      window.location.assign(buyLink);
+      return;
+    }
 
     // ad hoc price, use the dialog.
     if (state?.selectedPrice?.ad_hoc) {
