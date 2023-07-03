64dccc0e0fd03677147ddf6b26ee3e23dd5c6c5e
Rajkiran Bagal <rajkiranb@bsf.io>
2023-07-03 17:39:28 +0530

    Added test case to check Checkout ID should be created & updated in the URl when Add New is clicked in Orders.

--- packages/admin/checkouts/test/index.spec.ts
+++ packages/admin/checkouts/test/index.spec.ts
@@ -1,25 +1,26 @@
-import { test, Page, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Checkout ID should be created & updated in the URl when Add New is clicked in Orders.', async ({ page }) => {
-    
-    // navigate ot the order pages.
-    await page.goto(
-        '/wp-admin/admin.php?page=sc-orders'
-    );
+test.describe('Orders page', () => {
+    test('Checkout ID should be created & updated in the URl when Add New is clicked in Orders.', async ({ page }) => {
+        
+        // navigate ot the order pages.
+        await page.goto(
+            '/wp-admin/admin.php?page=sc-orders'
+        );
 
-    // confirm that you can see the add new button.
-    const addNewOrder = page.locator(
-        'button[data-test-id="add-new-button"]'
-    );
+        // confirm that you can see the add new button.
+        const addNewOrder = page.locator(
+            'a[data-test-id="add-new-button"]'
+        );
 
-    await addNewOrder.click();
+        await addNewOrder.click();
+        
+        await page.waitForURL('/wp-admin/admin.php?page=sc-checkouts&action=edit&id=\**');
+       
+        const urlParams = new URLSearchParams(page.url());
 
-   // Get the current page URL
-    const url = await page.url();
-    console.log('Current URL:', url);
-
-    const pattern = /^https:\/\/surecartdev\.com\/wp-admin\/admin\.php\?page=sc-checkouts&action=edit&id=[a-f0-9-]+$/;
-
-    // Assert the URL matches the pattern
-    expect(url).toMatch(pattern);
+        expect(
+            urlParams.has('id')
+        ).toBe(true);
+    });
 });
