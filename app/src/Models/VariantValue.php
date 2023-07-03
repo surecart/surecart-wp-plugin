cb0cf82eb479562141aa7c9a9b22594ad3270333
kiptoo <biikiptoo596@gmail.com>
2023-07-03 10:06:16 +0300

    Add the endpoints for the variant values

---
+++ app/src/Models/VariantValue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SureCart\Models;
+
+/**
+ * Variant Value model
+ */
+
+class VariantValue extends Model {
+	/**
+	 * Rest API endpoint
+	 *
+	 * @var string
+	 */
+	protected $endpoint = 'variant_values';
+
+	/**
+	 * Object name
+	 *
+	 * @var string
+	 */
+	protected $object_name = 'variant_value';
+}
