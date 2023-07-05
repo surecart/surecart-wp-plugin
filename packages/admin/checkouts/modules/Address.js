1a4f21c5ffe970dbd479f0681fe66a4ffa44069e
Andre Gagnon <ajgagnon@uwalumni.com>
2023-07-05 14:42:03 -0500

    Use footer slot for address.

--- packages/admin/checkouts/modules/Address.js
+++ packages/admin/checkouts/modules/Address.js
@@ -6,70 +6,89 @@ import {
 	ScIcon,
 	ScBlockUi,
 	ScDialog,
-	ScAddress
+	ScAddress,
 } from '@surecart/components-react';
 import { useState } from '@wordpress/element';
 import { Notice } from '@wordpress/components';
 
-export default ({ address = {}, label, loading, onAddressChange, busyCustomer, busy }) => {
-	
-	const [customerShippingAddress, setCustomerShippingAddress] = useState(false);
+export default ({
+	address = {},
+	label,
+	loading,
+	onAddressChange,
+	busyCustomer,
+	busy,
+}) => {
+	const [customerShippingAddress, setCustomerShippingAddress] =
+		useState(false);
 	const [open, setOpen] = useState(false);
 
 	return (
 		<>
-		<Box 
-			title={label || __('Address', 'surecart')} 
-			loading={loading}
-			header_action={
-				<ScButton type="text" slot="trigger" circle onClick={() => setOpen(true)}>
-					<ScIcon name="edit" />
-				</ScButton>
-			}
-		>
-			{
-				0 !== address?.length ? (
+			<Box
+				title={label || __('Address', 'surecart')}
+				loading={loading}
+				header_action={
+					<ScButton
+						type="text"
+						slot="trigger"
+						circle
+						onClick={() => setOpen(true)}
+					>
+						<ScIcon name="edit" />
+					</ScButton>
+				}
+			>
+				{0 !== address?.length ? (
 					<AddressDisplay address={address} />
 				) : (
 					<div>
 						<Notice status="warning" isDismissible={false}>
-							{__('Update shipping address for creating a order.', 'surecart')}
+							{__(
+								'Update shipping address for creating a order.',
+								'surecart'
+							)}
 						</Notice>
 					</div>
-				)
-			}
-			
-			{(!!busy || !!busyCustomer ) && (
-				<ScBlockUi spinner />
-			)}
-		</Box>
-		<ScDialog
-			label={__('Edit Shipping & Tax Address', 'surecart')}
-			open={open}
-			style={{ '--dialog-body-overflow': 'visible' }}
-			onScRequestClose={() => setOpen(false)}
-		>
-			<ScAddress
-				required={false}
-				onScInputAddress={(e) => setCustomerShippingAddress(e?.detail)}
-			/>
-			{(!!busy || !!busyCustomer ) && (
-				<ScBlockUi spinner />
-			)}
+				)}
 
-			<ScButton
-				type="primary"
-				onClick={() => {
-					onAddressChange(customerShippingAddress)
-					setOpen(false)
-				}}
-				style={{
-					marginTop: '17.5px'
-				}}
+				{(!!busy || !!busyCustomer) && <ScBlockUi spinner />}
+			</Box>
+
+			<ScDialog
+				label={__('Edit Shipping & Tax Address', 'surecart')}
+				open={open}
+				style={{ '--dialog-body-overflow': 'visible' }}
+				onScRequestClose={() => setOpen(false)}
 			>
-				{__('Update', 'surecart')}
-			</ScButton>
-		</ScDialog>
+				<ScAddress
+					required={false}
+					onScInputAddress={(e) =>
+						setCustomerShippingAddress(e?.detail)
+					}
+				/>
+
+				<ScButton
+					type="text"
+					onClick={() => setOpen(false)}
+					slot="footer"
+				>
+					{__('Cancel', 'surecart')}
+				</ScButton>
+
+				<ScButton
+					type="primary"
+					onClick={() => {
+						onAddressChange(customerShippingAddress);
+						setOpen(false);
+					}}
+					slot="footer"
+				>
+					{__('Update', 'surecart')}
+				</ScButton>
+
+				{(!!busy || !!busyCustomer) && <ScBlockUi spinner />}
+			</ScDialog>
 		</>
 	);
 };
