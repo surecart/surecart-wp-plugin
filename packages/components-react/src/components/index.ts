8d6f7b40ebc2b2ad9375d0356bee6a78551cd4c6
Andre Gagnon <ajgagnon@uwalumni.com>
2023-07-26 13:59:55 -0500

    Refactor to password ui component for use in multiple places.

--- packages/components-react/src/components/index.ts
+++ packages/components-react/src/components/index.ts
@@ -124,6 +124,7 @@ export const ScOrderSummary = /*@__PURE__*/createReactComponent<JSX.ScOrderSumma
 export const ScOrderTaxIdInput = /*@__PURE__*/createReactComponent<JSX.ScOrderTaxIdInput, HTMLScOrderTaxIdInputElement>('sc-order-tax-id-input');
 export const ScOrdersList = /*@__PURE__*/createReactComponent<JSX.ScOrdersList, HTMLScOrdersListElement>('sc-orders-list');
 export const ScPagination = /*@__PURE__*/createReactComponent<JSX.ScPagination, HTMLScPaginationElement>('sc-pagination');
+export const ScPassword = /*@__PURE__*/createReactComponent<JSX.ScPassword, HTMLScPasswordElement>('sc-password');
 export const ScPasswordNag = /*@__PURE__*/createReactComponent<JSX.ScPasswordNag, HTMLScPasswordNagElement>('sc-password-nag');
 export const ScPayment = /*@__PURE__*/createReactComponent<JSX.ScPayment, HTMLScPaymentElement>('sc-payment');
 export const ScPaymentMethod = /*@__PURE__*/createReactComponent<JSX.ScPaymentMethod, HTMLScPaymentMethodElement>('sc-payment-method');
