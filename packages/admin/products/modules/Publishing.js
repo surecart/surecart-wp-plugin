0082240fa6e1f6f3c3e827034f82cd8be27f3997
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-07-27 19:52:59 +0600

    Removed product type switcher

--- packages/admin/products/modules/Publishing.js
+++ packages/admin/products/modules/Publishing.js
@@ -9,7 +9,6 @@ import SelectTemplate from '../components/SelectTemplate';
 import SelectTemplatePart from '../components/SelectTemplatePart';
 import Status from '../components/Status';
 import Url from '../components/Url';
-import EditProductType from './ProductType/EditProductType';
 
 export default ({ product, updateProduct, loading }) => {
 	const tag = document.querySelector('#wp-admin-bar-view-product-page');
@@ -58,11 +57,6 @@ export default ({ product, updateProduct, loading }) => {
 					/>
 				)}
 				<Url product={product} updateProduct={updateProduct} />
-
-				<EditProductType
-					product={product}
-					updateProduct={updateProduct}
-				/>
 			</div>
 		</Box>
 	);
