2093fd27b43e07ec49792fc251ec6506bb465cc1
kiptoo <biikiptoo596@gmail.com>
2023-07-27 15:35:20 +0300

    remove unused code

--- packages/blocks/Blocks/Product/Collections/Block.php
+++ packages/blocks/Blocks/Product/Collections/Block.php
@@ -10,61 +10,6 @@ use SureCartBlocks\Util\BlockStyleAttributes;
  * Product Collection Block
  */
 class Block extends BaseBlock {
-	// public function getVars( $attr, $prefix ) {
-	// $style = '';
-	// padding.
-	// if ( ! empty( $attr['style']['spacing']['padding'] ) ) {
-	// $padding = $attr['style']['spacing']['padding'];
-	// $style  .= $prefix . '-padding-top: ' . $this->getSpacingPresetCssVar( array_key_exists( 'top', $padding ) ? $padding['top'] : '0' ) . ';';
-	// $style  .= $prefix . '-padding-bottom: ' . $this->getSpacingPresetCssVar( array_key_exists( 'bottom', $padding ) ? $padding['bottom'] : '0' ) . ';';
-	// $style  .= $prefix . '-padding-left: ' . $this->getSpacingPresetCssVar( array_key_exists( 'left', $padding ) ? $padding['left'] : '0' ) . ';';
-	// $style  .= $prefix . '-padding-right: ' . $this->getSpacingPresetCssVar( array_key_exists( 'right', $padding ) ? $padding['right'] : '0' ) . ';';
-	// }
-	// margin.
-	// if ( ! empty( $attr['style']['spacing']['margin'] ) ) {
-	// $margin = $attr['style']['spacing']['margin'];
-	// $style .= $prefix . '-margin-top: ' . $this->getSpacingPresetCssVar( array_key_exists( 'top', $margin ) ? $margin['top'] : '0' ) . ';';
-	// $style .= $prefix . '-margin-bottom: ' . $this->getSpacingPresetCssVar( array_key_exists( 'bottom', $margin ) ? $margin['bottom'] : '0' ) . ';';
-	// $style .= $prefix . '-margin-left: ' . $this->getSpacingPresetCssVar( array_key_exists( 'left', $margin ) ? $margin['left'] : '0' ) . ';';
-	// $style .= $prefix . '-margin-right: ' . $this->getSpacingPresetCssVar( array_key_exists( 'right', $margin ) ? $margin['right'] : '0' ) . ';';
-	// }
-
-	// border width.
-	// if ( ! empty( $attr['style']['border']['width'] ) ) {
-	// $style .= $prefix . '-border-width: ' . $attr['style']['border']['width'] . ';';
-	// }
-	// border radius.
-	// if ( ! empty( $attr['style']['border']['radius'] ) ) {
-	// $style .= $prefix . '-border-radius: ' . $attr['style']['border']['radius'] . ';';
-	// }
-	// font weight.
-	// if ( ! empty( $attr['style']['typography']['fontWeight'] ) ) {
-	// $style .= $prefix . '-font-weight: ' . $attr['style']['typography']['fontWeight'] . ';';
-	// }
-	// font size.
-	// if ( ! empty( $attr['fontSize'] ) || ! empty( $attr['style']['typography']['fontSize'] ) ) {
-	// $font_size = ! empty( $attr['fontSize'] ) ? $this->getFontSizePresetCssVar( $attr['fontSize'] ) : $attr['style']['typography']['fontSize'];
-	// $style    .= $prefix . '-font-size: ' . $font_size . ';';
-	// }
-	// border color.
-	// if ( ! empty( $attr['borderColor'] ) || ! empty( $attr['style']['border']['color'] ) ) {
-	// $border_color = ! empty( $attr['borderColor'] ) ? $this->getColorPresetCssVar( $attr['borderColor'] ) : $attr['style']['border']['color'];
-	// $style       .= $prefix . '-border-color: ' . $border_color . ';';
-	// }
-	// text color.
-	// if ( ! empty( $attr['textColor'] ) || ! empty( $attr['style']['color']['text'] ) ) {
-	// $text_color = ! empty( $attr['textColor'] ) ? $this->getColorPresetCssVar( $attr['textColor'] ) : $attr['style']['color']['text'];
-	// $style     .= $prefix . '-text-color: ' . $text_color . ';';
-	// }
-	// background color.
-	// if ( ! empty( $attr['backgroundColor'] ) || ! empty( $attr['style']['color']['background'] ) ) {
-	// $text_color = ! empty( $attr['backgroundColor'] ) ? $this->getColorPresetCssVar( $attr['backgroundColor'] ) : $attr['style']['color']['background'];
-	// $style     .= $prefix . '-background-color: ' . $text_color . ';';
-	// }
-
-	// return $style;
-	// }
-
 	/**
 	 * Render the block
 	 *
