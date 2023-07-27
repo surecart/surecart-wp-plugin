9090dad738a79ba0916a1eb57535e3e0190f8e3a
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-07-27 10:42:17 +0600

    Temp Fix: Fixed product response being array or object

--- app/src/Rest/ProductsRestServiceProvider.php
+++ app/src/Rest/ProductsRestServiceProvider.php
@@ -222,25 +222,55 @@ class ProductsRestServiceProvider extends RestServiceProvider implements RestSer
 		$response = parent::filter_response_by_context( $model, $context );
 
 		if ( 'edit' === $context ) {
-			$response['variant_options'] = $response['variant_options']['data'] ?? [];
-			$response['variants']        = $response['variants']['data'] ?? [];
+			// if $response['variants'] is object, then get data by obj->data or obj['data'].
+			if ( 'array' === gettype( $response ) && isset( $response['variants'] ) ) {
+				$response['variants'] = is_object( $response['variants'] ) ? $response['variants']->data ?? [] : $response['variants']['data'] ?? [];
+			} elseif ( 'object' === gettype( $response ) && isset( $response->variants ) ) {
+				$response['variants'] = $response->variants->data ?? [];
+			}
+
+			// if $response['variant_options'] is object, then get data by obj->data or obj['data'].
+			if ( 'array' === gettype( $response ) && isset( $response['variant_options'] ) ) {
+				$response['variant_options'] = is_object( $response['variant_options'] ) ? $response['variant_options']->data ?? [] : $response['variant_options']['data'] ?? [];
+			} elseif ( 'object' === gettype( $response ) && isset( $response->variant_options ) ) {
+				$response['variant_options'] = $response->variant_options->data ?? [];
+			}
 
 			// Process the variant_options values column. currently its like string[].
 			// Lets make it like {label: string, index: number}[].
-			foreach ( $response['variant_options'] as $index => $variant_option ) {
-				$response['variant_options'][ $index ]['values'] = array_map(
-					function( $value, $value_index ) {
-						return [
-							'label' => $value,
-							'index' => $value_index,
-						];
-					},
-					$variant_option['values'],
-					array_keys( $variant_option['values'] )
-				);
+			if ( 'array' === gettype( $response ) && isset( $response['variant_options'] ) ) {
+				$response['variant_options'] = $this->getProcessedVariantOptions( $response['variant_options'] );
+			} elseif ( 'object' === gettype( $response ) && isset( $response->variant_options ) ) {
+				$response['variant_options'] = $this->getProcessedVariantOptions( $response->variant_options );
 			}
 		}
 
 		return $response;
+	}
+
+	/**
+	 * Get processed variant options
+	 *
+	 * Convert values column to {label: string, index: number}[].
+	 *
+	 * @param array $variant_options Variant options.
+	 *
+	 * @return array
+	 */
+	private function getProcessedVariantOptions( array $variant_options ): array {
+		foreach ( $variant_options as $index => $variant_option ) {
+			$variant_options[ $index ]['values'] = array_map(
+				function( $value, $value_index ) {
+					return [
+						'label' => $value,
+						'index' => $value_index,
+					];
+				},
+				$variant_option['values'],
+				array_keys( $variant_option['values'] )
+			);
+		}
+
+		return $variant_options;
 	}
 }
