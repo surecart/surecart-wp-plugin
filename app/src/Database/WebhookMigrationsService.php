6c4b561e62798887ba6caeface79b9e7eb752e00
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-01 13:04:47 -0500

    Update webhook migration.

--- app/src/Database/WebhookMigrationsService.php
+++ app/src/Database/WebhookMigrationsService.php
@@ -2,8 +2,7 @@
 
 namespace SureCart\Database;
 
-use SureCart\Models\Webhook;
-use SureCart\Support\Encryption;
+use SureCart\Models\RegisteredWebhook;
 
 /**
  * Run this migration when version changes or for new installations.
@@ -14,7 +13,7 @@ class WebhookMigrationsService extends GeneralMigration {
 	 *
 	 * @var string
 	 */
-	protected $version = '2.1.1';
+	protected $version = '2.4.0';
 
 	/**
 	 * Run the migration.
@@ -23,40 +22,15 @@ class WebhookMigrationsService extends GeneralMigration {
 	 */
 	public function run(): void {
 		// Get the registered webhooks.
-		$webhook_events = \SureCart::config()->webhook_events;
+		$webhook = RegisteredWebhook::get();
 
-		// Get the registered webhooks.
-		$registered_webhook = \SureCart::webhooks()->getRegisteredWebhook();
-
-		// Stop if webhook is not found.
-		if ( ! $registered_webhook ) {
+		// Stop if webhook is not found or there is some sort of error.
+		if ( ! $webhook || is_wp_error( $webhook ) || empty( $webhook->id ) || empty( $webhook->url ) ) {
 			return;
 		}
-
-		// Update the webhook events on the server.
+		// Update the webhook. This will update the events on the server.
 		try {
-			$webhook = Webhook::find( $registered_webhook['id'] );
-
-			// Stop if the webhook is not found or there is some error.
-			if ( is_wp_error( $webhook ) || empty( $webhook->id ) || empty( $webhook->url ) ) {
-				return;
-			}
-
-			$webhook->update(
-				[
-					'webhook_events' => $webhook_events,
-				]
-			);
-
-			// Update the webhooks to webhook history.
-			\SureCart::webhooks()->saveRegisteredWebhook(
-				[
-					'id'             => $webhook['id'],
-					'url'            => $webhook['url'],
-					'webhook_events' => $webhook['webhook_events'],
-					'signing_secret' => Encryption::encrypt( $webhook['signing_secret'] ),
-				]
-			);
+			RegisteredWebhook::update();
 		} catch ( \Exception $exception ) {
 			wp_die( 'Webhook migration failed. Error: ' . esc_attr( $exception->getMessage() ) );
 		}
