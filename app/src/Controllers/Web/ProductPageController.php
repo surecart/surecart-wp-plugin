6f8d4aa8430aafdbc009614c8d535e66794b5597
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-08-02 18:57:31 +0600

    Remove unnecessary seeder page and fixed model set for product page

--- app/src/Controllers/Web/ProductPageController.php
+++ app/src/Controllers/Web/ProductPageController.php
@@ -4,7 +4,7 @@ namespace SureCart\Controllers\Web;
 use SureCart\Models\Form;
 
 /**
- * Handles webhooks
+ * Handles Product page requests for frontend.
  */
 class ProductPageController extends BasePageController {
 	/**
@@ -13,33 +13,33 @@ class ProductPageController extends BasePageController {
 	 * @param \SureCartCore\Requests\RequestInterface $request Request.
 	 * @param \SureCartCore\View                      $view View.
 	 *
-	 * @return function
+	 * @return function|void
 	 */
 	public function show( $request, $view ) {
 		// get the product from the query var.
 		$id = get_query_var( 'sc_product_page_id' );
 
 		// fetch the product by id/slug.
-		$this->product = \SureCart\Models\Product::with( [ 'prices', 'image' ] )->find( $id );
+		$product = \SureCart\Models\Product::with( [ 'prices', 'image' ] )->find( $id );
 
 		// Set the model to handle similar tasks.
-		$this->setModel( $this->product );
+		$this->setModel( $product );
 
-		if ( is_wp_error( $this->product ) ) {
-			return $this->handleError( $this->product );
+		if ( is_wp_error( $product ) ) {
+			return $this->handleError( $product );
 		}
 
 		// if this product is a draft, check read permissions.
-		if ( 'draft' === $this->product->status && ! current_user_can( 'read_sc_products' ) ) {
+		if ( 'draft' === $product->status && ! current_user_can( 'read_sc_products' ) ) {
 			return $this->notFound();
 		}
 
 		// slug changed or we are using the id, redirect.
-		if ( $this->product->slug !== $id ) {
-			return \SureCart::redirect()->to( $this->product->permalink );
+		if ( $product->slug !== $id ) {
+			return \SureCart::redirect()->to( $product->permalink );
 		}
 
-		set_query_var( 'sc_product_page_id', $this->product->id );
+		set_query_var( 'sc_product_page_id', $product->id );
 
 		// add the filters.
 		$this->filters();
@@ -47,7 +47,7 @@ class ProductPageController extends BasePageController {
 		// handle block theme.
 		if ( wp_is_block_theme() ) {
 			global  $_wp_current_template_content;
-			$_wp_current_template_content = $this->product->template->content ?? '';
+			$_wp_current_template_content = $product->template->content ?? '';
 		}
 
 		// include the default view.
