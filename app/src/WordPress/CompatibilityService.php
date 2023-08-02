b8868a4467a9d4911a6488caf653a7a4f8d14a88
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-02 16:02:06 -0500

    Add a compatibility service.

---
+++ app/src/WordPress/CompatibilityService.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package   SureCartAppCore
+ * @author    SureCart <support@surecart.com>
+ * @copyright  SureCart
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0
+ * @link      https://surecart.com
+ */
+
+namespace SureCart\WordPress;
+
+/**
+ * Provides compatibility with other plugins.
+ */
+class CompatibilityService {
+	/**
+	 * Bootstrap the service.
+	 *
+	 * @return void
+	 */
+	public function bootstrap() {
+		// UAG fix.
+		if ( ! class_exists( '\UAGB_Post_Assets' ) ) {
+			add_action( 'render_block_data', [ $this, 'maybeEnqueueUAGBAssets' ] );
+		}
+	}
+
+	/**
+	 * Render block data.
+	 *
+	 * @param array $block_data Block data.
+	 *
+	 * @return array
+	 */
+	public function maybeEnqueueUAGBAssets( $parsed_block ) {
+		// must be our checkout form block.
+		if ( 'surecart/checkout-form' !== $parsed_block['blockName'] ) {
+			return $parsed_block;
+		}
+
+		// must have an ID.
+		if ( empty( $parsed_block['attrs']['id'] ) ) {
+			return $parsed_block;
+		}
+
+		// If Spectra Blocks are present in the form, enqueue the assets.
+		$post_assets_instance = new \UAGB_Post_Assets( $parsed_block['attrs']['id'] );
+		$post_assets_instance->enqueue_scripts();
+
+		return $parsed_block;
+	}
+}
