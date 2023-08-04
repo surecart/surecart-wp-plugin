c0f5d7a835d216de12adefb1958872b1551d50ad
kiptoo <biikiptoo596@gmail.com>
2023-08-04 14:05:05 +0300

    Update to show correct message when the shipping address is not set

--- packages/admin/customers/modules/ShippingAddress/index.js
+++ packages/admin/customers/modules/ShippingAddress/index.js
@@ -1,22 +1,26 @@
 import { __ } from '@wordpress/i18n';
 import Box from '../../../ui/Box';
-import { ScButton, ScFlex } from '@surecart/components-react';
+import { ScButton, ScEmpty, ScFlex, ScIcon } from '@surecart/components-react';
 import AddressDisplay from '../../../components/AddressDisplay';
 
 export default ({ onEditAddress, shippingAddress, loading }) => {
 	return (
 		<Box title={__('Shipping & Tax Address', 'surecart')} loading={loading}>
-			<ScFlex>
-				<ScFlex
-					flexDirection="column"
-					style={{ gap: 'var(--sc-spacing-x-small)' }}
-				>
+			{!!shippingAddress?.id ? (
+				<ScFlex>
 					<AddressDisplay address={shippingAddress} />
+					<ScButton size="small" onClick={onEditAddress}>
+						{__('Edit', 'surecart')}
+					</ScButton>
 				</ScFlex>
-				<ScButton size="small" onClick={onEditAddress}>
-					{__('Edit', 'surecart')}
-				</ScButton>
-			</ScFlex>
+			) : (
+				<ScEmpty>
+					{__('Shipping address is not set.', 'surecart')}
+					<ScButton size="small" onClick={onEditAddress}>
+						{__('Edit', 'surecart')}
+					</ScButton>
+				</ScEmpty>
+			)}
 		</Box>
 	);
 };
