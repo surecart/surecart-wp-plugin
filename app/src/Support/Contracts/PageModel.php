76e2ad4d7d31f98598b938a0b1979306bf196f3a
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-11 09:58:24 -0500

    Add PageModel contract.

---
+++ app/src/Support/Contracts/PageModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SureCart\Support\Contracts;
+
+interface PageModel {
+	/**
+	 * Get the page title attribute.
+	 *
+	 * @return string
+	 */
+	public function getPageTitleAttribute() : string;
+
+	/**
+	 * Get the page descriptoin attribute
+	 *
+	 * @return string
+	 */
+	public function getMetaDescriptionAttribute(): string;
+
+	/**
+	 * Get the page permalink attribute
+	 *
+	 * @return string
+	 */
+	public function getPermalinkAttribute(): string;
+
+	/**
+	 * Get the template id.
+	 *
+	 * @return string|false
+	 */
+	public function getTemplateIdAttribute(): string;
+
+	/**
+	 * Get the template.
+	 *
+	 * @return \WP_Template
+	 */
+	public function getTemplateAttribute();
+
+	/**
+	 * Get the template id.
+	 *
+	 * @return string|false
+	 */
+	public function getTemplatePartIdAttribute(): string;
+
+	/**
+	 * Get the template part template.
+	 *
+	 * @return \WP_Template
+	 */
+	public function getTemplatePartAttribute();
+}
