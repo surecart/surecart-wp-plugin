f81a9cbea76a47e61f5cc06b31245f55c9ac0003
Rajkiran Bagal <rajkiranb@bsf.io>
2023-08-17 12:26:02 +0530

    Updated function name & commenting

--- packages/components/src/store/processors/getters.ts
+++ packages/components/src/store/processors/getters.ts
@@ -19,9 +19,9 @@ export const availableProcessors = () =>
 export const getAvailableProcessor = (type: string) => availableProcessors().find(({ processor_type }) => processor_type === type);
 
 /**
- * Gets other available processor type.
+ * This method accepts a processor name as its input parameter and subsequently evaluates the presence of any additional processors apart from the one provided as the parameter.
  */
-export const getOtherAvailableProcessors = (type: string) => availableProcessors().find(({ processor_type }) => processor_type !== type);
+export const hasOtherAvailableProcessor = (type: string) => availableProcessors().find(({ processor_type }) => processor_type !== type);
 
 /**
  * Get a sorted array of manual payment methods
