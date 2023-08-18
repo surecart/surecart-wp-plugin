6ea9b7a4f91cff550fca5f877eadd39a4f244531
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-18 14:39:54 -0500

    Fix icon stroking stuff.

--- app/src/Controllers/Admin/Products/ProductsListTable.php
+++ app/src/Controllers/Admin/Products/ProductsListTable.php
@@ -288,11 +288,9 @@ class ProductsListTable extends ListTable {
 	public function column_featured( $product ) {
 		ob_start();
 		?>
-		<?php if ( $product->featured ) : ?>
-			<sc-icon name="star" style="--sc-icon-fill: currentColor;"></sc-icon>
-		<?php else : ?>
-			<sc-icon name="star"></sc-icon>
-		<?php endif; ?>
+			<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="<?php echo $product->featured ? 'currentColor' : 'none'; ?>" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+			</svg>
 		<?php
 		return ob_get_clean();
 	}
