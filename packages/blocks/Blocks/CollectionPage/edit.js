aa4269fc3381eed85c867a30b8e5626381e4e2ad
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-22 16:14:48 -0500

    Formatting

--- packages/blocks/Blocks/CollectionPage/edit.js
+++ packages/blocks/Blocks/CollectionPage/edit.js
@@ -4,74 +4,75 @@ import { createNavigationLink } from './use-convert-to-navigation-links';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { Popover } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { 
-	store as blockEditorStore, 
-	useBlockProps
+import {
+	store as blockEditorStore,
+	useBlockProps,
 } from '@wordpress/block-editor';
 import ModelSelector from '../../../admin/components/ModelSelector';
 
 export default ({ clientId }) => {
-	
 	const [collectionPage, setCollectionPage] = useState(null);
 	const [modal, setModal] = useState(false);
-	const [placeholderText, setPlaceholderText] = useState(__('Select Collection Page', 'surecart'));
+	const [placeholderText, setPlaceholderText] = useState(
+		__('Select Collection Page', 'surecart')
+	);
 	const [loadingPage, setLoadingPage] = useState(false);
-	const { replaceBlock, selectBlock } = useDispatch( blockEditorStore );
-	const blockProps = useBlockProps();
-	const {
-		parentClientId,
-	} = useSelect(
-		( select ) => {
-			const {
-				getBlockParentsByBlockName,
-			} = select( blockEditorStore );
+	const { replaceBlock, selectBlock } = useDispatch(blockEditorStore);
+	const blockProps = useBlockProps({
+		ref: anchorRef,
+	});
+	const { parentClientId } = useSelect(
+		(select) => {
+			const { getBlockParentsByBlockName } = select(blockEditorStore);
 			const navigationBlockParents = getBlockParentsByBlockName(
 				clientId,
 				'core/navigation',
 				true
 			);
 			return {
-				parentClientId: navigationBlockParents[ 0 ],
+				parentClientId: navigationBlockParents[0],
 			};
 		},
-		[ clientId ]
+		[clientId]
 	);
 
 	useEffect(() => {
-		if ( ! collectionPage ) {
+		if (!collectionPage) {
 			setModal(true);
 		}
 	}, []);
 
 	const getCollectionPage = async (id) => {
-		
 		const collectionPage = await apiFetch({
 			path: `/surecart/v1/product_collections/${id}`,
 		});
 		setCollectionPage(collectionPage);
-		useConvertToNavigationLinks({clientId, collectionPage, parentClientId});
+		useConvertToNavigationLinks({
+			clientId,
+			collectionPage,
+			parentClientId,
+		});
 	};
 
-	const useConvertToNavigationLinks = ( {
+	const useConvertToNavigationLinks = ({
 		clientId,
 		collectionPage,
 		parentClientId,
-	} ) => {
-		
-		const navigationLink = createNavigationLink( collectionPage );
+	}) => {
+		const navigationLink = createNavigationLink(collectionPage);
 
 		// Replace the Page List block with the Navigation Links.
-		replaceBlock( clientId, navigationLink );
+		replaceBlock(clientId, navigationLink);
 
 		// Select the Navigation block to reveal the changes.
-		selectBlock( parentClientId );
-	}
+		selectBlock(parentClientId);
+	};
 
 	return (
 		<>
-			{ (!collectionPage || loadingPage) && (
-				<div 
-					{...blockProps} 
+			{(!collectionPage || loadingPage) && (
+				<div
+					{...blockProps}
 					onClick={() => setModal(true)}
 					style={{
 						cursor: 'pointer',
@@ -102,7 +103,9 @@ export default ({ clientId }) => {
 							onSelect={(collectionId) => {
 								setModal(false);
 								setLoadingPage(true);
-								setPlaceholderText(__('Loading...', 'surecart'));
+								setPlaceholderText(
+									__('Loading...', 'surecart')
+								);
 								getCollectionPage(collectionId);
 							}}
 							style={{ width: '100%' }}
