af7a5c2c0239e5fdcf6414a67ccbcb43baf79a65
kiptoo <biikiptoo596@gmail.com>
2023-08-30 12:17:30 +0300

    Update snapshot tests

--- packages/components/src/components/controllers/product/sc-product-price/test/sc-product-price.spec.tsx
+++ packages/components/src/components/controllers/product/sc-product-price/test/sc-product-price.spec.tsx
@@ -9,4 +9,28 @@ describe('sc-product-prices', () => {
     });
     expect(page.root).toMatchSnapshot();
   });
+
+  it('aligns the text to the left when textAlign is left', async () => {
+    const page = await newSpecPage({
+      components: [ScProductPrice],
+      html: `<sc-product-price text-align="left"></sc-product-price>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('aligns the text to the center when textAlign is center', async () => {
+    const page = await newSpecPage({
+      components: [ScProductPrice],
+      html: `<sc-product-price text-align="center"></sc-product-price>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  })
+
+  it('aligns the text to the right when textAlign is right', async () => {
+    const page = await newSpecPage({
+      components: [ScProductPrice],
+      html: `<sc-product-price text-align="right"></sc-product-price>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
 });
