4b011083300fdc56a294f434262531e6a77d6a8c
Andre Gagnon <ajgagnon@uwalumni.com>
2023-08-30 15:00:04 -0500

    Wording changes

--- views/admin/notices/webhook-change.php
+++ views/admin/notices/webhook-change.php
@@ -14,39 +14,25 @@
 			>
 			<polyline points="9 18 15 12 9 6" />
 		</svg>
-		<span><?php esc_html_e( 'Safe Mode', 'surecart' ); ?></span>
+		<span><?php esc_html_e( 'Connection', 'surecart' ); ?></span>
 	</div>
-	<h1><?php esc_html_e( 'Safe Mode has been activated', 'surecart' ); ?></h1>
+	<h1><?php esc_html_e( 'There are two websites connected to the same SureCart store.', 'surecart' ); ?></h1>
 	<p class="description">
 		<?php
 		esc_html_e(
-			'Your site is in Safe Mode because you have 2 SureCart stores that appear to be duplicates.',
+			'Two sites that are telling SureCart they are the same site. Please let us know how to treat this website change.',
 			'surecart'
 		);
 		?>
-		<br />
-		<?php
-		esc_html_e(
-			'Two sites that are telling SureCart they are the same site.',
-			'surecart'
-		);
-		?>
-		<a href="https://docs.surecart.com/article/what-is-safe-mode" target="_blank" class="learn-more-safe-mode">
-			<?php esc_html_e( 'Learn more about Safe Mode.', 'surecart' ); ?>
+		<a href="https://surecart.com/docs/" target="_blank" class="learn-more-safe-mode">
+			<?php esc_html_e( 'Learn More', 'surecart' ); ?>
 		</a>
 	</p>
 	<div class="webhook-cards">
 		<div class="webhook-card">
-			<h2><?php esc_html_e( 'Update SureCart site connection', 'surecart' ); ?></h2>
+			<h2><?php echo esc_html( sprintf( __( 'I want to update this site connection to the store "%s".', 'surecart' ), \SureCart::account()->name ) ); ?></h2>
 			<p>
-			<?php
-			printf(
-				/* translators: 1) current webhook url 2) previous webhook url */
-				wp_kses_post( __( 'Your connection will be updated to <b>%1$s</b>.<br><b>%2$s</b> will be disconnected from SureCart.', 'surecart' ) ),
-				esc_url( $current_web_url ),
-				esc_url( $previous_web_url )
-			);
-			?>
+			<?php echo esc_html( sprintf( __( 'We will update the SureCart connection to the your new url. This is often the case when you have changed your website url or have migrated your site to a new domain.', 'surecart' ), \SureCart::account()->name ) ); ?>
 			</p>
 			<div class="webhook-links">
 				<span class="previous-webhook"><?php echo esc_url( $previous_web_url ); ?></span>
@@ -60,16 +46,9 @@
 			</div>
 		</div>
 		<div class="webhook-card">
-			<h2><?php esc_html_e( 'Treat each as independent sites', 'surecart' ); ?></h2>
+			<h2><?php echo esc_html( sprintf( __( 'I want to have both sites connected to the store "%s".', 'surecart' ), \SureCart::account()->name ) ); ?></h2>
 			<p>
-			<?php
-			printf(
-				/* translators: 1) current webhook url 2) previous webhook url */
-				wp_kses_post( __( '<b>%1$s</b> will add a new connection to SureCart.<br><b>%2$s</b> will keep its existing connection.', 'surecart' ) ),
-				esc_url( $current_web_url ),
-				esc_url( $previous_web_url )
-			);
-			?>
+			<?php esc_html_e( 'We will create a new connection for this site. This can happen if you are using a staging site or want to have more than one website connected to the same store.', 'surecart' ); ?>
 			</p>
 			<div class="webhook-links">
 				<span class="current-webhook"><?php echo esc_url( $previous_web_url ); ?></span>
