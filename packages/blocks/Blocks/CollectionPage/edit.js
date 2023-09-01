e0bbfb578771d7a39e758242288660c7f6b082c7
Rajkiran Bagal <rajkiranb@bsf.io>
2023-09-01 18:27:25 +0530

    Added Ability to search the collections, added loaders, updated UI

--- packages/blocks/Blocks/CollectionPage/edit.js
+++ packages/blocks/Blocks/CollectionPage/edit.js
@@ -2,13 +2,12 @@ import apiFetch from '@wordpress/api-fetch';
 import { useState, useEffect, useRef } from '@wordpress/element';
 import { createNavigationLink } from './use-convert-to-navigation-links';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { Popover } from '@wordpress/components';
+import { Popover, Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import {
 	store as blockEditorStore,
 	useBlockProps,
 } from '@wordpress/block-editor';
-import ModelSelector from '../../../admin/components/ModelSelector';
 import { ScInput } from '@surecart/components-react';
 import { MenuItem, TextHighlight, MenuGroup } from '@wordpress/components';
 import {
@@ -17,13 +16,14 @@ import {
 	category,
 } from '@wordpress/icons';
 import { __unstableStripHTML as stripHTML } from '@wordpress/dom';
-import { safeDecodeURI, filterURLForDisplay } from '@wordpress/url';
+import { safeDecodeURI, filterURLForDisplay, addQueryArgs } from '@wordpress/url';
 
 export default ({ clientId }) => {
 	const anchorRef = useRef(null);
 	const [collectionPage, setCollectionPage] = useState(null);
 	const [searchText, setSearchText] = useState('');
 	const [modal, setModal] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const [placeholderText, setPlaceholderText] = useState(
 		__('Select Collection Page', 'surecart')
 	);
@@ -54,10 +54,18 @@ export default ({ clientId }) => {
 			setModal(true);
 		}
 		if (!collectionPages) {
+			setLoading(true);
 			getCollectionPages();
 		}
 	}, []);
 
+	useEffect(() => {
+		if (searchText) {
+			setLoading(true);
+			getCollectionPages();
+		}
+	}, [searchText]);
+	
 	const getCollectionPage = async (id) => {
 		const collectionPage = await apiFetch({
 			path: `/surecart/v1/product_collections/${id}`,
@@ -72,9 +80,12 @@ export default ({ clientId }) => {
 
 	const getCollectionPages = async () => {
 		const pages = await apiFetch({
-			path: '/surecart/v1/product_collections',
+			path: addQueryArgs('/surecart/v1/product_collections', {
+				query: searchText,
+			}),
 		});
 		setCollectionPages(pages);
+		setLoading(false);
 	};
 
 	const useConvertToNavigationLinks = ({
@@ -115,6 +126,7 @@ export default ({ clientId }) => {
 				>
 					<div
 						style={{
+							minWidth: '25em',
 							padding: '1em',
 						}}
 					>
@@ -131,44 +143,69 @@ export default ({ clientId }) => {
 								setSearchText(e?.target?.value);
 							}}
 						></ScInput>
-						<div
-							style={{
-								marginTop: '1em',
-							}}
-						>
-						<MenuGroup>
-							{ collectionPages?.map((page) => {
-								return (
-								<MenuItem
-									iconPosition="left"
-									info={ filterURLForDisplay( safeDecodeURI( page?.permalink ), 24 ) }
-									shortcut={ 'collection' }
-									icon={
-										<Icon
-											className="block-editor-link-control__search-item-icon"
-											icon={ category }
+
+						{ loading && (
+							<div
+								style={{
+									marginTop: '1em',
+									textAlign: 'center',
+								}}
+							>
+								<Spinner />
+							</div>
+						)}
+						{ !loading && collectionPages?.length && (
+							<div
+								style={{
+									marginTop: '1em',
+								}}
+							>
+							<MenuGroup>
+								{ collectionPages?.map((page) => {
+									return (
+									<MenuItem
+										iconPosition="left"
+										info={ filterURLForDisplay( safeDecodeURI( page?.permalink ), 24 ) }
+										shortcut={ 'collection' }
+										icon={
+											<Icon
+												className="block-editor-link-control__search-item-icon"
+												icon={ category }
+											/>
+										}
+										onClick={ () => {
+											setModal(false);
+											setLoadingPage(true);
+											setPlaceholderText(
+												__('Loading...', 'surecart')
+											);
+											getCollectionPage(page?.id);
+										}}
+										className="block-editor-link-control__search-item"
+									>
+										<TextHighlight
+											// The component expects a plain text string.
+											text={ stripHTML(page?.name) }
+											highlight={ searchText }
 										/>
-									}
-									onClick={ () => {
-										setModal(false);
-										setLoadingPage(true);
-										setPlaceholderText(
-											__('Loading...', 'surecart')
-										);
-										getCollectionPage(page?.id);
-									}}
-									className="block-editor-link-control__search-item"
-								>
-									<TextHighlight
-										// The component expects a plain text string.
-										text={ stripHTML(page?.name) }
-										highlight={ searchText }
-									/>
-								</MenuItem>
-								);
-							})}
-						</MenuGroup>
-						</div>
+									</MenuItem>
+									);
+								})}
+							</MenuGroup>
+							</div>
+						)}
+						{ !loading && !collectionPages?.length && (
+							<div
+								style={{
+									marginTop: '1em',
+								}}
+							>
+								{__(
+									'No collections found.',
+									'surecart'
+								)}
+							</div>
+						)}
 					</div>
 				</Popover>
 			)}
