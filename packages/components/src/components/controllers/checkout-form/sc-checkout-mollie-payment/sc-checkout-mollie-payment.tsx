00b77da2bfcd313d541819f7c5442954cd4b4d69
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-09-04 18:52:19 +0600

    Add error notice for mollie

--- packages/components/src/components/controllers/checkout-form/sc-checkout-mollie-payment/sc-checkout-mollie-payment.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-checkout-mollie-payment/sc-checkout-mollie-payment.tsx
@@ -14,6 +14,7 @@ import { lockCheckout, unLockCheckout } from '@store/checkout/mutations';
 import apiFetch from '../../../../functions/fetch';
 
 import { ManualPaymentMethods } from '../payment/ManualPaymentMethods';
+import { createErrorNotice } from '@store/notices/mutations';
 
 @Component({
   tag: 'sc-checkout-mollie-payment',
@@ -56,7 +57,7 @@ export class ScCheckoutMolliePayment {
       };
       processorsState.methods = response?.data || [];
     } catch (e) {
-      this.scError.emit(e);
+      createErrorNotice(e);
       console.error(e);
     } finally {
       unLockCheckout('methods');
