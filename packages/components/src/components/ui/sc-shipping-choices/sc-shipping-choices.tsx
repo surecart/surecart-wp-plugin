20f8bda929d00358832d679067e1b7a6e21dac00
kiptoo <biikiptoo596@gmail.com>
2023-09-06 14:19:56 +0300

    Update the shipping options to show message when the country is not selected

--- packages/components/src/components/ui/sc-shipping-choices/sc-shipping-choices.tsx
+++ packages/components/src/components/ui/sc-shipping-choices/sc-shipping-choices.tsx
@@ -1,7 +1,7 @@
 import { Component, Prop, h, EventEmitter, Event, Host } from '@stencil/core';
 import { __ } from '@wordpress/i18n';
 import { state as checkoutState } from '@store/checkout';
-import { Checkout, ResponseError, ShippingMethod } from '../../../types';
+import { Address, Checkout, ResponseError, ShippingMethod } from '../../../types';
 import { lockCheckout, unLockCheckout } from '@store/checkout/mutations';
 import { createOrUpdateCheckout } from '@services/session';
 import { checkoutIsLocked } from '@store/checkout/getters';
@@ -44,6 +44,15 @@ export class ScShippingChoices {
     // shipping choice is not rewquired.
     if (!checkoutState?.checkout?.selected_shipping_choice_required) {
       return <Host style={{ display: 'none' }}></Host>;
+    }
+
+    // no shipping choices but no country either
+    if (!checkoutState?.checkout?.shipping_choices?.data?.length && !(checkoutState?.checkout?.shipping_address as Address)?.country) {
+      return (
+        <sc-form-control label={this.label || __('Shipping', 'surecart')}>
+          <div class="shipping-choice__empty">{__('Please select a shipping country to see shipping choices.', 'surecart')}</div>
+        </sc-form-control>
+      );
     }
 
     // no shipping choices yet.
