7095a3cbd8a6e7419794eb2590fa1382d25689cd
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-07 17:10:52 -0500

    Add additional snapshot testing.

--- packages/components/src/components/controllers/checkout-form/checkout-form-errors/test/checkout-form-errors.spec.ts
+++ packages/components/src/components/controllers/checkout-form/checkout-form-errors/test/checkout-form-errors.spec.ts
@@ -1,9 +1,10 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { ScCheckoutFormErrors } from '../sc-checkout-form-errors';
 import { createErrorNotice } from '@store/notices/mutations';
+import { updateFormState } from '@store/form/mutations';
 
 describe('ScCheckoutFormErrors', () => {
-  it('should render correctly with a error title and additional errors', async () => {
+  it('should render errors', async () => {
     // Create a mock error notice with additional errors.
     createErrorNotice(
       {
@@ -30,6 +31,40 @@ describe('ScCheckoutFormErrors', () => {
       html: `<sc-checkout-form-errors></sc-checkout-form-errors>`,
     });
 
-    expect(page.root).toMatchSnapshot();
+    expect(page.root).toMatchSnapshot('initial errors');
+
+    // make sure it removes if fetching.
+    updateFormState('FETCH');
+    await page.waitForChanges();
+    expect(page.root).toMatchSnapshot('cleared due to fetching.');
+
+    // Resolve the update.
+    updateFormState('RESOLVE');
+    createErrorNotice(
+      {
+        code: 'test',
+        message: 'Error title',
+        additional_errors: [
+          {
+            code: 'test2',
+            message: 'Additional error 1',
+          },
+          {
+            code: 'test3',
+            message: 'Additional error 2',
+          },
+        ],
+      },
+      {
+        dismissible: true,
+      },
+    );
+
+    await page.waitForChanges();
+    expect(page.root).toMatchSnapshot('re-added errors after clearing.');
+
+    updateFormState('FINALIZE');
+    await page.waitForChanges();
+    expect(page.root).toMatchSnapshot('cleared due to finalizing.');
   });
 });
