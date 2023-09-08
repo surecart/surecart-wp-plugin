4c0786c66b483b3f6fc988351adfa770da90c92b
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-08 12:44:33 -0500

    Change icon to grid

--- packages/blocks/Blocks/ProductItemList/index.js
+++ packages/blocks/Blocks/ProductItemList/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { pages as icon } from '@wordpress/icons';
+import { grid as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
