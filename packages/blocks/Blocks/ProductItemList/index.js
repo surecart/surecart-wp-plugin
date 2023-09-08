11af9a5a6b00ae0f68754ae2bc491c45cb0fff1f
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-08 12:22:05 -0500

    Make sure parent of product item blocks is product item.

--- packages/blocks/Blocks/ProductItemList/index.js
+++ packages/blocks/Blocks/ProductItemList/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { list as icon } from '@wordpress/icons';
+import { pages as icon } from '@wordpress/icons';
 
 /**
  * Internal dependencies
--- packages/blocks/products.js
+++ packages/blocks/products.js
@@ -6,16 +6,18 @@ import * as ProductItemList from '@blocks/ProductItemList';
 import * as ProductItemPrice from '@blocks/ProductItemPrice';
 import * as ProductItemTitle from '@blocks/ProductItemTitle';
 
+import { registerBlocks } from './register-block';
+
+registerBlocks([
+	ProductItem,
+	ProductItemImage,
+	ProductItemPrice,
+	ProductItemTitle,
+]);
 // unregister these blocks on product page templates.
 // @todo Refactor when there will be possible to show a block according on a template/post with a Gutenberg API. https://github.com/WordPress/gutenberg/pull/41718
 registerBlocksForTemplates({
-	blocks: [
-		ProductItem,
-		ProductItemImage,
-		ProductItemList,
-		ProductItemPrice,
-		ProductItemTitle,
-	],
+	blocks: [ProductItemList],
 	// exclude for these templates.
 	exclude: [
 		'surecart/surecart//product-info',
