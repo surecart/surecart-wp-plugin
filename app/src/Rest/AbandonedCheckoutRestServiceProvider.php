b34ce50e6a26a5923c51b709e533f192f9b11ab7
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-09-11 22:13:29 +0600

    Added rest endpoint for abandoned checkout update

--- app/src/Rest/AbandonedCheckoutRestServiceProvider.php
+++ app/src/Rest/AbandonedCheckoutRestServiceProvider.php
@@ -28,7 +28,7 @@ class AbandonedCheckoutRestServiceProvider extends RestServiceProvider implement
 	 *
 	 * @var array
 	 */
-	protected $methods = [ 'index', 'find' ];
+	protected $methods = [ 'index', 'find', 'edit' ];
 
 	/**
 	 * Get our sample schema for a post.
@@ -106,6 +106,16 @@ class AbandonedCheckoutRestServiceProvider extends RestServiceProvider implement
 	 * @return true|\WP_Error True if the request has access to create items, WP_Error object otherwise.
 	 */
 	public function get_items_permissions_check( $request ) {
+		return current_user_can( 'read_sc_checkouts' );
+	}
+
+	/**
+	 * Need priveleges to update.
+	 *
+	 * @param \WP_REST_Request $request Full details about the request.
+	 * @return true|\WP_Error True if the request has access to create items, WP_Error object otherwise.
+	 */
+	public function update_item_permissions_check( $request ) {
 		return current_user_can( 'read_sc_checkouts' );
 	}
 }
