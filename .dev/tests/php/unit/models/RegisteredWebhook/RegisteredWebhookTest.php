b8c5c5676222ce47f4c4d3b6d4a5c6e47582bf4a
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-11 12:05:01 -0500

    Remove unused tests.

--- .dev/tests/php/unit/models/RegisteredWebhook/RegisteredWebhookTest.php
+++ .dev/tests/php/unit/models/RegisteredWebhook/RegisteredWebhookTest.php
@@ -8,22 +8,6 @@ use SureCart\Tests\SureCartUnitTestCase;
 
 class RegisteredWebhookTest extends SureCartUnitTestCase
 {
-	public function test_backwards_compatibility()
-	{
-		update_option('ce_registered_webhook', [
-			'id' => 'test',
-			'url'            => 'url',
-			'webhook_events' => ['test.event'],
-			'signing_secret' => Encryption::encrypt('zMc12r7mGwGYcxa8Wg3i36zy') // this will be encrypted already when stored.
-		]);
-
-		$webhook = RegisteredWebhook::registration()->get();
-		$this->assertSame('test', $webhook->id);
-		$this->assertSame('url', $webhook->url);
-		$this->assertSame(['test.event'], $webhook->webhook_events);
-		$this->assertSame('zMc12r7mGwGYcxa8Wg3i36zy', $webhook->signing_secret); // we want to make sure it's not encrypted.
-	}
-
 	/**
 	 * This test is important to make sure encrypt/decrypt works as expected.
 	 */
