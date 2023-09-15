e509cc99986164b8f8159a473f4d5a891d48d0a0
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-15 10:47:15 -0500

    Update comments.

--- packages/components/src/store/product/events.ts
+++ packages/components/src/store/product/events.ts
@@ -5,8 +5,8 @@ import { on } from './store';
  * Purchase complete event.
  */
 on('set', (key, product: Product, oldProduct: Product) => {
-  if (key !== 'product') return; // we only care about checkout
-  if (oldProduct?.id) return; // we only care about new checkouts.
+  if (key !== 'product') return; // we only care about product
+  if (oldProduct?.id) return; // we only care about new product views.
 
   // emit the new event.
   const event = new CustomEvent('scProductViewed', {
