8e49298724d019287d23d2b623982c7ea7fa0dc5
kiptoo <biikiptoo596@gmail.com>
2023-09-19 18:10:17 +0300

    Add the wordpress rest api endpoints

---
+++ app/src/Models/ReturnItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SureCart\Models;
+
+/**
+ * ReturnItem model.
+ */
+class ReturnItem extends Model {
+	/**
+	 * Rest API endpoint
+	 */
+	protected $endpoint = 'return_items';
+
+	/**
+	 * Object name
+	 *
+	 * @var string
+	 */
+	protected $object_name = 'return_item';
+}
---
+++ app/src/Models/ReturnRequest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Surecart\Models;
+
+use SureCart\Models\Model;
+
+/**
+ * ReturnRequest model.
+ */
+class ReturnRequest extends Model {
+	/**
+	 * Rest API endpoint
+	 */
+	protected $endpoint = 'return-requests';
+
+	/**
+	 * Object name
+	 *
+	 * @var string
+	 */
+	protected $object_name = 'return_request';
+
+	/**
+	 * Open a return request.
+	 *
+	 * @param string $id Model id.
+	 * @return $this|\WP_Error
+	 */
+	protected function open( $id = null ) {
+		if ( $id ) {
+			$this->setAttribute( 'id', $id );
+		}
+
+		if ( $this->fireModelEvent( 'opening' ) === false ) {
+			return false;
+		}
+
+		if ( empty( $this->id ) ) {
+			return new \WP_Error( 'not_saved', 'You can only open an existing return request.' );
+		}
+
+		$attributes = $this->attributes;
+		unset( $attributes['id'] );
+
+		$opened = $this->makeRequest(
+			[
+				'method' => 'PATCH',
+				'query'  => $this->query,
+				'body'   => [
+					$this->object_name => $attributes,
+				],
+			],
+			$this->endpoint . '/' . $this->id . '/open/'
+		);
+
+		if ( is_wp_error( $opened ) ) {
+			return $opened;
+		}
+
+		$this->resetAttributes();
+		$this->fill( $opened );
+		$this->fireModelEvent( 'opened' );
+
+		return $this;
+	}
+
+	/**
+	 * Complete a return request.
+	 *
+	 * @param string $id Model id.
+	 * @return $this|\WP_Error
+	 */
+	protected function complete( $id = null ) {
+		if ( $id ) {
+			$this->setAttribute( 'id', $id );
+		}
+
+		if ( $this->fireModelEvent( 'completing' ) === false ) {
+			return false;
+		}
+
+		if ( empty( $this->id ) ) {
+			return new \WP_Error( 'not_saved', 'You can only complete an existing return request.' );
+		}
+
+		$attributes = $this->attributes;
+		unset( $attributes['id'] );
+
+		$completed = $this->makeRequest(
+			[
+				'method' => 'PATCH',
+				'query'  => $this->query,
+				'body'   => [
+					$this->object_name => $attributes,
+				],
+			],
+			$this->endpoint . '/' . $this->id . '/complete/'
+		);
+
+		if ( is_wp_error( $completed ) ) {
+			return $completed;
+		}
+
+		$this->resetAttributes();
+		$this->fill( $completed );
+		$this->fireModelEvent( 'completed' );
+
+		return $this;
+	}
+}
