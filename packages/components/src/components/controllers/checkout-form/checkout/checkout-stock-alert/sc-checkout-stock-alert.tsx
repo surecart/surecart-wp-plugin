22b00c68638d9610278a8de78b140f0122c4bfd6
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-21 17:29:41 -0500

    Refactor stock alert.

--- packages/components/src/components/controllers/checkout-form/checkout/checkout-stock-alert/sc-checkout-stock-alert.tsx
+++ packages/components/src/components/controllers/checkout-form/checkout/checkout-stock-alert/sc-checkout-stock-alert.tsx
@@ -28,61 +28,53 @@ export class ScCheckoutStockAlert {
 
   @Watch('order')
   handleStockAlert() {
-    const { line_items: lineItems } = this.order;
-    const stockErrors = [];
-
-    (lineItems?.data || []).forEach(lineItem => {
+    this.stockErrors = (this.getOutOfStockLineItems() || []).map(lineItem => {
       const product = lineItem.price?.product as Product;
-
-      const stockEnabled = product?.stock_enabled || false;
-      const allowOutOfStockPurchases = product?.allow_out_of_stock_purchases || false;
-      const stockQuantity = lineItem?.variant?.stock || product?.stock || 0;
       const variantImage = typeof lineItem?.variant?.image !== 'string' ? lineItem?.variant?.image?.url : null;
-
-      // if stock is enabled and out of stock purchases are not allowed.
-      if (stockEnabled && !allowOutOfStockPurchases && stockQuantity < lineItem.quantity) {
-        stockErrors.push({
-          name: product?.name,
-          image_url: variantImage || product?.image_url,
-          quantity: lineItem.quantity,
-          stock: stockQuantity,
-        });
-      }
+      return {
+        name: product?.name,
+        image_url: variantImage || product?.image_url,
+        quantity: lineItem.quantity,
+        stock: lineItem?.variant?.stock || product?.stock,
+      };
     });
+  }
 
-    this.stockErrors = stockErrors;
+  /** Get the out of stock line items. */
+  getOutOfStockLineItems() {
+    return (this.order?.line_items?.data || []).filter(lineItem => {
+      const product = lineItem.price?.product as Product;
+      return product?.stock_enabled && !product?.allow_out_of_stock_purchases && (lineItem?.variant?.stock || product?.stock < lineItem.quantity);
+    });
   }
 
   /**
    * Update the checkout line items stock to the max available.
    */
-  async updateCheckoutLineItemsStock() {
-    const { line_items: lineItems } = this.order;
-
-    (lineItems?.data || []).forEach(lineItem => {
+  async onSubmit() {
+    const lineItems = this.getOutOfStockLineItems().map(lineItem => {
       const product = lineItem.price?.product as Product;
-
-      const stockEnabled = product?.stock_enabled || false;
-      const allowOutOfStockPurchases = product?.allow_out_of_stock_purchases || false;
       const stockQuantity = lineItem?.variant?.stock || product?.stock || 0;
-
-      // if stock is enabled and out of stock purchases are not allowed.
-      if (stockEnabled && !allowOutOfStockPurchases && stockQuantity < lineItem.quantity) {
-        lineItem.quantity = stockQuantity;
-      }
+      return {
+        ...lineItem,
+        quantity: stockQuantity,
+      };
     });
 
     this.busy = true;
     checkoutState.checkout = (await updateCheckout({
       id: checkoutState.checkout.id,
       data: {
-        line_items: (lineItems?.data || []).map(lineItem => {
-          return {
-            price_id: lineItem.price?.id,
-            quantity: lineItem.quantity,
-            variant: lineItem.variant?.id,
-          };
-        }),
+        line_items: (lineItems || [])
+          // .filter(lineItem => !!lineItem.quantity)
+          .map(lineItem => {
+            return {
+              id: lineItem.id,
+              price_id: lineItem.price?.id,
+              quantity: lineItem.quantity,
+              ...(lineItem?.variant?.id ? { variant: lineItem.variant.id } : {}),
+            };
+          }),
       },
     })) as Checkout;
     this.busy = false;
@@ -91,51 +83,49 @@ export class ScCheckoutStockAlert {
   render() {
     return (
       <Host>
-        <sc-dialog open={!!this.stockErrors.length} noHeader={true} onScRequestClose={e => e.preventDefault()}>
-          <sc-flex justifyContent="flex-start" alignItems="center" style={{ marginBottom: '30px' }}>
-            <div>
-              <sc-icon style={{ fontSize: '50px', color: 'var(--sc-color-gray-300)' }} name="alert-circle"></sc-icon>
-            </div>
-            <div>
-              <sc-text style={{ '--font-size': 'var(--sc-font-size-large)', '--font-weight': 'var(--sc-font-weight-bold)', '--line-height': '2' }}>
-                {__('Out of Stock', 'surecart')}
-              </sc-text>
-              <sc-text style={{ marginBottom: '1em', lineHeight: '1.5', color: 'var(--sc-color-gray-500)' }}>
-                {__('Some items are no longer available. Your cart has been updated.', 'surecart')}
-              </sc-text>
-            </div>
-          </sc-flex>
+        <sc-dialog style={{ '--body-spacing': 'var(--sc-spacing-x-large)' }} open={!!this.stockErrors.length} noHeader={true} onScRequestClose={e => e.preventDefault()}>
+          <sc-dashboard-module
+            class="subscription-cancel"
+            // error={this.error}
+            style={{ '--sc-dashboard-module-spacing': '1em' }}
+          >
+            <sc-flex slot="heading" align-items="center" justify-content="flex-start">
+              <sc-icon name="alert-circle" style={{ color: 'var(--sc-color-primary-500' }}></sc-icon>
+              {__('Out of Stock', 'surecart')}
+            </sc-flex>
+            <span slot="description"> {__('Some items are no longer available. Your cart has been updated.', 'surecart')}</span>
+            <sc-card no-padding>
+              <sc-table>
+                <sc-table-cell slot="head">{__('Description', 'surecart')}</sc-table-cell>
+                <sc-table-cell slot="head" style={{ width: '100px', textAlign: 'right' }}>
+                  {__('Quantity', 'surecart')}
+                </sc-table-cell>
 
-          <div style={{ marginLeft: '60px' }}>
-            <sc-table>
-              <sc-table-cell slot="head">{__('Description', 'surecart')}</sc-table-cell>
-              <sc-table-cell slot="head" style={{ width: '100px', textAlign: 'right' }}>
-                {__('Quantity', 'surecart')}
-              </sc-table-cell>
+                {this.stockErrors.map((item, index) => {
+                  const isLastChild = index === this.stockErrors.length - 1;
+                  return (
+                    <sc-table-row style={{ '--columns': '2', ...(isLastChild ? { border: 'none' } : {}) }}>
+                      <sc-table-cell>
+                        <sc-flex justifyContent="flex-start" alignItems="center">
+                          <img src={item?.image_url} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+                          <h4>{item.name}</h4>
+                        </sc-flex>
+                      </sc-table-cell>
+                      <sc-table-cell style={{ width: '100px', textAlign: 'right' }}>
+                        <span style={{ '--color': 'var(--sc-color-gray-500)' }}>{item?.quantity}</span> → <span>{item?.stock}</span>
+                      </sc-table-cell>
+                    </sc-table-row>
+                  );
+                })}
+              </sc-table>
+            </sc-card>
+          </sc-dashboard-module>
 
-              {this.stockErrors.map(item => {
-                return (
-                  <sc-table-row style={{ '--columns': '2' }}>
-                    <sc-table-cell>
-                      <sc-flex justifyContent="flex-start" alignItems="center">
-                        <img src={item?.image_url} style={{ width: '50px', height: '50px', marginRight: '10px' }} />
-                        <h4>{item.name}</h4>
-                      </sc-flex>
-                    </sc-table-cell>
-                    <sc-table-cell style={{ width: '100px', textAlign: 'right' }}>
-                      <span style={{ '--color': 'var(--sc-color-gray-500)' }}>{item?.quantity}</span> → <span>{item?.stock}</span>
-                    </sc-table-cell>
-                  </sc-table-row>
-                );
-              })}
-            </sc-table>
-            <div slot="footer" style={{ marginTop: '50px' }}>
-              <sc-button type="primary" loading={this.busy} onClick={() => this.updateCheckoutLineItemsStock()}>
-                {__('Continue', 'surecart')}
-                <sc-icon name="arrow-right" slot="suffix" />
-              </sc-button>
-            </div>
-          </div>
+          <sc-button slot="footer" type="primary" loading={this.busy} onClick={() => this.onSubmit()}>
+            {__('Continue', 'surecart')}
+            <sc-icon name="arrow-right" slot="suffix" />
+          </sc-button>
+
           {this.busy && <sc-block-ui spinner></sc-block-ui>}
         </sc-dialog>
       </Host>
