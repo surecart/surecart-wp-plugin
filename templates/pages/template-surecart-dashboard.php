b377fa2cfa62ea25732e4717df7af57032ebc670
Rajkiran Bagal <rajkiranb@bsf.io>
2023-09-25 18:44:33 +0530

    Added missing textdomain

--- templates/pages/template-surecart-dashboard.php
+++ templates/pages/template-surecart-dashboard.php
@@ -77,7 +77,7 @@ $data       = $controller->getData();
 				<div class="sc-dashboard__back sc-pin-bottom">
 					<sc-button href="<?php echo esc_url( get_home_url() ); ?>" type="text" class="sc-link-home">
 						<sc-icon name="arrow-left" slot="prefix"></sc-icon>
-						<?php echo esc_html__( apply_filters( 'sc_customer_dashboard_back_home_text', 'Back Home' ) ); ?>
+						<?php echo esc_html__( apply_filters( 'sc_customer_dashboard_back_home_text', 'Back Home' ), 'surecart' ); ?>
 					</sc-button>
 				</div>
 
