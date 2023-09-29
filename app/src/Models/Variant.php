860869ea2ee1d11f17c84ab4f0c170b3e9626dee
Andre Gagnon <ajgagnon@uwalumni.com>
2023-09-29 15:00:44 -0500

    Fix unnecessary fetching of image.

--- app/src/Models/Variant.php
+++ app/src/Models/Variant.php
@@ -40,21 +40,4 @@ class Variant extends Model {
 	public function setImageAttribute( $value ) {
 		$this->setRelation( 'image', $value, Media::class );
 	}
-
-	/**
-	 * Get the image url.
-	 *
-	 * TODO: Remove this when we have image url from the API.
-	 *
-	 * @return string|null
-	 */
-	public function getImageUrlAttribute() {
-		// Find image from media, by this image id, then get the url.
-		if ( ! empty( $this->image->url ) ) return $this->image->url;
-
-		// If image url is empty, then check media by this image.
-		$media = (new Media())->find( $this->image );
-
-		return $media->url ?? null;
-	}
 }
