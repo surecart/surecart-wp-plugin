8b7282aa611034236a92289845ad8e29e330a1cc
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-10-13 18:37:29 +0600

    Updated first payment total text

--- packages/blocks/Blocks/Total/edit.js
+++ packages/blocks/Blocks/Total/edit.js
@@ -57,6 +57,9 @@ export default ({ attributes, setAttributes }) => {
 				<span slot="subscription-title">
 					{subscription_text || text}
 				</span>
+				<span slot="first-payment-total-description">
+					{first_payment_total_text}
+				</span>
 				<span slot="free-trial-description">{free_trial_text}</span>
 			</sc-line-item-total>
 		</Fragment>
