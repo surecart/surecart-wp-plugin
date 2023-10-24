52cca5049b4ea67ff1cc81628cca23aa469003d8
Andre Gagnon <ajgagnon@uwalumni.com>
2023-10-24 15:02:44 -0500

    Remove uneeded ScFormControl

--- packages/admin/orders/modules/ReturnRequest/ReturnReasonsSelector.js
+++ packages/admin/orders/modules/ReturnRequest/ReturnReasonsSelector.js
@@ -7,19 +7,17 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies.
  */
 import ModelSelector from '../../../components/ModelSelector';
-import { ScFormControl } from '@surecart/components-react';
 
 export default ({ returnRequest, onSelect }) => {
 	return (
-		<ScFormControl label={__('Return Reason', 'surecart')}>
-			<ModelSelector
-				placeholder={__('Unknown', 'surecart')}
-				name="return_reason"
-				onSelect={onSelect}
-				value={returnRequest?.return_reason}
-				display={(item) => item?.description}
-				style={{ width: '100%' }}
-			></ModelSelector>
-		</ScFormControl>
+		<ModelSelector
+			label={__('Return Reason', 'surecart')}
+			placeholder={__('Unknown', 'surecart')}
+			name="return_reason"
+			onSelect={onSelect}
+			value={returnRequest?.return_reason}
+			display={(item) => item?.description}
+			style={{ width: '100%' }}
+		/>
 	);
 };
