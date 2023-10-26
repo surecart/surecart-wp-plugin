23c94ef42a7d28127bd794fe03224172338d77b2
Andre Gagnon <ajgagnon@uwalumni.com>
2023-10-26 13:43:08 -0500

    Add line item mutations to checkout store.

--- packages/components/src/store/checkout/mutations.ts
+++ packages/components/src/store/checkout/mutations.ts
@@ -1,5 +1,8 @@
 import state from './store';
 import { clearCheckout as clearSavedCheckout } from '../checkouts/mutations';
+import { updateFormState } from '@store/form/mutations';
+import { createErrorNotice } from '@store/notices/mutations';
+import { addLineItem, removeLineItem, updateLineItem } from '@services/session';
 
 /**
  * Clear the current checkout.
@@ -17,3 +20,58 @@ export const lockCheckout = lockName => (state.locks = [...state.locks, lockName
  * Pass an optional lock name to only unlock a specific lock
  */
 export const unLockCheckout = (lockName = '') => (state.locks = !!lockName ? state.locks.filter(name => name !== lockName) : []);
+
+/**
+ * Update the checkout line item
+ */
+export const updateCheckoutLineItem = async ({ id, data }) => {
+  try {
+    updateFormState('FETCH');
+    state.checkout = await updateLineItem({
+      id: id,
+      data,
+    });
+    updateFormState('RESOLVE');
+  } catch (e) {
+    console.error(e);
+    createErrorNotice(e);
+    updateFormState('REJECT');
+  }
+};
+
+/**
+ * Remove the checkout line item.
+ */
+export const removeCheckoutLineItem = async id => {
+  try {
+    updateFormState('FETCH');
+    state.checkout = await removeLineItem({
+      checkoutId: state.checkout.id,
+      itemId: id,
+    });
+    updateFormState('RESOLVE');
+  } catch (e) {
+    console.error(e);
+    createErrorNotice(e);
+    updateFormState('REJECT');
+  }
+};
+
+/**
+ * Add the checkout line item.
+ */
+export const addCheckoutLineItem = async data => {
+  try {
+    updateFormState('FETCH');
+    state.checkout = await addLineItem({
+      checkout: state.checkout,
+      data,
+      live_mode: state?.mode === 'live',
+    });
+    updateFormState('RESOLVE');
+  } catch (e) {
+    console.error(e);
+    createErrorNotice(e);
+    updateFormState('REJECT');
+  }
+};
