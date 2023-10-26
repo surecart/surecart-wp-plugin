1cd2e848e6a942480e813fad05d993ad78e8f988
Andre Gagnon <ajgagnon@uwalumni.com>
2023-10-26 15:18:45 -0500

    Don't select variable prices in price selector

--- packages/blocks/Blocks/PriceChoice/edit.js
+++ packages/blocks/Blocks/PriceChoice/edit.js
@@ -48,7 +48,8 @@ export default ({ attributes, setAttributes, isSelected }) => {
 			<div {...blockProps}>
 				<PriceSelector
 					ad_hoc={false}
-					onSelect={({price_id}) => setAttributes({ price_id })}
+					variable={false}
+					onSelect={({ price_id }) => setAttributes({ price_id })}
 				/>
 			</div>
 		);
