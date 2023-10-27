67898d091fb89377c89ab5fbd8ed32fef0d83676
KipRashon <biikiptoo596@gmail.com>
2023-10-27 11:32:11 +0300

    Ensure the width of the status column does not change in large screens

--- views/layouts/partials/admin-index-styles.php
+++ views/layouts/partials/admin-index-styles.php
@@ -2,16 +2,20 @@
 	.column-mode {
 		width: 50px;
 	}
-	.column-total {
+	.column-total, .column-status{
 		min-width: 96px;
-	}
-	.column-status {
-		width: 92px;
 	}
 	.column-order {
 		width: 200px;
 	}
 	.column-fulfillment_status, .column-shipment_status {
 		width: 120px;
+	}
+
+	/* on mobile screens less than 1024px */
+	@media (max-width: 1024px) {
+		 .column-status{
+			width: 96px;
+		}
 	}
 </style>
