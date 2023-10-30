4fe816046fe24f7356e5138e4226c6739c081942
Rajkiran Bagal <rajkiranb@bsf.io>
2023-10-30 18:05:37 +0530

    Added initial line items

--- packages/blocks/Blocks/ProductDonation/Block.php
+++ packages/blocks/Blocks/ProductDonation/Block.php
@@ -41,11 +41,14 @@ class Block extends BaseBlock {
 		// set initial state.
 		sc_initial_state(
 			[
+				'checkout' => [
+					'initialLineItems' => $this->getInitialLineItems($product, $amounts),
+				],
 				'productDonation' => [
 					$attributes['product_id'] => [
 						'product'       => $product->toArray(),
 						'amounts'       => $amounts,
-						'ad_hoc_amount' => $amounts[0] ?? '',
+						'ad_hoc_amount' => $amounts[0] ?? '' ?? '',
 						'selectedPrice' => $product->prices->data[0]->toArray(),
 					],
 				],
@@ -69,5 +72,49 @@ class Block extends BaseBlock {
 		</sc-product-donation-choices>
 		<?php
 		return ob_get_clean();
+	}
+
+	/**
+	 * Get the initial line items.
+	 *
+	 * @return array
+	 */
+	public function getInitialLineItems( $product, $amounts ) {
+		if ( empty($product->prices->data[0]) || empty($amounts) ) {
+			return [];
+		}
+
+		// Get first value from amounts that is in range of price ad_hoc_min_amount & ad_hoc_max_amount.
+		$ad_hoc_amount = $amounts[0] ?? '';
+
+		foreach ( $amounts as $amount ) {
+			if ( $amount >= $product->prices->data[0]->ad_hoc_min_amount && $amount <= $product->prices->data[0]->ad_hoc_max_amount ) {
+				$ad_hoc_amount = $amount;
+				break;
+			}
+		}
+
+		$line_items = array (
+			[
+				'price' => $product->prices->data[0]->id,
+				'ad_hoc_amount' => $ad_hoc_amount,
+				'quantity'   => 1,
+			],
+		);
+
+		$existing   = $this->getExistingLineItems();
+
+		// merge any existing with the new ones.
+		return array_merge( $existing, $line_items );
+	}
+
+	/**
+	 * Get any existing line items.
+	 *
+	 * @return array
+	 */
+	public function getExistingLineItems() {
+		$initial = sc_initial_state();
+		return ! empty( $initial['checkout']['initialLineItems'] ) ? $initial['checkout']['initialLineItems'] : [];
 	}
 }
