7f35f7537494c8975e5436335fcc3b0c61982e7d
KipRashon <biikiptoo596@gmail.com>
2023-11-02 14:56:39 +0300

    Be sure to update the name of the block

--- app/config.php
+++ app/config.php
@@ -226,7 +226,7 @@ return [
 		'surecart/payment'                   => [ 'sc-payment', 'sc-toggles', 'sc-toggle', 'sc-tag' ],
 		'surecart/price-choice'              => [ 'sc-price-choice', 'sc-choice', 'sc-skeleton' ],
 		'surecart/price-selector'            => [ 'sc-price-choices' ],
-		'surecart/price-variant-selector'    => [ 'sc-checkout-product-price-variant-selector' ],
+		'surecart/variant-price-selector'    => [ 'sc-checkout-product-price-variant-selector' ],
 		'surecart/submit'                    => [ 'sc-order-submit', 'sc-button', 'sc-total', 'sc-paypal-buttons', 'sc-format-number', 'sc-spinner' ],
 		'surecart/subtotal'                  => [ 'sc-line-item-total', 'sc-format-number' ],
 		'surecart/total'                     => [ 'sc-line-item-total', 'sc-format-number' ],
