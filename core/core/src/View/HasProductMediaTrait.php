ff82c658d555c90f0c2ad93f5e3533ddec290f51
Andre Gagnon <ajgagnon@uwalumni.com>
2023-11-07 07:51:29 -0600

    phpdoc comment.

--- core/core/src/View/HasProductMediaTrait.php
+++ core/core/src/View/HasProductMediaTrait.php
@@ -14,17 +14,17 @@ trait HasProductMediaTrait {
 	/**
 	 * Returns the product media image attributes.
 	 *
-	 * @param Product $product
+	 * @param Product $product The product.
 	 *
 	 * @return object
 	 */
 	public function getFeaturedProductMediaAttributes( $product ) {
 		$featured_product_media = $product->featured_product_media;
 
-		return (object) [
+		return (object) array(
 			'alt'   => $featured_product_media->media->alt ?? $product->title ?? $product->name ?? '',
 			'title' => $featured_product_media->media->title ?? '',
 			'url'   => $featured_product_media->media->url ?? $product->image_url,
-		];
+		);
 	}
 }
