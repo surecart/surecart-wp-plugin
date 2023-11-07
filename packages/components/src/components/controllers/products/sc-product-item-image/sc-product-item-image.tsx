c8c7f1b5001276ec0f2ac069fb19726023233054
Andre Gagnon <ajgagnon@uwalumni.com>
2023-11-07 14:31:42 -0600

    Pass entire product to product image.

--- packages/components/src/components/controllers/products/sc-product-item-image/sc-product-item-image.tsx
+++ packages/components/src/components/controllers/products/sc-product-item-image/sc-product-item-image.tsx
@@ -1,7 +1,8 @@
 import { Component, h, Host, Prop } from '@stencil/core';
-import { FeaturedProductMediaAttributes } from '../../../../types';
+import { Media, Product, ProductMedia } from '../../../../types';
 import { sizeImage } from '../../../../functions/media';
 import { applyFilters } from '@wordpress/hooks';
+import { getFeaturedProductMediaAttributes } from 'src/functions/media';
 
 @Component({
   tag: 'sc-product-item-image',
@@ -9,25 +10,26 @@ import { applyFilters } from '@wordpress/hooks';
   shadow: true,
 })
 export class ScProductItemImage {
-  /* Product Media */
-  @Prop() productMedia: FeaturedProductMediaAttributes;
+  /* Product image url */
+  @Prop() product: Product;
 
   /* Product image sizing */
   @Prop() sizing: 'cover' | 'contain';
 
   getSrc() {
-    if (this.productMedia?.url) {
-      return this.productMedia?.url;
+    if ((this.product?.featured_product_media as ProductMedia)?.url) {
+      return (this.product?.featured_product_media as ProductMedia)?.url;
     }
 
-    if (this.productMedia.url) {
-      return sizeImage(this.productMedia.url, applyFilters('surecart/product-list/media/size', 900));
+    if (((this.product?.featured_product_media as ProductMedia)?.media as Media)?.url) {
+      return sizeImage(((this.product?.featured_product_media as ProductMedia)?.media as Media)?.url, applyFilters('surecart/product-list/media/size', 900));
     }
 
     return '';
   }
 
   render() {
+    const { alt, title } = getFeaturedProductMediaAttributes(this.product);
     return (
       <Host style={{ borderStyle: 'none' }}>
         <div
@@ -37,11 +39,7 @@ export class ScProductItemImage {
             'is_covered': this.sizing === 'cover',
           }}
         >
-          {!!this.getSrc() ? (
-            <img src={this.getSrc()} alt={this.productMedia.alt} {...(this.productMedia.title ? { title: this.productMedia.title } : {})} />
-          ) : (
-            <div class="product-img_placeholder" />
-          )}
+          {!!this.getSrc() ? <img src={this.getSrc()} alt={alt} {...(title ? { title } : {})} /> : <div class="product-img_placeholder" />}
         </div>
       </Host>
     );
--- packages/components/src/components/controllers/products/sc-product-item/sc-product-item.tsx
+++ packages/components/src/components/controllers/products/sc-product-item/sc-product-item.tsx
@@ -1,7 +1,6 @@
 import { Component, h, Prop } from '@stencil/core';
 import { Product } from '../../../../types';
 import { LayoutConfig } from '../sc-product-item-list/sc-product-item-list';
-import { getFeaturedProductMediaAttributes } from 'src/functions/media';
 
 @Component({
   tag: 'sc-product-item',
@@ -26,9 +25,7 @@ export class ScProductItem {
                 return <sc-product-item-title part="title">{this.product?.name}</sc-product-item-title>;
 
               case 'surecart/product-item-image':
-                return (
-                  <sc-product-item-image part="image" productMedia={getFeaturedProductMediaAttributes(this.product)} sizing={layout.attributes?.sizing}></sc-product-item-image>
-                );
+                return <sc-product-item-image part="image" product={this.product} sizing={layout.attributes?.sizing}></sc-product-item-image>;
 
               case 'surecart/product-item-price':
                 return <sc-product-item-price part="price" prices={this.product?.prices.data} range={!!attributes?.range} metrics={this.product?.metrics}></sc-product-item-price>;
