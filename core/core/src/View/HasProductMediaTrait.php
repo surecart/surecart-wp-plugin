2bb22ab04d26bb661800760201bf52e15119c7ff
Andre Gagnon <ajgagnon@uwalumni.com>
2023-11-07 07:52:29 -0600

    Fix php doc comment.

--- core/core/src/View/HasProductMediaTrait.php
+++ core/core/src/View/HasProductMediaTrait.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * @package   SureCartCore
- * @author    SureCart <support@surecart.com>
- * @copyright 2017-2019 SureCart
- * @license   https://www.gnu.org/licenses/gpl-2.0.html GPL-2.0
- * @link      https://surecart.com/
+ * Handles product media url alt and title.
  */
 
 namespace SureCartCore\View;
 
 trait HasProductMediaTrait {
-
 	/**
 	 * Returns the product media image attributes.
 	 *
