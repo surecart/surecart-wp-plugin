ebdd21c158d87123f94de95f1f7fbd9448fd706f
Andre Gagnon <ajgagnon@uwalumni.com>
2023-11-09 15:24:06 -0600

    Formatting

--- packages/blocks/Blocks/CustomDonationAmount/Block.php
+++ packages/blocks/Blocks/CustomDonationAmount/Block.php
@@ -17,13 +17,13 @@ class Block extends BaseBlock {
 	 * @return string
 	 */
 	public function render( $attributes, $content ) {
-        ob_start(); ?>
-            <sc-product-donation-custom-amount
-            	currencyCode="<?php echo esc_attr( $attributes['currency'] ?? 'USD' ); ?>"
+		ob_start(); ?>
+			<sc-product-donation-custom-amount
+				currencyCode="<?php echo esc_attr( $attributes['currency'] ?? 'USD' ); ?>"
 				product-id="<?php echo esc_attr( $this->block->context['surecart/product-donation/product_id'] ?? '' ); ?>"
-            >
-            </sc-product-donation-custom-amount>
-        <?php
+			>
+			</sc-product-donation-custom-amount>
+		<?php
 		return ob_get_clean();
 	}
 }
