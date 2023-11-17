05f1c1c64ba1f4df726ba461ee5a4dae911e815d
KipRashon <biikiptoo596@gmail.com>
2023-11-17 14:09:34 +0300

    Fix the failing editor css

--- app/config.php
+++ app/config.php
@@ -167,6 +167,7 @@ return [
 		\SureCartBlocks\Blocks\Password\Block::class,
 		\SureCartBlocks\Blocks\CartMenuButton\Block::class,
 		\SureCartBlocks\Blocks\CartSubmit\Block::class,
+		\SureCartBlocks\Blocks\Cart\Block::class,
 
 		// Deprecated.
 		\SureCartBlocks\Blocks\Dashboard\Deprecated\CustomerInvoices\Block::class,
