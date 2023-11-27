a8e2fcfd37628e1fe57b252f03993b3c030592f9
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2023-11-27 18:08:22 +0600

    Used re-usable function for getting max qty

--- packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
+++ packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
@@ -4,10 +4,11 @@ import { __ } from '@wordpress/i18n';
 import { state as checkoutState } from '@store/checkout';
 import { hasSubscription } from '../../../../functions/line-items';
 import { intervalString } from '../../../../functions/price';
-import { LineItem, Product, FeaturedProductMediaAttributes } from '../../../../types';
+import { LineItem, Product, FeaturedProductMediaAttributes, Variant } from '../../../../types';
 import { getFeaturedProductMediaAttributes } from '../../../../functions/media';
 import { removeCheckoutLineItem, updateCheckoutLineItem } from '@store/checkout/mutations';
 import { formBusy } from '@store/form/getters';
+import { getMaxStockQuantity } from '../../../../functions/quantity';
 
 /**
  * @part base - The component base
@@ -59,30 +60,6 @@ export class ScLineItems {
     return this.editable;
   }
 
-  /**
-   * Get the max stock quantity for the line item.
-   */
-  getMaxStockQty(item: LineItem) {
-    const product = item?.price?.product as Product;
-
-    // check purchase limit.
-    if (product?.purchase_limit) {
-      return product.purchase_limit;
-    }
-
-    // If stock is not enabled, return null.
-    const isStockNeedsToBeChecked = !!(product?.stock_enabled && !product?.allow_out_of_stock_purchases);
-    if (!isStockNeedsToBeChecked) {
-      return null;
-    }
-
-    // If no variant is selected, check against product stock.
-    if (!item?.variant) return product?.available_stock;
-
-    // Check against selected variant's stock.
-    return item.variant?.available_stock;
-  }
-
   render() {
     if (!!formBusy() && !checkoutState?.checkout?.line_items?.data?.length) {
       return (
@@ -110,7 +87,7 @@ export class ScLineItems {
                 name={(item?.price?.product as Product)?.name}
                 priceName={item?.price?.name}
                 variantLabel={(item?.variant_options || []).filter(Boolean).join(' / ') || null}
-                max={this.getMaxStockQty(item)}
+                max={getMaxStockQuantity(item?.price?.product as Product, item?.variant as Variant)}
                 editable={this.isEditable(item)}
                 removable={this.removable}
                 quantity={item.quantity}
