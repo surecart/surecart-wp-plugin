db68f78d00d99567fab106bb05838706e12cdab5
Andre Gagnon <ajgagnon@uwalumni.com>
2023-12-13 15:12:31 -0600

    Add relationships table

--- app/src/Database/MigrationsServiceProvider.php
+++ app/src/Database/MigrationsServiceProvider.php
@@ -4,6 +4,7 @@ namespace SureCart\Database;
 
 use SureCart\Database\Tables\IncomingWebhook;
 use SureCart\Database\Tables\Integrations;
+use SureCart\Database\Tables\Relationships;
 use SureCartCore\ServiceProviders\ServiceProviderInterface;
 
 /**
@@ -24,11 +25,15 @@ class MigrationsServiceProvider implements ServiceProviderInterface {
 			return new IncomingWebhook( new Table() );
 		};
 
-		$container['surecart.migrations.usermeta'] = function() {
+		$container['surecart.tables.relationships'] = function () {
+			return new Relationships( new Table() );
+		};
+
+		$container['surecart.migrations.usermeta'] = function () {
 			return new UserMetaMigrationsService();
 		};
 
-		$container['surecart.migrations.webhook'] = function() {
+		$container['surecart.migrations.webhook'] = function () {
 			return new WebhookMigrationsService();
 		};
 	}
@@ -41,6 +46,7 @@ class MigrationsServiceProvider implements ServiceProviderInterface {
 	public function bootstrap( $container ) {
 		$container['surecart.tables.integrations']->install();
 		$container['surecart.tables.webhooks.incoming']->install();
+		$container['surecart.tables.relationships']->install();
 		$container['surecart.migrations.usermeta']->bootstrap();
 		$container['surecart.migrations.webhook']->bootstrap();
 	}
