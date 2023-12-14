da12ef1b0827a08f8262c737c89c210ab02364ef
KipRashon <biikiptoo596@gmail.com>
2023-12-14 13:32:37 +0300

    Test that existing template part do not get updated and other template parts are not affected by the migration

--- .dev/tests/php/unit/Migrations/UpdateMigrationServiceProviderTest.php
+++ .dev/tests/php/unit/Migrations/UpdateMigrationServiceProviderTest.php
@@ -36,7 +36,7 @@ class UpdateMigrationServiceProviderTest extends \WP_UnitTestCase {
 		$this->assertEquals( $test_cart->post_content, $created_template_part->post_content );
 	}
 
-	public function test_existing_template_part_get_updated(){
+	public function test_existing_template_part_not_updated(){
 		$existing_template_part = self::factory()->post->create_and_get(array(
 			'post_type' => 'wp_template_part',
 			'post_title' => _x( 'Cart', 'Cart title', 'surecart' ),
@@ -59,17 +59,17 @@ class UpdateMigrationServiceProviderTest extends \WP_UnitTestCase {
 		])[0] ?? null;
 
 		$this->assertNotEmpty( $updated_template_part );
-		$this->assertEquals( $test_cart->post_content, $updated_template_part->post_content );
+		$this->assertNotEquals( $test_cart->post_content, $updated_template_part->post_content );
 		$this->assertEquals( $existing_template_part->ID, $updated_template_part->ID );
 	}
 
 	public function test_prevent_duplicate_template_parts(){
-		self::factory()->post->create_and_get(array(
+		self::factory()->post->create(array(
 			'post_type' => 'sc_cart',
 			'post_content' => 'This is the test cart post content <div>hello</div>'
 		));
 
-		self::factory()->post->create_and_get(array(
+		self::factory()->post->create(array(
 			'post_type' => 'wp_template_part',
 			'post_title' => _x( 'Cart', 'Cart title', 'surecart' ),
 			'post_content' => 'This is the existing template part content <div>hello</div>'
@@ -86,5 +86,31 @@ class UpdateMigrationServiceProviderTest extends \WP_UnitTestCase {
 		]);
 
 		$this->assertCount( 1, $posts );
+	}
+
+	public function test_prevent_replace_existing_template_parts(){
+		self::factory()->post->create(array(
+			'post_type' => 'sc_cart',
+			'post_content' => 'This is the test cart post content <div>hello</div>'
+		));
+
+		self::factory()->post->create(array(
+			'post_type' => 'wp_template_part',
+			'post_title' => _x( 'Test', 'Test title', 'surecart' ),
+			'post_content' => 'This is a test template part.'
+		));
+
+		// call the handle cart migration function
+		$update_migration_service_provider = new UpdateMigrationServiceProvider();
+		$update_migration_service_provider->handleCartMigration();
+
+		// get the created template part
+		$posts = get_posts([
+			'post_type' => 'wp_template_part',
+			'post_title' => _x( 'Test', 'Test title', 'surecart' ),
+		]);
+
+		$this->assertCount( 1, $posts );
+		$this->assertEquals( 'This is a test template part.', $posts[0]->post_content );
 	}
 }
