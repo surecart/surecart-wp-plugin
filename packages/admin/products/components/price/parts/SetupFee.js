a4d0950fb0a6aefe2cf668c7b861148af34c6309
KipRashon <biikiptoo596@gmail.com>
2023-12-19 13:14:41 +0300

    Way to change whether a price is discount or fee in the setup section

--- packages/admin/products/components/price/parts/SetupFee.js
+++ packages/admin/products/components/price/parts/SetupFee.js
@@ -1,16 +1,35 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 
 import {
+	ScButton,
+	ScDropdown,
+	ScIcon,
 	ScInput,
+	ScMenu,
+	ScMenuItem,
 	ScPremiumTag,
 	ScPriceInput,
 	ScSwitch,
 	ScUpgradeRequired,
 } from '@surecart/components-react';
 
+const SETUP_AMOUNT_TYPES = {
+	fee: __('fee', 'surecart'),
+	discount: __('discount', 'surecart'),
+};
+
 export default ({ price, updatePrice }) => {
+	const [amountType, setAmountType] = useState(() => {
+		if (price.setup_fee_amount < 0) {
+			return 'discount';
+		}
+
+		return 'fee';
+	});
+
 	return (
 		<ScUpgradeRequired
 			required={!scData?.entitlements?.subscription_setup_fees}
@@ -25,7 +44,7 @@ export default ({ price, updatePrice }) => {
 					updatePrice({ setup_fee_enabled: e.target.checked })
 				}
 			>
-				{__('Setup fee', 'surecart')}
+				{__('Add discount/fee', 'surecart')}
 				{!scData?.entitlements?.subscription_setup_fees && (
 					<>
 						{' '}
@@ -46,7 +65,10 @@ export default ({ price, updatePrice }) => {
 					`}
 				>
 					<ScInput
-						label={__('Setup fee name', 'surecart')}
+						label={sprintf(
+							__('Setup %s name', 'surecart'),
+							SETUP_AMOUNT_TYPES[amountType]
+						)}
 						value={price?.setup_fee_name}
 						onScInput={(e) => {
 							updatePrice({
@@ -57,16 +79,67 @@ export default ({ price, updatePrice }) => {
 						required
 					/>
 					<ScPriceInput
-						label={__('Setup fee amount', 'surecart')}
+						label={sprintf(
+							__('Setup %s amount', 'surecart'),
+							SETUP_AMOUNT_TYPES[amountType]
+						)}
 						currencyCode={price?.currency || scData.currency_code}
-						value={price?.setup_fee_amount}
+						value={Math.abs(price?.setup_fee_amount)}
 						onScInput={(e) =>
 							updatePrice({
-								setup_fee_amount: e.target.value,
+								setup_fee_amount:
+									amountType === 'fee'
+										? e.target.value
+										: -e.target.value,
 							})
 						}
 						required
-					/>
+					>
+						<ScDropdown
+							slot="suffix"
+							placement="bottom-end"
+							css={css`
+								margin-right: var(--sc-spacing-x-small);
+							`}
+						>
+							<ScButton
+								type="text"
+								slot="trigger"
+								circle
+								css={css`
+									text-transform: capitalize;
+								`}
+							>
+								{SETUP_AMOUNT_TYPES[amountType]}
+								<ScIcon name="chevron-down" />
+							</ScButton>
+							<ScMenu>
+								{Object.keys(SETUP_AMOUNT_TYPES).map((type) => (
+									<ScMenuItem
+										onClick={() => {
+											setAmountType(type);
+											updatePrice({
+												setup_fee_amount:
+													type === 'fee'
+														? Math.abs(
+																price.setup_fee_amount
+														  )
+														: -Math.abs(
+																price.setup_fee_amount
+														  ),
+											});
+										}}
+										key={type}
+										css={css`
+											text-transform: capitalize;
+										`}
+									>
+										{SETUP_AMOUNT_TYPES[type]}
+									</ScMenuItem>
+								))}
+							</ScMenu>
+						</ScDropdown>
+					</ScPriceInput>
 				</div>
 			)}
 		</ScUpgradeRequired>
