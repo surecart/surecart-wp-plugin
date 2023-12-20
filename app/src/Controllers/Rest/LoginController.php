1cfe2ea246423e418d3a1c22940157f8f873edac
Rajkiran Bagal <rajkiranb@bsf.io>
2023-12-20 14:14:31 +0530

    Optimised code

--- app/src/Controllers/Rest/LoginController.php
+++ app/src/Controllers/Rest/LoginController.php
@@ -7,23 +7,29 @@ namespace SureCart\Controllers\Rest;
  */
 class LoginController extends RestController {
 	/**
-	 * Login user
+	 * Login user.
 	 *
-	 * @return Model
+	 * @param WP_REST_Request $request The REST request object.
+	 *
+	 * @return array|\WP_Error Returns an array with user details on success, or WP_Error on failure.
 	 */
 	public function authenticate( \WP_REST_Request $request ) {
-		// Authenticate the user
+		// Authenticate the user.
 		$user = wp_authenticate( $request->get_param( 'login' ), $request->get_param( 'password' ) );
-		// flush all caches.
+
+		// Flush all caches.
 		wp_cache_flush();
-		
+
 		if ( is_wp_error( $user ) ) {
 			return $user;
 		}
 
-		// Set the current user
+		// Set the current user.
+		clean_user_cache( $user->ID );
+		wp_clear_auth_cookie();
 		wp_set_current_user( $user->ID );
-		wp_set_auth_cookie( $user->ID );
+		wp_set_auth_cookie( $user->ID, true, false );
+		update_user_caches( $user->getUser() );
 
 		return [
 			'name'         => $user->display_name,
