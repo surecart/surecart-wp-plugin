3e59395fc1914d0eb93d0bbb6a1caf43f67851d9
KipRashon <biikiptoo596@gmail.com>
2023-12-21 15:40:39 +0300

    Update the add to cart event to have the required params

--- packages/components/src/store/checkout/facebook.ts
+++ packages/components/src/store/checkout/facebook.ts
@@ -1,5 +1,5 @@
-import { maybeConvertAmount } from "src/functions/currency";
-import { LineItem, Product } from "src/types";
+import { maybeConvertAmount } from 'src/functions/currency';
+import { LineItem, Product } from 'src/types';
 
 /**
  * Handle add to cart event.
@@ -13,12 +13,32 @@ window.addEventListener('scAddedToCart', function (e: CustomEvent) {
   // sanity check.
   if (!item?.price?.product) return;
 
+  const product = item?.price?.product as Product;
+  const productCollections: string[] = product?.product_collections?.data?.map(collection => collection.name) || [];
+
   window.fbq('track', 'AddToCart', {
-    content_ids: (item.price?.product as Product)?.id,
-    content_name: (item.price?.product as Product)?.name,
+    ...(productCollections.length ? { content_category: productCollections.join(', ') } : {}),
+    content_ids: [product.id],
+    content_name: product?.name + (item?.variant_options?.length ? ` - ${item?.variant_options.join(' / ')}` : ''),
     content_type: 'product',
-    currency: item.price?.currency,
-    value: maybeConvertAmount(item?.price?.amount || 0, item.price?.currency || 'USD'),
-    num_items: item.quantity,
+    currency: item?.price?.currency,
+    value: maybeConvertAmount(item?.price?.amount || 0, item?.price?.currency || 'USD'),
   });
- });
+});
+
+/**
+ * Handle purchase initiated event.
+ */
+window.addEventListener('scCheckoutInitiated', function (e: CustomEvent) {
+  if (!window?.fbq) return;
+
+  const checkout = e.detail;
+  // content_category, content_ids, contents, currency, num_items, value
+  window.fbq('track', 'InitiateCheckout', {
+    content_ids: (checkout?.line_items?.data || []).map(item => (item?.price?.product as Product)?.id),
+    content_type: 'product',
+    currency: checkout?.currency,
+    value: maybeConvertAmount(checkout?.total_amount, checkout?.currency || 'USD'),
+    num_items: (checkout?.line_items?.data || []).reduce((total, item) => total + item.quantity, 0),
+  });
+});
