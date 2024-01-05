eec18070300a3baae63cebb4297811673210f2ae
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-01-05 13:09:09 +0600

    Added View Upsell Link in Edit Page

--- app/src/Controllers/Admin/Upsells/UpsellsController.php
+++ app/src/Controllers/Admin/Upsells/UpsellsController.php
@@ -4,6 +4,7 @@ namespace SureCart\Controllers\Admin\Upsells;
 
 use SureCart\Controllers\Admin\AdminController;
 use SureCart\Models\Product;
+use SureCart\Models\Upsell;
 use SureCartCore\Responses\RedirectResponse;
 
 /**
@@ -34,6 +35,16 @@ class UpsellsController extends AdminController {
 		// enqueue needed script.
 		add_action( 'admin_enqueue_scripts', \SureCart::closure()->method( UpsellScriptsController::class, 'enqueue' ) );
 
+		$upsell = null;
+
+		if ( $request->query( 'id' ) ) {
+			$upsell = Upsell::with( [ 'price' ] )->find( $request->query( 'id' ) );
+
+			if ( is_wp_error( $upsell ) ) {
+				wp_die( implode( ' ', array_map( 'esc_html', $upsell->get_error_messages() ) ) );
+			}
+		}
+
 		$this->preloadPaths(
 			[
 				'/wp/v2/users/me',
@@ -41,6 +52,24 @@ class UpsellsController extends AdminController {
 				'/wp/v2/types?context=edit',
 				'/surecart/v1/upsells/' . $request->query( 'id' ) . '?context=edit',
 			]
+		);
+
+		// Add View Upsell link.
+		add_action(
+			'admin_bar_menu',
+			function( $wp_admin_bar ) use ( $upsell ) {
+				$wp_admin_bar->add_node(
+					[
+						'id'    => 'view-upsell-page',
+						'title' => __( 'View Upsell', 'surecart' ),
+						'href'  => esc_url( $upsell->permalink ?? '#' ),
+						'meta'  => [
+							'class' => empty( $upsell->permalink ) ? 'hidden' : '',
+						],
+					]
+				);
+			},
+			99
 		);
 
 		// return view.
