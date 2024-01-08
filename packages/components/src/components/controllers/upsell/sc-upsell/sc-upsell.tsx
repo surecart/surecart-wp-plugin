7f6b85c0853ec852d76f92a26bda19353ae2d931
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-08 16:39:51 -0600

    Use getters for busy

--- packages/components/src/components/controllers/upsell/sc-upsell/sc-upsell.tsx
+++ packages/components/src/components/controllers/upsell/sc-upsell/sc-upsell.tsx
@@ -7,9 +7,10 @@ import { Component, Host, h } from '@stencil/core';
  * Internal dependencies.
  */
 import { state } from '@store/upsell';
-import { createOrUpdateUpsell, redirectUpsell } from '@store/upsell/mutations';
-import { isUpsellExpired } from '@store/upsell/getters';
+import { isBusy } from '@store/upsell/getters';
+import { update } from '@store/upsell/mutations';
 import { __ } from '@wordpress/i18n';
+import { ManualPaymentMethod } from 'src/types';
 
 @Component({
   tag: 'sc-upsell',
@@ -18,28 +19,16 @@ import { __ } from '@wordpress/i18n';
 })
 export class ScUpsell {
   componentWillLoad() {
-    createOrUpdateUpsell();
-  }
-
-  componentDidLoad() {
-    // check for upsell redirection every second.
-    this.maybeRedirectUpsell();
-    setInterval(() => {
-      this.maybeRedirectUpsell();
-    }, 1000);
-  }
-
-  maybeRedirectUpsell() {
-    if (isUpsellExpired()) {
-      redirectUpsell();
-    }
+    update();
   }
 
   render() {
+    const manualPaymentMethod = state.checkout?.manual_payment_method as ManualPaymentMethod;
+
     return (
       <Host>
         <slot />
-        {['loading', 'busy'].includes(state?.loading || '') && <sc-block-ui style={{ 'z-index': '30' }}></sc-block-ui>}
+        {isBusy() && <sc-block-ui style={{ 'z-index': '30' }}></sc-block-ui>}
 
         <sc-dialog open={state.loading === 'complete'} style={{ '--body-spacing': 'var(--sc-spacing-xxx-large)' }} noHeader onScRequestClose={e => e.preventDefault()}>
           <div class="confirm__icon">
@@ -53,14 +42,14 @@ export class ScUpsell {
           >
             <span slot="description">{state?.text?.success?.description || __('Your payment was successful. A receipt is on its way to your inbox.', 'surecart')}</span>
 
-            {/* {!!manualPaymentMethod?.name && !!manualPaymentMethod?.instructions && (
+            {!!manualPaymentMethod?.name && !!manualPaymentMethod?.instructions && (
               <sc-alert type="info" open style={{ 'text-align': 'left' }}>
                 <span slot="title">{manualPaymentMethod?.name}</span>
                 {manualPaymentMethod?.instructions.split('\n').map(i => {
                   return <p>{i}</p>;
                 })}
               </sc-alert>
-            )} */}
+            )}
 
             <sc-button href={window?.scData?.pages?.dashboard} size="large" type="primary" autofocus={true}>
               {state?.text?.success?.button || __('Continue', 'surecart')}
