d57afd6bb3d37075a8860caffd57c84131686cdb
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-08 16:38:44 -0600

    Remove addLineItem from upsells model

--- app/src/Controllers/Rest/UpsellsController.php
+++ app/src/Controllers/Rest/UpsellsController.php
@@ -14,20 +14,4 @@ class UpsellsController extends RestController {
 	 * @var string
 	 */
 	protected $class = Upsell::class;
-
-	/**
-	 * Add a line item to an upsell.
-	 *
-	 * @param \WP_REST_Request $request Request object.
-	 *
-	 * @return \WP_REST_Response
-	 */
-	public function addLineItem( $request ) {
-		$model = $this->middleware( new $this->class(), $request );
-		if ( is_wp_error( $model ) ) {
-			return $model;
-		}
-
-		return $model->where( $request->get_query_params() )->addLineItem( $request );
-	}
 }
