73af6738e6eb71dffc66877c01d5e5e0a15c8c0b
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-08 16:40:00 -0600

    Handle no thanks button

--- packages/components/src/components/controllers/upsell/sc-upsell-no-thanks-button/sc-upsell-no-thanks-button.tsx
+++ packages/components/src/components/controllers/upsell/sc-upsell-no-thanks-button/sc-upsell-no-thanks-button.tsx
@@ -1,18 +1,14 @@
 import { Component, Host, h } from '@stencil/core';
-// import { redirectUpsell } from '@store/upsell/mutations';
+import { cancel } from '@store/upsell/mutations';
 
 @Component({
   tag: 'sc-upsell-no-thanks-button',
   styleUrl: 'sc-upsell-no-thanks-button.scss',
 })
 export class ScUpsellNoThanksButton {
-  handleNoThanksClick() {
-    // redirectUpsell();
-  }
-
   render() {
     return (
-      <Host onClick={() => this.handleNoThanksClick()}>
+      <Host onClick={() => cancel()}>
         <slot />
       </Host>
     );
