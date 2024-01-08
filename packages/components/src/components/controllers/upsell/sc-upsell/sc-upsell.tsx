d9f0b3c73701f7ef295ea9e11cb56380494412f6
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-08 08:25:25 -0600

    Remove is previewed

--- packages/components/src/components/controllers/upsell/sc-upsell/sc-upsell.tsx
+++ packages/components/src/components/controllers/upsell/sc-upsell/sc-upsell.tsx
@@ -2,7 +2,6 @@
  * External dependencies.
  */
 import { Component, Host, h } from '@stencil/core';
-import { addQueryArgs } from '@wordpress/url';
 
 /**
  * Internal dependencies.
@@ -22,12 +21,6 @@ export class ScUpsell {
   }
 
   componentDidLoad() {
-    // For Preview mode, prevent it from redirecting to the upsell page.
-    const isPreviewed = addQueryArgs(window.location.href, { preview: true });
-    if (isPreviewed) {
-      return;
-    }
-
     this.maybeRedirectUpsell();
   }
 
