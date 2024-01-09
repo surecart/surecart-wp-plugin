e29655d059b61655aac90729ed5d309b61437fb3
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-09 12:53:29 -0600

    Refactor innerBlocksProps in Upsell edit.js

--- packages/blocks/Blocks/Upsell/Upsell/edit.js
+++ packages/blocks/Blocks/Upsell/Upsell/edit.js
@@ -4,8 +4,6 @@
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
 export default () => {
-	const innerBlocksProps = useInnerBlocksProps(useBlockProps(), {
-		// allowedBlocks: ALLOWED_BLOCKS,,
-	});
+	const innerBlocksProps = useInnerBlocksProps(useBlockProps());
 	return <div {...innerBlocksProps} />;
 };
