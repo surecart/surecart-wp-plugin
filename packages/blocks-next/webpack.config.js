3f28076a39fe0564f57333ecebc3ae370ed166bc
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-15 11:01:05 -0600

    Add product page script and update webpack config

--- packages/blocks-next/webpack.config.js
+++ packages/blocks-next/webpack.config.js
@@ -14,6 +14,28 @@ module.exports = {
 			entries[`styles/${name}`] = path.resolve(__dirname, file);
 		});
 
+		const scriptFiles = glob.sync('./src/scripts/**/index.js');
+		scriptFiles.forEach((file) => {
+			const name = file.replace('./src/scripts/', '').replace('.js', '');
+			entries[`scripts/${name}`] = path.resolve(__dirname, file);
+		});
+
+		// TODO: remove this once the issue is fixed in @wordpress/scripts
+		// This is not working because of the new module thing.
+		// const viewFiles = glob.sync('./src/**/view.js');
+		// viewFiles.forEach((file) => {
+		// 	const entryName = file
+		// 		.replace('.js', '')
+		// 		.replace('src', '')
+		// 		.replace(/\\/g, '/');
+		// 	entries[entryName] = path.resolve(__dirname, file);
+		// });
+
 		return entries;
+	},
+
+	externals: {
+		...defaultConfig.externals,
+		'@wordpress/interactivity': '@wordpress/interactivity',
 	},
 };
