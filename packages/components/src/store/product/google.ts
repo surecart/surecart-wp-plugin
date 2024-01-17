b66c77d03702d992ecbdf3330ca11290af9dc389
KipRashon <biikiptoo596@gmail.com>
2024-01-17 14:17:21 +0300

    view item event

--- packages/components/src/store/product/google.ts
+++ packages/components/src/store/product/google.ts
@@ -1,3 +1,4 @@
+import { maybeConvertAmount } from 'src/functions/currency';
 import { ProductsSearchedParams } from 'src/types';
 
 /**
@@ -23,6 +24,45 @@ window.addEventListener('scSearched', function (e: CustomEvent) {
       ecommerce: {
         search_term: eventDetail?.searchString,
       },
+    });
+  }
+});
+
+/**
+ * Handle view item event.
+ */
+window.addEventListener('scProductViewed', function (e: CustomEvent) {
+  if (!window?.dataLayer && !window?.gtag) return;
+
+  const product = e.detail;
+  const data = {
+    value: maybeConvertAmount(product.price?.amount || 0, product.price?.currency || 'USD'),
+    currency: product.price?.currency,
+    items: [
+      {
+        item_id: product?.id,
+        item_name: product?.name,
+        currency: product?.price?.currency,
+        discount: product?.discount_amount ? maybeConvertAmount(product?.discount_amount, product?.price?.currency) : 0,
+        price: maybeConvertAmount(product?.price?.amount, product?.price?.currency),
+        quantity: product?.quantity,
+        item_variant: (product?.variant_options || []).join(' / '),
+        ...(product?.product_collections?.data?.length ? { item_category: product?.product_collections?.data?.map(collection => collection.name).join(', ') } : {}),
+      },
+    ],
+  };
+
+  // handle google analytics script
+  if (window?.gtag) {
+    window.gtag('event', 'view_item', data);
+  }
+
+  // handle datalayer
+  if (window?.dataLayer) {
+    window.dataLayer.push({ ecommerce: null });
+    window.dataLayer.push({
+      event: 'view_item',
+      ecommerce: data,
     });
   }
 });
