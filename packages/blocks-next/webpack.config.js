5086afa698e0f9b60301f8a6328f11d73e00909d
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-17 16:12:30 -0600

    Don't need to transfer view modules if referenced in block.json

--- packages/blocks-next/webpack.config.js
+++ packages/blocks-next/webpack.config.js
@@ -2,7 +2,6 @@ const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
 const glob = require('glob');
 
-console.log(defaultConfig);
 module.exports = [
 	{
 		...defaultConfig[0],
@@ -17,35 +16,8 @@ module.exports = [
 				entries[`styles/${name}`] = path.resolve(__dirname, file);
 			});
 
-			// TODO: remove this once the issue is fixed in @wordpress/scripts
-			// This is not working because of the new module thing.
-			// const viewFiles = glob.sync('./src/**/view.js');
-			// viewFiles.forEach((file) => {
-			// 	const entryName = file
-			// 		.replace('.js', '')
-			// 		.replace('src', '')
-			// 		.replace(/\\/g, '/');
-			// 	entries[entryName] = path.resolve(__dirname, file);
-			// });
-
 			return entries;
 		},
 	},
-	{
-		...defaultConfig[1],
-		entry: function () {
-			const entries = defaultConfig[1].entry();
-
-			const globalStores = glob.sync('./src/scripts/**/index.js');
-			globalStores.forEach((file) => {
-				const entryName = file
-					.replace('.js', '')
-					.replace('src', '')
-					.replace(/\\/g, '/');
-				entries[entryName] = path.resolve(__dirname, file);
-			});
-
-			return entries;
-		},
-	},
+	defaultConfig[1],
 ];
