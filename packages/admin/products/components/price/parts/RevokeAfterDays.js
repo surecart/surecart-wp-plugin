2b55f11b5b8b4f30600fe9b93176b9adecfd6ab0
Andre Gagnon <ajgagnon@uwalumni.com>
2024-01-29 15:32:23 -0600

    Change naming and add toggle.

--- packages/admin/products/components/price/parts/RevokeAfterDays.js
+++ packages/admin/products/components/price/parts/RevokeAfterDays.js
@@ -9,28 +9,40 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies.
  */
-import { ScInput } from '@surecart/components-react';
+import { ScInput, ScSwitch } from '@surecart/components-react';
 
 export default ({ price, updatePrice }) => {
 	return (
-		<ScInput
-			label={__('Revoke after', 'surecart')}
-			className="sc-revoke-after-days"
-			css={css`
-				flex: 1 1 50%;
-			`}
-			type="number"
-			min={1}
-			value={price?.revoke_after_days}
-			onScInput={(e) =>
-				updatePrice({
-					revoke_after_days: parseInt(
-						e.target.value
-					),
-				})
-			}
-		>
-			<span slot="suffix">{__('Days', 'surecart')}</span>
-		</ScInput>
+		<>
+			<ScSwitch
+				checked={price?.revoke_after_days}
+				onScChange={(e) =>
+					updatePrice({
+						revoke_after_days: e.target.checked ? 30 : null,
+					})
+				}
+			>
+				{__('Limited time purchase', 'surecart')}
+			</ScSwitch>
+			{!!price?.revoke_after_days && (
+				<ScInput
+					label={__('Limit purchase to', 'surecart')}
+					className="sc-revoke-after-days"
+					css={css`
+						flex: 1 1 50%;
+					`}
+					type="number"
+					min={1}
+					value={price?.revoke_after_days}
+					onScInput={(e) =>
+						updatePrice({
+							revoke_after_days: parseInt(e.target.value),
+						})
+					}
+				>
+					<span slot="suffix">{__('Days', 'surecart')}</span>
+				</ScInput>
+			)}
+		</>
 	);
 };
