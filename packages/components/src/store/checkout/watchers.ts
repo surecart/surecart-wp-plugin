6dc29f7f45b27f9b82335bde9f8e66b8e4d28d32
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-01-30 17:01:27 +0300

    Speak out changes in the total amount and the amount due in the checkout

--- packages/components/src/store/checkout/watchers.ts
+++ packages/components/src/store/checkout/watchers.ts
@@ -1,5 +1,9 @@
 import state, { onChange, on } from './store';
 import { getCheckout, setCheckout } from '../checkouts/mutations';
+import { Checkout } from 'src/types';
+import { speak } from '@wordpress/a11y';
+import { __, sprintf } from '@wordpress/i18n';
+import { getFormattedPrice } from '../../functions/price';
 
 /**
  * When the checkout changes, update the
@@ -14,4 +18,14 @@ on('get', prop => {
       state.checkout = checkout;
     }
   }
+});
+
+on('set', (key: string, checkout: Checkout, oldCheckout: Checkout) => {
+  if (key !== 'checkout') return; // we only care about checkout
+  if (checkout.total_amount === oldCheckout.total_amount && checkout.amount_due === oldCheckout.total_amount) return; // we only care about total_amount and amount_due
+
+  const amountDue = getFormattedPrice({ amount: checkout.amount_due, currency: checkout.currency });
+  const totalAmount = getFormattedPrice({ amount: checkout.total_amount, currency: checkout.currency });
+
+  speak(sprintf(__('Checkout updated. The total amount for the checkout is %1$s and the amount due is %1$s.', 'surecart'), totalAmount, amountDue));
 });
