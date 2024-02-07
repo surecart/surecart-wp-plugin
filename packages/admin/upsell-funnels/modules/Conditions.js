7c758447dbaddbaf8a2cab75e574ddc92e8bf577
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-07 10:44:45 -0600

    Fix conditions logic.

--- packages/admin/upsell-funnels/modules/Conditions.js
+++ packages/admin/upsell-funnels/modules/Conditions.js
@@ -1,7 +1,11 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import {
 	ScButton,
 	ScEmpty,
 	ScIcon,
+	ScRadio,
+	ScRadioGroup,
 	ScSelect,
 } from '@surecart/components-react';
 import { __ } from '@wordpress/i18n';
@@ -13,54 +17,109 @@ import NewCondition from './NewCondition';
 export default ({ loading, funnel, updateFunnel }) => {
 	const [newDialog, setNewDialog] = useState(false);
 
-	const hasConditions =
+	const shouldMatch = !!funnel?.filter_match_type;
+
+	const empty = !(
 		funnel?.filter_price_ids?.length ||
 		funnel?.filter_product_ids?.length ||
-		funnel?.filter_product_group_ids?.length;
+		funnel?.filter_product_group_ids?.length
+	);
 
 	return (
 		<Box
 			title={__('Display Conditions', 'surecart')}
 			loading={loading}
 			footer={
-				<ScButton onClick={() => setNewDialog(true)}>
-					<ScIcon name="plus" slot="prefix" />
-					{__('Add A Condition', 'surecart')}
-				</ScButton>
+				shouldMatch && (
+					<ScButton onClick={() => setNewDialog(true)}>
+						<ScIcon name="plus" slot="prefix" />
+						{__('Add A Condition', 'surecart')}
+					</ScButton>
+				)
 			}
 		>
-			{hasConditions ? (
-				<>
-					<ScSelect
-						label={__('Show Upsell Offer If', 'surecart')}
-						value={funnel?.filter_match_type}
-						choices={[
-							{
-								label: __(
-									'All of these items are in the cart.',
-									'surecart'
-								),
-								value: 'all',
-							},
-							{
-								label: __(
-									'Any of these items are in the cart.',
-									'surecart'
-								),
-								value: 'any',
-							},
-							{
-								label: __(
-									'None of these items are in the cart.',
-									'surecart'
-								),
-								value: 'none',
-							},
-						]}
-						onScChange={(e) =>
-							updateFunnel({ filter_match_type: e.target.value })
+			<ScRadioGroup>
+				<div
+					css={css`
+						display: grid;
+						gap: 1em;
+						--sc-input-font-weight: var(--sc-font-weight-semibold);
+					`}
+				>
+					<ScRadio
+						checked={!funnel?.filter_match_type}
+						value=""
+						onClick={() =>
+							updateFunnel({
+								filter_match_type: null,
+							})
 						}
-					/>
+					>
+						{__('All purchases', 'surecart')}
+						<span slot="description">
+							{__(
+								'Show this upsell offer on all purchases.',
+								'surecart'
+							)}
+						</span>
+					</ScRadio>
+					<ScRadio
+						checked={!!funnel?.filter_match_type}
+						value="all"
+						onClick={() =>
+							updateFunnel({
+								filter_match_type: 'all',
+							})
+						}
+					>
+						{__('Specific purchases', 'surecart')}
+						<span slot="description">
+							{__(
+								'Select conditions to show this upsell.',
+								'surecart'
+							)}
+						</span>
+					</ScRadio>
+				</div>
+			</ScRadioGroup>
+
+			{shouldMatch && (
+				<>
+					{!empty && (
+						<ScSelect
+							label={__('Show Upsell Offer If', 'surecart')}
+							value={funnel?.filter_match_type}
+							choices={[
+								{
+									label: __(
+										'All of these items are in the cart.',
+										'surecart'
+									),
+									value: 'all',
+								},
+								{
+									label: __(
+										'Any of these items are in the cart.',
+										'surecart'
+									),
+									value: 'any',
+								},
+								{
+									label: __(
+										'None of these items are in the cart.',
+										'surecart'
+									),
+									value: 'none',
+								},
+							]}
+							onScChange={(e) =>
+								updateFunnel({
+									filter_match_type: e.target.value,
+								})
+							}
+						/>
+					)}
+
 					<Filters
 						label={__('Prices', 'surecart')}
 						type="filter_price_ids"
@@ -81,14 +140,16 @@ export default ({ loading, funnel, updateFunnel }) => {
 						item={funnel}
 						updateItem={updateFunnel}
 					/>
-				</>
-			) : (
-				<ScEmpty icon="zap">
-					{__(
-						'Add some conditions to display this funnel.',
-						'surecart'
+
+					{empty && (
+						<ScEmpty icon="zap">
+							{__(
+								'Add some conditions to display this upsell funnel.',
+								'surecart'
+							)}
+						</ScEmpty>
 					)}
-				</ScEmpty>
+				</>
 			)}
 
 			{newDialog && (
