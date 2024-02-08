b5c9891b5648f7bc74533f9d55678d5383c97c1c
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-08 17:27:21 -0600

    Formatting, escaping

--- app/src/Middleware/UpsellMiddleware.php
+++ app/src/Middleware/UpsellMiddleware.php
@@ -18,9 +18,9 @@ class UpsellMiddleware {
 	 */
 	public function handle( RequestInterface $request, Closure $next ) {
 		if ( empty( $request->query( 'sc_checkout_id' ) ) && ! current_user_can( 'edit_sc_orders' ) ) {
-			wp_die( __( 'You do not have permission to access this page.', 'surecart' ), __( 'Error', 'surecart' ), [ 'response' => 403 ] );
+			wp_die( esc_html__( 'You do not have permission to access this page.', 'surecart' ), esc_html__( 'Error', 'surecart' ), [ 'response' => 403 ] );
 		}
 
-		return $next($request);
+		return $next( $request );
 	}
 }
