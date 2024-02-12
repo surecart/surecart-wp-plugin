6b5f43c5f524f62ddda04f97cfbb8f976e109f35
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-12 13:29:24 -0600

    Fixes SUR-173

--- packages/admin/bumps/modules/Priority.js
+++ packages/admin/bumps/modules/Priority.js
@@ -1,23 +1,48 @@
-import { ScInput } from '@surecart/components-react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { RangeControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import Box from '../../ui/Box';
 
 export default ({ bump, updateBump, loading }) => {
 	return (
 		<Box title={__('Priority', 'surecart')} loading={loading}>
-			<ScInput
-				label={__('Bump Priority (1-5)')}
-				help={__(
-					'The priority of this bump in relation to other bumps. The higher the number, the higher the priority.'
-				)}
-				type="number"
-				min="1"
-				max="5"
-				step="1"
+			<RangeControl
+				label={__('Priority', 'surecart')}
+				hideLabelFromVision={true}
+				css={css`
+					padding: 0 15px 15px;
+					span.components-range-control__marks {
+						margin-top: 15px;
+					}
+				`}
+				min={1}
+				max={5}
+				__next40pxDefaultSize={true}
+				__nextHasNoMarginBottom={true}
+				withInputField={false}
+				marks={[
+					{
+						value: 1,
+						label: __('Low', 'surecart'),
+					},
+					{
+						value: 2,
+					},
+					{
+						value: 3,
+						label: __('Medium', 'surecart'),
+					},
+					{
+						value: 4,
+					},
+					{
+						value: 5,
+						label: __('High', 'surecart'),
+					},
+				]}
 				value={bump?.priority}
-				onScInput={(e) =>
-					updateBump({ priority: parseInt(e.target.value) })
-				}
+				onChange={(value) => updateBump({ priority: value })}
 			/>
 		</Box>
 	);
