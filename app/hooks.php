85302cb50119a20037a7cc92e9b80f5b96a3071f
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-12 09:46:42 -0600

    Add some other development functions

--- app/hooks.php
+++ app/hooks.php
@@ -89,8 +89,11 @@ add_filter(
 		 */
 		$settings['render_callback'] = static function ( $attributes, $content, $block ) use ( $controller_path, $metadata ) {
 			$view = require $controller_path;
-			if ( empty( $view ) ) {
-				return '';
+
+			// if not using 'file:', then it's content output.
+			$view_path = remove_block_asset_path_prefix( $view );
+			if ( $view_path === $view ) {
+				return $view;
 			}
 
 			$template_path = wp_normalize_path(
@@ -110,3 +113,43 @@ add_filter(
 	10,
 	2
 );
+
+/**
+ * Adds CSS classes and inline styles for colors to the incoming attributes array.
+ * This will be applied to the block markup in the front-end.
+ *
+ * @since 5.6.0
+ * @since 6.1.0 Implemented the style engine to generate CSS and classnames.
+ * @access private
+ *
+ * @param  WP_Block_Type $block_type       Block type.
+ * @param  array         $block_attributes Block attributes.
+ *
+ * @return array Colors CSS classes and inline styles.
+ */
+function sc_get_color_styles( $block_attributes, $types = [ 'text', 'background', 'gradient' ] ) {
+	$color_block_styles = array();
+
+	// Text colors.
+	if ( in_array( 'text', $types, true ) ) {
+		$preset_text_color          = array_key_exists( 'textColor', $block_attributes ) ? "var:preset|color|{$block_attributes['textColor']}" : null;
+		$custom_text_color          = isset( $block_attributes['style']['color']['text'] ) ? $block_attributes['style']['color']['text'] : null;
+		$color_block_styles['text'] = $preset_text_color ? $preset_text_color : $custom_text_color;
+	}
+
+	// Background colors.
+	if ( in_array( 'background', $types, true ) ) {
+		$preset_background_color          = array_key_exists( 'backgroundColor', $block_attributes ) ? "var:preset|color|{$block_attributes['backgroundColor']}" : null;
+		$custom_background_color          = isset( $block_attributes['style']['color']['background'] ) ? $block_attributes['style']['color']['background'] : null;
+		$color_block_styles['background'] = $preset_background_color ? $preset_background_color : $custom_background_color;
+	}
+
+	// Gradients.
+	if ( in_array( 'gradient', $types, true ) ) {
+		$preset_gradient_color          = array_key_exists( 'gradient', $block_attributes ) ? "var:preset|gradient|{$block_attributes['gradient']}" : null;
+		$custom_gradient_color          = isset( $block_attributes['style']['color']['gradient'] ) ? $block_attributes['style']['color']['gradient'] : null;
+		$color_block_styles['gradient'] = $preset_gradient_color ? $preset_gradient_color : $custom_gradient_color;
+	}
+
+	return wp_style_engine_get_styles( array( 'color' => $color_block_styles ), array( 'convert_vars_to_classnames' => true ) );
+}
