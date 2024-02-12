a02cfd1236906d77d5505e851098cb3b223cc179
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-12 15:49:08 -0600

    Simplify license-item

--- packages/admin/components/data-tables/licenses-data-table/license-item.js
+++ packages/admin/components/data-tables/licenses-data-table/license-item.js
@@ -1,81 +1,52 @@
-/** @jsx jsx */
-import { css, jsx } from '@emotion/core';
-
 /**
  * External dependencies.
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
+import { addQueryArgs } from '@wordpress/url';
 import { ScButton } from '@surecart/components-react';
 
 /**
  * Internal dependencies.
  */
-import { getFeaturedProductMediaAttributes } from '@surecart/components';
-import { ScFormatNumber, ScTag } from '@surecart/components-react';
-import { intervalString } from '../../../util/translations';
+import ProductLineItem from '../../../ui/ProductLineItem';
 
 export default (license) => {
-	const { purchase } = license;
-	const { id, variant, price } = purchase;
-	const product = purchase.product;
-	const media = getFeaturedProductMediaAttributes(product);
-	const variantLabel = [
-		variant?.option_1,
-		variant?.option_2,
-		variant?.option_3,
-	]
-		.filter(Boolean)
-		.join(' / ');
+	const {
+		purchase: { id, variant, price, product },
+	} = license;
 
 	return {
 		item: (
-			<sc-line-item key={id}>
-				{(!!media?.url || variant?.image_url) && (
-					<img
-						src={variant?.image_url || media.url}
-						alt={media.alt}
-						{...(media.title ? { title: media.title } : {})}
-						slot="image"
-					/>
-				)}
-				<span slot="title">{product?.name}</span>
-				<span className="product__description" slot="description">
-					{variantLabel && (
-						<span
-							css={css`
-								display: block;
-							`}
-						>
-							{variantLabel}
-						</span>
-					)}
-					{price?.amount && (
-						<>
-							<ScFormatNumber
-								type="currency"
-								currency={price?.currency || 'usd'}
-								value={
-									!!price?.ad_hoc && ad_hoc_amount
-										? ad_hoc_amount
-										: price?.amount
-								}
-							/>
-							{intervalString(price)}
-						</>
-					)}
-					<ScTag type="info" style={{ minWidth: 'max-content' }}>
-						{sprintf(
-							__('%1s of %2s Activations Used'),
-							parseInt(license?.activations_count || 0),
-							parseInt(license?.activation_limit) || '∞'
-						)}
-					</ScTag>
-				</span>
-			</sc-line-item>
+			<ProductLineItem
+				key={id}
+				lineItem={{
+					price: {
+						...price,
+						product,
+					},
+					variant,
+					variant_options: [
+						variant?.option_1,
+						variant?.option_2,
+						variant?.option_3,
+					],
+				}}
+			/>
 		),
 		actions: (
-			<ScButton onClick={() => window.location.href = `admin.php?page=sc-licenses&action=edit&id=${license?.id}`} size="small">
-				{__('View more', 'surecart')}
+			<ScButton
+				onClick={() =>
+					window.location.assign(
+						addQueryArgs('admin.php', {
+							page: 'sc-licenses',
+							action: 'edit',
+							id: license?.id,
+						})
+					)
+				}
+				size="small"
+			>
+				{__('View', 'surecart')}
 			</ScButton>
 		),
 	};
