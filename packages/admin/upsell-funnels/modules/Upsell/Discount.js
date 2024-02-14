d70bcfd97337a76bebf009cd73548640d9dc9416
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-14 15:26:36 -0600

    Refactor Upsell/Discount component to use useRef for input focus

--- packages/admin/upsell-funnels/modules/Upsell/Discount.js
+++ packages/admin/upsell-funnels/modules/Upsell/Discount.js
@@ -2,7 +2,7 @@
  * External dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useRef, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies.
@@ -16,6 +16,7 @@ import {
 
 export default ({ upsell, onUpdate }) => {
 	const [type, setType] = useState('none');
+	const inputRef = useRef(null);
 
 	useEffect(() => {
 		if (upsell?.amount_off) {
@@ -29,39 +30,47 @@ export default ({ upsell, onUpdate }) => {
 		}
 	}, [upsell?.percent_off]);
 
-	useEffect(() => {
-		if ('none' === type) {
-			onUpdate({
-				amount_off: null,
-				percent_off: null,
-			});
-		}
-	}, [type]);
-
 	return (
 		<>
 			<ScRadioGroup label={__('Discount', 'surecart')}>
 				<ScRadio
 					checked={type === 'none'}
-					onClick={() => setType('none')}
+					onClick={() => {
+						setType('none');
+						onUpdate({
+							amount_off: null,
+							percent_off: null,
+						});
+					}}
 				>
 					{__('No discount', 'surecart')}
 				</ScRadio>
 				<ScRadio
 					checked={type === 'percentage'}
-					onClick={() => setType('percentage')}
+					onClick={() => {
+						setType('percentage');
+						setTimeout(() => {
+							inputRef.current.triggerFocus();
+						});
+					}}
 				>
 					{__('Percentage discount', 'surecart')}
 				</ScRadio>
 				<ScRadio
 					checked={type === 'fixed'}
-					onClick={() => setType('fixed')}
+					onClick={() => {
+						setType('fixed');
+						setTimeout(() => {
+							inputRef.current.triggerFocus();
+						});
+					}}
 				>
 					{__('Fixed amount discount', 'surecart')}
 				</ScRadio>
 			</ScRadioGroup>
 			{type === 'percentage' && (
 				<ScInput
+					ref={inputRef}
 					label={__('Percentage off', 'surecart')}
 					className="sc-percent-off"
 					type="number"
@@ -84,6 +93,7 @@ export default ({ upsell, onUpdate }) => {
 			)}
 			{type === 'fixed' && (
 				<ScPriceInput
+					ref={inputRef}
 					label={__('Discount amount', 'surecart')}
 					className="sc-amount-off"
 					currencyCode={scData?.currency}
