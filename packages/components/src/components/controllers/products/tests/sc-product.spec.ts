6262ec1f5e0bbfa33c35d430479d048206573a24
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-02-20 16:41:20 +0600

    Refactorred test-cases to run in one test for Product list page

--- packages/components/src/components/controllers/products/tests/sc-product.spec.ts
+++ packages/components/src/components/controllers/products/tests/sc-product.spec.ts
@@ -6,25 +6,19 @@ import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 const API_BASE_PATH = '/surecart/v1/products';
 const API_MEDIA_PATH = '/surecart/v1/product_medias';
 const API_PRICE_PATH = '/surecart/v1/prices';
-const API_PRODUCT_COLLECTIO_PATH = '/surecart/v1/product_collections';
+const API_PRODUCT_COLLECTION_PATH = '/surecart/v1/product_collections';
 
 test.describe('Product', () => {
-  let product1 = null;
-  let product2 = null;
-  const products = [];
-
   let collection1 = null;
   let collection2 = null;
-  const collections = [];
 
   test.beforeAll(async ({ requestUtils, admin }) => {
     // Insert some product collections.
-    collection1 = createProductCollection(requestUtils, 'Collection 1');
-    collection2 = createProductCollection(requestUtils, 'Collection 2');
-    collections.push(collection1, collection2);
+    collection1 = await createProductCollection(requestUtils, 'Collection 1');
+    collection2 = await createProductCollection(requestUtils, 'Collection 2');
 
     // Insert some products.
-    product1 = createProduct(requestUtils, {
+    await createProduct(requestUtils, {
       name: 'Product 1',
       status: 'published',
       amount: 1000,
@@ -32,17 +26,13 @@ test.describe('Product', () => {
       product_collection_ids: [collection1.id],
     });
 
-    // Wait for 1 second, so that sorting will be different.
-    setTimeout(() => {
-      product2 = createProduct(requestUtils, {
-        name: 'Product 2',
-        status: 'published',
-        amount: 2000,
-        scratch_amount: 3000,
-        product_collection_ids: [collection2.id],
-      });
-    }, 1000);
-    products.push(product1, product2);
+    await createProduct(requestUtils, {
+      name: 'Product 2',
+      status: 'published',
+      amount: 2000,
+      scratch_amount: 3000,
+      product_collection_ids: [collection2.id],
+    });
 
     // Go to permalink settings page and set the shop page.
     await admin.page.goto('/wp-admin/options-permalink.php');
@@ -54,141 +44,77 @@ test.describe('Product', () => {
     await admin.page.locator('input[type="submit"]').click();
   });
 
-  test('Should render product list page', async ({ page }) => {
+  test('Product page - Product List', async ({ page }) => {
     await page.goto('/shop');
-    await page.waitForTimeout(3000);
+    await page.waitForLoadState('networkidle');
 
+    // Test: Check if the Shop heading is showing.
     await expect(page.locator('h1')).toHaveText('Shop');
 
-    // Check if Product 1 and Product 2 are showing in the list.
-    const productTitles = await page.locator('sc-product-item-title');
+    // Test: Check if Product 1 and Product 2 are showing in the list.
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 2');
+    await expect(await page.locator('sc-product-item-title').nth(1).innerText()).toBe('Product 1');
 
-    // Get the text content of each element
-    const firstProductText = await productTitles.nth(0).innerText();
-    const secondProductText = await productTitles.nth(1).innerText();
-
-    // Check if Product 1 and Product 2 are showing in the list
-    await expect(firstProductText).toBe('Product 2'); // It should be descending order - Latest first.
-    await expect(secondProductText).toBe('Product 1');
-  });
-
-  test('Should search product', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
-
+    // Test: Search Product list.
     await page.getByPlaceholder('Search').fill('Product 2');
     await page.click('.search-button');
-    await page.waitForTimeout(3000);
+    await page.waitForLoadState('networkidle');
 
-    // Check if Product 2 is showing in the list.
-    const productTitlesAfterSearch = await page.locator('sc-product-item-title');
-    const firstProductTextAfterSearch = await productTitlesAfterSearch.innerText();
-    await expect(firstProductTextAfterSearch).toBe('Product 2');
-  });
+    // Test: if Product 2 is showing in the list.
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 2');
 
-  test('Should clear search product', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
-
-    await page.getByPlaceholder('Search').fill('Product 2');
-    await page.click('.search-button');
-    await page.waitForTimeout(3000);
-
+    // Test: Clear search.
     await page.click('.tag--clearable');
-    await page.waitForTimeout(3000);
-    await page.getByPlaceholder('Search').fill('Other');
-    await page.click('.search-button');
-    await page.waitForTimeout(3000);
-  });
+    await page.waitForLoadState('networkidle');
 
-  test('Sort Product by order', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
-
-    // Test with Alphabetical ascending sorting.
-    await page.click('.product-item-list__sort sc-dropdown');
-    await page.click('text=Alphabetical, A-Z');
-    await page.waitForTimeout(3000);
-
-    // Check if Product 1 and Product 2 are showing in the list.
-    const productTitles = await page.locator('sc-product-item-title');
-
-    // Get the text content of each element
-    const firstProductText = await productTitles.nth(0).innerText();
-    const secondProductText = await productTitles.nth(1).innerText();
-
-    // Check if Product 1 and Product 2 are showing in the list
-    await expect(firstProductText).toBe('Product 1');
-    await expect(secondProductText).toBe('Product 2');
-
-    // Test with Alphabetical descending sorting.
-    await page.locator('.product-item-list__sort sc-dropdown').getByText('Dropdown to sort products. Alphabetical, A-Z selected.LatestOldestAlphabetical, ').click();
+    // Test: Alphabetical descending sorting.
+    await page.locator('.product-item-list__sort sc-dropdown').nth(0).click();
     await page.click('text=Alphabetical, Z-A');
-    await page.waitForTimeout(3000);
-    // await page.waitForTimeout(3000);
-    const productTitlesAfterAlphaDescendingSort = await page.locator('sc-product-item-title');
-    const firstProductTextAfterAlphaDescendingSort = await productTitlesAfterAlphaDescendingSort.nth(0).innerText();
-    const secondProductTextAfterAlphaDescendingSort = await productTitlesAfterAlphaDescendingSort.nth(1).innerText();
-    await expect(firstProductTextAfterAlphaDescendingSort).toBe('Product 2');
-    await expect(secondProductTextAfterAlphaDescendingSort).toBe('Product 1');
-  });
+    await page.waitForResponse(resp => resp.url().includes('surecart/v1/products'));
 
-  test('Should see product price', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
+    // Test: if Product 1 and Product 2 are showing in the list in the Z-A order.
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 2');
+    await expect(await page.locator('sc-product-item-title').nth(1).innerText()).toBe('Product 1');
 
-    // Check if Product 1 and Product 2 are showing in the list.
-    const productPrices = await page.locator('sc-format-number');
+    // Test: Alphabetical ascending sorting.
+    await page.locator('.product-item-list__sort sc-dropdown').nth(0).click();
+    await page.click('text=Alphabetical, A-Z');
+    await page.waitForResponse(resp => resp.url().includes('surecart/v1/products'));
+    await page.waitForLoadState('networkidle');
 
-    // Get the text content of each element
-    const firstProductPrice = await productPrices.nth(0).innerText();
-    const secondProductPrice = await productPrices.nth(1).innerText();
+    // Test: if Product 1 and Product 2 are showing in the list in the A-Z order.
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 1');
+    await expect(await page.locator('sc-product-item-title').nth(1).innerText()).toBe('Product 2');
 
-    await expect(firstProductPrice).toBe('$10');
-    await expect(secondProductPrice).toBe('$20');
-  });
+    // Test: if Product 1 and Product 2 are showing in the list.
+    await expect(await page.locator('sc-format-number').nth(0).innerText()).toBe('$10');
+    await expect(await page.locator('sc-format-number').nth(1).innerText()).toBe('$20');
 
-  test('Should see product image', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
+    // Test: Product image.
+    // Check if Product 1 is showing in the list with the image.
+    await expect(await page.locator('sc-product-item-image .product-img img').nth(0).getAttribute('src')).toBe('https://placehold.co/600x400/EEE/31343C');
+    await expect(await page.locator('sc-product-item-image .product-img img').nth(1).getAttribute('src')).not.toBeNull();
 
-    // Check if Product 1 and Product 2 are showing in the list.
-    const productImages = await page.locator('sc-product-item-image .product-img img');
-
-    // Get the text content of each element
-    const firstProductImage = await productImages.nth(0).getAttribute('src');
-    await expect(firstProductImage).toBe('https://placehold.co/600x400/EEE/31343C');
-  });
-
-  test('Should filter by product collection', async ({ page }) => {
-    await page.goto('/shop');
-    await page.waitForTimeout(3000);
-
-    const collectionFilters = await page.locator('.product-item-list__sort sc-dropdown');
-
-    // Get the text content of each element
-    await collectionFilters.nth(1).click();
+    // Test: filter by product collection.
+    await page.locator('.product-item-list__sort sc-dropdown').nth(1).click();
 
     // Click on Collection 1.
     await page.click('text=Collection 1');
-    await page.waitForTimeout(3000);
+    await page.waitForResponse(resp => resp.url().includes('surecart/v1/products'));
 
     // Check if Product 1 is showing in the list.
-    const productTitles = await page.locator('sc-product-item-title');
-    const firstProductText = await productTitles.innerText();
-    await expect(firstProductText).toBe('Product 1');
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 1');
 
     // Clear Collection filter.
     await page.locator('sc-tag').getByText('Collection 1', { exact: true }).click();
-    await page.waitForTimeout(3000);
-    const productTitlesAfterClearFilter = await page.locator('sc-product-item-title');
-    const firstProductTextAfterClearFilter = await productTitlesAfterClearFilter.innerText();
-    await expect(firstProductTextAfterClearFilter).toBe('Product 1');
+    await page.waitForResponse(resp => resp.url().includes('surecart/v1/products'));
+
+    await expect(await page.locator('sc-product-item-title').nth(0).innerText()).toBe('Product 1'); // As already in Ascending order.
+    await expect(await page.locator('sc-product-item-title').nth(1).innerText()).toBe('Product 2');
   });
 
-  test('Product detil page - Should visible all default block fields', async ({ page }) => {
+  test('Product page - Product Detail', async ({ page }) => {
     await page.goto('/products/product-1');
-    await page.waitForTimeout(3000);
 
     // Product 1 heading text.
     await expect(page.locator('h1')).toHaveText('Product 1');
@@ -211,17 +137,15 @@ test.describe('Product', () => {
     await expect(buyButtonText).toBe('Buy Now');
   });
 
-  test('Product collection page', async ({ page }) => {
+  test('Product page - Product Collection', async ({ page }) => {
     await page.goto('/collections/collection-1');
+    await page.waitForResponse(resp => resp.url().includes('surecart/v1/products'));
     await page.waitForTimeout(3000);
 
-    await expect(page.locator('h2')).toHaveText('Collection 1');
-    await page.waitForTimeout(3000);
+    await expect(page.getByRole('heading').getByText('Collection 1', { exact: true })).toBeVisible();
 
     // Check if Product 1 are showing in the list.
-    const productTitles = await page.locator('sc-product-item-title');
-    const firstProductText = await productTitles.innerText();
-    await expect(firstProductText).toBe('Product 1');
+    await expect(page.locator('sc-product-item-title').nth(0).getByText('Product 1', { exact: true })).toBeVisible();
   });
 });
 
@@ -229,18 +153,22 @@ export const createProductCollection = async (requestUtils, collectionName) => {
   try {
     return await requestUtils.rest({
       method: 'POST',
-      path: API_PRODUCT_COLLECTIO_PATH,
+      path: API_PRODUCT_COLLECTION_PATH,
       data: {
         name: collectionName,
       },
     });
   } catch (error) {
-    if (error.response?.data?.code === 'product_collection.name.taken') {
-      // Fetch and return the existing collection.
-      return await requestUtils.rest({
-        method: 'GET',
-        path: API_PRODUCT_COLLECTIO_PATH + '?query=' + collectionName,
-      });
+    // If collection already exists, return the existing collection.
+    if (error?.code === 'product_collection.invalid') {
+      const collections = await requestUtils
+        .rest({
+          method: 'GET',
+          path: API_PRODUCT_COLLECTION_PATH + '?query=' + collectionName,
+        })
+        .catch(() => []);
+
+      return collections?.[0] || null;
     }
   }
 };
