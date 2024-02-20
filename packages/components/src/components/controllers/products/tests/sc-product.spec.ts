ef8f144d0b2e3276e2f60e8eca940748e99a4b7a
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-02-20 11:54:59 +0600

    Refactorred tests

--- packages/components/src/components/controllers/products/tests/sc-product.spec.ts
+++ packages/components/src/components/controllers/products/tests/sc-product.spec.ts
@@ -4,53 +4,61 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
 
 const API_BASE_PATH = '/surecart/v1/products';
+const API_MEDIA_PATH = '/surecart/v1/product_medias';
 const API_PRICE_PATH = '/surecart/v1/prices';
-const API_PRODUCT_COLLECTION_BASE_PATH = '/surecart/v1/product_collections';
+const API_PRODUCT_COLLECTIO_PATH = '/surecart/v1/product_collections';
 
-test.describe('Product List Page', () => {
-  const products = [];
+test.describe('Product', () => {
   let product1 = null;
   let product2 = null;
+  const products = [];
 
   let collection1 = null;
   let collection2 = null;
-  let collections = [];
+  const collections = [];
 
-  test.beforeAll(async ({ requestUtils }) => {
+  test.beforeAll(async ({ requestUtils, admin }) => {
     // Insert some product collections.
-    // collection1 = createProductCollection(requestUtils, 'Collection 1');
-    // collection2 = createProductCollection(requestUtils, 'Collection 2');
-    // collections = [collection1, collection2];
-    // Insert some products.
-    // product1 = createProduct(requestUtils, {
-    //   name: 'Product 1',
-    //   status: 'published',
-    //   amount: 1000,
-    //   image_url: 'https://placehold.co/600x400/EEE/31343C',
-    //   product_collection_ids: [collection1.id],
-    // });
-    // Wait for 1 second, so that sorting will be different.
-    // setTimeout(() => {
-    //   product2 = createProduct(requestUtils, {
-    //     name: 'Product 2',
-    //     status: 'published',
-    //     amount: 2000,
-    //     scratch_amount: 3000,
-    //     product_collection_ids: [collection2.id],
-    //   });
-    // }, 1000);
-    //   products.push(product1, product2);
-  });
+    collection1 = createProductCollection(requestUtils, 'Collection 1');
+    collection2 = createProductCollection(requestUtils, 'Collection 2');
+    collections.push(collection1, collection2);
 
-  // Fetch the shop page ID.
-  // ?p=
-  // Pagename key wise option table fetch.
+    // Insert some products.
+    product1 = createProduct(requestUtils, {
+      name: 'Product 1',
+      status: 'published',
+      amount: 1000,
+      image_url: 'https://placehold.co/600x400/EEE/31343C',
+      product_collection_ids: [collection1.id],
+    });
+
+    // Wait for 1 second, so that sorting will be different.
+    setTimeout(() => {
+      product2 = createProduct(requestUtils, {
+        name: 'Product 2',
+        status: 'published',
+        amount: 2000,
+        scratch_amount: 3000,
+        product_collection_ids: [collection2.id],
+      });
+    }, 1000);
+    products.push(product1, product2);
+
+    // Go to permalink settings page and set the shop page.
+    await admin.page.goto('/wp-admin/options-permalink.php');
+
+    // Click on Post name radio button permalink.
+    await admin.page.getByText('Post name').click();
+
+    // Click on Save Changes button.
+    await admin.page.locator('input[type="submit"]').click();
+  });
 
   test('Should render product list page', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     await expect(page.locator('h1')).toHaveText('Shop');
-    await page.waitForLoadState('networkidle');
 
     // Check if Product 1 and Product 2 are showing in the list.
     const productTitles = await page.locator('sc-product-item-title');
@@ -66,10 +74,10 @@ test.describe('Product List Page', () => {
 
   test('Should search product', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     await page.getByPlaceholder('Search').fill('Product 2');
     await page.click('.search-button');
-    // await page.waitForLoadState('networkidle')
     await page.waitForTimeout(3000);
 
     // Check if Product 2 is showing in the list.
@@ -80,22 +88,27 @@ test.describe('Product List Page', () => {
 
   test('Should clear search product', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
-    await page.click('.tag--clearable'); // TODO: Should be accessible.
-    await page.waitForLoadState('networkidle');
+    await page.getByPlaceholder('Search').fill('Product 2');
+    await page.click('.search-button');
+    await page.waitForTimeout(3000);
+
+    await page.click('.tag--clearable');
+    await page.waitForTimeout(3000);
     await page.getByPlaceholder('Search').fill('Other');
     await page.click('.search-button');
-    // await page.waitForLoadState('networkidle');
     await page.waitForTimeout(3000);
   });
 
   test('Sort Product by order', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     // Test with Alphabetical ascending sorting.
     await page.click('.product-item-list__sort sc-dropdown');
     await page.click('text=Alphabetical, A-Z');
-    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(3000);
 
     // Check if Product 1 and Product 2 are showing in the list.
     const productTitles = await page.locator('sc-product-item-title');
@@ -109,9 +122,9 @@ test.describe('Product List Page', () => {
     await expect(secondProductText).toBe('Product 2');
 
     // Test with Alphabetical descending sorting.
-    await page.click('.product-item-list__sort sc-dropdown');
+    await page.locator('.product-item-list__sort sc-dropdown').getByText('Dropdown to sort products. Alphabetical, A-Z selected.LatestOldestAlphabetical, ').click();
     await page.click('text=Alphabetical, Z-A');
-    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(3000);
     // await page.waitForTimeout(3000);
     const productTitlesAfterAlphaDescendingSort = await page.locator('sc-product-item-title');
     const firstProductTextAfterAlphaDescendingSort = await productTitlesAfterAlphaDescendingSort.nth(0).innerText();
@@ -122,6 +135,7 @@ test.describe('Product List Page', () => {
 
   test('Should see product price', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     // Check if Product 1 and Product 2 are showing in the list.
     const productPrices = await page.locator('sc-format-number');
@@ -136,6 +150,7 @@ test.describe('Product List Page', () => {
 
   test('Should see product image', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     // Check if Product 1 and Product 2 are showing in the list.
     const productImages = await page.locator('sc-product-item-image .product-img img');
@@ -147,6 +162,7 @@ test.describe('Product List Page', () => {
 
   test('Should filter by product collection', async ({ page }) => {
     await page.goto('/shop');
+    await page.waitForTimeout(3000);
 
     const collectionFilters = await page.locator('.product-item-list__sort sc-dropdown');
 
@@ -155,7 +171,7 @@ test.describe('Product List Page', () => {
 
     // Click on Collection 1.
     await page.click('text=Collection 1');
-    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(3000);
 
     // Check if Product 1 is showing in the list.
     const productTitles = await page.locator('sc-product-item-title');
@@ -164,16 +180,15 @@ test.describe('Product List Page', () => {
 
     // Clear Collection filter.
     await page.locator('sc-tag').getByText('Collection 1', { exact: true }).click();
-    await page.waitForLoadState('networkidle');
+    await page.waitForTimeout(3000);
     const productTitlesAfterClearFilter = await page.locator('sc-product-item-title');
     const firstProductTextAfterClearFilter = await productTitlesAfterClearFilter.innerText();
     await expect(firstProductTextAfterClearFilter).toBe('Product 1');
   });
-});
 
-test.describe('Product Detail Page', () => {
-  test('Should visible all default block fields', async ({ page }) => {
-    await page.goto('/shop/product-1');
+  test('Product detil page - Should visible all default block fields', async ({ page }) => {
+    await page.goto('/products/product-1');
+    await page.waitForTimeout(3000);
 
     // Product 1 heading text.
     await expect(page.locator('h1')).toHaveText('Product 1');
@@ -183,9 +198,6 @@ test.describe('Product Detail Page', () => {
 
     // Product 1 image.
     await expect(page.locator('.wp-block-surecart-product-media img')).toHaveAttribute('src', 'https://placehold.co/600x400/EEE/31343C');
-
-    // Product 1 Collection badge.
-    // await expect(page.locator('.sc-product-collection-badge')).toHaveText('Collection 1');
 
     // Quantity Selection is Present there.
     await expect(page.locator('sc-quantity-select')).toBeVisible();
@@ -198,52 +210,93 @@ test.describe('Product Detail Page', () => {
     await expect(addToCartButtonText).toBe('Add To Cart');
     await expect(buyButtonText).toBe('Buy Now');
   });
+
+  test('Product collection page', async ({ page }) => {
+    await page.goto('/collections/collection-1');
+    await page.waitForTimeout(3000);
+
+    await expect(page.locator('h2')).toHaveText('Collection 1');
+    await page.waitForTimeout(3000);
+
+    // Check if Product 1 are showing in the list.
+    const productTitles = await page.locator('sc-product-item-title');
+    const firstProductText = await productTitles.innerText();
+    await expect(firstProductText).toBe('Product 1');
+  });
 });
 
-export const createProductCollection = (requestUtils, collectionName) => {
-  return requestUtils.rest({
-    method: 'POST',
-    path: API_PRODUCT_COLLECTION_BASE_PATH,
-    data: {
-      name: collectionName,
-    },
-  });
+export const createProductCollection = async (requestUtils, collectionName) => {
+  try {
+    return await requestUtils.rest({
+      method: 'POST',
+      path: API_PRODUCT_COLLECTIO_PATH,
+      data: {
+        name: collectionName,
+      },
+    });
+  } catch (error) {
+    if (error.response?.data?.code === 'product_collection.name.taken') {
+      // Fetch and return the existing collection.
+      return await requestUtils.rest({
+        method: 'GET',
+        path: API_PRODUCT_COLLECTIO_PATH + '?query=' + collectionName,
+      });
+    }
+  }
 };
 
-export const createProduct = (requestUtils, data) => {
-  const product = requestUtils.rest({
-    method: 'POST',
-    path: API_BASE_PATH,
-    data: {
-      name: data.name,
-      status: data.status,
-      product_collection_ids: data?.product_collection_ids || [],
-    },
+export const createProduct = async (requestUtils, data) => {
+  let product = null;
+  let needsCreatePriceImage = true;
+
+  // Check first if same product is found.
+  const existingProduct = await requestUtils.rest({
+    method: 'GET',
+    path: API_BASE_PATH + '?query=' + data.name,
   });
 
-  // Create a product media.
-  // if (data.image_url) {
-  //   requestUtils.rest({
-  //     method: 'POST',
-  //     path: `surecart/v1/product_medias`,
-  //     data: {
-  //       product_id: product.id,
-  //       url: data.image_url,
-  //     },
-  //   });
-  // }
+  if (existingProduct.length) {
+    product = existingProduct[0];
+    needsCreatePriceImage = false;
+  }
 
-  // Create price.
-  // requestUtils.rest({
-  //     method: 'POST',
-  //     path: API_PRICE_PATH,
-  //     data: {
-  //         product: product.id,
-  //         name: data?.price_name || 'One Time',
-  //         amount: data?.amount || 0,
-  //         ...(data?.scratch_amount && { scratch_amount: data.scratch_amount }),
-  //     },
-  // });
+  if (!product) {
+    product = await requestUtils.rest({
+      method: 'POST',
+      path: API_BASE_PATH,
+      data: {
+        name: data.name,
+        status: data.status,
+        product_collections: data?.product_collection_ids || [],
+      },
+    });
+  }
+
+  // Create a product media.
+  if (data.image_url && needsCreatePriceImage) {
+    await requestUtils.rest({
+      method: 'POST',
+      path: API_MEDIA_PATH,
+      data: {
+        product_id: product.id,
+        url: data.image_url,
+      },
+    });
+  }
+
+  // Create a product price.
+  if (needsCreatePriceImage) {
+    await requestUtils.rest({
+      method: 'POST',
+      path: API_PRICE_PATH,
+      data: {
+        amount: data?.amount || '0',
+        product: product.id,
+        name: data?.price_name || 'One Time',
+        ...(data?.scratch_amount && { scratch_amount: data.scratch_amount }),
+      },
+    });
+  }
 
   return product;
 };
