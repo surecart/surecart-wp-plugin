b842465106875826d0ceff48205ef83674de8993
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-02-27 14:50:57 +0600

    Fixed SUR-367 - Group collection overrides by collection

--- packages/admin/settings/tax-override/TaxOverrideList.js
+++ packages/admin/settings/tax-override/TaxOverrideList.js
@@ -14,18 +14,15 @@ import {
 	ScAlert,
 	ScButton,
 	ScCard,
-	ScDropdown,
 	ScEmpty,
-	ScFlex,
 	ScIcon,
-	ScMenu,
-	ScMenuItem,
 	ScStackedList,
 	ScStackedListRow,
 	ScText,
 } from '@surecart/components-react';
 import TaxOverrideModal from './TaxOverrideModal';
 import TaxOverrideDeleteModal from './TaxOverrideDeleteModal';
+import TaxOverrideRows from './TaxOverrideRows';
 import PrevNextButtons from '../../ui/PrevNextButtons';
 import usePagination from '../../hooks/usePagination';
 import { TAX_OVERRIDE_PER_PAGE } from './useTaxOverrides';
@@ -114,117 +111,13 @@ export default ({
 						) : (
 							<ScCard noPadding loading={fetching}>
 								<ScStackedList>
-									{taxOverrides.map((taxOverride) => {
-										if (!taxOverride) return null;
-										return (
-											<>
-												{/* Add a product collection name, if it's for product collection */}
-												{taxOverride?.product_collection
-													?.id && (
-													<ScStackedListRow
-														css={css`
-															--sc-list-row-background-color: var(
-																--sc-color-gray-50
-															);
-														`}
-													>
-														<ScFlex flexDirection="column">
-															<ScText
-																css={css`
-																	--font-weight: bold;
-																`}
-															>
-																{
-																	taxOverride
-																		?.product_collection
-																		.name
-																}
-															</ScText>
-														</ScFlex>
-													</ScStackedListRow>
-												)}
+									<TaxOverrideRows
+										type={type}
+										taxOverrides={taxOverrides}
+										onEdit={onEdit}
+										onRemove={onRemove}
+									/>
 
-												<ScStackedListRow>
-													<div
-														css={css`
-															display: flex;
-															flex-direction: column;
-															gap: var(
-																--sc-spacing-xx-small
-															);
-														`}
-													>
-														<ScText
-															css={css`
-																--font-weight: bold;
-															`}
-														>
-															{taxOverride
-																?.tax_zone
-																?.state_name ||
-																taxOverride
-																	?.tax_zone
-																	?.country_name}
-														</ScText>
-														<ScText>
-															{taxOverride?.rate ||
-																taxOverride
-																	?.tax_zone
-																	?.default_rate}
-															%
-														</ScText>
-													</div>
-
-													<ScDropdown
-														slot="suffix"
-														placement="bottom-end"
-													>
-														<ScButton
-															type="text"
-															slot="trigger"
-															circle
-														>
-															<ScIcon name="more-horizontal" />
-														</ScButton>
-														<ScMenu>
-															<ScMenuItem
-																onClick={() =>
-																	onEdit(
-																		taxOverride
-																	)
-																}
-															>
-																<ScIcon
-																	slot="prefix"
-																	name="edit"
-																/>
-																{__(
-																	'Edit',
-																	'surecart'
-																)}
-															</ScMenuItem>
-															<ScMenuItem
-																onClick={() =>
-																	onRemove(
-																		taxOverride
-																	)
-																}
-															>
-																<ScIcon
-																	slot="prefix"
-																	name="trash"
-																/>
-																{__(
-																	'Delete',
-																	'surecart'
-																)}
-															</ScMenuItem>
-														</ScMenu>
-													</ScDropdown>
-												</ScStackedListRow>
-											</>
-										);
-									})}
 									<ScStackedListRow>
 										<ScButton
 											type="default"
