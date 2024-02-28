ac41b7bd011b2572be89b23c77bf287b3419a009
Andre Gagnon <ajgagnon@uwalumni.com>
2024-02-28 12:11:42 -0600

    Add price interval and trial blocks

---
+++ packages/blocks-next/src/blocks/product-selected-price-trial/view.php
@@ -0,0 +1,3 @@
+<div <?php echo get_block_wrapper_attributes()?> data-wp-text="state.trialDisplayText" data-wp-bind--hidden="!state.trialDisplayText" hidden>
+	<?php  // TODO: get setup fee from model ?>
+</div>
