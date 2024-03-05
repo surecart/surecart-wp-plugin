4672405f8f6be7f5d3e7e1e12e941bc8f32a4fd1
Andre Gagnon <ajgagnon@uwalumni.com>
2024-03-05 14:50:39 -0600

    Refactor

--- app/src/Controllers/Web/CheckoutFormsController.php
+++ app/src/Controllers/Web/CheckoutFormsController.php
@@ -15,33 +15,31 @@ class CheckoutFormsController {
 	 * @param \SureCartCore\Requests\RequestInterface $request Request.
 	 */
 	public function changeMode( $request ) {
-		$form_post_id     = get_query_var( 'sc_checkout_change_mode' );
-		$checkout_post_id = $request->query( 'sc_checkout_post' ) ?? null;
+		$form_post_id       = get_query_var( 'sc_checkout_change_mode' );
+		$checkout_post_id   = $request->query( 'sc_checkout_post' ) ?? null;
+		$form_post          = get_post( $form_post_id );
+		$checkout_page_post = get_post( $checkout_post_id );
 
-		if ( ! $form_post_id || ! $checkout_post_id ) {
-			return;
+		if ( empty( $form_post ) || ! empty( $checkout_page_post ) ) {
+			wp_die( esc_html__( 'Invalid request.', 'surecart' ) );
 		}
 
-		$post = get_post( $form_post_id );
-		if ( ! $post ) {
-			return;
+		$form_block = \SureCart::post()->getFormBlock( $checkout_page_post );
+		if ( ! $form_block ) {
+			wp_die( esc_html__( 'There is no checkout form block on this page.', 'surecart' ) );
 		}
 
-		$checkout_form_post = \SureCart::blockModeSwitcher()->getBlockFromPost( $post );
-		if ( ! $checkout_form_post ) {
-			return;
-		}
-
-		$default_mode = $checkout_form_post['attrs']['mode'] ?? 'live';
+		// Get the mode.
+		$mode = $form_block['attrs']['mode'] ?? 'live';
 
 		// Change the mode.
-		$checkout_form_post['attrs']['mode'] = 'test' === $default_mode ? 'live' : 'test';
+		$form_block['attrs']['mode'] = 'test' === $mode ? 'live' : 'test';
 
 		// Update the post.
 		wp_update_post(
 			array(
 				'ID'           => $form_post_id,
-				'post_content' => serialize_blocks( array( $checkout_form_post ) ),
+				'post_content' => serialize_blocks( [ $form_block ] ),
 			)
 		);
 
