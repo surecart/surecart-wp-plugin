621f6341db38cd2fc3cdb5b702a5304d1d8614ce
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-03-07 13:23:05 +0600

    Added Affiliate request create UI

--- packages/admin/affiliation-requests/AffiliationRequest.js
+++ packages/admin/affiliation-requests/AffiliationRequest.js
@@ -3,11 +3,13 @@
  */
 import { useSelect } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies.
  */
 import EditAffiliationRequest from './EditAffiliationRequest';
+import CreateAffiliationRequest from './CreateAffiliationRequest';
 import { store } from '@surecart/data';
 
 /**
@@ -22,8 +24,34 @@ export function getEditURL(id) {
 }
 
 export default () => {
+	const [historyId, setHistoryId] = useState(null);
+
+	/**
+	 * Replaces the browser URL with a edit link for a given id ID.
+	 *
+	 * Note it is important that, since this function may be called when the
+	 * editor first loads, the result generated `getPostEditURL` matches that
+	 * produced by the server. Otherwise, the URL will change unexpectedly.
+	 *
+	 * @param {number} id id for the model for which to generate edit URL.
+	 */
+	const setBrowserURL = (id) => {
+		window.history.replaceState({ id }, 'Post ' + id, getEditURL(id));
+		setHistoryId(id);
+	};
+
+	const setId = (id) => {
+		if (id && id !== historyId) {
+			setBrowserURL(id);
+		}
+	};
+
 	// Get the id from the page url.
 	const id = useSelect((select) => select(store).selectPageId());
 
-	return id ? <EditAffiliationRequest id={id} /> : null;
+	return id ? (
+		<EditAffiliationRequest id={id} />
+	) : (
+		<CreateAffiliationRequest setId={setId} />
+	);
 };
---
+++ packages/admin/affiliation-requests/CreateAffiliationRequest.js
@@ -0,0 +1,169 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+/**
+ * External dependencies.
+ */
+import { __ } from '@wordpress/i18n';
+import { store as coreStore } from '@wordpress/core-data';
+import { useDispatch } from '@wordpress/data';
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies.
+ */
+import {
+	ScButton,
+	ScForm,
+	ScFormRow,
+	ScInput,
+	ScTextarea,
+} from '@surecart/components-react';
+import Box from '../ui/Box';
+import Error from '../components/Error';
+import CreateTemplate from '../templates/CreateModel';
+
+export default ({ id, setId }) => {
+	const [isSaving, setIsSaving] = useState(false);
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [payoutEmail, setPayoutEmail] = useState('');
+	const [bio, setBio] = useState('');
+	const [error, setError] = useState(null);
+	const { saveEntityRecord } = useDispatch(coreStore);
+
+	// Create the affiliation request.
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setIsSaving(true);
+			const affiliationRequest = await saveEntityRecord(
+				'surecart',
+				'affiliation-request',
+				{
+					first_name: firstName,
+					last_name: lastName,
+					email,
+					payout_email: payoutEmail,
+					bio,
+				},
+				{ throwOnError: true }
+			);
+			setId(affiliationRequest.id);
+		} catch (e) {
+			console.error(e);
+			setError(e);
+			setIsSaving(false);
+		}
+	};
+
+	return (
+		<CreateTemplate id={id}>
+			<Error error={error} />
+
+			<Box title={__('Create New Affiliate Request', 'surecart')}>
+				<ScForm onScSubmit={onSubmit}>
+					<div
+						css={css`
+							display: grid;
+							gap: var(--sc-spacing-large);
+						`}
+					>
+						<ScFormRow>
+							<ScInput
+								label={__('First Name', 'surecart')}
+								className="sc-affiliation-request-first-name"
+								help={__(
+									"Affiliate request's first name.",
+									'surecart'
+								)}
+								onScChange={(e) => {
+									setFirstName(e.target.value);
+								}}
+								value={firstName}
+								name="first_name"
+								required
+								autofocus
+							/>
+							<ScInput
+								label={__('Last Name', 'surecart')}
+								className="sc-affiliate-request-last-name"
+								help={__(
+									"Affiliate request's last name.",
+									'surecart'
+								)}
+								onScChange={(e) => {
+									setLastName(e.target.value);
+								}}
+								value={lastName}
+								name="last_name"
+							/>
+						</ScFormRow>
+						<ScFormRow>
+							<ScInput
+								label={__('Email', 'surecart')}
+								className="sc-affiliate-request-email"
+								help={__(
+									"Affiliate request's email.",
+									'surecart'
+								)}
+								onScChange={(e) => {
+									setEmail(e.target.value);
+								}}
+								value={email}
+								name="email"
+								type="email"
+								required
+							/>
+							<ScInput
+								label={__('Payout Email', 'surecart')}
+								className="sc-affiliate-request-payout-email"
+								help={__(
+									"Affiliate request's payout email.",
+									'surecart'
+								)}
+								onScChange={(e) => {
+									setPayoutEmail(e.target.value);
+								}}
+								value={payoutEmail}
+								name="payout_email"
+								type="email"
+							/>
+						</ScFormRow>
+						<ScFormRow>
+							<ScTextarea
+								label={__('Bio', 'surecart')}
+								className="sc-affiliate-request-bio"
+								help={__(
+									'Short blurb from this affiliate describing how they will promote this store.',
+									'surecart'
+								)}
+								onScChange={(e) => {
+									setBio(e.target.value);
+								}}
+								value={bio}
+								name="bio"
+								required
+							/>
+						</ScFormRow>
+
+						<div
+							css={css`display: flex gap: var(--sc-spacing-small);`}
+						>
+							<ScButton type="primary" submit loading={isSaving}>
+								{__('Create', 'surecart')}
+							</ScButton>
+							<ScButton
+								href={'admin.php?page=sc-affiliate-requests'}
+								type="text"
+							>
+								{__('Cancel', 'surecart')}
+							</ScButton>
+						</div>
+					</div>
+				</ScForm>
+			</Box>
+		</CreateTemplate>
+	);
+};
