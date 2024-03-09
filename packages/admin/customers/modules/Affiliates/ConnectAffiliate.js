a8f83a60d5a5efc878f60f617b56a171416625a8
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-03-09 06:21:37 +0600

    Use ModelSelector for showing affiliations

--- packages/admin/customers/modules/Affiliates/ConnectAffiliate.js
+++ packages/admin/customers/modules/Affiliates/ConnectAffiliate.js
@@ -2,65 +2,34 @@
  * External dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { useSelect } from '@wordpress/data';
-import { store as coreStore } from '@wordpress/core-data';
 
 /**
  * Internal dependencies.
  */
-import { ScSelect } from '@surecart/components-react';
+import ModelSelector from '../../../components/ModelSelector';
+import { ScFormControl } from '@surecart/components-react';
 
 export default ({ customer, updateCustomer }) => {
-	const { affiliations, loading } = useSelect(
-		(select) => {
-			if (!customer?.id) {
-				return {
-					affiliations: [],
-					loading: false,
-				};
-			}
-			const entityData = [
-				'surecart',
-				'affiliation',
-				{
-					per_page: 100,
-				},
-			];
-
-			const affiliations =
-				select(coreStore)?.getEntityRecords?.(...entityData) || [];
-
-			return {
-				affiliations,
-				loading:
-					!select(coreStore)?.hasFinishedResolution?.(
-						'getEntityRecords',
-						[...entityData]
-					) && !affiliations?.length,
-			};
-		},
-		[customer?.id]
-	);
-
 	return (
-		<ScSelect
-			label={__('Connect an affiliate', 'surecart')}
-			value={customer?.affiliation}
-			choices={affiliations.map((affiliation) => ({
-				value: affiliation?.id,
-				label: affiliation?.first_name + ' ' + affiliation?.last_name,
-			}))}
-			onScChange={(e) =>
-				updateCustomer({
-					affiliation: e?.target?.value,
-				})
-			}
-			placeholder={__('Select an affiliate', 'surecart')}
-			loading={loading}
-			help={__(
-				'This will give the affiliate commissions for a specified period of time.',
-				'surecart'
-			)}
-		/>
+		<ScFormControl label={__('Connect an affiliate', 'surecart')}>
+			<ModelSelector
+				unselect={false}
+				name="affiliation"
+				value={customer?.affiliation?.id || customer?.affiliation}
+				onSelect={(affiliation) =>
+					updateCustomer({
+						affiliation,
+					})
+				}
+				display={(affiliation) =>
+					`${affiliation.first_name} ${affiliation.last_name || ''}`
+				}
+				placeholder={__('Select an affiliate', 'surecart')}
+				help={__(
+					'This will give the affiliate commissions for a specified period of time.',
+					'surecart'
+				)}
+			></ModelSelector>
+		</ScFormControl>
 	);
 };
