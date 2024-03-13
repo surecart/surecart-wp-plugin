41a77065113eadefbbab179a42c29c0f61aa5582
Andre Gagnon <ajgagnon@uwalumni.com>
2024-03-13 15:37:59 -0500

    Add new dialog

---
+++ packages/blocks-next/src/scripts/dialog/index.js
@@ -0,0 +1,21 @@
+/**
+ * WordPress dependencies
+ */
+import { store } from '@wordpress/interactivity';
+
+// controls the product page.
+store('surecart/dialog', {
+	callbacks: {
+		toggle: (e) => {
+			const dialog = e.target.dataset.target
+				? document.querySelector(e.target.dataset.target)
+				: e.target.closest('dialog');
+			dialog.open ? dialog.close() : dialog.showModal();
+		},
+		closeOverlay: (e) => {
+			if (e.target === e.currentTarget) {
+				e.currentTarget.close();
+			}
+		},
+	},
+});
