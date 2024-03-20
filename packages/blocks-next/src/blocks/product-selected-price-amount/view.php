90633691ecd05f7590f8e75f78f05525524c6a37
Andre Gagnon <ajgagnon@uwalumni.com>
2024-03-20 11:29:58 -0500

    Refactor product-selected-price-amount view.php

--- packages/blocks-next/src/blocks/product-selected-price-amount/view.php
+++ packages/blocks-next/src/blocks/product-selected-price-amount/view.php
@@ -1,3 +1,1 @@
-<div <?php echo get_block_wrapper_attributes(); ?>
-	data-wp-text="state.selectedDisplayAmount">
-</div>
+<div <?php echo get_block_wrapper_attributes(); ?> data-wp-text="state.selectedDisplayAmount"></div>
