beefb2b09562635a63670c223b51b531524032b5
Andre Gagnon <ajgagnon@uwalumni.com>
2024-03-26 13:04:38 -0500

    Refactor AffiliationClicksListTable class and update search form labels

--- views/admin/affiliation-requests/index.php
+++ views/admin/affiliation-requests/index.php
@@ -3,13 +3,13 @@
 	\SureCart::render(
 		'layouts/partials/admin-index-header',
 		[
-			'title' => __( 'Affiliate Requests', 'surecart' ),
+			'title'    => __( 'Affiliate Requests', 'surecart' ),
 			'new_link' => \SureCart::getUrl()->edit( 'affiliate-requests' ),
 		]
 	);
 	?>
 
-	<?php $table->search_form( __( 'Search Affiliate Requests', 'surecart' ), 'sc-search-affiliate-requests' ); ?>
+	<?php $table->search_form( __( 'Search', 'surecart' ), 'sc-search-affiliate-requests' ); ?>
 
 	<form id="affiliate-requests-filter" method="get">
 		<?php $table->views(); ?>
