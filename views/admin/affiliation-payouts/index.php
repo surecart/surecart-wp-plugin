a7a529ec38f790818a805669b60c34ccbf1a68ea
Andre Gagnon <ajgagnon@uwalumni.com>
2024-04-01 12:38:49 -0500

    Formatting

--- views/admin/affiliation-payouts/index.php
+++ views/admin/affiliation-payouts/index.php
@@ -3,12 +3,13 @@
 		display: inline !important;
 	}
 </style>
+
 <div class="wrap">
 	<?php
 	\SureCart::render(
 		'layouts/partials/admin-index-header',
 		[
-			'title' => __( 'Affiliate Payouts', 'surecart' ),
+			'title'    => __( 'Affiliate Payouts', 'surecart' ),
 			'new_link' => \SureCart::getUrl()->edit( 'affiliate-payout' ),
 		]
 	);
