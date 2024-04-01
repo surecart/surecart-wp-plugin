28882944edec921c42f7439d4feedb1408bbf7c8
Andre Gagnon <ajgagnon@uwalumni.com>
2024-04-01 09:08:13 -0500

    Add ScBlockUi component and implement busy state in TestModeToggle.js

--- packages/admin/test-mode-toggle/TestModeToggle.js
+++ packages/admin/test-mode-toggle/TestModeToggle.js
@@ -7,19 +7,22 @@ import { css, jsx } from '@emotion/core';
 /**
  * Internal dependencies
  */
-import { ScSwitch } from '@surecart/components-react';
+import { ScBlockUi, ScSwitch } from '@surecart/components-react';
 
 /**
  * Wordpress dependencies
  */
+import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { getQueryArgs, addQueryArgs } from '@wordpress/url';
 
 export default () => {
 	const { live_mode } = getQueryArgs(window.location.href);
+	const [busy, setBusy] = useState(false);
 
 	const onSwitchToggle = (e) => {
 		const checked = e.target.checked;
+		setBusy(true);
 
 		window.location.href = addQueryArgs(window.location.href, {
 			live_mode: checked ? 'false' : 'true',
@@ -27,14 +30,24 @@ export default () => {
 	};
 
 	return (
-		<ScSwitch checked={live_mode === 'false'} onScChange={onSwitchToggle}>
-			<span
-				css={css`
-					white-space: nowrap;
-				`}
+		<div
+			css={css`
+				position: relative;
+			`}
+		>
+			<ScSwitch
+				checked={live_mode === 'false'}
+				onScChange={onSwitchToggle}
 			>
-				{__('Test Mode', 'surecart')}
-			</span>
-		</ScSwitch>
+				<span
+					css={css`
+						white-space: nowrap;
+					`}
+				>
+					{__('Test Mode', 'surecart')}
+				</span>
+			</ScSwitch>
+			{busy && <ScBlockUi />}
+		</div>
 	);
 };
