4e62a466e649888b8e368a3b03411e383ae068fa
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-04-01 16:53:07 +0600

    Revert back dialog script

--- packages/blocks-next/src/scripts/dialog/index.js
+++ packages/blocks-next/src/scripts/dialog/index.js
@@ -1,24 +1,16 @@
 /**
  * WordPress dependencies
  */
-import { store, getContext } from '@wordpress/interactivity';
+import { store } from '@wordpress/interactivity';
 
 // controls the product page.
 store('surecart/dialog', {
 	callbacks: {
 		toggle: (e) => {
-			const context = getContext() || {};
-
-			const dialog = e.target.dataset.Target
-				? document.querySelector(e.target.dataset.Target)
+			const dialog = e.target.dataset.target
+				? document.querySelector(e.target.dataset.target)
 				: e.target.closest('dialog');
-
-			// Set the dialog to the context.
-			context.dialog = dialog;
-			context.isOpen = dialog?.open ? false : true;
-
-			// Toggle the dialog.
-			dialog?.open ? dialog?.close() : dialog?.showModal();
+			dialog.open ? dialog.close() : dialog.showModal();
 		},
 		closeOverlay: (e) => {
 			if (e.target === e.currentTarget) {
