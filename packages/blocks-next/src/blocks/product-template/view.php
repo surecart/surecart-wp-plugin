8114b4f51d8588b362bcd415ddac60f75b93aac2
Andre Gagnon <ajgagnon@uwalumni.com>
2024-04-08 14:49:36 -0500

    Clean up

--- packages/blocks-next/src/blocks/product-template/view.php
+++ packages/blocks-next/src/blocks/product-template/view.php
@@ -1,23 +1,19 @@
-<div
-	<?php echo get_block_wrapper_attributes( array( 'class' => 'product-item-list' . $class ) ); ?>
-	style="<?php
-		echo '--sc-product-item-list-column:' . $columns . '; ';
-		echo esc_attr($style);
-	?>"
->
+<div <?php echo get_block_wrapper_attributes( array(
+	'class' => 'product-item-list' . $class,
+	'style' => esc_attr($style)
+) ); ?>>
 	<?php if ( empty( $products ) ) : ?>
 		<div class="no-products-found">
 			<?php esc_html_e( 'No products found.', 'surecart' ); ?>
 		</div>
 	<?php endif; ?>
+
 	<?php foreach ($products as $product) : ?>
 		<div
-			<?php echo wp_kses_data(
-					wp_interactivity_data_wp_context(
-						[
-							'product' => $product,
-						]
-					)
+			<?php echo wp_interactivity_data_wp_context(
+				[
+					'product' => $product,
+				]
 			); ?>
 			class="product-item"
 		>
