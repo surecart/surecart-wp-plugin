6aa99317d17751545cb0c644ff542c56497874f7
Rajkiran Bagal <rajkiranb@bsf.io>
2024-04-10 20:34:32 +0530

    Added height width & aspect ratio settings

--- packages/blocks-next/src/blocks/product-image/edit.js
+++ packages/blocks-next/src/blocks/product-image/edit.js
@@ -1,19 +1,32 @@
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	InspectorControls,
+	useSettings,
+} from '@wordpress/block-editor';
 import classnames from 'classnames';
 import { __ } from '@wordpress/i18n';
 import { store as coreStore } from '@wordpress/core-data';
 import { useSelect } from '@wordpress/data';
 import {
 	PanelBody,
+	SelectControl,
 	__experimentalToggleGroupControl as ToggleGroupControl,
 	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+	__experimentalUnitControl as UnitControl,
+	__experimentalToolsPanelItem as ToolsPanelItem,
+	__experimentalUseCustomUnits as useCustomUnits,
 } from '@wordpress/components';
 
 export default ({
-	attributes: { sizing },
+	attributes: { sizing, aspectRatio, height, width },
 	context: { 'surecart/productId': productId },
 	setAttributes,
+	clientId,
 }) => {
+	const [availableUnits] = useSettings('spacing.units');
+	const units = useCustomUnits({
+		availableUnits: availableUnits || ['px', '%', 'vw', 'em', 'rem'],
+	});
 	const classes = classnames({
 		'product-img': true,
 		is_contained: sizing === 'contain',
@@ -22,8 +35,21 @@ export default ({
 
 	const blockProps = useBlockProps({
 		className: classes,
+		style: { width, height, aspectRatio },
 	});
 
+	const onDimensionChange = (dimension, nextValue) => {
+		const parsedValue = parseFloat(nextValue);
+		/**
+		 * If we have no value set and we change the unit,
+		 * we don't want to set the attribute, as it would
+		 * end up having the unit as value without any number.
+		 */
+		if (isNaN(parsedValue) && nextValue) return;
+		setAttributes({
+			[dimension]: parsedValue < 0 ? '0' : nextValue,
+		});
+	};
 	const product = useSelect(
 		(select) => {
 			if (!productId) {
@@ -59,6 +85,69 @@ export default ({
 							label={__('Cover', 'surecart')}
 						/>
 					</ToggleGroupControl>
+					<SelectControl
+						__nextHasNoMarginBottom
+						label={__('Aspect ratio', 'surecart')}
+						value={aspectRatio}
+						options={[
+							// These should use the same values as AspectRatioDropdown in @wordpress/block-editor
+							{
+								label: __('Original', 'surecart'),
+								value: 'auto',
+							},
+							{
+								label: __('Square', 'surecart'),
+								value: '1',
+							},
+							{
+								label: __('16:9', 'surecart'),
+								value: '16/9',
+							},
+							{
+								label: __('4:3', 'surecart'),
+								value: '4/3',
+							},
+							{
+								label: __('3:2', 'surecart'),
+								value: '3/2',
+							},
+							{
+								label: __('9:16', 'surecart'),
+								value: '9/16',
+							},
+							{
+								label: __('3:4', 'surecart'),
+								value: '3/4',
+							},
+							{
+								label: __('2:3', 'surecart'),
+								value: '2/3',
+							},
+						]}
+						onChange={(nextAspectRatio) =>
+							setAttributes({ aspectRatio: nextAspectRatio })
+						}
+					/>
+					<UnitControl
+						label={__('Height', 'surecart')}
+						labelPosition="top"
+						value={height || ''}
+						min={0}
+						onChange={(nextHeight) =>
+							onDimensionChange('height', nextHeight)
+						}
+						units={units}
+					/>
+					<UnitControl
+						label={__('Width', 'surecart')}
+						labelPosition="top"
+						value={width || ''}
+						min={0}
+						onChange={(nextWidth) =>
+							onDimensionChange('width', nextWidth)
+						}
+						units={units}
+					/>
 				</PanelBody>
 			</InspectorControls>
 			<div {...blockProps}>
--- packages/blocks-next/src/blocks/product-image/view.php
+++ packages/blocks-next/src/blocks/product-image/view.php
@@ -1,12 +1,24 @@
 <?php
-    $styles = sc_get_block_styles();
-    $style = $styles['css'] ?? '';
-    $class = $styles['classnames'] ?? '';
+    
     $sizing = $attributes['sizing'] ? 'contain' === $attributes['sizing'] ? ' is_contained' : ' is_covered' : ' is_covered';
+    $aspect_ratio = ! empty( $attributes['aspectRatio'] )
+		? esc_attr( safecss_filter_attr( 'aspect-ratio:' . $attributes['aspectRatio'] ) ) . ';'
+		: '';
+	$width        = ! empty( $attributes['width'] )
+		? esc_attr( safecss_filter_attr( 'width:' . $attributes['width'] ) ) . ';'
+		: '';
+	$height       = ! empty( $attributes['height'] )
+		? esc_attr( safecss_filter_attr( 'height:' . $attributes['height'] ) ) . ';'
+		: '';
+
+    if ( ! $height && ! $width && ! $aspect_ratio ) {
+        $wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'product-img ' . $class . $sizing ) );
+    } else {
+        $wrapper_attributes = get_block_wrapper_attributes( array( 'style' => $aspect_ratio . $width . $height, 'class' => 'product-img ' . $class . $sizing ) );
+    }
 ?>
 <div
-    <?php echo get_block_wrapper_attributes( array( 'class' => 'product-img ' . $class . $sizing ) ); ?>
-    style="<?php echo esc_attr( $style ); ?>"
+    <?php echo $wrapper_attributes; ?>
 >
     <img data-wp-bind--src="context.product.featured_media.src"/>
 </div>
