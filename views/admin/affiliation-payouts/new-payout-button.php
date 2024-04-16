bd89327db4d5c49a17aa0b5791f1d948348eb50d
Andre Gagnon <ajgagnon@uwalumni.com>
2024-04-16 08:30:08 -0500

    Use WP button

--- views/admin/affiliation-payouts/new-payout-button.php
+++ views/admin/affiliation-payouts/new-payout-button.php
@@ -1,7 +1,14 @@
 <sc-dropdown>
-	<sc-button slot="trigger" caret>Add New</sc-button>
+	<button slot="trigger" class="page-title-action">
+		<?php esc_html_e( 'Add New', 'surecart' ); ?>
+		<sc-icon name="chevron-down" style="vertical-align: middle; margin-top: -2px;"></sc-icon>
+	</button>
 	<sc-menu>
-		<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->edit( 'affiliate-payout' ) ); ?>">Payout</sc-menu-item>
-		<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->edit( 'affiliate-payout-group' ) ); ?>">Payout Batch</sc-menu-item>
+		<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->edit( 'affiliate-payout' ) ); ?>">
+			<?php esc_html_e( 'Payout', 'surecart' ); ?>
+		</sc-menu-item>
+		<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->edit( 'affiliate-payout-group' ) ); ?>">
+			<?php esc_html_e( 'Payout Batch', 'surecart' ); ?>
+		</sc-menu-item>
 	</sc-menu>
 </sc-dropdown>
