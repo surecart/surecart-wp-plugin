d92346c88e725374b0c49f1a6754ada23a561a9d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-04-19 14:34:12 +0600

    Fixed some design shrinking issue for drawer

--- packages/blocks-next/src/blocks/cart/view.php
+++ packages/blocks-next/src/blocks/cart/view.php
@@ -15,28 +15,30 @@
 	</button>
 
 	<dialog class="sc-drawer" data-wp-on--click="actions.closeOverlay">
-		<div class="sc-drawer__header">
-			<button
-				autofocus
-				data-wp-on--click='actions.toggle'
-				style="border: 0; background: transparent; cursor: pointer;"
-				title="<?php esc_attr_e( 'Close drawer', 'surecart' ); ?>"
-			>
-				<?php echo wp_kses( SureCart::svg()->get('arrow-left'), sc_allowed_svg_html() ); ?>
-			</button>
-			<p><?php esc_html_e( 'My Carts', 'surecart' ); ?></p>
-			<button>1</button>
-		</div>
-		<div class="sc-drawer__body">
-			<p><?php esc_html_e( 'Cart Items...', 'surecart' ); ?></p>
-		</div>
-		<div class="sc-drawer__footer">
-			<button
-				data-wp-on--click='actions.toggle'
-				class="sc-drawer__footer__button"
-			>
-				<?php esc_html_e( 'Close', 'surecart' ); ?>
-			</button>
+		<div class="sc-drawer__wrapper">
+			<div class="sc-drawer__header">
+				<button
+					autofocus
+					data-wp-on--click='actions.toggle'
+					style="border: 0; background: transparent; cursor: pointer;"
+					title="<?php esc_attr_e( 'Close drawer', 'surecart' ); ?>"
+				>
+					<?php echo wp_kses( SureCart::svg()->get('arrow-left'), sc_allowed_svg_html() ); ?>
+				</button>
+				<p><?php esc_html_e( 'My Carts', 'surecart' ); ?></p>
+				<button>1</button>
+			</div>
+			<div class="sc-drawer__body">
+				<p><?php esc_html_e( 'Cart Items...', 'surecart' ); ?></p>
+			</div>
+			<div class="sc-drawer__footer">
+				<button
+					data-wp-on--click='actions.toggle'
+					class="sc-drawer__footer__button"
+				>
+					<?php esc_html_e( 'Close', 'surecart' ); ?>
+				</button>
+			</div>
 		</div>
 	</dialog>
 </div>
