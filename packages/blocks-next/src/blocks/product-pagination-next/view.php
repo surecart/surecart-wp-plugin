c838704b4e4d3cbec036bf7250ebdb383a497bcb
Rajkiran Bagal <rajkiranb@bsf.io>
2024-04-26 20:04:39 +0530

    Added rendering for pagination prev & next arrow

--- packages/blocks-next/src/blocks/product-pagination-next/view.php
+++ packages/blocks-next/src/blocks/product-pagination-next/view.php
@@ -1,3 +1,12 @@
+<?php 
+$arrowMap = [
+	"none" => '',
+	"arrow" => '→',
+	"chevron" => '»',
+];
+$paginationArrow = $block->context['paginationArrow'] ?? '';
+$displayArrow = $arrowMap[$paginationArrow] ?? '';
+?>
 <a
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-bind--href="context.nextPageLink"
@@ -7,4 +16,11 @@
 	data-wp-class--disabled="!context.nextPageLink"
 >
 	<?php echo wp_kses_post($attributes['label'] ?? __( 'Next', 'surecart' )); ?>
+	<?php if ( $displayArrow ) : ?>
+	<span
+		class="wp-block-product-pagination-next-arrow is-arrow-<?php echo esc_attr( $paginationArrow ); ?>"
+	>
+		<?php echo esc_html( $displayArrow ); ?>
+	</span>
+	<?php endif; ?>
 </a>
--- packages/blocks-next/src/blocks/product-pagination-previous/view.php
+++ packages/blocks-next/src/blocks/product-pagination-previous/view.php
@@ -1,3 +1,12 @@
+<?php 
+$arrowMap = [
+	"none" => '',
+	"arrow" => '←',
+	"chevron" => '«',
+];
+$paginationArrow = $block->context['paginationArrow'] ?? '';
+$displayArrow = $arrowMap[$paginationArrow] ?? '';
+?>
 <a
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-bind--href="context.previousPageLink"
@@ -6,5 +15,12 @@
 	data-wp-watch="surecart/product-list::callbacks.prefetch"
 	data-wp-class--disabled="!context.previousPageLink"
 >
+	<?php if ( $displayArrow ) : ?>
+	<span
+		class="wp-block-product-pagination-next-arrow is-arrow-<?php echo esc_attr( $paginationArrow ); ?>"
+	>
+		<?php echo esc_html( $displayArrow ); ?>
+	</span>
+	<?php endif; ?>
 	<?php echo wp_kses_post($attributes['label'] ?? __( 'Previous', 'surecart' )); ?>
 </a>
