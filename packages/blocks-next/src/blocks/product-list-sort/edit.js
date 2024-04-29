bf3964a05849133ffd700dab0eb668cdf27056a4
Rajkiran Bagal <rajkiranb@bsf.io>
2024-04-29 19:38:54 +0530

    Filter & Sort Blocks Dropdown Rendering in editor & styling fixes

--- packages/blocks-next/src/blocks/product-list-sort/edit.js
+++ packages/blocks-next/src/blocks/product-list-sort/edit.js
@@ -1,16 +1,32 @@
 import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
 
 export default () => {
+	const blockProps = useBlockProps({
+		className: 'sc-dropdown',
+	});
 	return (
-		<select className="sc-dropdown">
-			<option value="created_at:desc">{__('Latest', 'surecart')}</option>
-			<option value="created_at:asc">{__('Oldest', 'surecart')}</option>
-			<option value="name:asc">
-				{__('Alphabetical, A-Z', 'surecart')}
-			</option>
-			<option value="name:desc">
-				{__('Alphabetical, Z-A', 'surecart')}
-			</option>
-		</select>
+		<div {...blockProps} role="menu" tabIndex="-1">
+			<button className="sc-dropdown__trigger sc-button sc-button--standard sc-button--medium sc-button--caret sc-button--has-label sc-button--text">
+				<span className="sc-button__label">
+					{__('Sort', 'surecart')}
+				</span>
+				<span className="sc-button__caret">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<polyline points="6 9 12 15 18 9" />
+					</svg>
+				</span>
+			</button>
+		</div>
 	);
 };
