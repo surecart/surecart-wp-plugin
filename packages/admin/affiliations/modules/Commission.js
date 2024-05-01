98c22dd117a9b65d1ea01a39977d7451c6179cf3
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-05-01 23:02:21 +0600

    Refactorred Commission to use newly created CommissionSidebarForm

--- packages/admin/affiliations/modules/Commission.js
+++ packages/admin/affiliations/modules/Commission.js
@@ -3,95 +3,114 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
+import { select, useDispatch } from '@wordpress/data';
+import { store as noticesStore } from '@wordpress/notices';
+import { store as coreStore } from '@wordpress/core-data';
+import { addQueryArgs } from '@wordpress/url';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies.
  */
-import { ScButton, ScIcon } from '@surecart/components-react';
-import Definition from '../../ui/Definition';
-import CommissionForm from '../../components/affiliates/commission/CommissionForm';
-import EmptyCommissions from '../../components/affiliates/commission/EmptyCommissions';
-import Box from '../../ui/Box';
+import CommissionSidebarForm from '../../components/affiliates/commission/CommissionSidebarForm';
 
-export default ({ affiliation, updateAffiliation, loading, error }) => {
+export default ({ affiliation, loading }) => {
+	if (!affiliation?.id) {
+		return null;
+	}
+
+	const [error, setError] = useState(null);
 	const [modal, setModal] = useState(false);
+	const [saving, setSaving] = useState(false);
+	const { createSuccessNotice } = useDispatch(noticesStore);
+	const { receiveEntityRecords } = useDispatch(coreStore);
+	const [commissionStructure, setCommissionStructure] = useState(
+		affiliation.commission_structure
+	);
 
-	const onChange = (data) => {
-		updateAffiliation({
-			commission_structure: data?.commission_structure || {},
-		});
+	const handleSubmit = async (e, commissionStructureData, message) => {
+		try {
+			const { baseURL } = select(coreStore).getEntityConfig(
+				'surecart',
+				'affiliation'
+			);
+
+			setSaving(true);
+			const affiliationData = await apiFetch({
+				path: addQueryArgs(`${baseURL}/${affiliation?.id}`),
+				method: 'PATCH',
+				data: {
+					commission_structure: commissionStructureData,
+				},
+			});
+
+			receiveEntityRecords(
+				'surecart',
+				'affiliation',
+				{
+					...affiliationData,
+					commission_structure: affiliationData?.commission_structure,
+				},
+				undefined,
+				false,
+				{
+					commission_structure: affiliationData?.commission_structure,
+				}
+			);
+
+			setCommissionStructure(affiliationData.commission_structure);
+
+			createSuccessNotice(message, {
+				type: 'snackbar',
+			});
+
+			setModal(false);
+		} catch (error) {
+			console.error(error);
+			setError(error);
+		} finally {
+			setSaving(false);
+		}
 	};
 
-	const { commission_structure: commissionStructure } = affiliation || {};
+	const onChange = (data) =>
+		setCommissionStructure(data?.commission_structure);
+
+	const onDelete = async (e) => {
+		await handleSubmit(
+			e,
+			null,
+			__('Affiliate commission deleted.', 'surecart')
+		);
+	};
 
 	return (
-		<>
-			<Box
-				title={__('Custom Commission', 'surecart')}
-				loading={loading}
-				header_action={
-					commissionStructure?.id ? (
-						<ScButton type="text" onClick={() => setModal('edit')}>
-							<ScIcon
-								name="edit"
-								title={__('Edit', 'surecart')}
-							/>
-						</ScButton>
-					) : null
-				}
-			>
-				{commissionStructure?.id ? (
-					<>
-						<Definition title={__('Commission', 'surecart')}>
-							{commissionStructure.discount_amount}
-						</Definition>
-						<Definition
-							title={__(
-								'Subscription Commission Duration',
-								'surecart'
-							)}
-						>
-							{commissionStructure.subscription_commission || '-'}
-						</Definition>
-						<Definition
-							title={__(
-								'Lifetime Commission Duration',
-								'surecart'
-							)}
-						>
-							{commissionStructure.lifetime_commission || '-'}
-						</Definition>
-					</>
-				) : (
-					<EmptyCommissions
-						message={__(
-							'Add custom commission for this affiliate.',
-							'surecart'
-						)}
-						openModal={() => setModal('create')}
-					/>
-				)}
-			</Box>
-
-			{!!modal && (
-				<CommissionForm
-					title={
-						commissionStructure?.id
-							? __('Edit Commission', 'surecart')
-							: __('Add Commission', 'surecart')
-					}
-					submitButtonTitle={__('Save', 'surecart')}
-					error={error}
-					open={modal === 'create' || modal === 'edit'}
-					onRequestClose={() => setModal(false)}
-					onSubmit={() => setModal(false)}
-					onChange={onChange}
-					affiliationItem={{
-						commission_structure: commissionStructure,
-					}}
-					loading={loading}
-				/>
+		<CommissionSidebarForm
+			headerTitle={__('Custom Commission', 'surecart')}
+			formTitle={
+				commissionStructure?.id
+					? __('Edit Commission', 'surecart')
+					: __('Add Commission', 'surecart')
+			}
+			submitButtonTitle={__('Save', 'surecart')}
+			onSubmitMessage={
+				commissionStructure?.id
+					? __('Affiliate commission updated.', 'surecart')
+					: __('Affiliate commission added.', 'surecart')
+			}
+			emptyCommissionMessage={__(
+				'Add a custom commission for this affiliate.',
+				'surecart'
 			)}
-		</>
+			loading={loading || saving}
+			commissionStructure={commissionStructure}
+			modal={modal}
+			setModal={setModal}
+			onDelete={onDelete}
+			error={error}
+			setError={setError}
+			onChange={onChange}
+			onSubmit={handleSubmit}
+		/>
 	);
 };
