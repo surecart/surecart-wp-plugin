6b73109bb81a8b9c80abb805e4a7894f28fe7164
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-05-07 13:03:49 +0600

    Moved from ScDialog to ScDrawer

--- packages/admin/subscriptions/edit/modules/Modals/UpdateRecentPrice.js
+++ packages/admin/subscriptions/edit/modules/Modals/UpdateRecentPrice.js
@@ -17,13 +17,13 @@ import { store as coreStore } from '@wordpress/core-data';
  */
 import {
 	ScButton,
-	ScDialog,
 	ScFormatNumber,
 	ScSwitch,
 	ScText,
 	ScCard,
 	ScLineItem,
 	ScDivider,
+	ScDrawer,
 } from '@surecart/components-react';
 import { intervalString } from '../../../../util/translations';
 import LineItem from '../../../show/modules/LineItems/LineItem';
@@ -92,200 +92,214 @@ export default ({
 	};
 
 	return (
-		<ScDialog
-			label={__('Confirm Price Change', 'surecart')}
+		<ScDrawer
+			style={{ '--sc-drawer-size': '480px' }}
 			open={open}
+			label={__('Confirm Price Change', 'surecart')}
 			onScRequestClose={onRequestClose}
-			style={{ '--dialog-body-overflow': 'visible' }}
 		>
-			<ScText
+			<div
 				css={css`
-					display: block;
+					padding: var(--sc-drawer-body-spacing);
 				`}
 			>
-				{__(
-					'Are you sure you want to update this subscription to use the current price? This will update this users subscription to use the current price.',
-					'surecart'
-				)}
-			</ScText>
-
-			<ScCard
-				css={css`
-					display: block;
-					margin-top: var(--sc-spacing-small);
-					--font-weight: var(--sc-font-weight-bold);
-				`}
-			>
-				<span
+				<ScText
 					css={css`
+						display: block;
+					`}
+				>
+					{__(
+						'Are you sure you want to update this subscription to use the current price? This will update this users subscription to use the current price.',
+						'surecart'
+					)}
+				</ScText>
+
+				<ScCard
+					css={css`
+						display: block;
+						margin-top: var(--sc-spacing-small);
 						--font-weight: var(--sc-font-weight-bold);
 					`}
 				>
-					{__('Price Change:', 'surecart')} &nbsp;
-				</span>
-				<del
-					css={css`
-						color: var(--sc-color-gray-500);
-					`}
-				>
-					<ScFormatNumber
-						type="currency"
-						value={
-							price?.ad_hoc && subscription?.ad_hoc_amount
-								? subscription?.ad_hoc_amount
-								: price?.amount
-						}
-						currency={price?.currency}
-					/>
-					{intervalString(price, {
-						labels: { interval: '/' },
-					})}
-				</del>
-				&nbsp;
-				{__('to', 'surecart')}{' '}
-				<ScFormatNumber
-					type="currency"
-					value={
-						recentPrice?.ad_hoc && subscription?.ad_hoc_amount
-							? subscription?.ad_hoc_amount
-							: recentPrice?.amount
-					}
-					currency={recentPrice?.currency}
-				/>
-				{intervalString(recentPrice, {
-					labels: { interval: '/' },
-				})}
-			</ScCard>
-
-			{!subscription?.finite && (
-				<ScSwitch
-					checked={immediateUpdate}
-					onScChange={(e) => setImmediateUpdate(e.target.checked)}
-					css={css`
-						margin-left: auto;
-						margin-top: var(--sc-spacing-medium);
-					`}
-				>
-					{__('Update Immediately', 'surecart')}
-					<span slot="description">
-						{__(
-							'Immediately updates the subscription to this new price, instead of waiting until renewal.',
-							'surecart'
-						)}
-					</span>
-				</ScSwitch>
-			)}
-
-			{immediateUpdate && !!upcoming?.checkout?.proration_amount && (
-				<>
-					<div
+					<span
 						css={css`
-							margin-top: var(--sc-spacing-medium);
-						`}
-					>
-						<ScSwitch
-							checked={!skipProration}
-							onClick={(e) => setSkipProration(!e.target.checked)}
-						>
-							{__('Prorate Charges', 'surecart')}
-							<span slot="description">
-								{__(
-									'Apply a credit of unused time from the current billing period.',
-									'surecart'
-								)}
-							</span>
-						</ScSwitch>
-					</div>
-
-					<ScCard
-						css={css`
-							display: block;
-							margin-top: var(--sc-spacing-small);
 							--font-weight: var(--sc-font-weight-bold);
 						`}
 					>
-						<LineItem
-							label={__('Subtotal', 'surecart')}
-							currency={upcoming?.checkout?.currency}
-							value={upcoming?.checkout?.subtotal_amount}
+						{__('Price Change:', 'surecart')} &nbsp;
+					</span>
+					<del
+						css={css`
+							color: var(--sc-color-gray-500);
+						`}
+					>
+						<ScFormatNumber
+							type="currency"
+							value={
+								price?.ad_hoc && subscription?.ad_hoc_amount
+									? subscription?.ad_hoc_amount
+									: price?.amount
+							}
+							currency={price?.currency}
 						/>
+						{intervalString(price, {
+							labels: { interval: '/' },
+						})}
+					</del>
+					&nbsp;
+					{__('to', 'surecart')}{' '}
+					<ScFormatNumber
+						type="currency"
+						value={
+							recentPrice?.ad_hoc && subscription?.ad_hoc_amount
+								? subscription?.ad_hoc_amount
+								: recentPrice?.amount
+						}
+						currency={recentPrice?.currency}
+					/>
+					{intervalString(recentPrice, {
+						labels: { interval: '/' },
+					})}
+				</ScCard>
 
-						{!!upcoming?.checkout?.proration_amount && (
-							<LineItem
-								label={__('Proration', 'surecart')}
-								currency={upcoming?.checkout?.currency}
-								value={upcoming?.checkout?.proration_amount}
-							/>
-						)}
+				{!subscription?.finite && (
+					<ScSwitch
+						checked={immediateUpdate}
+						onScChange={(e) => setImmediateUpdate(e.target.checked)}
+						css={css`
+							margin-left: auto;
+							margin-top: var(--sc-spacing-medium);
+						`}
+					>
+						{__('Update Immediately', 'surecart')}
+						<span slot="description">
+							{__(
+								'Immediately updates the subscription to this new price, instead of waiting until renewal.',
+								'surecart'
+							)}
+						</span>
+					</ScSwitch>
+				)}
 
-						{!!upcoming?.checkout?.applied_balance_amount && (
-							<LineItem
-								label={__('Applied Balance', 'surecart')}
-								currency={upcoming?.checkout?.currency}
-								value={
-									upcoming?.checkout?.applied_balance_amount
-								}
-							/>
-						)}
-
-						{!!upcoming?.checkout?.trial_amount && (
-							<LineItem
-								label={__('Trial', 'surecart')}
-								currency={upcoming?.checkout?.currency}
-								value={upcoming?.checkout?.trial_amount}
-							/>
-						)}
-
-						{!!upcoming?.checkout?.discount_amount && (
-							<LineItem
-								label={__('Discounts', 'surecart')}
-								currency={upcoming?.checkout?.currency}
-								value={upcoming?.checkout?.discount_amount}
-							/>
-						)}
-
-						{!!upcoming?.checkout?.shipping_amount && (
-							<LineItem
-								label={__('Shipping', 'surecart')}
-								currency={upcoming?.checkout?.currency}
-								value={upcoming?.checkout?.shipping_amount}
-							/>
-						)}
-
-						{!!upcoming?.checkout?.tax_amount && (
-							<LineItem
-								label={`${formatTaxDisplay(
-									upcoming?.checkout?.tax_label
-								)} (${upcoming?.checkout?.tax_percent}%)`}
-								currency={upcoming?.checkout?.currency}
-								value={upcoming?.checkout?.tax_amount}
-							/>
-						)}
-						<ScDivider
-							style={{ '--spacing': 'var(--sc-spacing-small)' }}
-						/>
-
-						<ScLineItem
-							style={{
-								width: '100%',
-								'--price-size': 'var(--sc-font-size-x-large)',
-							}}
+				{immediateUpdate && !!upcoming?.checkout?.proration_amount && (
+					<>
+						<div
+							css={css`
+								margin-top: var(--sc-spacing-medium);
+							`}
 						>
-							<span slot="title">{__('Total', 'surecart')}</span>
-							<span slot="price">
-								<ScFormatNumber
-									type="currency"
+							<ScSwitch
+								checked={!skipProration}
+								onClick={(e) =>
+									setSkipProration(!e.target.checked)
+								}
+							>
+								{__('Prorate Charges', 'surecart')}
+								<span slot="description">
+									{__(
+										'Apply a credit of unused time from the current billing period.',
+										'surecart'
+									)}
+								</span>
+							</ScSwitch>
+						</div>
+
+						<ScCard
+							css={css`
+								display: block;
+								margin-top: var(--sc-spacing-small);
+								--font-weight: var(--sc-font-weight-bold);
+							`}
+						>
+							<LineItem
+								label={__('Subtotal', 'surecart')}
+								currency={upcoming?.checkout?.currency}
+								value={upcoming?.checkout?.subtotal_amount}
+							/>
+
+							{!!upcoming?.checkout?.proration_amount && (
+								<LineItem
+									label={__('Proration', 'surecart')}
 									currency={upcoming?.checkout?.currency}
-									value={upcoming?.checkout?.amount_due}
-								></ScFormatNumber>
-							</span>
-							<span slot="currency">
-								{upcoming?.checkout?.currency}
-							</span>
-						</ScLineItem>
-					</ScCard>
-				</>
-			)}
+									value={upcoming?.checkout?.proration_amount}
+								/>
+							)}
+
+							{!!upcoming?.checkout?.applied_balance_amount && (
+								<LineItem
+									label={__('Applied Balance', 'surecart')}
+									currency={upcoming?.checkout?.currency}
+									value={
+										upcoming?.checkout
+											?.applied_balance_amount
+									}
+								/>
+							)}
+
+							{!!upcoming?.checkout?.trial_amount && (
+								<LineItem
+									label={__('Trial', 'surecart')}
+									currency={upcoming?.checkout?.currency}
+									value={upcoming?.checkout?.trial_amount}
+								/>
+							)}
+
+							{!!upcoming?.checkout?.discount_amount && (
+								<LineItem
+									label={__('Discounts', 'surecart')}
+									currency={upcoming?.checkout?.currency}
+									value={upcoming?.checkout?.discount_amount}
+								/>
+							)}
+
+							{!!upcoming?.checkout?.shipping_amount && (
+								<LineItem
+									label={__('Shipping', 'surecart')}
+									currency={upcoming?.checkout?.currency}
+									value={upcoming?.checkout?.shipping_amount}
+								/>
+							)}
+
+							{!!upcoming?.checkout?.tax_amount && (
+								<LineItem
+									label={`${formatTaxDisplay(
+										upcoming?.checkout?.tax_label
+									)} (${upcoming?.checkout?.tax_percent}%)`}
+									currency={upcoming?.checkout?.currency}
+									value={upcoming?.checkout?.tax_amount}
+								/>
+							)}
+							<ScDivider
+								style={{
+									'--spacing': 'var(--sc-spacing-small)',
+								}}
+							/>
+
+							<ScLineItem
+								style={{
+									width: '100%',
+									'--price-size':
+										'var(--sc-font-size-x-large)',
+								}}
+							>
+								<span slot="title">
+									{__('Total', 'surecart')}
+								</span>
+								<span slot="price">
+									<ScFormatNumber
+										type="currency"
+										currency={upcoming?.checkout?.currency}
+										value={upcoming?.checkout?.amount_due}
+									></ScFormatNumber>
+								</span>
+								<span slot="currency">
+									{upcoming?.checkout?.currency}
+								</span>
+							</ScLineItem>
+						</ScCard>
+					</>
+				)}
+			</div>
 
 			<ScButton
 				type="primary"
@@ -300,6 +314,6 @@ export default ({
 			<ScButton type="text" onClick={onRequestClose} slot="footer">
 				{__('Cancel', 'surecart')}
 			</ScButton>
-		</ScDialog>
+		</ScDrawer>
 	);
 };
