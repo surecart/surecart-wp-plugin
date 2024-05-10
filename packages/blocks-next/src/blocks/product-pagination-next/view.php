728b9612b54739fd2a3f65dde6bfcd0771377aa0
Andre Gagnon <ajgagnon@uwalumni.com>
2024-05-10 16:39:27 -0500

    Pagination styles.

--- packages/blocks-next/src/blocks/product-pagination-next/view.php
+++ packages/blocks-next/src/blocks/product-pagination-next/view.php
@@ -1,5 +1,5 @@
 <a
-	<?php echo get_block_wrapper_attributes(['class' => 'sc-product-pagination-next has-arrow-type-' . esc_attr($pagination_arrow)]); ?>
+	<?php echo get_block_wrapper_attributes(['class' => 'has-arrow-type-' . esc_attr($pagination_arrow)]); ?>
 	data-wp-bind--href="context.nextPageLink"
 	data-wp-class--disabled="!context.nextPageLink"
 	data-wp-on--click="surecart/product-list::actions.navigate"
@@ -10,6 +10,6 @@
 	<?php endif; ?>
 
 	<?php if (!empty($arrow_name)) : ?>
-		<?php echo \SureCart::svg()->get($arrow_name); ?>
+		<?php echo \SureCart::svg()->get($arrow_name, ['class' => 'wp-block-surecart-product-pagination-next__icon']); ?>
 	<?php endif; ?>
 </a>
--- packages/blocks-next/src/blocks/product-pagination-previous/view.php
+++ packages/blocks-next/src/blocks/product-pagination-previous/view.php
@@ -1,12 +1,12 @@
 <a
-	<?php echo get_block_wrapper_attributes(['class' => 'sc-product-pagination-previous has-arrow-type-' . esc_attr($pagination_arrow)]); ?>
+	<?php echo get_block_wrapper_attributes(['class' => 'has-arrow-type-' . esc_attr($pagination_arrow)]); ?>
 	data-wp-bind--href="context.previousPageLink"
 	data-wp-class--disabled="!context.previousPageLink"
 	data-wp-on--click="surecart/product-list::actions.navigate"
 	data-wp-on--mouseenter="surecart/product-list::actions.prefetch"
 	>
 	<?php if (!empty($arrow_name)) : ?>
-		<?php echo \SureCart::svg()->get($arrow_name); ?>
+		<?php echo \SureCart::svg()->get($arrow_name, ['class' => 'wp-block-surecart-product-pagination-previous__icon']); ?>
 	<?php endif; ?>
 
 	<?php if (!empty($show_label)) : ?>
