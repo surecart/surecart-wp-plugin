e11a09ed618b3b72a753fda470db1187deaabdd3
Andre Gagnon <ajgagnon@uwalumni.com>
2024-05-14 17:05:35 -0500

    Add screen reader text to scratch amount

--- packages/blocks-next/src/blocks/product-scratch-price/view.php
+++ packages/blocks-next/src/blocks/product-scratch-price/view.php
@@ -1,1 +1,4 @@
-<div <?php echo get_block_wrapper_attributes(); ?> data-wp-text="context.product.scratch_display_amount">></div>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<span class="sc-screen-reader-text"><?php esc_html_e('Compare to', 'surecart');?></span>
+	<span data-wp-text="context.product.scratch_display_amount"></span>
+</div>
