4cb4e76471f567eb6795e1276a5c927fba6a8542
Andre Gagnon <ajgagnon@uwalumni.com>
2024-05-16 09:24:52 -0500

    Remove redundant class, accessibility

--- packages/blocks-next/src/blocks/cart-header/view.php
+++ packages/blocks-next/src/blocks/cart-header/view.php
@@ -1,11 +1,12 @@
-<div
-    class="wp-block-surecart-cart-header-v2"
-    <?php echo get_block_wrapper_attributes( array( 'style' => $style ) ); ?>
->
-    <?php echo wp_kses( SureCart::svg()->get('arrow-right', [ 'class' => 'wp-block-surecart-cart-header-v2__close' ] ), sc_allowed_svg_html() ); ?>
+<div <?php echo get_block_wrapper_attributes( array( 'style' => $style ) ); ?>>
+	<span class="wp-block-surecart-cart-header-v2__close" role="button" tabindex="0">
+    	<?php echo wp_kses( SureCart::svg()->get('arrow-right' ), sc_allowed_svg_html() ); ?>
+	</span>
+
     <span class="wp-block-surecart-cart-header-v2__title">
         <?php echo esc_html( $attributes['text'] ); ?>
     </span>
+
     <span class="sc-tag sc-tag--default">
         <span data-wp-text="state.getItemsCount"></span>
     </span>
