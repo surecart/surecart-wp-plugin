18b34e7ba424056ffa4b8698c779aac71eb5dd5e
Andre Gagnon <ajgagnon@uwalumni.com>
2024-05-17 10:48:00 -0500

    phpcs

--- packages/blocks-next/src/blocks/product-list/view.php
+++ packages/blocks-next/src/blocks/product-list/view.php
@@ -1,21 +1,24 @@
 <div
-    <?php echo get_block_wrapper_attributes(); ?>
-    data-wp-interactive='{ "namespace": "surecart/product-list" }'
-    data-wp-router-region="<?php echo 'products-' . $block_id ?>"
-    data-wp-init="callbacks.setQueryRef"
-    <?php echo  wp_interactivity_data_wp_context(
-		[
-			'nextPageLink' => $next_page_link,
-			'previousPageLink' => $previous_page_link,
-			'pages' => $pages,
-			'blockId' => $block_id,
-			'autoScroll' => (bool) $attributes['pagination_auto_scroll'] ?? true,
-			'products' => $products->data,
-			'hasProducts' => !empty($products->total()),
-		]
-	); ?>
+	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+	data-wp-interactive='{ "namespace": "surecart/product-list" }'
+	data-wp-router-region="<?php echo esc_attr( 'products-' . $block_id ); ?>"
+	<?php
+	echo wp_kses_data(
+		wp_interactivity_data_wp_context(
+			[
+				'nextPageLink'     => $next_page_link,
+				'previousPageLink' => $previous_page_link,
+				'pages'            => $pagination_pages,
+				'blockId'          => $block_id,
+				'autoScroll'       => (bool) $attributes['pagination_auto_scroll'] ?? true,
+				'products'         => $products->data,
+				'hasProducts'      => ! empty( $products->total() ),
+			]
+		)
+	);
+	?>
 >
-    <?php echo $content; ?>
+	<?php echo $content; ?>
 
 	<div class="sc-block-ui" data-wp-bind--hidden="!state.loading" hidden></div>
 </div>
