cd2ed8327a079b23d5dee51413159739a78d37b0
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-05-21 15:07:41 +0300

    Formatting the buy button

--- packages/blocks-next/src/blocks/product-buy-button/view.php
+++ packages/blocks-next/src/blocks/product-buy-button/view.php
@@ -1,20 +1,25 @@
-<div <?php echo get_block_wrapper_attributes(['class' => 'wp-block-button']); ?>
-	<?php echo wp_interactivity_data_wp_context( [
-		'checkoutUrl' =>  esc_url( \SureCart::pages()->url( 'checkout' ) ),
-		'text' => esc_attr($attributes['text'] ?? __('Add To Cart', 'surecart')),
-		'outOfStockText' => esc_attr($attributes['out_of_stock_text'] ?? __('Sold Out', 'surecart')),
-		'addToCart' => $attributes['add_to_cart'] ?? true,
-	 ] ); ?>>
+<div <?php echo get_block_wrapper_attributes( [ 'class' => 'wp-block-button' ] ); ?>
+	<?php
+	echo wp_interactivity_data_wp_context(
+		array(
+			'checkoutUrl'    => esc_url( \SureCart::pages()->url( 'checkout' ) ),
+			'text'           => esc_attr( $attributes['text'] ?? __( 'Add To Cart', 'surecart' ) ),
+			'outOfStockText' => esc_attr( $attributes['out_of_stock_text'] ?? __( 'Sold Out', 'surecart' ) ),
+			'addToCart'      => $attributes['add_to_cart'] ?? true,
+		)
+	);
+	?>
+	>
 	<button
-		class="wp-block-button__link wp-element-button sc-button__link <?php echo esc_attr($class); ?>"
-		style="<?php echo esc_attr($style); ?>"
+		class="wp-block-button__link wp-element-button sc-button__link <?php echo esc_attr( $class ); ?>"
+		style="<?php echo esc_attr( $style ); ?>"
 		data-wp-bind--disabled="state.isUnavailable"
 		data-wp-bind--value="state.checkoutUrl"
 		<?php echo $attributes['add_to_cart'] ? 'data-wp-class--sc-button__link--busy="state.busy"' : ''; ?>
 	>
 		<span class="sc-spinner" aria-hidden="true"></span>
 		<span class="sc-button__link-text" data-wp-text="state.buttonText">
-			<?php echo wp_kses_post( $attributes['text'] ?? __('Add To Cart', 'surecart') ); ?>
+			<?php echo wp_kses_post( $attributes['text'] ?? __( 'Add To Cart', 'surecart' ) ); ?>
 		</span>
 	</button>
 </div>
