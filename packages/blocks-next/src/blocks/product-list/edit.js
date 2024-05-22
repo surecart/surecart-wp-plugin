59431cf14e285af4ea9b70d520322de6285cf552
Rajkiran Bagal <rajkiranb@bsf.io>
2024-05-22 20:54:08 +0530

    Added Product Selector component that let us select products & also search while selecting

--- packages/blocks-next/src/blocks/product-list/edit.js
+++ packages/blocks-next/src/blocks/product-list/edit.js
@@ -9,18 +9,57 @@ import {
 	RangeControl,
 	UnitControl as __stableUnitControl,
 	__experimentalUnitControl,
+	Spinner,
+	Placeholder,
+	SelectControl,
 } from '@wordpress/components';
-
+import { useSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 import { TEMPLATE } from './template';
+import ProductSelector from '../../components/ProductSelector';
 
 export default function ProductListEdit({
 	setAttributes,
-	attributes: { limit },
+	attributes: { limit, ids, type },
 }) {
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps(blockProps, {
 		template: TEMPLATE,
 	});
+	const { products, loading } = useSelect((select) => {
+		const queryArgs = [
+			'surecart',
+			'product',
+			{
+				archived: false,
+				status: ['published'],
+				page: 1,
+				per_page: limit || 15,
+				ids: ids,
+			},
+		];
+		return {
+			products: select(coreStore).getEntityRecords(...queryArgs),
+			loading: select(coreStore).isResolving(
+				'getEntityRecords',
+				queryArgs
+			),
+		};
+	});
+	const onProductSelect = (product) => {
+		setAttributes({
+			ids: [...ids, product.id],
+		});
+	};
+	if (loading) {
+		return (
+			<Placeholder>
+				<Spinner />
+			</Placeholder>
+		);
+	}
+	console.log(ids);
+	console.log(products);
 	return (
 		<>
 			<InspectorControls>
@@ -33,6 +72,33 @@ export default function ProductListEdit({
 						min={1}
 						max={40}
 					/>
+				</PanelBody>
+				<PanelBody title={__('Products', 'surecart')}>
+					<SelectControl
+						label={__('Products To Show', 'surecart')}
+						value={type}
+						options={[
+							{
+								value: 'all',
+								label: __('All Products', 'surecart'),
+							},
+							{
+								value: 'featured',
+								label: __('Featured Products', 'surecart'),
+							},
+							{
+								value: 'custom',
+								label: __('Hand Pick Products', 'surecart'),
+							},
+						]}
+						onChange={(type) => setAttributes({ type })}
+					/>
+					{type === 'custom' && (
+						<ProductSelector
+							onProductSelect={onProductSelect}
+							currentSelectedIds={ids}
+						/>
+					)}
 				</PanelBody>
 			</InspectorControls>
 			<div {...innerBlocksProps} />
