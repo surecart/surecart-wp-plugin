9767a30df0708201de5f8ebf12c24f26a564a482
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-05-28 14:00:08 +0300

    update the docs for the product collection badges

--- packages/blocks-next/src/blocks/product-collection-badges/view.php
+++ packages/blocks-next/src/blocks/product-collection-badges/view.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * PHP file to use when rendering the product collection badges block type on the server to show on the front end.
+ *
+ * The following variables are exposed to the file:
+ *   $attributes (array): The block attributes.
+ *   $content (string): The block content.
+ *   $collections (array): The product collections.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+?>
 <div
 	<?php echo get_block_wrapper_attributes( array( 'class' => 'is-layout-flex sc-product-collection-badges' ) ); ?>
 	<?php echo wp_interactivity_data_wp_context( array( 'collections' => $collections ) ); ?>
@@ -6,7 +18,7 @@
 		data-wp-each--collection="context.collections"
 	>
 		<span data-wp-key="context.collection.id">
-			<?php echo wp_kses_post( $content ); ?>
+			<?php echo  $content; ?>
 		</span>
 	</template>
 </div>
