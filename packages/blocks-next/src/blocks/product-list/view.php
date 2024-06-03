25382862486d94ee4e3db796ad8c4fd996b3e51b
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-03 15:40:39 -0500

    Fix missing blockId context.

--- packages/blocks-next/src/blocks/product-list/view.php
+++ packages/blocks-next/src/blocks/product-list/view.php
@@ -1,5 +1,6 @@
 <div
 	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+	<?php echo wp_kses_data( wp_interactivity_data_wp_context( array( 'blockId' => $block_id ) ) ); ?>
 	data-wp-interactive='{ "namespace": "surecart/product-list" }'
 	data-wp-router-region="<?php echo esc_attr( 'products-' . $block_id ); ?>"
 >
