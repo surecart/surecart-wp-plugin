e7c2220b696a34fc2f1deafa6bb232d98342bb10
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-06 15:17:09 -0500

    phpcs fixes

--- packages/blocks-next/src/blocks/cart-menu-button/controller.php
+++ packages/blocks-next/src/blocks/cart-menu-button/controller.php
@@ -1,15 +1,15 @@
 <?php
 
-$style = '';
+$style  = '';
 $style .= ! empty( $attributes['backgroundColor'] )
 	? esc_attr( safecss_filter_attr( 'background-color:' . $attributes['backgroundColor'] ) ) . ';'
 	: '';
 $style .= ! empty( $attributes['textColor'] )
-    ? esc_attr( safecss_filter_attr( 'color:' . $attributes['textColor'] ) ) . ';'
-    : '';
+	? esc_attr( safecss_filter_attr( 'color:' . $attributes['textColor'] ) ) . ';'
+	: '';
 
-$form = \SureCart::cart()->getForm();
-$mode = \SureCart::cart()->getMode();
+$form      = \SureCart::cart()->getForm();
+$form_mode = \SureCart::cart()->getMode();
 
 // Stop if no form or mode found as for deletion.
 if ( empty( $form->ID ) || empty( $mode ) ) {
@@ -29,7 +29,7 @@ if ( ! \SureCart::cart()->isCartEnabled() ) {
  */
 $icon = apply_filters(
 	'sc_cart_menu_icon',
-	SureCart::svg()->get( $attributes['cart_icon'] ?? 'shopping-bag'),
+	SureCart::svg()->get( $attributes['cart_icon'] ?? 'shopping-bag' ),
 );
 
 // return the view.
--- packages/blocks-next/src/blocks/cart-menu-button/view.php
+++ packages/blocks-next/src/blocks/cart-menu-button/view.php
@@ -1,10 +1,10 @@
 <a
-    data-wp-interactive='{ "namespace": "surecart/checkout" }'
-    <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
-    data-wp-on--click="surecart/checkout::actions.toggleCartSidebar"
-    cart-menu-always-shown='<?php echo esc_attr( ! empty( $attributes['cart_menu_always_shown'] ) ? 'true' : 'false' ); ?>'
-    form-id='<?php echo esc_attr( $form->ID ); ?>'
-    mode='<?php echo esc_attr( $mode ); ?>'
+	data-wp-interactive='{ "namespace": "surecart/checkout" }'
+	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+	data-wp-on--click="surecart/checkout::actions.toggleCartSidebar"
+	cart-menu-always-shown='<?php echo esc_attr( ! empty( $attributes['cart_menu_always_shown'] ) ? 'true' : 'false' ); ?>'
+	form-id='<?php echo esc_attr( $form->ID ); ?>'
+	mode='<?php echo esc_attr( $form_mode ); ?>'
 >
-    <?php echo wp_kses( $icon, sc_allowed_svg_html() ); ?>
+	<?php echo wp_kses( $icon, sc_allowed_svg_html() ); ?>
 </a>
