4cd4b33c6402865ba5dc7dfea55c706844e5818c
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-06 15:24:48 -0500

    phpcs

--- packages/blocks-next/src/blocks/product-buy-button/view.php
+++ packages/blocks-next/src/blocks/product-buy-button/view.php
@@ -1,21 +1,30 @@
 <div
-	<?php echo wp_kses_data( get_block_wrapper_attributes(
-		[
-			'class' => 'wp-block-button ' . $class,
-			'style' => 'width: 100%; ' . $style,
-		]
-	) ); ?>
+	<?php echo wp_kses_data(
+		get_block_wrapper_attributes(
+			[
+				'class' => 'wp-block-button ' . $class,
+				'style' => 'width: 100%; ' . $style,
+			]
+		)
+	); ?>
 	data-wp-interactive='{ "namespace": "surecart/checkout" }'
 
-	<?php echo wp_interactivity_data_wp_context( [
-		'checkoutUrl' =>  esc_url( \SureCart::pages()->url( 'checkout' ) ),
-		'text' => esc_attr($attributes['text'] ?? __('Add To Cart', 'surecart')),
-		'outOfStockText' => esc_attr($attributes['out_of_stock_text'] ?? __('Sold Out', 'surecart')),
-		'addToCart' => $attributes['add_to_cart'] ?? true,
-	 ] ); ?>>
+	<?php
+	echo wp_kses_data(
+		wp_interactivity_data_wp_context(
+			[
+				'checkoutUrl'    => esc_url( \SureCart::pages()->url( 'checkout' ) ),
+				'text'           => esc_attr( $attributes['text'] ?? __( 'Add To Cart', 'surecart' ) ),
+				'outOfStockText' => esc_attr( $attributes['out_of_stock_text'] ?? __( 'Sold Out', 'surecart' ) ),
+				'addToCart'      => $attributes['add_to_cart'] ?? true,
+			]
+		)
+	);
+	?>
+	 >
 	<button
-		class="wp-block-button__link wp-element-button sc-button__link <?php echo esc_attr($class); ?>"
-		style="<?php echo esc_attr($style); ?>"
+		class="wp-block-button__link wp-element-button sc-button__link <?php echo esc_attr( $class ); ?>"
+		style="<?php echo esc_attr( $style ); ?>"
 		data-wp-bind--disabled="state.isUnavailable"
 		data-wp-bind--value="state.checkoutUrl"
 		data-wp-on--click="surecart/checkout::actions.addToCart"
@@ -23,7 +32,7 @@
 	>
 		<span class="sc-spinner" aria-hidden="true"></span>
 		<span class="sc-button__link-text">
-			<?php echo wp_kses_post( $attributes['text'] ?? __('Add To Cart', 'surecart') ); ?>
+			<?php echo wp_kses_post( $attributes['text'] ?? __( 'Add To Cart', 'surecart' ) ); ?>
 		</span>
 	</button>
 </div>
