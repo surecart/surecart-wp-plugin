101271ff731c0e2e33d3ab57e31f3b5e57040dd3
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-06 15:21:17 -0500

    phpcs fixes

--- packages/blocks-next/src/blocks/cart-submit/controller.php
+++ packages/blocks-next/src/blocks/cart-submit/controller.php
@@ -1,5 +1,5 @@
 <?php
-$style = '';
+$style  = '';
 $style .= ! empty( $attributes['border'] )
 	? esc_attr( safecss_filter_attr( 'border' . $attributes['border'] ) ) . ';'
 	: '';
@@ -10,8 +10,8 @@ $style .= ! empty( $attributes['sectionBackgroundColor'] )
 	? esc_attr( safecss_filter_attr( 'background-color:' . $attributes['sectionBackgroundColor'] ) ) . ';'
 	: '';
 $style .= ! empty( $attributes['textColor'] )
-    ? esc_attr( safecss_filter_attr( 'color:' . $attributes['textColor'] ) ) . ';'
-    : '';
+	? esc_attr( safecss_filter_attr( 'color:' . $attributes['textColor'] ) ) . ';'
+	: '';
 
 // Return the view.
 return 'file:./view.php';
