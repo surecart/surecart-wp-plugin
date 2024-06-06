4dfb2a7028bcb95d6c7edd7d6b8b1e1ed10c1afb
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-06 15:21:50 -0500

    phpcs fixes

--- packages/blocks-next/src/blocks/cart-submit/view.php
+++ packages/blocks-next/src/blocks/cart-submit/view.php
@@ -1,16 +1,17 @@
 <div
-    class="wp-block-button sc-cart-submit__wrapper"
-    <?php echo wp_kses_data( get_block_wrapper_attributes(
-		array(
-			'style' => $style,
+	class="wp-block-button sc-cart-submit__wrapper"
+	<?php echo wp_kses_data(
+		get_block_wrapper_attributes(
+			array(
+				'style' => $style,
+			)
 		)
-	) ); ?>
+	); ?>
 >
-    <a
-        href="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
-        class="wp-block-button wp-block-button__link wp-element-button sc-button"
-    >
-        <span><?php echo wp_kses_post( $attributes['text'] ); ?></span>
-    </a>
-
+	<a
+		href="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
+		class="wp-block-button wp-block-button__link wp-element-button sc-button"
+	>
+		<span><?php echo wp_kses_post( $attributes['text'] ); ?></span>
+	</a>
 </div>
