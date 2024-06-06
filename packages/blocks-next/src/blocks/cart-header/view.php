e45ecf8b8eadb9bcde39e8da7121940f3c8497e2
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-06 15:12:39 -0500

    phpcs fixes

--- packages/blocks-next/src/blocks/cart-header/view.php
+++ packages/blocks-next/src/blocks/cart-header/view.php
@@ -1,22 +1,24 @@
 <div
-    <?php echo wp_kses_data( get_block_wrapper_attributes(
-		array(
-			'style' => $style,
+	<?php echo wp_kses_data(
+		get_block_wrapper_attributes(
+			array(
+				'style' => $style,
+			)
 		)
-	) ); ?>
+	); ?>
 >
 	<button
-        class="wp-block-surecart-cart-header-v2__close"
-        data-wp-on--click="surecart/dialog::actions.toggle"
-    >
-    	<?php echo wp_kses( SureCart::svg()->get('arrow-right' ), sc_allowed_svg_html() ); ?>
+		class="wp-block-surecart-cart-header-v2__close"
+		data-wp-on--click="surecart/dialog::actions.toggle"
+	>
+		<?php echo wp_kses( SureCart::svg()->get( 'arrow-right' ), sc_allowed_svg_html() ); ?>
 	</button>
 
-    <span class="wp-block-surecart-cart-header-v2__title">
-        <?php echo esc_html( $attributes['text'] ); ?>
-    </span>
+	<span class="wp-block-surecart-cart-header-v2__title">
+		<?php echo esc_html( $attributes['text'] ); ?>
+	</span>
 
-    <span class="sc-tag sc-tag--default">
-        <span data-wp-text="surecart/checkout::state.getItemsCount"></span>
-    </span>
+	<span class="sc-tag sc-tag--default">
+		<span data-wp-text="surecart/checkout::state.getItemsCount"></span>
+	</span>
 </div>
