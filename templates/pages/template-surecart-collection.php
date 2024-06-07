2105565b97878b5db2055cf629f6aed4fd8a2b53
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-07 15:12:09 -0500

    Change our collection template to use block_template_part

--- templates/pages/template-surecart-collection.php
+++ templates/pages/template-surecart-collection.php
@@ -11,13 +11,10 @@ echo '<style>
 	margin-right: auto;
 }
 </style>';
-
-$template = get_term_meta( get_queried_object_id(), '_wp_page_template_part', true );
-$part = get_block_template( $template, 'wp_template_part' );
 ?>
 <div class="wp-block-group is-layout-constrained" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
 	<div class="wp-block-group alignwide">
-		<?php echo surecart_get_the_block_template_html( $part->content ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+		<?php block_template_part( get_term_meta( get_queried_object_id(), '_wp_page_template_part', true ) ) ?>
 	</div>
 </div>
 <?php
