45dff50ac274f3c9e2d78c588a927ff7e8f2be36
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-06-10 13:28:33 +0300

    migration for the title block

--- app/config.php
+++ app/config.php
@@ -205,7 +205,6 @@ return array(
 		\SureCartBlocks\Blocks\Dashboard\Deprecated\CustomerCharges\Block::class,
 
 		\SureCartBlocks\Blocks\Product\Description\Block::class,
-		\SureCartBlocks\Blocks\Product\Title\Block::class,
 		\SureCartBlocks\Blocks\Product\Price\Block::class,
 		\SureCartBlocks\Blocks\Product\PriceChoices\Block::class,
 		\SureCartBlocks\Blocks\Product\VariantChoices\Block::class,
