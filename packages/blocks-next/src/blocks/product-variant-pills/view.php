a1b9ab39dafb6abdaaf66ec0e3bd8ee7a17eed6d
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-11 13:36:44 -0500

    Move block_wrapper_attributes first

--- packages/blocks-next/src/blocks/product-variant-pills/view.php
+++ packages/blocks-next/src/blocks/product-variant-pills/view.php
@@ -1,8 +1,8 @@
 <div class="sc-product-variants__wrapper">
 	<?php foreach ( $product->variant_options->data as $key => $option ) : ?>
 		<div
-			<?php echo wp_kses_data( wp_interactivity_data_wp_context( array( 'optionNumber' => (int) $key + 1 ) ) ); ?>
 			<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+			<?php echo wp_kses_data( wp_interactivity_data_wp_context( array( 'optionNumber' => (int) $key + 1 ) ) ); ?>
 		>
 			<label class="sc-form-label">
 				<?php echo wp_kses_post( $option->name ); ?>
