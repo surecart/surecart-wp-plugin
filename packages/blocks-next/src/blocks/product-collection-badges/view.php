845d37e533314122e38eb392e3de3b78054c888a
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-11 08:25:35 -0500

    phpcs fixes

--- packages/blocks-next/src/blocks/product-collection-badges/view.php
+++ packages/blocks-next/src/blocks/product-collection-badges/view.php
@@ -11,14 +11,14 @@
  */
 ?>
 <div
-	<?php echo get_block_wrapper_attributes( array( 'class' => 'is-layout-flex sc-product-collection-badges' ) ); ?>
-	<?php echo wp_interactivity_data_wp_context( array( 'collections' => $collections ) ); ?>
+	<?php echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => 'is-layout-flex sc-product-collection-badges' ) ) ); ?>
+	<?php echo wp_kses_data( wp_interactivity_data_wp_context( array( 'collections' => $collections ) ) ); ?>
 >
 	<template
 		data-wp-each--collection="context.collections"
 	>
 		<span data-wp-key="context.collection.id">
-			<?php echo $content; ?>
+			<?php echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 		</span>
 	</template>
 </div>
