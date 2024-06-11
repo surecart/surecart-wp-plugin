8547f5d4c8102939fd636940faed46428a3141b8
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-11 08:19:49 -0500

    Move button text initial state.

--- packages/blocks-next/src/blocks/product-buy-button/controller.php
+++ packages/blocks-next/src/blocks/product-buy-button/controller.php
@@ -10,5 +10,13 @@ if ( ! empty( $attributes['width'] ) ) {
 	$class .= ' ' . 'has-custom-width sc-button__width-' . $attributes['width'];
 }
 
+wp_interactivity_state(
+	'surecart/product-page',
+	array(
+		'buttonText' => $attributes['text'] ?? ( $attributes['add_to_cart'] ? esc_html__( 'Add to Cart', 'surecart' ) : esc_html__( 'Buy Now', 'surecart' ) ),
+	)
+);
+
+
 // return the view.
 return 'file:./view.php';
