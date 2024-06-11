b15480251bb1e7ff5351f36b71d70c12d81595d2
Andre Gagnon <ajgagnon@uwalumni.com>
2024-06-11 13:23:02 -0500

    Remove unused comments.

--- packages/blocks-next/src/blocks/product-collection-badges/view.php
+++ packages/blocks-next/src/blocks/product-collection-badges/view.php
@@ -1,17 +1,3 @@
-<?php
-/**
- * PHP file to use when rendering the product collection badges block type on the server to show on the front end.
- *
- * The following variables are exposed to the file:
- *   $attributes (array): The block attributes.
- *   $content (string): The block content.
- *   $collections (array): The product collections.
- *
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-
-?>
-
 <ul <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php
 	foreach ( $collections as $collection ) :
