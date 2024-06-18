bb505f73fb4013c8c7763c3defeff4fcb93a53d3
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-06-18 12:40:47 +0300

    add price to context for selection purposes

--- packages/blocks-next/src/blocks/product-price-choice-template/view.php
+++ packages/blocks-next/src/blocks/product-price-choice-template/view.php
@@ -1,6 +1,7 @@
 <div
 	<?php
 		echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => 'sc-choice' ) ) );
+		echo wp_kses_data( wp_interactivity_data_wp_context( array( 'price' => $price ) ) );
 	?>
 	data-wp-on--click="callbacks.setPrice"
 	data-wp-class--sc-choice--checked="state.isPriceSelected"
