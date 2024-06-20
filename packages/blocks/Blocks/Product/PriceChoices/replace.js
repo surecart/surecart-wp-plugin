3f0112d99cd883d235efff92c3032ea728c167d7
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-06-20 08:49:42 +0300

    Create the block replacers

---
+++ packages/blocks/Blocks/Product/PriceChoices/replace.js
@@ -0,0 +1,24 @@
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { createBlock } from '@wordpress/blocks';
+import { useSelect, useDispatch } from '@wordpress/data';
+
+export default ({ clientId, attributes }) => {
+	const block = useSelect(
+		(select) => select(blockEditorStore).getBlock(clientId || ''),
+		[clientId]
+	);
+	const { replaceBlock } = useDispatch(blockEditorStore);
+
+	// if the block is not set return.
+	if (!block?.name || !replaceBlock || !clientId) {
+		return;
+	}
+
+	replaceBlock(clientId, [
+		createBlock('surecart/product-price-choices-v2', {
+			limit: attributes?.limit,
+			style: attributes?.style,
+			type: attributes?.type,
+		}),
+	]);
+};
