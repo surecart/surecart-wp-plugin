8caab95e41e08c72f40012187b8779ee7c985bef
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-06-25 19:02:36 +0600

    Fixed Checkout button styles

--- packages/blocks-next/src/blocks/cart-submit/view.php
+++ packages/blocks-next/src/blocks/cart-submit/view.php
@@ -1,17 +1,19 @@
-<div
-	class="wp-block-button sc-cart-submit__wrapper"
-	<?php echo wp_kses_data(
-		get_block_wrapper_attributes(
-			array(
-				'style' => $style,
+<div class="sc-cart-submit__wrapper wp-block-buttons">
+	<div
+		class="wp-block-button"
+		<?php echo wp_kses_data(
+			get_block_wrapper_attributes(
+				array(
+					'style' => $style,
+				)
 			)
-		)
-	); ?>
->
-	<a
-		href="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
-		class="wp-block-button wp-block-button__link wp-element-button sc-button"
+		); ?>
 	>
-		<span><?php echo wp_kses_post( $attributes['text'] ); ?></span>
-	</a>
+		<a
+			href="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
+			class="wp-block-button__link wp-element-button sc-button"
+		>
+			<span><?php echo wp_kses_post( $attributes['text'] ); ?></span>
+		</a>
+	</div>
 </div>
