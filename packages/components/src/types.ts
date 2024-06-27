6ce59f213a7eb5645c62d4baa10ff1e903fd7413
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-06-27 11:10:17 +0300

    correct the types for purchasable status

--- packages/components/src/types.ts
+++ packages/components/src/types.ts
@@ -446,7 +446,7 @@ export interface LineItem extends Object {
   updated_at: number;
   price?: Price;
   price_id: string;
-  purchasable_status: string;
+  purchasable_status: 'price_gone' | 'price_old_version' | 'variant_mising' | 'variant_gone' | 'variant_old_version' | 'out_of_stock' | 'exceeds_purchase_limit' | 'purchasable';
   purchasable_status_display: string;
   variant_options: Array<string>;
   variant?: Variant;
