d97ed6e986e97f7d7abfbe11189284de5cac298e
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-06-27 10:28:43 +0300

    Update icons

--- packages/blocks-next/src/blocks/price-amount/index.js
+++ packages/blocks-next/src/blocks/price-amount/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies
  */
-import { layout as icon } from '@wordpress/icons';
+import { currencyDollar as icon } from '@wordpress/icons';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
