2aecd19b7c23c3a5b4107e32520700383b8e53b7
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-02 12:50:39 +0300

    Remove unused error logs and styles

--- packages/blocks-next/src/blocks/product-variant-pills/controller.php
+++ packages/blocks-next/src/blocks/product-variant-pills/controller.php
@@ -1,11 +1,4 @@
 <?php
-$styles = sc_get_block_styles();
-$style  = $styles['css'] ?? '';
-$class  = $styles['classnames'] ?? '';
-
-error_log( print_r( $styles, true ) );
-error_log( print_r( $class, true ) );
-
 // get product from initial state.
 $product = sc_get_product();
 
