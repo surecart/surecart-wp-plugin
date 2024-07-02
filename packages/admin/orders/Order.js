5f5a3f097a7f56ff4a4275db25df6c231deb9bc4
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-07-02 18:35:22 +0600

    Moved models logics to Sidebar instead Order

--- packages/admin/orders/Order.js
+++ packages/admin/orders/Order.js
@@ -39,22 +39,9 @@ import Sidebar from './Sidebar';
 import Fulfillment from './modules/Fulfillment';
 import CreateReturnRequest from './modules/ReturnRequest/CreateReturnRequest';
 import ReturnItems from './modules/ReturnRequest/ReturnItems';
-import EditShippingAddressModal from './modules/EditShippingAddressModal';
-import EditBillingAddressModal from './modules/EditBillingAddressModal';
-import Confirm from '../components/confirm';
-import { checkoutOrderExpands } from '../util/orders';
-
-const modals = {
-	EDIT_SHIPPING_ADDRESS: 'EDIT_SHIPPING_ADDRESS',
-	CONFIRM_DELETE_ADDRESS: 'CONFIRM_DELETE_ADDRESS',
-	EDIT_BILLING_ADDRESS: 'EDIT_BILLING_ADDRESS',
-	CONFIRM_DELETE_BILLING_ADDRESS: 'CONFIRM_DELETE_BILLING_ADDRESS',
-};
 
 export default () => {
 	const [modal, setModal] = useState();
-	const [saving, setSaving] = useState(false);
-	const [error, setError] = useState(null);
 	const id = useSelect((select) => select(dataStore).selectPageId());
 
 	const { receiveEntityRecords } = useDispatch(coreStore);
@@ -242,36 +229,6 @@ export default () => {
 
 	const menuItems = getMenuItems(order?.status);
 
-	const deleteConfirmMessage = __(
-		'Are you sure? This cannot be undone.',
-		'surecart'
-	);
-
-	const onDeleteAddress = async (data, successMessage) => {
-		try {
-			setSaving(true);
-			const checkout = await apiFetch({
-				path: addQueryArgs(
-					`/surecart/v1/checkouts/${order?.checkout?.id}`,
-					{
-						expand: checkoutOrderExpands,
-					}
-				),
-				method: 'PATCH',
-				data,
-			});
-			receiveEntityRecords('surecart', 'order', checkout.order);
-			createSuccessNotice(successMessage, {
-				type: 'snackbar',
-			});
-			setModal('');
-		} catch (e) {
-			setError(e);
-		} finally {
-			setSaving(false);
-		}
-	};
-
 	return (
 		<UpdateModel
 			title={
@@ -332,83 +289,12 @@ export default () => {
 				</ScDropdown>
 			}
 			sidebar={
-				<>
-					<Sidebar
-						order={order}
-						checkout={order?.checkout}
-						customer={order?.checkout?.customer}
-						loading={!hasLoadedOrder}
-						modal={modal}
-						setModal={setModal}
-						modals={modals}
-					/>
-
-					{!!modal ? (
-						<>
-							<EditShippingAddressModal
-								open={modal === modals.EDIT_SHIPPING_ADDRESS}
-								shippingAddress={
-									order?.checkout?.shipping_address
-								}
-								onRequestClose={() => setModal('')}
-								checkoutId={order?.checkout?.id}
-							/>
-
-							<Confirm
-								open={modal === modals.CONFIRM_DELETE_ADDRESS}
-								onRequestClose={() => setModal('')}
-								onConfirm={() =>
-									onDeleteAddress(
-										{
-											shipping_address: {},
-										},
-										__(
-											'Shipping address deleted.',
-											'surecart'
-										)
-									)
-								}
-								loading={saving}
-								error={error}
-							>
-								{deleteConfirmMessage}
-							</Confirm>
-
-							<EditBillingAddressModal
-								open={modal === modals.EDIT_BILLING_ADDRESS}
-								billingAddress={
-									order?.checkout?.billing_address
-								}
-								onRequestClose={() => setModal('')}
-								checkoutId={order?.checkout?.id}
-							/>
-
-							<Confirm
-								open={
-									modal ===
-									modals.CONFIRM_DELETE_BILLING_ADDRESS
-								}
-								onRequestClose={() => setModal('')}
-								onConfirm={() =>
-									onDeleteAddress(
-										{
-											billing_matches_shipping: false,
-											billing_address: {},
-										},
-										__(
-											'Billing address deleted.',
-											'surecart'
-										)
-									)
-								}
-								loading={saving}
-								error={error}
-							>
-								{deleteConfirmMessage}
-							</Confirm>
-						</>
-					) : null}
-				</>
+				<Sidebar
+					order={order}
+					checkout={order?.checkout}
+					customer={order?.checkout?.customer}
+					loading={!hasLoadedOrder}
+				/>
 			}
 		>
 			<>
--- packages/admin/orders/Sidebar.js
+++ packages/admin/orders/Sidebar.js
@@ -1,13 +1,68 @@
+/**
+ * External dependencies.
+ */
+import { Fragment, useState } from '@wordpress/element';
+import { store as coreStore } from '@wordpress/core-data';
+import { store as noticesStore } from '@wordpress/notices';
+import { useDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+
 import Customer from './modules/Customer';
 import Phone from './modules/Phone';
 import MetaData from './modules/MetaData';
 import Purchases from './modules/Purchases';
 import TaxInfo from './modules/TaxInfo';
-import { Fragment } from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
 import ViewAddress from '../components/address/ViewAddress';
+import EditShippingAddressModal from './modules/EditShippingAddressModal';
+import EditBillingAddressModal from './modules/EditBillingAddressModal';
+import Confirm from '../components/confirm';
+import { checkoutOrderExpands } from '../util/orders';
 
-export default ({ setModal, order, checkout, customer, loading, modals }) => {
+const modals = {
+	EDIT_SHIPPING_ADDRESS: 'EDIT_SHIPPING_ADDRESS',
+	CONFIRM_DELETE_ADDRESS: 'CONFIRM_DELETE_ADDRESS',
+	EDIT_BILLING_ADDRESS: 'EDIT_BILLING_ADDRESS',
+	CONFIRM_DELETE_BILLING_ADDRESS: 'CONFIRM_DELETE_BILLING_ADDRESS',
+};
+
+export default ({ order, checkout, customer, loading }) => {
+	const [modal, setModal] = useState('');
+	const [saving, setSaving] = useState(false);
+	const [error, setError] = useState(null);
+
+	const { receiveEntityRecords } = useDispatch(coreStore);
+	const { createSuccessNotice } = useDispatch(noticesStore);
+
+	const deleteConfirmMessage = __(
+		'Are you sure? This cannot be undone.',
+		'surecart'
+	);
+
+	const onDeleteAddress = async (data, successMessage) => {
+		try {
+			setSaving(true);
+			const checkout = await apiFetch({
+				path: addQueryArgs(
+					`/surecart/v1/checkouts/${order?.checkout?.id}`,
+					{
+						expand: checkoutOrderExpands,
+					}
+				),
+				method: 'PATCH',
+				data,
+			});
+			receiveEntityRecords('surecart', 'order', checkout.order);
+			createSuccessNotice(successMessage, {
+				type: 'snackbar',
+			});
+			setModal('');
+		} catch (e) {
+			setError(e);
+		} finally {
+			setSaving(false);
+		}
+	};
+
 	return (
 		<Fragment>
 			<Customer customer={customer} loading={loading} />
@@ -45,6 +100,59 @@ export default ({ setModal, order, checkout, customer, loading, modals }) => {
 			)}
 			<Purchases checkoutId={checkout?.id} />
 			<MetaData order={order} loading={loading} />
+
+			{!!modal ? (
+				<>
+					<EditShippingAddressModal
+						open={modal === modals.EDIT_SHIPPING_ADDRESS}
+						shippingAddress={checkout?.shipping_address}
+						onRequestClose={() => setModal('')}
+						checkoutId={checkout?.id}
+					/>
+
+					<Confirm
+						open={modal === modals.CONFIRM_DELETE_ADDRESS}
+						onRequestClose={() => setModal('')}
+						onConfirm={() =>
+							onDeleteAddress(
+								{
+									shipping_address: {},
+								},
+								__('Shipping address deleted.', 'surecart')
+							)
+						}
+						loading={saving}
+						error={error}
+					>
+						{deleteConfirmMessage}
+					</Confirm>
+
+					<EditBillingAddressModal
+						open={modal === modals.EDIT_BILLING_ADDRESS}
+						billingAddress={checkout?.billing_address}
+						onRequestClose={() => setModal('')}
+						checkoutId={checkout?.id}
+					/>
+
+					<Confirm
+						open={modal === modals.CONFIRM_DELETE_BILLING_ADDRESS}
+						onRequestClose={() => setModal('')}
+						onConfirm={() =>
+							onDeleteAddress(
+								{
+									billing_matches_shipping: false,
+									billing_address: {},
+								},
+								__('Billing address deleted.', 'surecart')
+							)
+						}
+						loading={saving}
+						error={error}
+					>
+						{deleteConfirmMessage}
+					</Confirm>
+				</>
+			) : null}
 		</Fragment>
 	);
 };
