5e9ece68e362f59a7178bf0ad94eb3f9d814b2f7
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-02 14:40:21 -0500

    Enhance to use current block.

--- app/src/Models/Blocks/ProductListBlock.php
+++ app/src/Models/Blocks/ProductListBlock.php
@@ -39,8 +39,8 @@ class ProductListBlock {
 	 *
 	 * @param \WP_Block $block The block.
 	 */
-	public function __construct( \WP_Block $block ) {
-		$this->block = $block;
+	public function __construct( \WP_Block $block = null ) {
+		$this->block = $block ? $block : \WP_Block_Supports::$block_to_render;
 		$this->url   = \SureCart::block()->urlParams( 'products' )->setInstanceId( $block->context['surecart/product-list/block_id'] ?? '' );
 	}
 
