e714ab1b3e2a5e5e1bb2cbbd2c736a8cfadae182
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-02 16:22:00 -0500

    Use tag classes for badges

--- packages/blocks-next/src/blocks/product-collection-tag/view.php
+++ packages/blocks-next/src/blocks/product-collection-tag/view.php
@@ -1,3 +1,10 @@
-<a <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> href="<?php echo esc_url( $url ); ?>">
+<a <?php echo wp_kses_data(
+	get_block_wrapper_attributes(
+		[
+			'class' => 'sc-tag sc-tag--default sc-tag--medium',
+			'style' => 'cursor: pointer; text-decoration: none;',
+		]
+	)
+); ?> href="<?php echo esc_url( $url ); ?>">
 	<?php echo wp_kses_post( $collection->name ); ?>
 </a>
