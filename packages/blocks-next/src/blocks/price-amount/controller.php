29e801c5d0b4c88bbfc009f8c76476eff272ab43
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-03 14:28:29 -0500

    Show interval count on price amount

--- packages/blocks-next/src/blocks/price-amount/controller.php
+++ packages/blocks-next/src/blocks/price-amount/controller.php
@@ -12,5 +12,7 @@ if ( is_wp_error( $price ) ) {
 // translators: %1$s: amount, %2$s: interval.
 $display_amount = sprintf( esc_attr__( '%1$s %2$s', 'surecart' ), $price->display_amount, $price->short_interval_text );
 
+$display_amount .= $price->short_interval_count_text;
+
 // return the view.
 return 'file:./view.php';
