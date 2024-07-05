9a8f1786ab703a227bc85fea48dd37ce3680efcb
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-06 00:39:54 +0300

    Fix the migration and make layouts to one column at smaller screens

--- app/src/BlockLibrary/ProductPriceChoicesMigrationService.php
+++ app/src/BlockLibrary/ProductPriceChoicesMigrationService.php
@@ -54,7 +54,7 @@ class ProductPriceChoicesMigrationService {
 	 */
 	public function renderPriceAmountTrialAndSetupFee() {
 		$text_color        = $this->attributes['textColor'] ?? '#8a8a8a';
-		$this->block_html .= '<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->';
+		$this->block_html .= '<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->';
 		$this->block_html .= '<div class="wp-block-group">';
 		$this->block_html .= '<!-- wp:surecart/price-amount {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-3"}}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"accent-3"} /-->';
 		$this->block_html .= '<!-- wp:surecart/price-trial {"style":{"color":{"text":"' . $text_color . '"},"elements":{"link":{"color":{"text":"' . $text_color . '"}}}},"fontSize":"small"} /-->';
@@ -90,14 +90,10 @@ class ProductPriceChoicesMigrationService {
 		$this->block_html .= '<!-- wp:surecart/product-price-chooser ' . wp_json_encode( $choices_attributes ) . ' -->';
 		$this->block_html .= '<!-- wp:surecart/product-price-choices-template {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|20"}},"layout":{"type":"grid","columnCount":2,"justifyContent":"stretch","orientation":"vertical","flexWrap":"nowrap","verticalAlignment":"center"}} -->';
 		$this->block_html .= '<!-- wp:surecart/product-price-choice-template ' . wp_json_encode( $template_attributes ) . ' -->';
-		$this->block_html .= '<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->';
-		$this->block_html .= '<div class="wp-block-group">';
 		$this->renderPriceName();
 		if ( $this->attributes['show_price'] ) {
 			$this->renderPriceAmountTrialAndSetupFee();
 		}
-		$this->block_html .= '</div>';
-		$this->block_html .= '<!-- /wp:group -->';
 		$this->block_html .= '<!-- /wp:surecart/product-price-choice-template -->';
 		$this->block_html .= '<!-- /wp:surecart/product-price-choices-template -->';
 		$this->block_html .= '<!-- /wp:surecart/product-price-chooser -->';
