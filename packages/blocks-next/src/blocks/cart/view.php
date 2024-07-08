9ff5cd172296309951fc87012e018dbf44558050
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-08 09:16:27 -0500

    Formatting

--- packages/blocks-next/src/blocks/cart/view.php
+++ packages/blocks-next/src/blocks/cart/view.php
@@ -18,13 +18,15 @@
 		class="sc-drawer"
 		data-wp-on--click="surecart/cart::actions.closeOverlay"
 		aria-labelledby="sc-cart-title"
-		<?php echo wp_kses_data(
+		<?php
+		echo wp_kses_data(
 			get_block_wrapper_attributes(
 				array(
 					'style' => $style,
 				)
 			)
-		); ?>
+		);
+		?>
 	>
 		<div class="sc-drawer__wrapper">
 			<!-- Cart alert -->
