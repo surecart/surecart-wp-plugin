df2d278540f1689517ec471582bcd82ce11d1b05
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-08 14:39:42 -0500

    Early return on price chooser, not template

---
+++ packages/blocks-next/src/blocks/product-price-chooser/controller.php
@@ -0,0 +1,10 @@
+<?php
+// get product from initial state.
+$product = sc_get_product();
+
+// make sure we have a product.
+if ( empty( $product->id ) || ! $product->has_multiple_prices ) {
+	return '';
+}
+
+return 'file:./view.php';
