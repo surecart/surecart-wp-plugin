54bd47096e03d60f82ff9b40f8ba5854e2d9c3ea
Rajkiran Bagal <rajkiranb@bsf.io>
2024-07-09 19:45:23 +0530

    Removed the Make title a link as it is not needed & is messing the  markup

--- packages/blocks-next/src/blocks/product-title/edit.js
+++ packages/blocks-next/src/blocks/product-title/edit.js
@@ -10,7 +10,6 @@ import {
 	AlignmentControl,
 	BlockControls,
 	useBlockProps,
-	InspectorControls,
 } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { useEntityRecord } from '@wordpress/core-data';
@@ -19,10 +18,9 @@ import { useEntityRecord } from '@wordpress/core-data';
  * Internal dependencies
  */
 import HeadingLevelDropdown from '../../components/HeadingLebelDropdown';
-import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
 
 export default ({
-	attributes: { level, textAlign, isLink, rel, linkTarget },
+	attributes: { level, textAlign },
 	setAttributes,
 	context: { 'surecart/productId': productId },
 }) => {
@@ -54,38 +52,6 @@ export default ({
 					}}
 				/>
 			</BlockControls>
-
-			<InspectorControls>
-				<PanelBody title={__('Settings', 'surecart')}>
-					<ToggleControl
-						__nextHasNoMarginBottom
-						label={__('Make title a link')}
-						onChange={(isLink) => setAttributes({ isLink })}
-						checked={isLink}
-					/>
-
-					{isLink && (
-						<>
-							<ToggleControl
-								__nextHasNoMarginBottom
-								label={__('Open in new tab')}
-								onChange={(value) =>
-									setAttributes({
-										linkTarget: value ? '_blank' : '_self',
-									})
-								}
-								checked={linkTarget === '_blank'}
-							/>
-							<TextControl
-								__nextHasNoMarginBottom
-								label={__('Link rel')}
-								value={rel}
-								onChange={(rel) => setAttributes({ rel })}
-							/>
-						</>
-					)}
-				</PanelBody>
-			</InspectorControls>
 
 			<TagName {...blockProps}>
 				{product?.title?.raw || __('Product Name', 'surecart')}
