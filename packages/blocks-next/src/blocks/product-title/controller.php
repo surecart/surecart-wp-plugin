e00a25ed83c21c70a90369f746bd214397842250
Rajkiran Bagal <rajkiranb@bsf.io>
2024-07-09 19:50:37 +0530

    Fixed the text align issue

--- packages/blocks-next/src/blocks/product-title/controller.php
+++ packages/blocks-next/src/blocks/product-title/controller.php
@@ -1,4 +1,5 @@
 <?php
 $rel     = ! empty( $attributes['rel'] ) ? $attributes['rel'] : '';
+$text_align = ! empty( $attributes['textAlign'] ) ? 'has-text-align-' . $attributes['textAlign'] : 'has-text-align-left';
 
 return 'file:./view.php';
