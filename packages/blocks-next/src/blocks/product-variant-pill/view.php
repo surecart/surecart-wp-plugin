48a7f829554410a0ed734e9cf07fee575293df57
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-10 16:38:47 -0500

    Simplify block nesting.

--- packages/blocks-next/src/blocks/product-variant-pill/view.php
+++ packages/blocks-next/src/blocks/product-variant-pill/view.php
@@ -5,16 +5,18 @@ printf(
 	esc_attr( $attributes['highlight_text'] ?? '' ),
 	esc_attr( $attributes['highlight_border'] ?? '' )
 );
-
 ?>
+
 <button
 	<?php echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => 'sc-pill-option__button' ) ) ); ?>
+	<?php echo wp_kses_data( wp_interactivity_data_wp_context( array( 'option_value' => $block->context['value'] ) ) ); ?>
 	type="button"
-	data-wp-text="context.option_value"
 	data-wp-on--click="callbacks.setOption"
 	data-wp-class--sc-pill-option__button--selected="state.isOptionSelected"
 	data-wp-class--sc-pill-option__button--disabled="state.isOptionUnavailable"
 	data-wp-bind--aria-checked="state.isOptionSelected"
 	data-wp-bind--aria-disabled="state.isOptionUnavailable"
 	role="radio"
-></button>
+>
+	<?php echo esc_html( $block->context['value'] ); ?>
+</button>
--- packages/blocks-next/src/blocks/product-variant-pills/edit.js
+++ packages/blocks-next/src/blocks/product-variant-pills/edit.js
@@ -1,35 +1,29 @@
 import { __ } from '@wordpress/i18n';
 
-import { useBlockProps } from '@wordpress/block-editor';
-import {
-	useInnerBlocksProps as __stableUseInnerBlocksProps,
-	__experimentalUseInnerBlocksProps,
-} from '@wordpress/block-editor';
+import TemplateListEdit from '../../components/TemplateListEdit';
 
-export default () => {
-	const blockProps = useBlockProps();
-	const useInnerBlocksProps = __stableUseInnerBlocksProps
-		? __stableUseInnerBlocksProps
-		: __experimentalUseInnerBlocksProps;
+const TEST_VARIANTS = [
+	{
+		name: 'Red',
+		id: 1,
+		'surecart/productVariantPill/name': 'Red',
+		'surecart/productVariantPill/selected': true,
+	},
+	{ name: 'Blue', id: 2, 'surecart/productVariantPill/name': 'Blue' },
+	{ name: 'Green', id: 3, 'surecart/productVariantPill/name': 'Green' },
+];
 
-	const innerBlocksProps = useInnerBlocksProps(
-		{},
-		{
-			allowedBlocks: ['surecart/product-variant-pills-wrapper'],
-			renderAppender: false,
-			template: [['surecart/product-variant-pills-wrapper']],
-		}
-	);
-
+export default ({ __unstableLayoutClassNames, clientId }) => {
 	return (
-		<div class="sc-product-variants__wrapper">
-			<div {...blockProps}>
-				<label className="sc-form-label">
-					{__('Colors', 'surecart')}
-				</label>
-
-				<div {...innerBlocksProps}></div>
-			</div>
+		<div>
+			<label class="sc-form-label">Color</label>
+			<TemplateListEdit
+				template={[['surecart/product-variant-pill']]}
+				blockContexts={TEST_VARIANTS}
+				className={`sc-pill-option__wrapper ${__unstableLayoutClassNames}`}
+				clientId={clientId}
+				renderAppender={false}
+			/>
 		</div>
 	);
 };
