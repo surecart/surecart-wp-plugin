0dc096fcfa06221c4cd89f28c6386e0cb69502f4
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-11 08:38:40 -0500

    Remove alignment

--- packages/blocks-next/src/blocks/product-title/view.php
+++ packages/blocks-next/src/blocks/product-title/view.php
@@ -1,4 +1,4 @@
 <?php $html_tag = 'h' . (int) ( $attributes['level'] ?? 1 ); ?>
-<<?php echo esc_html( $html_tag ); ?> <?php echo wp_kses_data( get_block_wrapper_attributes( [ 'class' => $text_align ] ) ); ?>>
+<<?php echo esc_html( $html_tag ); ?> <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php the_title(); ?>
 </<?php echo esc_html( $html_tag ); ?>>
