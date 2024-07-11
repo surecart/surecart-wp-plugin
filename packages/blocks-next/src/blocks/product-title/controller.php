8f623652e86867011e9d50e5c30e3762b26a6e15
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-11 08:38:17 -0500

    Remove unused controller

--- packages/blocks-next/src/blocks/product-title/controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-$rel        = ! empty( $attributes['rel'] ) ? $attributes['rel'] : '';
-$text_align = ! empty( $attributes['textAlign'] ) ? 'has-text-align-' . $attributes['textAlign'] : 'has-text-align-left';
-
-return 'file:./view.php';
