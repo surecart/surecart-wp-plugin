9ade00de9ed8cae1bd663700bc8429c36d64cc25
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-14 15:24:56 +0300

    use the attach block props to fix the issue on whether to attach the block props

--- packages/blocks-next/src/blocks/product-variant-pills/edit.js
+++ packages/blocks-next/src/blocks/product-variant-pills/edit.js
@@ -25,6 +25,7 @@ export default ({ clientId }) => {
 				className={`sc-pill-option__wrapper `}
 				clientId={clientId}
 				renderAppender={false}
+				attachBlockProps={false}
 			/>
 		</div>
 	);
