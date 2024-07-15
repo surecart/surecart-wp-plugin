1ba814d5e8e6926193d65a3976a668071b1e8763
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-07-15 21:48:21 +0600

    Convert to form submit and support ad hoc amount

--- packages/blocks/Blocks/AddToCartButton/Block.php
+++ packages/blocks/Blocks/AddToCartButton/Block.php
@@ -4,8 +4,9 @@ namespace SureCartBlocks\Blocks\AddToCartButton;
 
 use SureCart\Models\Form;
 use SureCart\Models\Price;
+
 /**
- * Logout Button Block.
+ * AddToCart Button Block.
  */
 class Block extends \SureCartBlocks\Blocks\BuyButton\Block {
 	/**
@@ -74,28 +75,56 @@ class Block extends \SureCartBlocks\Blocks\BuyButton\Block {
 				echo wp_kses_data(
 					wp_interactivity_data_wp_context(
 						[
-							'formId'    => intval( $form->ID ),
-							'mode'      => esc_attr( Form::getMode( $form->ID ) ),
-							'priceId'   => $attributes['price_id'] ?? null,
-							'variantId' => $attributes['variant_id'] ?? null,
+							'formId'                  => intval( $form->ID ),
+							'mode'                    => esc_attr( Form::getMode( $form->ID ) ),
+							'priceId'                 => $attributes['price_id'] ?? null,
+							'variantId'               => $attributes['variant_id'] ?? null,
+							'currencySymbol'          => $price->currency_symbol,
+							'adHocAmount'             => null,
+							'isZeroDecimal'           => $price->is_zero_decimal,
+							'convertedAdHocMinAmount' => $price->converted_ad_hoc_min_amount,
+							'convertedAdHocMaxAmount' => $price->converted_ad_hoc_max_amount,
 						]
 					)
 				);
 				echo wp_kses_data( get_block_wrapper_attributes() );
 			?>
 		>
-			<div class="wp-block-button">
-				<button
-					class="wp-block-button__link wp-element-button sc-button"
-					data-wp-on--click="actions.addToCartByPriceOrVariant"
-					data-wp-class--sc-button__link--busy="state.loading"
-				>
-					<span class="sc-spinner"></span>
-					<span class="sc-button__link-text">
-						<?php echo wp_kses_post( $attributes['button_text'] ); ?>
-					</span>
-				</button>
-			</div>
+			<form class="sc-form" data-wp-on--submit="actions.addToCartByPriceOrVariant">
+				<?php if ( $price->ad_hoc ) : ?>
+					<label for="sc-product-custom-amount" class="sc-form-label">
+						<?php echo wp_kses_post( $attributes['ad_hoc_label'] ?? esc_html_e( 'Amount', 'surecart' ) ); ?>
+					</label>
+					<div class="sc-input-group" style="margin-bottom: 20px;">
+						<span class="sc-input-group-text" id="basic-addon1" data-wp-text="context.currencySymbol"></span>
+
+						<input
+							class="sc-form-control"
+							id="sc-product-custom-amount"
+							type="number"
+							required
+							placeholder="<?php echo esc_attr( $attributes['placeholder'] ?? '' ); ?>"
+							data-wp-bind--min="context.convertedAdHocMinAmount"
+							data-wp-bind--max="context.convertedAdHocMaxAmount"
+							data-wp-bind--value="context.adHocAmount"
+							data-wp-on--input="actions.setAdHocAmount"
+						/>
+					</div>
+				<?php endif; ?>
+
+				<div class="wp-block-button">
+					<button
+						type="submit"
+						class="wp-block-button__link wp-element-button sc-button"
+						data-wp-class--sc-button__link--busy="state.loading"
+					>
+						<span class="sc-spinner"></span>
+						<span class="sc-button__link-text">
+							<?php echo wp_kses_post( $attributes['button_text'] ); ?>
+						</span>
+					</button>
+				</div>
+			</form>
 		</div>
 		<?php
 		return ob_get_clean();
