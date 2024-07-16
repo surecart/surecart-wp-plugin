8a9850d6b133854e94f45466f93caefcdd2c0d90
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-16 18:02:39 +0300

    Add the elementor hook to add the product page wrapper

--- app/src/WordPress/PostTypes/ProductPostTypeService.php
+++ app/src/WordPress/PostTypes/ProductPostTypeService.php
@@ -73,6 +73,39 @@ class ProductPostTypeService {
 			// validate FSE template and return single if invalid.
 			add_filter( 'template_include', array( $this, 'validateFSETemplate' ), 10, 1 );
 		}
+
+		add_action( 'elementor/frontend/the_content', array( $this, 'addProductPageWrapper' ), 10 );
+	}
+
+	/**
+	 * Add the product page wrapper.
+	 *
+	 * @param string $content The content.
+	 *
+	 * @return string
+	 */
+	public function addProductPageWrapper( $content ) {
+		if ( ! is_singular( 'sc_product' ) ) {
+			return $content;
+		}
+
+		// check if the product page wrapper is not already added.
+		if ( false === strpos( $content, '<form class="wp-block-surecart-product-page"' ) ) {
+			$content = '<!-- wp:surecart/product-page -->' . $content . '<!-- /wp:surecart/product-page -->';
+		}
+
+		// check if the custom amount block is not already added.
+		if ( false === strpos( $content, 'class="wp-block-surecart-product-selected-price-ad-hoc-amount"' ) ) {
+			$content = str_replace(
+				'<div class="wp-block-button wp-block-surecart-product-buy-button"',
+				'<!-- wp:surecart/product-selected-price-ad-hoc-amount /-->' . PHP_EOL . '<div class="wp-block-button wp-block-surecart-product-buy-button"',
+				$content
+			);
+		}
+
+		$content = do_blocks( $content );
+
+		return $content;
 	}
 
 	/**
