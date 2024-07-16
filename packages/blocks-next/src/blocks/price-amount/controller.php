05dd3eaebafdff912362522f732c9a4c001feda9
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-16 16:10:04 +0300

    Add the screen reader text for the price choices

--- packages/blocks-next/src/blocks/price-amount/controller.php
+++ packages/blocks-next/src/blocks/price-amount/controller.php
@@ -10,9 +10,12 @@ if ( is_wp_error( $price ) ) {
 
 // get the display amount.
 // translators: %1$s: amount, %2$s: interval.
-$display_amount = sprintf( esc_attr__( '%1$s %2$s', 'surecart' ), $price->display_amount, $price->short_interval_text );
-
-$display_amount .= $price->short_interval_count_text;
+$display_amount     = sprintf( esc_attr__( '%1$s %2$s', 'surecart' ), $price->display_amount, $price->short_interval_text );
+$display_amount    .= $price->short_interval_count_text;
+$screen_reader_text = sprintf( esc_attr__( 'Price: %1$s %2$s', 'surecart' ), $price->display_amount, $price->interval_text );
+if ( ! empty( $price->payments_text ) ) {
+	$screen_reader_text .= ' ' . $price->payments_text;
+}
 
 // return the view.
 return 'file:./view.php';
