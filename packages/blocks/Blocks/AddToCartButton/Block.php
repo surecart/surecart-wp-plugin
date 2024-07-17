87a4ba64cb607e95d065b2634a294d0bc08e4a3e
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-17 16:58:09 -0500

    Change store usage to product-page.

--- packages/blocks/Blocks/AddToCartButton/Block.php
+++ packages/blocks/Blocks/AddToCartButton/Block.php
@@ -23,10 +23,19 @@ class Block extends \SureCartBlocks\Blocks\BuyButton\Block {
 			return '';
 		}
 
-		$price = Price::find( $attributes['price_id'] );
+		$price = Price::with( [ 'product', 'product.variants' ] )->find( $attributes['price_id'] );
 		if ( empty( $price->id ) ) {
 			return '';
 		}
+
+		$product  = $price->product;
+		$variants = $attributes['variant_id'] ? array_filter(
+			$product->variants->data ?? [],
+			function ( $variant ) use ( $attributes ) {
+				return $variant->id == $attributes['variant_id'];
+			}
+		) : null;
+		$variant  = $variants ? array_shift( $variants ) : null;
 
 		// need a form for checkout.
 		$form = \SureCart::forms()->getDefault();
@@ -70,23 +79,39 @@ class Block extends \SureCartBlocks\Blocks\BuyButton\Block {
 
 		ob_start(); ?>
 		<div
-			data-wp-interactive='{ "namespace": "surecart/checkout" }'
+			data-wp-interactive='{ "namespace": "surecart/product-page" }'
 			<?php
-				echo wp_kses_data(
-					wp_interactivity_data_wp_context(
-						[
-							'formId'        => intval( $form->ID ),
-							'mode'          => esc_attr( Form::getMode( $form->ID ) ),
-							'selectedPrice' => $price,
-							'priceId'       => $attributes['price_id'] ?? null,
-							'variantId'     => $attributes['variant_id'] ?? null,
-							'adHocAmount'   => null,
-						]
+			echo wp_kses_data(
+				wp_interactivity_data_wp_context(
+					array(
+						'formId'                       => \SureCart::forms()->getDefaultId(),
+						'mode'                         => \SureCart\Models\Form::getMode( \SureCart::forms()->getDefaultId() ),
+						'checkoutUrl'                  => \SureCart::pages()->url( 'checkout' ),
+						'product'                      => $product,
+						'prices'                       => [ $price ],
+						'selectedPrice'                => $price,
+						'variant_options'              => $product->variant_options->data ?? array(),
+						'variants'                     => $product->variants->data ?? array(),
+						'selectedVariant'              => $variant,
+						'quantity'                     => 1,
+						'selectedDisplayAmount'        => $product->display_amount,
+						'selectedScratchDisplayAmount' => ! empty( $product->initial_price ) ? $product->initial_price->scratch_display_amount : '',
+						'isOnSale'                     => ! empty( $product->initial_price ) ? $product->initial_price->is_on_sale : false,
+						'busy'                         => false,
+						'adHocAmount'                  => '',
+						'variantValues'                => (object) array_filter(
+							array(
+								'option_1' => $product->first_variant_with_stock->option_1 ?? null,
+								'option_2' => $product->first_variant_with_stock->option_2 ?? null,
+								'option_3' => $product->first_variant_with_stock->option_3 ?? null,
+							)
+						),
 					)
-				);
+				)
+			);
 			?>
 		>
-			<form class="sc-form" data-wp-on--submit="actions.addToCartByPriceOrVariant">
+			<form class="sc-form" data-wp-on--submit="callbacks.handleSubmit">
 				<?php if ( $price->ad_hoc ) : ?>
 					<label for="sc-product-custom-amount" class="sc-form-label">
 						<?php echo wp_kses_post( $attributes['ad_hoc_label'] ?? esc_html_e( 'Amount', 'surecart' ) ); ?>
@@ -128,10 +153,10 @@ class Block extends \SureCartBlocks\Blocks\BuyButton\Block {
 					<button
 						type="submit"
 						class="<?php echo esc_attr( $class ); ?>"
-						data-wp-class--sc-button__link--busy="state.loading"
+						data-wp-class--sc-button__link--busy="context.busy"
 						style="<?php echo esc_attr( $styles ); ?>"
 					>
-						<span class="sc-spinner" aria-hidden="true" data-wp-bind--hidden="!state.loading" hidden></span>
+						<span class="sc-spinner" aria-hidden="true" data-wp-bind--hidden="!context.busy" hidden></span>
 						<span class="sc-button__link-text">
 							<?php echo wp_kses_post( $attributes['button_text'] ); ?>
 						</span>
