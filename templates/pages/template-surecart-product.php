156d58fa58f5002c346b69a029783de0f6ec3e53
Rajkiran Bagal <rajkiranb@bsf.io>
2024-07-19 21:04:14 +0530

    Updated CSS & Classes

--- templates/pages/template-surecart-product.php
+++ templates/pages/template-surecart-product.php
@@ -2,18 +2,19 @@
 get_header();
 global $content_width;
 echo '<style>
-.wp-block-group {
+.sc-template-wrapper,
+.sc-template-container {
 	width: 100%;
 }
-.is-layout-constrained .alignwide {
+.sc-template-wrapper .sc-template-container {
 	max-width: var(--wp--style--global--wide-size, ' . (int) ( $content_width ?? 1170 ) . 'px) !important;
 	margin-left: auto;
 	margin-right: auto;
 }
 </style>';
 ?>
-<div class="wp-block-group is-layout-constrained" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
-	<div class="wp-block-group alignwide">
+<div class="wp-block-group is-layout-constrained sc-template-wrapper" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
+	<div class="wp-block-group alignwide sc-template-container">
 		<?php echo surecart_get_the_block_template_html( apply_filters( 'the_content', get_the_content() ) ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
 	</div>
 </div>
