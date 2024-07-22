33251102fcf22c8ac4e0e4fc493d052d27b8969e
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-22 16:56:55 -0500

    Fix missing display amount

--- packages/blocks-next/src/blocks/price-amount/controller.php
+++ packages/blocks-next/src/blocks/price-amount/controller.php
@@ -6,6 +6,11 @@ if ( empty( $price ) || is_wp_error( $price ) ) {
 	return '';
 }
 
+// get the display amount.
+// translators: %1$s: amount, %2$s: interval.
+$display_amount  = sprintf( esc_attr__( '%1$s %2$s', 'surecart' ), $price->display_amount, $price->short_interval_text );
+$display_amount .= $price->short_interval_count_text;
+
 // translators: %1$s: amount, %2$s: interval.
 $screen_reader_text = sprintf( esc_attr__( 'Price: %1$s %2$s', 'surecart' ), $price->display_amount, $price->interval_text );
 if ( ! empty( $price->payments_text ) ) {
