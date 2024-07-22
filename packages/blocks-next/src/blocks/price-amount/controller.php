6fcbead30c3f7064beea02cf0ea33620af7ce4ed
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-22 16:10:33 -0500

    chore: Remove unused log

--- packages/blocks-next/src/blocks/price-amount/controller.php
+++ packages/blocks-next/src/blocks/price-amount/controller.php
@@ -6,10 +6,7 @@ if ( empty( $price ) || is_wp_error( $price ) ) {
 	return '';
 }
 
-// get the display amount.
 // translators: %1$s: amount, %2$s: interval.
-$display_amount     = sprintf( esc_attr__( '%1$s %2$s', 'surecart' ), $price->display_amount, $price->short_interval_text );
-$display_amount    .= $price->short_interval_count_text;
 $screen_reader_text = sprintf( esc_attr__( 'Price: %1$s %2$s', 'surecart' ), $price->display_amount, $price->interval_text );
 if ( ! empty( $price->payments_text ) ) {
 	$screen_reader_text .= ' ' . $price->payments_text;
