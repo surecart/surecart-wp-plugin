4e664cfeb89a4149be4c2b5ee78c134343bfd34d
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-07-24 14:10:03 +0300

    Add the description message to the collection link

--- packages/blocks-next/src/blocks/product-collection-tag/view.php
+++ packages/blocks-next/src/blocks/product-collection-tag/view.php
@@ -1,10 +1,17 @@
-<a <?php echo wp_kses_data(
-	get_block_wrapper_attributes(
-		[
-			'class' => 'sc-tag sc-tag--default sc-tag--medium',
-			'style' => 'cursor: pointer; text-decoration: none;',
-		]
-	)
-); ?> href="<?php echo esc_url( $url ); ?>">
+<a
+	<?php
+	echo wp_kses_data(
+		get_block_wrapper_attributes(
+			[
+				'class' => 'sc-tag sc-tag--default sc-tag--medium',
+				'style' => 'cursor: pointer; text-decoration: none;',
+			]
+		)
+	);
+	?>
+	href="<?php echo esc_url( $url ); ?>"
+	<?php // translators: You are currently on a %s collection link. ?>
+	aria-label="<?php echo esc_attr( sprintf( __( 'You are currently on a %s collection link' ), $collection->name ) ); ?>"
+>
 	<?php echo wp_kses_post( $collection->name ); ?>
 </a>
