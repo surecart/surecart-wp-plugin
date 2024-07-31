18736e26450655480ab2c70c2303deb670d3189c
Andre Gagnon <ajgagnon@uwalumni.com>
2024-07-31 09:24:43 -0500

    Remove unnecessary button text state

--- packages/blocks-next/src/blocks/product-buy-button/controller.php
+++ packages/blocks-next/src/blocks/product-buy-button/controller.php
@@ -10,13 +10,5 @@ if ( ! empty( $attributes['width'] ) ) {
 	$class .= ' ' . 'has-custom-width wp-block-button__width-' . $attributes['width'];
 }
 
-wp_interactivity_state(
-	'surecart/product-page',
-	array(
-		'buttonText' => $attributes['text'] ?? ( $attributes['add_to_cart'] ? esc_html__( 'Add to Cart', 'surecart' ) : esc_html__( 'Buy Now', 'surecart' ) ),
-	)
-);
-
-
 // return the view.
 return 'file:./view.php';
