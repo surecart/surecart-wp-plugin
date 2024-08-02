137d16bce20b9e9979a69c2853a887b400448c71
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-02 19:54:46 +0600

    Fixed cart items key issue

--- packages/blocks-next/src/blocks/cart-items/view.php
+++ packages/blocks-next/src/blocks/cart-items/view.php
@@ -11,7 +11,7 @@
 >
 	<template
 		data-wp-each--line_item="state.checkoutLineItems"
-		data-wp-key="context.line_item.id"
+		data-wp-each-key="context.line_item.id"
 	>
 		<div class="sc-product-line-item" role="listitem">
 			<div class="sc-product-line-item__item">
