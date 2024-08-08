9650165d4ddad090530fe8ee3221247dcee5df02
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-08 09:32:19 -0500

    Replace pattern

---
+++ packages/blocks-next/src/blocks/product-list/list-toolbar.js
@@ -0,0 +1,26 @@
+/**
+ * WordPress dependencies
+ */
+import { ToolbarGroup, ToolbarButton } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { usePatterns } from '../utils';
+
+export default ({ openPatternSelectionModal, name, clientId }) => {
+	const hasPatterns = !!usePatterns(clientId, name).length;
+
+	return (
+		<>
+			{hasPatterns && (
+				<ToolbarGroup className="wp-block-template-part__block-control-group">
+					<ToolbarButton onClick={openPatternSelectionModal}>
+						{__('Replace')}
+					</ToolbarButton>
+				</ToolbarGroup>
+			)}
+		</>
+	);
+};
