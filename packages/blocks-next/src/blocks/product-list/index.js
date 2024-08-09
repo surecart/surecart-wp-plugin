1e7e8ecbe0a0a9a837e745af947e4172bae50fc4
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-09 09:14:52 -0500

    Allow margin on product list.

--- packages/blocks-next/src/blocks/product-list/index.js
+++ packages/blocks-next/src/blocks/product-list/index.js
@@ -10,7 +10,7 @@ import { grid as icon } from '@wordpress/icons';
 import edit from './edit';
 import save from './save';
 import metadata from './block.json';
-import './styles.scss';
+import './style.scss';
 
 /**
  * Every block starts by registering a new block type definition.
