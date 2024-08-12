86acf3f12cb00c4cb67f313ae03a3cd842047ec2
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-13 02:48:01 +0600

    Confirm invoice status change to draft component

---
+++ packages/admin/invoices/components/ConfirmChangeInvoiceStatus.js
@@ -0,0 +1,53 @@
+/**
+ * WordPress dependencies.
+ */
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { __experimentalConfirmDialog as ConfirmDialog } from '@wordpress/components';
+
+/**
+ * Internal dependencies.
+ */
+import { ScBlockUi } from '@surecart/components-react';
+import Error from '../../components/Error';
+
+export default ({ onRequestClose, open, invoice, changeInvoiceStatus }) => {
+	const invoiceId = invoice?.id;
+	if (!invoiceId) {
+		return null;
+	}
+
+	const [error, setError] = useState(null);
+	const [changingStatus, setChangingStatus] = useState(false);
+
+	const onChangeInvoiceStatusToDraft = async () => {
+		try {
+			setChangingStatus(true);
+			await changeInvoiceStatus('draft');
+			onRequestClose();
+		} catch (e) {
+			setChangingStatus(false);
+			setError(e);
+		}
+	};
+
+	return (
+		<ConfirmDialog
+			isOpen={open}
+			onConfirm={() => {
+				onChangeInvoiceStatusToDraft();
+			}}
+			onCancel={onRequestClose}
+		>
+			<Error error={error} />
+			{__('Are you sure you want to change the status of this invoice to draft?', 'surecart')}
+			{changingStatus && (
+				<ScBlockUi
+					style={{ '--sc-block-ui-opacity': '0.75' }}
+					zIndex="9"
+					spinner
+				/>
+			)}
+		</ConfirmDialog>
+	);
+};
