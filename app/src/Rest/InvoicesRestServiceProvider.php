d09261da1d98c326eee050984fa3feccc0522efd
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-15 12:08:41 +0600

    Moved redundant get_schema functionalities to abstract class

--- app/src/Rest/InvoicesRestServiceProvider.php
+++ app/src/Rest/InvoicesRestServiceProvider.php
@@ -40,20 +40,6 @@ class InvoicesRestServiceProvider extends RestServiceProvider implements RestSer
 
 		register_rest_route(
 			"$this->name/v$this->version",
-			$this->endpoint . '/(?P<id>\S+)/finalize/',
-			[
-				[
-					'methods'             => \WP_REST_Server::EDITABLE,
-					'callback'            => $this->callback( $this->controller, 'finalize' ),
-					'permission_callback' => [ $this, 'finalize_permissions_check' ],
-				],
-				// Register our schema callback.
-				'schema' => [ $this, 'get_item_schema' ],
-			]
-		);
-
-		register_rest_route(
-			"$this->name/v$this->version",
 			$this->endpoint . '/(?P<id>\S+)/make_draft/',
 			[
 				[
@@ -79,37 +65,6 @@ class InvoicesRestServiceProvider extends RestServiceProvider implements RestSer
 				'schema' => [ $this, 'get_item_schema' ],
 			]
 		);
-	}
-
-	/**
-	 * Get our sample schema for a post.
-	 *
-	 * @return array The sample schema for a post
-	 */
-	public function get_item_schema() {
-		if ( $this->schema ) {
-			// Since WordPress 5.3, the schema can be cached in the $schema property.
-			return $this->schema;
-		}
-
-		$this->schema = [
-			// This tells the spec of JSON Schema we are using which is draft 4.
-			'$schema'    => 'http://json-schema.org/draft-04/schema#',
-			// The title property marks the identity of the resource.
-			'title'      => $this->endpoint,
-			'type'       => 'object',
-			// In JSON Schema you can specify object properties in the properties attribute.
-			'properties' => [
-				'id' => [
-					'description' => esc_html__( 'Unique identifier for the object.', 'surecart' ),
-					'type'        => 'string',
-					'context'     => [ 'view', 'edit', 'embed' ],
-					'readonly'    => true,
-				],
-			],
-		];
-
-		return $this->schema;
 	}
 
 	/**
