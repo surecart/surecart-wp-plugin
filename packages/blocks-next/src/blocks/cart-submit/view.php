c4ecffb75b3e341bc02d380f003acd36fb715e70
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-15 14:49:34 -0500

    Add loading indicator to cart button.

--- packages/blocks-next/src/blocks/cart-submit/view.php
+++ packages/blocks-next/src/blocks/cart-submit/view.php
@@ -11,9 +11,12 @@
 	<div class="wp-block-button">
 		<a
 			href="<?php echo esc_attr( \SureCart::pages()->url( 'checkout' ) ); ?>"
-			class="wp-block-button__link wp-element-button sc-button"
+			class="wp-block-button__link wp-element-button sc-button__link"
+			data-wp-bind--disabled="state.loading"
+			data-wp-class--sc-button__link--busy="state.loading"
 		>
-			<span><?php echo wp_kses_post( $attributes['text'] ); ?></span>
+			<span class="sc-spinner" aria-hidden="false"></span>
+			<span class="sc-button__link-text"><?php echo wp_kses_post( $attributes['text'] ); ?></span>
 		</a>
 	</div>
 </div>
