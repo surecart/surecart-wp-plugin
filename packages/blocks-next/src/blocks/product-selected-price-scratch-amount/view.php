36f5a194b771044c13493ac1b38c946502773463
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-19 15:23:55 -0500

    switch to span. fix formatting

--- packages/blocks-next/src/blocks/product-selected-price-scratch-amount/view.php
+++ packages/blocks-next/src/blocks/product-selected-price-scratch-amount/view.php
@@ -1,8 +1,8 @@
 <span class="sc-screen-reader-text" data-wp-bind--aria-hidden="!context.selectedPrice.scratch_display_amount"><?php esc_html_e( 'Was', 'surecart' ); ?> </span>
-<div
+<span
 	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
 	data-wp-text="context.selectedPrice.scratch_display_amount"
 	data-wp-bind--hidden="!state.isOnSale"
 	hidden
 >
-</div>
+</span>
