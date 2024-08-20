5d32d1bd01f619e981f63acb5447131ee81c4325
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-21 00:48:33 +0600

    Fixed server side loading of invoice

--- app/src/Controllers/Admin/Invoices/InvoicesViewController.php
+++ app/src/Controllers/Admin/Invoices/InvoicesViewController.php
@@ -4,9 +4,10 @@ namespace SureCart\Controllers\Admin\Invoices;
 
 use SureCart\Controllers\Admin\AdminController;
 use SureCart\Controllers\Admin\Invoices\InvoicesListTable;
+use SureCart\Models\Invoice;
 
 /**
- * Handles product admin requests.
+ * Handles invoice admin requests.
  */
 class InvoicesViewController extends AdminController {
 	/**
@@ -33,11 +34,29 @@ class InvoicesViewController extends AdminController {
 	}
 
 	/**
-	 * Coupons edit.
+	 * Edit a invoice.
 	 */
-	public function edit() {
+	public function edit( $request ) {
 		// enqueue needed script.
 		add_action( 'admin_enqueue_scripts', \SureCart::closure()->method( InvoiceScriptsController::class, 'enqueue' ) );
+
+		$invoice = null;
+		if ( $request->query( 'id' ) ) {
+			$invoice = Invoice::find( $request->query( 'id' ) );
+
+			if ( is_wp_error( $invoice ) ) {
+				wp_die( implode( ' ', array_map( 'esc_html', $invoice->get_error_messages() ) ) );
+			}
+		}
+
+		if ( ! empty( $invoice ) ) {
+			$this->preloadPaths(
+				[
+					'/surecart/v1/invoices/' . $invoice->id . '?context=edit',
+				]
+			);
+		}
+
 		// return view.
 		return '<div id="app"></div>';
 	}
