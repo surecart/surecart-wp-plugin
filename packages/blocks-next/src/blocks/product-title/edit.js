c28b489579cb176cd89f7c9d81e5e25842c6b211
Rajkiran Bagal <rajkiranb@bsf.io>
2024-08-21 16:30:13 +0530

    Added the Link Attribute & it's settings & rendering for the Product Title Block

--- packages/blocks-next/src/blocks/product-title/edit.js
+++ packages/blocks-next/src/blocks/product-title/edit.js
@@ -1,7 +1,12 @@
 /**
  * WordPress dependencies
  */
-import { BlockControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	BlockControls,
+	useBlockProps,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useEntityRecord } from '@wordpress/core-data';
 
@@ -11,7 +16,7 @@ import { useEntityRecord } from '@wordpress/core-data';
 import HeadingLevelDropdown from '../../components/HeadingLebelDropdown';
 
 export default ({
-	attributes: { level },
+	attributes: { level, isLink, rel, linkTarget },
 	setAttributes,
 	context: { postId },
 }) => {
@@ -33,7 +38,37 @@ export default ({
 					onChange={(level) => setAttributes({ level })}
 				/>
 			</BlockControls>
+			<InspectorControls>
+				<PanelBody title={__('Settings', 'surecart')}>
+					<ToggleControl
+						__nextHasNoMarginBottom
+						label={__('Make title a link')}
+						onChange={(isLink) => setAttributes({ isLink })}
+						checked={isLink}
+					/>
 
+					{isLink && (
+						<>
+							<ToggleControl
+								__nextHasNoMarginBottom
+								label={__('Open in new tab')}
+								onChange={(value) =>
+									setAttributes({
+										linkTarget: value ? '_blank' : '_self',
+									})
+								}
+								checked={linkTarget === '_blank'}
+							/>
+							<TextControl
+								__nextHasNoMarginBottom
+								label={__('Link rel')}
+								value={rel}
+								onChange={(rel) => setAttributes({ rel })}
+							/>
+						</>
+					)}
+				</PanelBody>
+			</InspectorControls>
 			<TagName {...blockProps}>
 				{product?.title?.raw || __('Product Name', 'surecart')}
 			</TagName>
