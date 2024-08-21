5ee87450eafad2723748eb0c45805ca4b61114f3
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-21 13:31:56 -0500

    Simplify link and button detection.

--- packages/blocks-next/src/blocks/product-template/controller.php
+++ packages/blocks-next/src/blocks/product-template/controller.php
@@ -6,10 +6,8 @@ $controller = new ProductListBlock( $block );
 $query      = $controller->query();
 
 // If we have a button or a link, we need to wrap the content in a form element.
-$html       = new \WP_HTML_Tag_Processor( $content );
-$has_button = (bool) $html->next_tag( 'button' );
-$html       = new \WP_HTML_Tag_Processor( $content );
-$has_link   = (bool) $html->next_tag( 'a' );
+$has_button = ( new \WP_HTML_Tag_Processor( $content ?? '' ) )->next_tag( 'button' );
+$has_link   = ( new \WP_HTML_Tag_Processor( $content ?? '' ) )->next_tag( 'a' );
 $html_tag   = $has_link || $has_button ? 'form' : 'a';
 
 return 'file:./view.php';
