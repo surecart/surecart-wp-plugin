8b9f3d8668ceae2e5d602a1edd5bfdbbf8283e47
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-22 09:06:21 -0500

    Cart submit button missing color styles.

--- packages/blocks-next/src/blocks/cart-submit/controller.php
+++ packages/blocks-next/src/blocks/cart-submit/controller.php
@@ -1,17 +1,19 @@
 <?php
-$style  = '';
-$style .= ! empty( $attributes['border'] )
+$wrapper_style  = '';
+$wrapper_style .= ! empty( $attributes['border'] )
 	? esc_attr( safecss_filter_attr( 'border-bottom:var(--sc-drawer-border)' ) ) . ';'
 	: '';
-$style .= ! empty( $attributes['padding'] )
+$wrapper_style .= ! empty( $attributes['padding'] )
 	? esc_attr( safecss_filter_attr( 'padding:' . $attributes['padding']['top'] . ' ' . $attributes['padding']['right'] . ' ' . $attributes['padding']['bottom'] . ' ' . $attributes['padding']['left'] ) ) . ';'
 	: '';
-$style .= ! empty( $attributes['sectionBackgroundColor'] )
+$wrapper_style .= ! empty( $attributes['sectionBackgroundColor'] )
 	? esc_attr( safecss_filter_attr( 'background-color:' . $attributes['sectionBackgroundColor'] ) ) . ';'
 	: '';
-$style .= ! empty( $attributes['textColor'] )
+$wrapper_style .= ! empty( $attributes['textColor'] )
 	? esc_attr( safecss_filter_attr( 'color:' . $attributes['textColor'] ) ) . ';'
 	: '';
+
+$styles = sc_get_block_styles();
 
 // Return the view.
 return 'file:./view.php';
