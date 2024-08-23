a6860b7c686587d4839678e9241d18b720d7194d
Andre Gagnon <ajgagnon@uwalumni.com>
2024-08-23 15:28:55 -0500

    Wrap upsell in the product page block.

--- packages/blocks/Blocks/Upsell/Upsell/Block.php
+++ packages/blocks/Blocks/Upsell/Upsell/Block.php
@@ -2,6 +2,7 @@
 
 namespace SureCartBlocks\Blocks\Upsell\Upsell;
 
+use SureCart\Models\Blocks\ProductPageBlock;
 use SureCartBlocks\Blocks\BaseBlock;
 
 /**
@@ -17,8 +18,14 @@ class Block extends BaseBlock {
 	 * @return string
 	 */
 	public function render( $attributes, $content ) {
+		$controller = new ProductPageBlock();
+		$state      = $controller->state();
+		$context    = $controller->context();
+		wp_interactivity_state( 'surecart/product-page', $state );
+
 		return wp_sprintf(
-			'<sc-upsell %s>%s</sc-upsell>',
+			'<div %s> <sc-upsell %s>%s</sc-upsell>',
+			wp_kses_data( wp_interactivity_data_wp_context( $context ) ),
 			get_block_wrapper_attributes(),
 			filter_block_content( $content, 'post' )
 		);
