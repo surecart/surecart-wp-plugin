dcfd246887b12ca14186a2aef5b818b53b36e8ec
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-26 20:12:01 +0600

    Invoice status remove popover

--- packages/admin/invoices/components/Status.js
+++ packages/admin/invoices/components/Status.js
@@ -4,79 +4,29 @@ import { css, jsx } from '@emotion/react';
 /**
  * External dependencies.
  */
-import { useMemo, useState } from '@wordpress/element';
-import { Dropdown, PanelRow } from '@wordpress/components';
+import { PanelRow } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '@wordpress/block-editor';
-import { capitalize } from 'lodash';
 
 /**
  * Internal dependencies.
  */
-import PostDropdownButton from '../../components/PostDropdownButton';
-import PostDropdownContent from '../../components/PostDropdownContent';
 import { ScInvoiceStatusBadge } from '@surecart/components-react';
 
 export default ({ status }) => {
-	// Use internal state instead of a ref to make sure that the component
-	// re-renders when the popover's anchor updates.
-	const [popoverAnchor, setPopoverAnchor] = useState(null);
-
-	// Memoize popoverProps to avoid returning a new object every time.
-	const popoverProps = useMemo(
-		() => ({ anchor: popoverAnchor, placement: 'bottom-end' }),
-		[popoverAnchor]
-	);
-
-	const renderContent = ({ onClose }) => (
-		<PostDropdownContent>
-			<InspectorPopoverHeader
-				title={__('Invoice Status')}
-				onClose={onClose}
-				help={__(
-					'Status defines whether the invoice is paid, draft or open.',
-					'surecart'
-				)}
-			/>
-
-			<ScInvoiceStatusBadge status={status} />
-		</PostDropdownContent>
-	);
-
 	return (
 		<PanelRow
-			css={css`
-				align-items: flex-start;
+			css={css`=
 				justify-content: space-between;
-				width: 100%;
 			`}
-			ref={setPopoverAnchor}
 		>
-			<span
+			<div>{__('Invoice Status', 'surecart')}</div>
+			<div
 				css={css`
-					display: block;
-					flex-shrink: 0;
-					padding: 6px 0;
-					width: 45%;
+					padding-right: var(--sc-spacing-large);
 				`}
 			>
-				{__('Status', 'surecart')}
-			</span>
-			<Dropdown
-				popoverProps={popoverProps}
-				className="edit-post-post-url__dropdown"
-				contentClassName="edit-post-post-url__dialog"
-				focusOnMount
-				renderToggle={({ isOpen, onToggle }) => (
-					<PostDropdownButton
-						isOpen={isOpen}
-						onClick={onToggle}
-						title={capitalize(status)}
-						ariaLabel={__('invoice status', 'surecart')}
-					/>
-				)}
-				renderContent={renderContent}
-			/>
+				<ScInvoiceStatusBadge status={status} />
+			</div>
 		</PanelRow>
 	);
 };
