25e2f60f6aedab08a951882bdeaf825a2a834826
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-26 20:20:14 +0600

    Change Receipt download UI to remove popover

--- packages/admin/invoices/components/ReceiptDownload.js
+++ packages/admin/invoices/components/ReceiptDownload.js
@@ -4,77 +4,37 @@ import { css, jsx } from '@emotion/react';
 /**
  * External dependencies.
  */
-import { useMemo, useState } from '@wordpress/element';
-import { Dropdown, PanelRow } from '@wordpress/components';
+import { PanelRow } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies.
  */
-import PostDropdownButton from '../../components/PostDropdownButton';
-import PostDropdownContent from '../../components/PostDropdownContent';
 import { ScButton, ScIcon } from '@surecart/components-react';
 
 export default ({ orderPdfUrl }) => {
-	// Use internal state instead of a ref to make sure that the component
-	// re-renders when the popover's anchor updates.
-	const [popoverAnchor, setPopoverAnchor] = useState(null);
-
-	// Memoize popoverProps to avoid returning a new object every time.
-	const popoverProps = useMemo(
-		() => ({ anchor: popoverAnchor, placement: 'bottom-end' }),
-		[popoverAnchor]
-	);
-
-	const renderContent = ({ onClose }) => (
-		<PostDropdownContent>
-			<InspectorPopoverHeader
-				title={__('Download Receipt')}
-				onClose={onClose}
-				help={__('The receipt for the order.', 'surecart')}
-			/>
-
-			<ScButton href={orderPdfUrl} target="_blank">
-				{__('Download')} &nbsp;
-				<ScIcon slot="prefix" name="download" />
-			</ScButton>
-		</PostDropdownContent>
-	);
-
 	return (
 		<PanelRow
-			css={css`
-				align-items: flex-start;
+			css={css`=
 				justify-content: space-between;
-				width: 100%;
 			`}
-			ref={setPopoverAnchor}
 		>
-			<span
+			<div>{__('Receipt', 'surecart')}</div>
+			<div
 				css={css`
-					display: block;
-					flex-shrink: 0;
-					padding: 6px 0;
-					width: 45%;
+					padding-right: var(--sc-spacing-x-small);
 				`}
 			>
-				{__('Receipt', 'surecart')}
-			</span>
-			<Dropdown
-				popoverProps={popoverProps}
-				className="edit-post-post-url__dropdown"
-				contentClassName="edit-post-post-url__dialog"
-				focusOnMount
-				renderToggle={({ isOpen, onToggle }) => (
-					<PostDropdownButton
-						isOpen={isOpen}
-						onClick={onToggle}
-						title={__('Download', 'surecart')}
-					/>
-				)}
-				renderContent={renderContent}
-			/>
+				<ScButton
+					type="text"
+					href={orderPdfUrl}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					{__('Download', 'surecart')}
+					<ScIcon slot="suffix" name="external-link" />
+				</ScButton>
+			</div>
 		</PanelRow>
 	);
 };
