652b0946602508adb8773790654f73dcb3bbd109
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-08-27 14:43:43 +0600

    Improved Issue Date component with popover with date picker

--- packages/admin/invoices/components/IssueDate.js
+++ packages/admin/invoices/components/IssueDate.js
@@ -4,18 +4,83 @@ import { css, jsx } from '@emotion/react';
 /**
  * External dependencies.
  */
-import { PanelRow } from '@wordpress/components';
+import { __experimentalInspectorPopoverHeader as InspectorPopoverHeader } from '@wordpress/block-editor';
+import { DatePicker, Dropdown, PanelRow } from '@wordpress/components';
+import { useState, useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies.
  */
 import { ScAlert, ScFormatDate } from '@surecart/components-react';
-import DatePicker from '../../components/DatePicker';
 import PostDropdownButton from '../../components/PostDropdownButton';
+import PostDropdownContent from '../../components/PostDropdownContent';
 
 export default ({ invoice, updateInvoice }) => {
 	const isDraftInvoice = invoice?.status === 'draft';
+
+	// Use internal state instead of a ref to make sure that the component
+	// re-renders when the popover's anchor updates.
+	const [popoverAnchor, setPopoverAnchor] = useState(null);
+
+	// Memoize popoverProps to avoid returning a new object every time.
+	const popoverProps = useMemo(
+		() => ({ anchor: popoverAnchor, placement: 'bottom-end' }),
+		[popoverAnchor]
+	);
+
+	const getTitle = () => {
+		return invoice?.issue_date ? (
+			<ScFormatDate
+				type="timestamp"
+				month="short"
+				day="numeric"
+				year="numeric"
+				date={invoice?.issue_date}
+			/>
+		) : isDraftInvoice ? (
+			__('Set Issue Date', 'surecart')
+		) : (
+			__('No Issue Date', 'surecart')
+		);
+	};
+
+	const renderContent = ({ onClose }) => {
+		return (
+			<PostDropdownContent>
+				<InspectorPopoverHeader
+					title={__('Issue Date', 'surecart')}
+					actions={
+						invoice?.issue_date
+							? [
+									{
+										label: __('Clear', 'surecart'),
+										onClick: () => {
+											updateInvoice({ issue_date: null });
+											onClose();
+										},
+									},
+							  ]
+							: []
+					}
+					onClose={onClose}
+				/>
+				<DatePicker
+					currentDate={
+						invoice?.issue_date
+							? new Date(invoice?.issue_date * 1000)
+							: null
+					}
+					onChange={(issue_date) => {
+						updateInvoice({
+							issue_date: Date.parse(issue_date) / 1000,
+						});
+						onClose();
+					}}
+				/>
+			</PostDropdownContent>
+		);
+	};
 
 	return (
 		<PanelRow
@@ -24,6 +89,7 @@ export default ({ invoice, updateInvoice }) => {
 				justify-content: space-between;
 				width: 100%;
 			`}
+			ref={setPopoverAnchor}
 		>
 			<span
 				css={css`
@@ -37,41 +103,27 @@ export default ({ invoice, updateInvoice }) => {
 			</span>
 
 			{isDraftInvoice ? (
-				<DatePicker
-					title={__('Choose an issue date', 'surecart')}
-					placeholder={__('Issue date', 'surecart')}
-					currentDate={
-						invoice?.issue_date
-							? new Date(invoice?.issue_date * 1000)
-							: null
-					}
-					onChoose={(issue_date) => {
-						updateInvoice({
-							issue_date: Date.parse(issue_date) / 1000,
-						});
-					}}
-					onClear={() => {
-						updateInvoice({ issue_date: null });
-					}}
-				>
-					<PostDropdownButton>
-						{invoice?.issue_date ? (
-							<ScFormatDate
-								type="timestamp"
-								month="short"
-								day="numeric"
-								year="numeric"
-								date={invoice?.issue_date}
-							/>
-						) : isDraftInvoice ? (
-							__('Set Issue Date', 'surecart')
-						) : (
-							__('No Issue Date', 'surecart')
-						)}
-					</PostDropdownButton>
-				</DatePicker>
+				<Dropdown
+					popoverProps={popoverProps}
+					className="edit-post-post-url__dropdown"
+					contentClassName="edit-post-post-url__dialog"
+					focusOnMount
+					renderToggle={({ isOpen, onToggle }) => (
+						<PostDropdownButton
+							isOpen={isOpen}
+							onClick={onToggle}
+							title={getTitle()}
+							ariaLabel={__('Issue Date', 'surecart')}
+						/>
+					)}
+					renderContent={renderContent}
+				/>
 			) : (
-				<div>
+				<div
+					css={css`
+						padding-right: var(--sc-spacing-large);
+					`}
+				>
 					{invoice?.issue_date ? (
 						<ScFormatDate
 							type="timestamp"
