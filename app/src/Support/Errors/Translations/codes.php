2090a6c130b4aa676d02c1fa5b53deb0267c9a33
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-09-07 04:30:27 +0600

    Added some translations for memo and footer

--- app/src/Support/Errors/Translations/codes.php
+++ app/src/Support/Errors/Translations/codes.php
@@ -148,4 +148,6 @@ return array(
 	'invoice.missing_automatic_collection_payment_method'  => __( 'Please add a valid payment method to enable automatic collection.', 'surecart' ),
 	'checkout.invoice.draft'                               => __( 'This invoice is draft and cannot be paid.', 'surecart' ),
 	'invoice.due_date.greater_than_or_equal_to'            => __( 'The due date must be greater than or equal to the issue date.', 'surecart' ),
+	'invoice.memo.too_long'                                => __( 'The memo is too long. Please enter a shorter memo. Maximum allowed length is 1000 characters.', 'surecart' ),
+	'invoice.footer.too_long'                              => __( 'The footer is too long. Please enter a shorter footer. Maximum allowed length is 1000 characters.', 'surecart' ),
 );
