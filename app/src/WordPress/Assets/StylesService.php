6fc84e786b4ea6864097084d790d6b3a5d7811c6
Andre Gagnon <ajgagnon@uwalumni.com>
2024-09-11 15:04:51 -0500

    Add darker styles

--- app/src/WordPress/Assets/StylesService.php
+++ app/src/WordPress/Assets/StylesService.php
@@ -94,6 +94,8 @@ class StylesService {
 		?>
 		:root:root {
 			--wp-admin-theme-color: #007cba;
+			--wp-admin-theme-color-darker-10: #1f63a0;
+			--wp-admin-theme-color-darker-20: #1a5080;
 			--sc-color-primary-500: var(--wp-admin-theme-color);
 			--sc-focus-ring-color-primary: var(
 				--wp-admin-theme-color
