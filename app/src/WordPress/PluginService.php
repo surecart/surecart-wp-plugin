5a2839f31e748033809c254e5a5ccfe6570fefbe
Rajkiran Bagal <rajkiranb@bsf.io>
2024-09-13 13:01:48 +0530

    SUR-1767 - Enhancement - Added plugin update notice architecture for major & critical updates.

--- app/src/WordPress/PluginService.php
+++ app/src/WordPress/PluginService.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Plugin service.
+ * 
  * @package   SureCartAppCore
  * @author    SureCart <support@surecart.com>
  * @copyright  SureCart
@@ -23,14 +25,91 @@ class PluginService {
 	protected $app = null;
 
 	/**
+	 * List of versions to show the update notice.
+	 *
+	 * @var array
+	 */
+	protected $show_update_notice_versions = [
+		'3.0.0',
+	];
+
+	/**
 	 * Constructor.
 	 *
-	 * @param Application $app
+	 * @param Application $app Application instance.
 	 */
 	public function __construct( $app ) {
 		$this->app = $app;
+		add_action( 'in_plugin_update_message-' . SURECART_PLUGIN_BASE, array( $this, 'updateMessage' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueueScripts' ) );
 	}
 
+	/**
+	 * Enqueue scripts.
+	 *
+	 * @return void
+	 */
+	public function enqueueScripts() {
+		$screen = get_current_screen();
+		if ( ! $screen || 'plugins' !== $screen->base ) {
+			return;
+		}
+
+		wp_enqueue_style( 'surecart-plugin-upgrade-notice', plugins_url( 'styles/plugin-upgrade-notice.css', SURECART_PLUGIN_FILE ), '', $this->version(), 'all' );
+	}
+
+	/**
+	 * Display the update message.
+	 *
+	 * @param array $plugin_data Plugin data.
+	 *
+	 * @return void
+	 */
+	public function updateMessage( $plugin_data ) {
+		 // Get the highest version in the list to compare with.
+		 $highest_version = max( $this->show_update_notice_versions );
+    
+		 // Early return if the new version is not greater than or equal to the highest version.
+		 if ( version_compare( $plugin_data['new_version'], $highest_version, '<' ) ) {
+			return;
+		}
+	
+		// Display the update warning if the condition is met.
+		$this->versionUpdateWarning();
+	}
+
+	/**
+	 * Display a warning if the plugin version has changed.
+	 *
+	 * @return void
+	 */
+	public function versionUpdateWarning() {
+		?>
+		<hr class="sc-major-update-warning__separator" />
+		<div class="sc-major-update-warning">
+			<div class="sc-major-update-warning__icon">
+				<span class="dashicons dashicons-info"></span>
+			</div>
+			<div>
+				<div class="sc-major-update-warning__title">
+					<?php echo esc_html__( 'Heads up, Please backup before upgrade!', 'surecart' ); ?>
+				</div>
+				<div class="sc-major-update-warning__message">
+					<?php
+					printf(
+						/* translators: %1$s Link open tag, %2$s: Link close tag. */
+						esc_html__( 'We’re excited to announce that the latest update brings significant improvements to your plugin experience! To ensure a smooth transition, we strongly recommend you %1$sbackup your site %2$s before proceeding with the update and perform the upgrade in a staging environment first.', 'surecart' ),
+						'<a href="https://surecart.com">',
+						'</a>'
+					);
+
+					?>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+	
 	/**
 	 * Get the plugin version
 	 *
