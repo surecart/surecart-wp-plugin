2c625e06e233f6bed60c470c3dc13bdb416a66ad
Rajkiran Bagal <rajkiranb@bsf.io>
2024-09-23 18:40:52 +0530

    SUR-1866 - Enhancement - Add separate new blocks for scratch amount & interval for price selector to prevent misconfiguration.

---
+++ packages/blocks-next/src/blocks/price-scratch-amount/view.php
@@ -0,0 +1,5 @@
+<span <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+	aria-label="<?php echo esc_html( $screen_reader_text ); ?>"
+>
+	<?php echo esc_html( $scratch_amount ); ?>
+</span>
