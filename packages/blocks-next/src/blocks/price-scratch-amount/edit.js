4d15181ee513ee5a731762d01b490d73c4ed5e04
Rajkiran Bagal <rajkiranb@bsf.io>
2024-09-23 20:55:38 +0530

    Updated scratch amount editor html

--- packages/blocks-next/src/blocks/price-scratch-amount/edit.js
+++ packages/blocks-next/src/blocks/price-scratch-amount/edit.js
@@ -1,19 +1,10 @@
 import { __ } from '@wordpress/i18n';
-
 import { useBlockProps } from '@wordpress/block-editor';
-import { getFormattedPrice } from '../../../../admin/util';
 
 export default () => {
 	const blockProps = useBlockProps({
 		className: 'sc-price__amount',
 	});
 
-	return (
-		<span {...blockProps}>
-			{getFormattedPrice({
-				amount: 2000,
-				currency: scData?.currency,
-			})}
-		</span>
-	);
+	return <span {...blockProps}>{scData?.currency_symbol}20</span>;
 };
