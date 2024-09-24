3c2fe13014c9fe98bcc147512b18ae70e4651b5e
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-09-24 16:45:50 +0300

    Collection tags styling

--- app/src/Integrations/Elementor/Widgets/CollectionTags.php
+++ app/src/Integrations/Elementor/Widgets/CollectionTags.php
@@ -56,13 +56,11 @@ class CollectionTags extends \Elementor\Widget_Base {
 	}
 
 	/**
-	 * Register the widget controls.
+	 * Add the collection tags style controls.
 	 *
 	 * @return void
 	 */
-	protected function register_controls() {
-
-		// Collection Tags Style.
+	private function add_collection_tags_style_controls() {
 		$this->start_controls_section(
 			'section_collection_tags_style',
 			array(
@@ -70,74 +68,169 @@ class CollectionTags extends \Elementor\Widget_Base {
 				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
 			)
 		);
-		// "spacing": {
-		// "blockGap": {
-		// "__experimentalDefault": "3px"
-		// },
-		// "margin": ["top", "bottom"],
-		// "padding": ["horizontal", "vertical"]
-		// },
-		// "__experimentalBorder": {
-		// "width": true,
-		// "color": true,
-		// "radius": true,
-		// "__experimentalDefaultControls": {
-		// "radius": true,
-		// "color": true,
-		// "width": true
-		// }
-		// }
+
 		$this->add_control(
-			'collection_tag_spacing',
-			[
-				'label'      => esc_html__( 'Spacing', 'surecart' ),
-				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', 'em', '%' ],
-				'selectors'  => [
-					'.wp-block-surecart-product-collection-tags' => 'gap: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+			'collection_tags_direction',
+			array(
+				'label'     => esc_html__( 'Direction', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => array(
+					'row'            => array(
+						'title' => esc_html__( 'Row', 'surecart' ),
+						'icon'  => 'eicon-arrow-right',
+					),
+					'column'         => array(
+						'title' => esc_html__( 'Column', 'surecart' ),
+						'icon'  => 'eicon-arrow-down',
+					),
+					'row-reverse'    => array(
+						'title' => esc_html__( 'Row Reverse', 'surecart' ),
+						'icon'  => 'eicon-arrow-left',
+					),
+					'column-reverse' => array(
+						'title' => esc_html__( 'Column Reverse', 'surecart' ),
+						'icon'  => 'eicon-arrow-up',
+					),
+				),
+				'default'   => 'row',
+				'selectors' => [
+					'.wp-block-surecart-product-collection-tags' => 'flex-direction: {{VALUE}};',
 				],
-			]
+			)
 		);
 
 		$this->add_control(
-			'collection_tag_margin',
-			[
-				'label'      => esc_html__( 'Margin', 'surecart' ),
-				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', 'em', '%' ],
-				'selectors'  => [
-					'.wp-block-surecart-product-collection-tags' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+			'collection_tags_justify',
+			array(
+				'label'     => esc_html__( 'Justify', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => array(
+					'flex-start'    => array(
+						'title' => esc_html__( 'Start', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-start-h',
+					),
+					'center'        => array(
+						'title' => esc_html__( 'Center', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-center-h',
+					),
+					'flex-end'      => array(
+						'title' => esc_html__( 'End', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-end-h',
+					),
+					'space-between' => array(
+						'title' => esc_html__( 'Space Between', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-space-between-h',
+					),
+					'space-around'  => array(
+						'title' => esc_html__( 'Space Around', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-space-around-h',
+					),
+					'space-evenly'  => array(
+						'title' => esc_html__( 'Space Evenly', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-justify-space-evenly-h',
+					),
+				),
+				'default'   => 'flex-start',
+				'selectors' => [
+					'.wp-block-surecart-product-collection-tags' => 'justify-content: {{VALUE}};',
 				],
-			]
+			)
 		);
 
 		$this->add_control(
-			'collection_tag_padding',
-			[
-				'label'      => esc_html__( 'Padding', 'surecart' ),
-				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', 'em', '%' ],
-				'selectors'  => [
-					'.wp-block-surecart-product-collection-tags' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+			'collection_tags_align',
+			array(
+				'label'     => esc_html__( 'Align', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => array(
+					'flex-start' => array(
+						'title' => esc_html__( 'Start', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-align-start-v',
+					),
+					'center'     => array(
+						'title' => esc_html__( 'Center', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-align-center-v',
+					),
+					'flex-end'   => array(
+						'title' => esc_html__( 'End', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-align-end-v',
+					),
+					'stretch'    => array(
+						'title' => esc_html__( 'Stretch', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-align-stretch-v',
+					),
+				),
+				'default'   => 'flex-start',
+				'selectors' => [
+					'.wp-block-surecart-product-collection-tags' => 'align-items: {{VALUE}};',
 				],
-			]
+			)
 		);
 
 		$this->add_control(
-			'collection_tag_border',
-			[
-				'label'      => esc_html__( 'Border', 'surecart' ),
-				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+			'collection_tags_gap',
+			array(
+				'label'      => esc_html__( 'Gap', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
 				'size_units' => [ 'px', 'em', '%' ],
-				'selectors'  => [
-					'.wp-block-surecart-product-collection-tags' => 'border: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				'range'      => array(
+					'px' => array(
+						'min'  => 0,
+						'step' => 1,
+						'max'  => 100,
+					),
+					'em' => array(
+						'min'  => 0,
+						'step' => 0.1,
+						'max'  => 10,
+					),
+					'%'  => array(
+						'min'  => 0,
+						'step' => 1,
+						'max'  => 100,
+					),
+				),
+				'default'    => array(
+					'size' => 3,
+					'unit' => 'px',
+				),
+				'selectors'  => array(
+					'.wp-block-surecart-product-collection-tags' => 'gap: {{SIZE}}{{UNIT}} !important;',
+				),
+			)
+		);
+
+		$this->add_control(
+			'collection_tags_wrap',
+			array(
+				'label'     => esc_html__( 'Wrap', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => array(
+					'nowrap' => array(
+						'title' => esc_html__( 'No Wrap', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-nowrap',
+					),
+					'wrap'   => array(
+						'title' => esc_html__( 'Wrap', 'surecart' ),
+						'icon'  => 'eicon-flex eicon-wrap',
+					),
+				),
+				'default'   => 'nowrap',
+				'selectors' => [
+					'.wp-block-surecart-product-collection-tags' => 'flex-wrap: {{VALUE}};',
 				],
-			]
+			)
 		);
 
 		$this->end_controls_section();
+	}
 
-		// Collection Tag Style.
+	/**
+	 * Add the collection tag style controls.
+	 *
+	 * @return void
+	 */
+	private function add_collection_tag_style_controls() {
 		$this->start_controls_section(
 			'section_collection_tag_style',
 			array(
@@ -177,7 +270,7 @@ class CollectionTags extends \Elementor\Widget_Base {
 				'global'   => [
 					'default' => \Elementor\Core\Kits\Documents\Tabs\Global_Typography::TYPOGRAPHY_PRIMARY,
 				],
-				'selector' => '.sc-collection-item',
+				'selector' => '.sc-collection-item .sc-tag',
 			]
 		);
 
@@ -188,7 +281,7 @@ class CollectionTags extends \Elementor\Widget_Base {
 				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', 'em', '%' ],
 				'selectors'  => [
-					'.sc-collection-item' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'.sc-collection-item .sc-tag' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
@@ -200,12 +293,121 @@ class CollectionTags extends \Elementor\Widget_Base {
 				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', 'em', '%' ],
 				'selectors'  => [
-					'.sc-collection-item' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					'.sc-collection-item .sc-tag' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
 
+		$this->add_control(
+			'collection_tag_border_type',
+			array(
+				'label'     => esc_html__( 'Border Type', 'elementor' ),
+				'type'      => \Elementor\Controls_Manager::SELECT,
+				'options'   => array(
+					''       => esc_html__( 'Default', 'elementor' ),
+					'none'   => esc_html__( 'None', 'elementor' ),
+					'solid'  => esc_html__( 'Solid', 'elementor' ),
+					'double' => esc_html__( 'Double', 'elementor' ),
+					'dotted' => esc_html__( 'Dotted', 'elementor' ),
+					'dashed' => esc_html__( 'Dashed', 'elementor' ),
+					'groove' => esc_html__( 'Groove', 'elementor' ),
+				),
+				'selectors' => array(
+					'.sc-collection-item .sc-tag' => 'border-style: {{VALUE}};',
+				),
+				'default'   => '',
+			)
+		);
+
+		$this->add_control(
+			'collection_tag_border_width',
+			array(
+				'label'      => esc_html__( 'Border Width', 'elementor' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+				'range'      => array(
+					'px' => [
+						'max' => 20,
+					],
+					'em' => [
+						'max' => 2,
+					],
+				),
+				'selectors'  => array(
+					'.sc-collection-item .sc-tag' => 'border-width: {{SIZE}}{{UNIT}};',
+				),
+				'condition'  => array(
+					'collection_tag_border_type!' => [ '', 'none' ],
+				),
+			)
+		);
+
+		$this->add_control(
+			'collection_tag_border_color',
+			array(
+				'label'     => esc_html__( 'Border Color', 'elementor' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => array(
+					'.sc-collection-item .sc-tag' => 'border-color: {{VALUE}};',
+				),
+				'condition' => array(
+					'collection_tag_border_type!' => [ '', 'none' ],
+				),
+			)
+		);
+
+		$this->add_control(
+			'collection_tag_border_radius',
+			array(
+				'label'      => esc_html__( 'Border Radius', 'elementor' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => array(
+					'.sc-collection-item .sc-tag' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				),
+			)
+		);
+
 		$this->end_controls_section();
+	}
+
+	/**
+	 * Add content controls.
+	 *
+	 * @return void
+	 */
+	protected function add_content_controls() {
+		$this->start_controls_section(
+			'section_content',
+			array(
+				'label' => esc_html__( 'Content', 'surecart' ),
+			)
+		);
+
+		// add the count setting.
+		$this->add_control(
+			'count',
+			array(
+				'label'       => esc_html__( 'Count', 'surecart' ),
+				'type'        => \Elementor\Controls_Manager::NUMBER,
+				'description' => esc_html__( 'Number of tags to display.', 'surecart' ),
+				'default'     => 1,
+				'min'         => 1,
+			)
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * @return void
+	 */
+	protected function register_controls() {
+		$this->add_content_controls();
+		$this->add_collection_tags_style_controls();
+		$this->add_collection_tag_style_controls();
 	}
 
 	/**
@@ -214,9 +416,10 @@ class CollectionTags extends \Elementor\Widget_Base {
 	 * @return void
 	 */
 	protected function render() {
+		$settings = $this->get_settings_for_display();
 		?>
 		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
-			<!-- wp:surecart/product-collection-tags -->
+			<!-- wp:surecart/product-collection-tags {"count" : <?php echo esc_attr( $settings['count'] ?? 1 ); ?> } -->
 				<!-- wp:surecart/product-collection-tag /-->
 			<!-- /wp:surecart/product-collection-tags -->
 		</div>
@@ -230,16 +433,12 @@ class CollectionTags extends \Elementor\Widget_Base {
 	 */
 	protected function content_template() {
 		?>
-		<ul style="display: flex;gap: 3px;width: 100%;list-style: none;margin: 0;max-width: 100%;padding: 0;" class="wp-block-surecart-product-collection-tags">
-			<li class="sc-collection-item">
-				<a href="#" class="sc-tag sc-tag--default sc-tag--medium">Collection 1</a>
-			</li>
-			<li class="sc-collection-item">
-				<a href="#" class="sc-tag sc-tag--default sc-tag--medium">Collection 2</a>
-			</li>
-			<li class="sc-collection-item">
-				<a href="#" class="sc-tag sc-tag--default sc-tag--medium">Collection 3</a>
-			</li>
+		<ul style="display: flex;width: 100%;list-style: none;margin: 0;max-width: 100%;padding: 0;" class="wp-block-surecart-product-collection-tags">
+			<# for ( var i = 0; i < settings.count; i++ ) { #>
+				<li class="sc-collection-item">
+					<a href="#" class="sc-tag sc-tag--default sc-tag--medium">Collection {{ i + 1 }}</a>
+				</li>
+			<# } #>
 		</ul>
 		<?php
 	}
