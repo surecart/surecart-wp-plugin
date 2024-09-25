9c0a34795a9ad780f5685749c091f21b72ac4f5f
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-09-25 16:07:19 +0300

    sales badge styling

--- app/src/Integrations/Elementor/Widgets/SaleBadge.php
+++ app/src/Integrations/Elementor/Widgets/SaleBadge.php
@@ -47,11 +47,11 @@ class SaleBadge extends \Elementor\Widget_Base {
 	}
 
 	/**
-	 * Register the widget controls.
+	 * Register the widget content settings.
 	 *
 	 * @return void
 	 */
-	protected function register_controls() {
+	private function register_content_settings() {
 		$this->start_controls_section(
 			'section_sale_badge',
 			[
@@ -73,12 +73,190 @@ class SaleBadge extends \Elementor\Widget_Base {
 	}
 
 	/**
+	 * Register the widget style settings.
+	 *
+	 * @return void
+	 */
+	private function register_style_settings() {
+		$this->start_controls_section(
+			'section_collection_tag_style',
+			array(
+				'label' => esc_html__( 'Badge Style', 'surecart' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			)
+		);
+
+		$this->add_control(
+			'badge_text_color',
+			array(
+				'label'     => esc_html__( 'Text Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '#ffffff',
+				'selectors' => [
+					'{{WRAPPER}} .sc-tag' => 'color: {{VALUE}}',
+				],
+			)
+		);
+
+		$this->add_control(
+			'badge_background_color',
+			array(
+				'label'     => esc_html__( 'Background Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '#000000',
+				'selectors' => [
+					'{{WRAPPER}} .sc-tag' => 'background-color: {{VALUE}}',
+				],
+			)
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'badge_typography',
+				'global'   => [
+					'default' => \Elementor\Core\Kits\Documents\Tabs\Global_Typography::TYPOGRAPHY_PRIMARY,
+				],
+				'selector' => '{{WRAPPER}} .sc-tag',
+			]
+		);
+
+		$this->add_responsive_control(
+			'badge_alignment',
+			[
+				'label'     => esc_html__( 'Alignment', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => [
+					'left'   => [
+						'title' => esc_html__( 'Left', 'surecart' ),
+						'icon'  => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'surecart' ),
+						'icon'  => 'eicon-text-align-center',
+					],
+					'right'  => [
+						'title' => esc_html__( 'Right', 'surecart' ),
+						'icon'  => 'eicon-text-align-right',
+					],
+				],
+				'default'   => 'center',
+				'selectors' => [
+					'{{WRAPPER}} .sc-tag' => 'justify-content: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'badge_padding',
+			[
+				'label'      => esc_html__( 'Padding', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-tag' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'badge_margin',
+			[
+				'label'      => esc_html__( 'Margin', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-tag' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'badge_width',
+			[
+				'label'     => esc_html__( 'Width', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::SELECT,
+				'options'   => [
+					'fit-content' => esc_html__( 'Fit', 'surecart' ),
+					'100%'        => esc_html__( 'Fill', 'surecart' ),
+					'fixed'       => esc_html__( 'Fixed', 'surecart' ),
+				],
+				'default'   => 'fit-content',
+				'selectors' => [
+					'{{WRAPPER}} .sc-tag' => 'width: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'badge_fixed_width',
+			[
+				'label'      => esc_html__( 'Specify a fixed width', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'em', '%' ],
+				'condition'  => [
+					'badge_width' => 'fixed',
+				],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-tag' => 'width: {{SIZE}}{{UNIT}};',
+				],
+				'default'    => [
+					'size' => 100,
+					'unit' => 'px',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name'      => 'border',
+				'selector'  => '{{WRAPPER}} .sc-tag',
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_responsive_control(
+			'border_radius',
+			[
+				'label'      => esc_html__( 'Border Radius', 'elementor' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-tag' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * @return void
+	 */
+	protected function register_controls() {
+		$this->register_content_settings();
+		$this->register_style_settings();
+	}
+
+	/**
 	 * Render the widget output on the frontend.
 	 *
 	 * @return void
 	 */
 	protected function render() {
 		$settings = $this->get_settings_for_display();
+
+		if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+			?>
+			<div class="sc-tag sc-tag--primary">
+				<?php echo esc_html( $settings['text'] ); ?>
+			</div>
+			<?php
+			return;
+		}
 		?>
 		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
 			<!-- wp:surecart/product-sale-badge {"text" : "<?php echo esc_attr( $settings['text'] ); ?>", "style":{"border":{"radius":"15px"},"typography":{"fontSize":"12px","lineHeight":"2.1"},"layout":{"selfStretch":"fit","flexSize":null},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} /-->
