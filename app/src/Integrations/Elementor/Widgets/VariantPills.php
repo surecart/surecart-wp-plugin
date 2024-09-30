a043da016e8547f38e27e5f3db7d2b925d746e61
kipRaashon <46610863+KipRashon@users.noreply.github.com>
2024-09-30 21:21:31 +0300

    Variant pills selector

---
+++ app/src/Integrations/Elementor/Widgets/VariantPills.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace SureCart\Integrations\Elementor\Widgets;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Variant Pills widget.
+ */
+class VariantPills extends \Elementor\Widget_Base {
+	/**
+	 * Get widget name.
+	 *
+	 * @return string
+	 */
+	public function get_name() {
+		return 'surecart-variant-pills';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return esc_html__( 'Variant Pills', 'surecart' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * @return string
+	 */
+	public function get_icon() {
+		return 'eicon-tabs';
+	}
+
+	/**
+	 * Get the widget keywords.
+	 *
+	 * @return array
+	 */
+	public function get_keywords() {
+		return array( 'surecart', 'variant', 'pills' );
+	}
+
+	/**
+	 * Register the widget style settings.
+	 *
+	 * @return void
+	 */
+	private function register_style_settings() {
+		$this->start_controls_section(
+			'section_variant_pills_style',
+			[
+				'label' => esc_html__( 'Variant pills', 'surecart' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'text_color',
+			[
+				'label'     => esc_html__( 'Text Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-variant-pills' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_variant_pill_style',
+			[
+				'label' => esc_html__( 'Variant pill', 'surecart' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'pill_text_color',
+			[
+				'label'     => esc_html__( 'Text Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill:not(.sc-pill-option__button--selected)' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'pill_background_color',
+			[
+				'label'     => esc_html__( 'Background Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill:not(.sc-pill-option__button--selected)' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'pill_highlight_text_color',
+			[
+				'label'     => esc_html__( 'Highlight Text', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill.sc-pill-option__button--selected' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'pill_highlight_background_color',
+			[
+				'label'     => esc_html__( 'Highlight Background', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill.sc-pill-option__button--selected' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'pill_highlight_border_color',
+			[
+				'label'     => esc_html__( 'Highlight Border', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill.sc-pill-option__button--selected' => 'border-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'pill_typography',
+				'label'    => esc_html__( 'Typography', 'elementor' ),
+				'selector' => '{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill',
+			]
+		);
+
+		$this->add_responsive_control(
+			'pill_padding',
+			[
+				'label'      => esc_html__( 'Padding', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'pill_margin',
+			[
+				'label'      => esc_html__( 'Margin', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name'      => 'pill_border',
+				'selector'  => '{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill',
+				'separator' => 'before',
+			]
+		);
+
+		$this->add_control(
+			'pill_border_radius',
+			[
+				'label'      => esc_html__( 'Border Radius', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em', '%' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-pill-option__wrapper .wp-block-surecart-product-variant-pill' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * @return void
+	 */
+	protected function register_controls() {
+		$this->register_style_settings();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * @return void
+	 */
+	protected function render() {
+		if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+			$this->content_template();
+			return;
+		}
+
+		$settings           = $this->get_settings_for_display();
+		$variant_attributes = array(
+			'highlight_text'       => $settings['pill_highlight_text_color'] ?? '',
+			'highlight_background' => $settings['pill_highlight_background_color'] ?? '',
+			'highlight_border'     => $settings['pill_highlight_border_color'] ?? '',
+		);
+
+		?>
+		<div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
+		<!-- wp:surecart/product-variant-pills -->
+		<!-- wp:surecart/product-variant-pill <?php echo wp_json_encode( $variant_attributes ); ?> /-->
+		<!-- /wp:surecart/product-variant-pills -->
+		</div>
+		<?php
+	}
+
+	/**
+	 * Render the widget output on the editor.
+	 *
+	 * @return void
+	 */
+	protected function content_template() {
+		?>
+		<div class="wp-block-surecart-product-variant-pills">
+			<label for="" class="sc-form-label">Color</label>
+			<div class="sc-pill-option__wrapper">
+				<div class="sc-pill-option__button wp-block-surecart-product-variant-pill sc-pill-option__button--selected">Red</div>
+				<div class="sc-pill-option__button wp-block-surecart-product-variant-pill">Blue</div>
+				<div class="sc-pill-option__button wp-block-surecart-product-variant-pill">Green</div>
+			</div>
+		</div>
+		<?php
+	}
+}
