d5c7ae6fd5a243c0715c9002cc4606f70e1f3b00
Andre Gagnon <ajgagnon@uwalumni.com>
2024-10-04 15:32:06 -0500

    Update string

--- packages/admin/customers/EditCustomer.js
+++ packages/admin/customers/EditCustomer.js
@@ -80,7 +80,7 @@ export default () => {
 		const r = confirm(
 			sprintf(
 				__(
-					'Warning: Deleting customer "%s" will permanently remove all associated data, including orders and subscriptions. This action cannot be undone. Are you sure you want to continue?',
+					'Warning: Deleting "%s" will permanently remove all associated data, including orders and subscriptions. This action cannot be undone. Are you sure you want to continue?',
 					'surecart'
 				),
 				customer?.name ||
