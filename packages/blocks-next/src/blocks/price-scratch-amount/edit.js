d09493caf9b76b06d87e219dc3fb0e4dacf487ef
Andre Gagnon <ajgagnon@uwalumni.com>
2024-10-07 17:00:22 -0500

    Actually format currency

--- packages/blocks-next/src/blocks/price-scratch-amount/edit.js
+++ packages/blocks-next/src/blocks/price-scratch-amount/edit.js
@@ -1,10 +1,18 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { getFormattedPrice } from '../../utilities/currency';
 
 export default () => {
 	const blockProps = useBlockProps({
 		className: 'sc-price__amount',
 	});
 
-	return <span {...blockProps}>{scData?.currency_symbol}20</span>;
+	return (
+		<span {...blockProps}>
+			{getFormattedPrice({
+				amount: 2000,
+				currency: scData?.currency,
+			})}
+		</span>
+	);
 };
--- packages/blocks-next/src/utilities/currency.js
+++ packages/blocks-next/src/utilities/currency.js
@@ -32,12 +32,20 @@ export const maybeConvertAmount = (amount, currency) => {
 		: amount / 100;
 };
 
-export const getFormattedPrice = ({ amount, currency = 'usd' }) => {
+export const getFormattedPrice = ({
+	amount,
+	currency = 'usd',
+	options = {},
+}) => {
 	const converted = maybeConvertAmount(parseFloat(amount), currency);
+
+	const minimumFractionDigits = amount % 1 == 0 ? 0 : 2;
 
 	return `${new Intl.NumberFormat(undefined, {
 		style: 'currency',
 		currency,
+		minimumFractionDigits,
+		...options,
 	}).format(parseFloat(converted.toFixed(2)))}`;
 };
 
