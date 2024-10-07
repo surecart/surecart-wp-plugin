f981bad77c1de09ab989aef4bc8484bbbf92aee9
Andre Gagnon <ajgagnon@uwalumni.com>
2024-10-07 17:03:22 -0500

    Formatting

--- packages/blocks-next/src/blocks/price-interval/view.php
+++ packages/blocks-next/src/blocks/price-interval/view.php
@@ -1,4 +1,5 @@
-<span <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+<span
+	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
 	aria-label="<?php echo esc_html( $price->interval_text ); ?>"
 >
 	<?php echo esc_html( $price->short_interval_text . $price->short_interval_count_text ); ?>
