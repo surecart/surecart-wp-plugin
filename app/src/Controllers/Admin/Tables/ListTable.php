f07e9f52e4b42cd4ead7988471daa6a78192f604
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2024-10-10 22:25:13 +0600

    Update column_date in list table

--- app/src/Controllers/Admin/Tables/ListTable.php
+++ app/src/Controllers/Admin/Tables/ListTable.php
@@ -63,20 +63,16 @@ abstract class ListTable extends \WP_List_Table {
 	 * @return string
 	 */
 	public function column_date( $model ) {
-		$created = sprintf(
-			'<time datetime="%1$s" title="%2$s">%3$s</time>',
-			esc_attr( $model->created_at ),
-			esc_html( TimeDate::formatDateAndTime( $model->created_at ) ),
-			esc_html( TimeDate::humanTimeDiff( $model->created_at ) )
+		if ( $model->created_at === $model->updated_at ) {
+			// translators: %s: created at date.
+			return sprintf( 'Published %s', $model->created_at_date );
+		}
+
+		return sprintf(
+			// translators: %s: updated at date.
+			'Last Modified %s',
+			$model->updated_at_date
 		);
-		$updated = sprintf(
-			'%1$s <time datetime="%2$s" title="%3$s">%4$s</time>',
-			__( 'Updated', 'surecart' ),
-			esc_attr( $model->updated_at ),
-			esc_html( TimeDate::formatDateAndTime( $model->updated_at ) ),
-			esc_html( TimeDate::humanTimeDiff( $model->updated_at ) )
-		);
-		return $created . '<br /><small style="opacity: 0.75">' . $updated . '</small>';
 	}
 
 	/**
@@ -87,18 +83,9 @@ abstract class ListTable extends \WP_List_Table {
 	 * @return string
 	 */
 	public function column_created( $model ) {
-		return sprintf(
-			'<sc-format-date
-				date="%1$s"
-				month="long"
-				day="numeric"
-				year="numeric"
-				hour="numeric"
-				minute="numeric"
-				type="timestamp"></sc-format-date>',
-			esc_attr( $model->created_at )
-		);
+		return $model->created_at_date;
 	}
+
 	/**
 	 * The mode for the model.
 	 *
