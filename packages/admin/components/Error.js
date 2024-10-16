1d5753f19b0e7aa41fc4fa488d65123ec1b600fd
Rajkiran Bagal <rajkiranb@bsf.io>
2024-10-16 22:16:16 +0530

    Added clear description for invalid json error message conditionally

--- packages/admin/components/Error.js
+++ packages/admin/components/Error.js
@@ -1,4 +1,4 @@
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { ScAlert } from '@surecart/components-react';
 
 export default ({
@@ -17,31 +17,6 @@ export default ({
 		error = { message: error };
 	}
 
-	if (error?.code === 'invalid_json') {
-		error.message = __(
-			'The response is not a valid JSON response.',
-			'surecart'
-		);
-		const debugSettingsUrl =
-			'https://wordpress.org/support/article/debugging-in-wordpress/';
-		error.additional_errors = [
-			{
-				code: 'invalid_json',
-				message: sprintf(
-					/* translators: %s: URL to debug settings page */
-					__(
-						'If you are using debug logging, please ensure that WP_DEBUG_LOG, WP_DEBUG_DISPLAY, or other debug settings are disabled from the wp-config.php file. This may interfere with API responses. For more details, please check the %s or contact support.',
-						'surecart'
-					),
-					`<a href="${debugSettingsUrl}" target="_blank" rel="noopener noreferrer">${__(
-						'Debug Settings',
-						'surecart'
-					)}</a>`
-				),
-				isHTMl: true,
-			},
-		];
-	}
 	return (
 		<ScAlert
 			open={true}
@@ -57,19 +32,26 @@ export default ({
 			</span>
 			{error?.additional_errors?.length && (
 				<ul>
-					{(error?.additional_errors || []).map((error, index) =>
-						error.isHTMl ? (
-							<li
-								key={index}
-								dangerouslySetInnerHTML={{
-									__html: error?.message,
-								}}
-							/>
-						) : (
-							<li key={index}>{error?.message}</li>
-						)
-					)}
+					{(error?.additional_errors || []).map((error, index) => (
+						<li key={index}>{error?.message}</li>
+					))}
 				</ul>
+			)}
+			{error?.code === 'invalid_json' && (
+				<>
+					{__(
+						'If you are using debug logging, please ensure that WP_DEBUG_LOG, WP_DEBUG_DISPLAY, or other debug settings are disabled from the wp-config.php file. This may interfere with API responses. For more details, please check the ',
+						'surecart'
+					)}
+					<a
+						href="https://wordpress.org/support/article/debugging-in-wordpress/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{__('Debug Settings', 'surecart')}
+					</a>
+					{__(' or contact support.', 'surecart')}
+				</>
 			)}
 			{children}
 		</ScAlert>
