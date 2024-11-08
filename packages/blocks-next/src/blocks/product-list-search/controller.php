fe802e54047931e8361ec9b26deb5cce001a6ad6
Andre Gagnon <ajgagnon@uwalumni.com>
2024-11-08 15:41:59 -0600

    Fix search bar not showing

--- packages/blocks-next/src/blocks/product-list-search/controller.php
+++ packages/blocks-next/src/blocks/product-list-search/controller.php
@@ -2,7 +2,7 @@
 use SureCart\Models\Blocks\ProductListBlock;
 
 // we have already defined a search query.
-if ( empty( $block->context['query']['search'] ) ) {
+if ( ! empty( $block->context['query']['search'] ) ) {
 	return null;
 }
 
