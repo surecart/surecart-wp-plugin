4c08622efad86cebbc76e7f8421a167447b06f70
Andre Gagnon <ajgagnon@uwalumni.com>
2024-11-19 13:45:31 -0600

    Fix collections filter.

--- packages/blocks-next/src/blocks/product-list-filter-tags/view.php
+++ packages/blocks-next/src/blocks/product-list-filter-tags/view.php
@@ -46,7 +46,7 @@ if ( empty( $product_terms ) ) {
 	<?php echo wp_kses_data( wp_interactivity_data_wp_context( [ 'collections' => $product_terms ] ) ); ?>
 >
 <?php
-foreach ( $product_collections as $filter_tag ) :
+foreach ( $product_terms as $filter_tag ) :
 	// Get an instance of the current Post Template block.
 		$block_instance = $block->parsed_block;
 
