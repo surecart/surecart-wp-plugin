cb7476cc9eb10ae6b1aa09ed5c7d18d52aad93a3
Rajkiran Bagal <rajkiranb@bsf.io>
2024-11-20 17:01:10 +0530

    Added default spacing & accesibility

--- packages/blocks-next/src/blocks/product-list-filter-checkbox/view.php
+++ packages/blocks-next/src/blocks/product-list-filter-checkbox/view.php
@@ -1,12 +1,15 @@
 <a 
 	<?php echo wp_kses_data( get_block_wrapper_attributes([
 		'aria-label' => esc_html( $checkbox->label ),
+		'aria-checked' => $checkbox->checked ? 'true' : 'false',
+		'aria-labelledby' => $checkbox->label 
 	])  ); ?>
 	href="<?php echo $checkbox->href; ?>"
 	data-wp-on--click="surecart/product-list::actions.navigate"
 	data-wp-on--mouseenter="surecart/product-list::actions.prefetch"
+	role="checkbox"
 >
-	<input type="checkbox" id="<?php echo (int) $checkbox->value; ?>" <?php checked( $checkbox->checked ) ?> />
+	<input tabindex="-1" type="checkbox" id="<?php echo (int) $checkbox->value; ?>" <?php checked( $checkbox->checked ) ?> />
 	<label  for="<?php echo (int) $checkbox->value; ?>"><?php echo esc_html( $checkbox->label ); ?></label>
 </a>
 
