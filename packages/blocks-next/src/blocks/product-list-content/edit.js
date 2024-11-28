3f8acbb9a2d7627a58d558f5cc875ed76b5c34a6
Rajkiran Bagal <rajkiranb@bsf.io>
2024-11-28 12:51:13 +0530

    Added new product list content block which wraps the template & sidebar & added default styling to it

---
+++ packages/blocks-next/src/blocks/product-list-content/edit.js
@@ -0,0 +1,28 @@
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
+const TEMPLATE = [
+	[
+		'surecart/product-list-sidebar',
+		{ style: { layout: { selfStretch: 'fixed', flexSize: '20%' } } },
+	],
+	[
+		'surecart/product-template',
+		{
+			style: {
+				spacing: { blockGap: '30px' },
+				layout: { selfStretch: 'fit', flexSize: null },
+			},
+			layout: { type: 'grid', columnCount: 4 },
+		},
+	],
+];
+
+export default () => {
+	const blockProps = useBlockProps();
+	const innerBlocksProps = useInnerBlocksProps(blockProps, {
+		template: TEMPLATE,
+	});
+
+	return <div {...innerBlocksProps} />;
+};
