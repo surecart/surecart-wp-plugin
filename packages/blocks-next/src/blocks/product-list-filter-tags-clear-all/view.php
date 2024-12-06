f395b3bf02f8e8c42becfe4bae4c3d0a71062215
Rajkiran Bagal <rajkiranb@bsf.io>
2024-12-06 19:21:08 +0530

    Updated the Sidebar Pattern with new Sort Radio & Clear All Blocks.
    Accesibility testing & fixes for Clear All Block.

--- packages/blocks-next/src/blocks/product-list-filter-tags-clear-all/view.php
+++ packages/blocks-next/src/blocks/product-list-filter-tags-clear-all/view.php
@@ -1,8 +1,7 @@
 <a
-	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
 	<?php echo wp_kses_data( get_block_wrapper_attributes([
-		'aria-label' => esc_html( $attributes['label'] ),
-		'aria-labelledby' => esc_html( $attributes['label'] )
+		'aria-label' => wp_strip_all_tags( $attributes['label'] ),
+		'aria-labelledby' => wp_strip_all_tags( $attributes['label'] )
 	])  ); ?>
 	href="<?php echo $clear_all_url; ?>"
 	data-wp-on--click="surecart/product-list::actions.navigate"
