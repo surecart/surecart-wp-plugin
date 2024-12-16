da545b198189b33c7a2a574ff607d89b513e214b
Rajkiran Bagal <rajkiranb@bsf.io>
2024-12-16 16:41:26 +0530

    Update the sidebar template

--- packages/blocks-next/src/blocks/product-list-sidebar/edit.js
+++ packages/blocks-next/src/blocks/product-list-sidebar/edit.js
@@ -14,19 +14,129 @@ import {
 
 const TEMPLATE = [
 	[
-		'surecart/product-list-filter-checkboxes',
+		'surecart/product-list-filter-tags',
 		{
+			layout: {
+				type: 'flex',
+				orientation: 'vertical',
+			},
+		},
+		[
+			[
+				'surecart/product-list-filter-tags-label',
+				{
+					label: '<strong>Applied Filters</strong>',
+					fontSize: 'medium',
+				},
+			],
+			[
+				'surecart/product-list-filter-tags-template',
+				{
+					label: '<strong>Applied Filters</strong>',
+					style: {
+						spacing: {
+							blockGap: '8px',
+						},
+					},
+					fontSize: 'medium',
+				},
+				[
+					[
+						'surecart/product-list-filter-tag',
+						{
+							style: {
+								typography: {
+									fontSize: '14px',
+								},
+							},
+						},
+					],
+				],
+			],
+			[
+				'surecart/product-list-filter-tags-clear-all',
+				{
+					label: '<strong>Clear all</strong>',
+					style: {
+						typography: {
+							textDecoration: 'underline',
+						},
+					},
+					fontSize: 'small',
+				},
+			],
+		],
+	],
+	[
+		'surecart/product-list-sort-radio-group',
+		{
+			layout: {
+				type: 'flex',
+				orientation: 'vertical',
+			},
 			style: {
 				spacing: {
-					blockGap: '0',
-					margin: { top: '0', bottom: '0' },
-				},
-				layout: {
-					type: 'flex',
-					orientation: 'vertical',
+					blockGap: '4px',
 				},
 			},
 		},
+		[
+			[
+				'surecart/product-list-sort-radio-group-label',
+				{
+					label: '<strong>Sort by</strong>',
+					fontSize: 'medium',
+				},
+			],
+			[
+				'surecart/product-list-sort-radio-group-template',
+				{
+					style: {
+						spacing: {
+							blockGap: '4px',
+						},
+					},
+					fontSize: 'medium',
+				},
+				[['surecart/product-list-sort-radio', {}]],
+			],
+		],
+	],
+	[
+		'surecart/product-list-filter-checkboxes',
+		{
+			layout: {
+				type: 'flex',
+				orientation: 'vertical',
+			},
+			style: {
+				spacing: {
+					blockGap: '4px',
+				},
+			},
+		},
+		[
+			[
+				'surecart/product-list-filter-checkboxes-label',
+				{
+					label: '<strong>Filter by</strong>',
+					fontSize: 'medium',
+				},
+			],
+			[
+				'surecart/product-list-filter-checkboxes-template',
+				{
+					style: {
+						spacing: {
+							blockGap: '4px',
+							margin: { top: '0', bottom: '0' },
+						},
+					},
+					fontSize: 'medium',
+				},
+				[['surecart/product-list-filter-checkbox', {}]],
+			],
+		],
 	],
 ];
 
