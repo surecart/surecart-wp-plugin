ee658eb196ac10d721b3f9830b0b9fc19782331a
Andre Gagnon <ajgagnon@uwalumni.com>
2024-12-30 14:39:16 -0600

    Don't render parent

--- packages/blocks-next/src/blocks/product-list-filter-tags/view.php
+++ packages/blocks-next/src/blocks/product-list-filter-tags/view.php
@@ -1,16 +1,20 @@
 <?php
+if ( empty( $block->parsed_block['innerBlocks'] ) || empty( $block->parsed_block['innerBlocks'][0] ) ) {
+	return;
+}
 
-	if ( empty( $block->parsed_block['innerBlocks'] ) || empty( $block->parsed_block['innerBlocks'][0] ) ) {
-		return;
-	}
-	
-	if ( 'surecart/product-list-filter-tag' === $block->parsed_block['innerBlocks'][0]['blockName'] ) {
-		echo \SureCart::block()->productListFilterTagsMigration()->render();
-		return;
-	}
+if ( 'surecart/product-list-filter-tag' === $block->parsed_block['innerBlocks'][0]['blockName'] ) {
+	echo \SureCart::block()->productListFilterTagsMigration()->render();
+	return;
+}
+
+$all_taxonomies = \SureCart::block()->urlParams( 'products' )->getAllTaxonomyArgs();
+// no filters, don't render this block.
+if ( empty( $all_taxonomies ) ) {
+	return;
+}
+
 ?>
-<div
-	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> 
->
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </div>
