2a8c0c2f7d05d35593c2280e82898746ca3d9378
Andre Gagnon <ajgagnon@uwalumni.com>
2024-12-30 15:20:57 -0600

    Remove tests

--- packages/admin/orders/index.js
+++ packages/admin/orders/index.js
@@ -13,29 +13,4 @@ import Order from './Order';
  * Render
  */
 
-const registerAddon = window.surecart.registerAddon;
-registerAddon('custom-main', {
-	render: () => <div>Test Custom Main</div>,
-	scope: 'main',
-	title: 'Test Custom Main 1',
-});
-
-registerAddon('custom-main-tow', {
-	render: () => <div>Test Custom Main</div>,
-	scope: 'main',
-	title: 'Test Custom Main 2',
-});
-
-registerAddon('custom-sidebar', {
-	render: () => <div>Test Custom Sidebar</div>,
-	scope: 'sidebar',
-	title: 'Test Custom Sidebar',
-});
-
-registerAddon('custom-sidebar-two', {
-	render: () => <div>Test Custom Sidebar</div>,
-	scope: 'sidebar',
-	title: 'Test Custom Sidebar 2',
-});
-
 render(<Order />, document.getElementById('app'));
