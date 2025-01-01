e5018c2d7b8542218e585560986cde11ab4f3925
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-01 16:03:58 -0600

    Use simpler context. Actually load taxonomy terms.

--- packages/blocks-next/src/blocks/product-list-filter-checkboxes-template/edit.js
+++ packages/blocks-next/src/blocks/product-list-filter-checkboxes-template/edit.js
@@ -6,36 +6,81 @@ import {
 	useBlockProps,
 } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
+import { useEntityRecords } from '@wordpress/core-data';
+import { Spinner } from '@wordpress/components';
 
 const TEMPLATE = [['surecart/product-list-filter-checkbox']];
 
-export default ({ clientId, __unstableLayoutClassNames }) => {
+const FALLBACK_FILTER_TEMPLATE = [
+	{
+		id: 'filter-1',
+		'surecart/checkbox/name': __('Filter 1'),
+	},
+	{
+		id: 'filter-2',
+		'surecart/checkbox/name': __('Filter 2'),
+	},
+	{
+		id: 'filter-3',
+		'surecart/checkbox/name': __('Filter 3'),
+	},
+];
+
+export default ({
+	clientId,
+	__unstableLayoutClassNames,
+	context: { taxonomySlug },
+}) => {
 	const blockCount = useSelect((select) =>
 		select(blockEditorStore).getBlockCount(clientId)
+	);
+
+	// Fetch taxonomy terms
+	const { records: terms, hasResolved } = useEntityRecords(
+		'taxonomy',
+		taxonomySlug,
+		{
+			per_page: -1,
+			hide_empty: true,
+		}
 	);
 
 	const blockProps = useBlockProps({
 		className: __unstableLayoutClassNames,
 	});
 
+	// Create template contexts from terms
+	const termTemplates =
+		terms?.map((term) => ({
+			id: term.id.toString(),
+			'surecart/checkbox/name': term.name,
+		})) || [];
+
+	// Use fallback if no terms
+	const templateContexts = termTemplates.length
+		? termTemplates
+		: FALLBACK_FILTER_TEMPLATE;
+
+	if (!hasResolved) {
+		return (
+			<div
+				{...blockProps}
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					padding: '20px',
+				}}
+			>
+				<Spinner />
+			</div>
+		);
+	}
+
 	return (
 		<div {...blockProps}>
 			<TemplateListEdit
 				template={TEMPLATE}
-				blockContexts={[
-					{
-						id: 'filter-1',
-						'surecart/checkbox/name': __('Filter 1'),
-					},
-					{
-						id: 'filter-2',
-						'surecart/checkbox/name': __('Filter 2'),
-					},
-					{
-						id: 'filter-3',
-						'surecart/checkbox/name': __('Filter 3'),
-					},
-				]}
+				blockContexts={templateContexts}
 				className={__unstableLayoutClassNames}
 				clientId={clientId}
 				renderAppender={
