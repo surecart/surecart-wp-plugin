4b0aca2d2b61b893ca0a02a7ae3aed878367de71
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-01 15:45:04 -0600

    Make sure to add classes to edit.js too

--- packages/blocks-next/src/blocks/product-list-filter-checkbox/edit.js
+++ packages/blocks-next/src/blocks/product-list-filter-checkbox/edit.js
@@ -2,11 +2,13 @@ import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
 export default ({ context: { 'surecart/checkbox/name': name } }) => {
-	const blockProps = useBlockProps();
+	const blockProps = useBlockProps({
+		className: 'sc-form-check',
+	});
 
 	return (
 		<div {...blockProps}>
-			<input type="checkbox" />
+			<input className="sc-check-input" type="checkbox" />
 			<label>{name}</label>
 		</div>
 	);
--- packages/blocks-next/src/blocks/product-list-sort-radio/edit.js
+++ packages/blocks-next/src/blocks/product-list-sort-radio/edit.js
@@ -2,11 +2,13 @@ import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
 export default ({ context: { 'surecart/radio/name': name } }) => {
-	const blockProps = useBlockProps();
+	const blockProps = useBlockProps({
+		className: 'sc-form-check',
+	});
 
 	return (
 		<div {...blockProps}>
-			<input type="radio" />
+			<input className="sc-check-input" type="radio" />
 			<label>{name}</label>
 		</div>
 	);
