793d609ce616a1e8335182a201a3e1932ecd94d8
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-01 15:57:35 -0600

    Add label to checkboxes.

--- packages/blocks-next/src/blocks/product-list-filter-checkboxes/index.js
+++ packages/blocks-next/src/blocks/product-list-filter-checkboxes/index.js
@@ -3,6 +3,7 @@
  */
 import { registerBlockType } from '@wordpress/blocks';
 import { check as icon } from '@wordpress/icons';
+import { __, sprintf } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -11,6 +12,9 @@ import edit from './edit';
 import metadata from './block.json';
 import save from './save';
 import './style.scss';
+import { store as coreStore } from '@wordpress/core-data';
+import { select } from '@wordpress/data';
+
 /**
  * Every block starts by registering a new block type definition.
  */
@@ -18,4 +22,26 @@ registerBlockType(metadata.name, {
 	icon,
 	edit,
 	save,
+	__experimentalLabel: (attributes) => {
+		const taxonomies = select(coreStore).getEntityRecords(
+			'root',
+			'taxonomy',
+			{
+				per_page: -1,
+			}
+		);
+
+		if (!taxonomies) return false;
+
+		const taxonomy = taxonomies.find(
+			(t) => t.slug === attributes?.taxonomy
+		);
+
+		if (!taxonomy || !taxonomy?.name) return false;
+
+		return sprintf(
+			'%s Checkboxes',
+			taxonomy.labels?.singular_name ?? taxonomy.name
+		);
+	},
 });
