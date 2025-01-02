c3623c8acda0263be73800cb64057187eb8159f0
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-02 14:29:45 -0600

    Remove unused styles. Cleanup.

--- packages/blocks-next/src/blocks/product-list-content/index.js
+++ packages/blocks-next/src/blocks/product-list-content/index.js
@@ -10,7 +10,6 @@ import { columns as icon } from '@wordpress/icons';
 import edit from './edit';
 import metadata from './block.json';
 import save from './save';
-import './style.scss';
 /**
  * Every block starts by registering a new block type definition.
  */
