5032977a268f8e0454318f82058ebe3614af1a89
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-02 14:55:33 -0600

    Capitalization

--- packages/blocks-next/src/blocks/product-list-filter-checkboxes-label/view.php
+++ packages/blocks-next/src/blocks/product-list-filter-checkboxes-label/view.php
@@ -2,11 +2,11 @@
 	<?php echo wp_kses_data(
 		get_block_wrapper_attributes(
 			[
-				'aria-label'      => wp_strip_all_tags( $attributes['label'] ),
-				'aria-labelledby' => wp_strip_all_tags( $attributes['label'] ),
+				'aria-label' => wp_strip_all_tags( $attributes['label'] ),
 			]
 		)
 	); ?>
 >
 	<?php echo wp_kses_post( $attributes['label'] ); ?>
 </span>
+
