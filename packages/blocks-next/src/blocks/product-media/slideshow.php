e13618a720804662a35c719b1bb9cb4c83d3cd8a
Andre Gagnon <ajgagnon@uwalumni.com>
2025-01-06 08:36:43 -0600

    Move interactive

--- packages/blocks-next/src/blocks/product-media/slideshow.php
+++ packages/blocks-next/src/blocks/product-media/slideshow.php
@@ -5,8 +5,8 @@
 	data-wp-watch="actions.updateSlider"
 	<?php echo wp_kses_data( wp_interactivity_data_wp_context( $slider_options ) ); ?>
 >
-	<div class="swiper" style="height: <?php echo esc_attr( $height ); ?>" data-wp-interactive='{ "namespace": "surecart/lightbox" }' <?php echo wp_kses_data( wp_interactivity_data_wp_context( [ 'images' => $product->gallery_ids ] ) ); ?>>
-		<div class="swiper-wrapper">
+	<div class="swiper" style="height: <?php echo esc_attr( $height ); ?>">
+		<div class="swiper-wrapper" data-wp-interactive='{ "namespace": "surecart/lightbox" }' <?php echo wp_kses_data( wp_interactivity_data_wp_context( [ 'images' => $product->gallery_ids ] ) ); ?>>
 			<?php foreach ( $gallery as $index => $image ) : ?>
 				<div
 					data-wp-interactive='{ "namespace": "surecart/product-page" }'
