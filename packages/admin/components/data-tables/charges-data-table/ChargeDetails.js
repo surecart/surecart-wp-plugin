ff2197368795131984cfe863b19f16b8f82cf9ae
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-01-30 15:12:37 +0530

    Displayed the Platform Fee & Service Fee in UI

--- packages/admin/components/data-tables/charges-data-table/ChargeDetails.js
+++ packages/admin/components/data-tables/charges-data-table/ChargeDetails.js
@@ -10,11 +10,12 @@ import {
 	ScLineItem,
 	ScPaymentMethod,
 	ScTag,
+	ScDivider,
 } from '@surecart/components-react';
 import { getProcessorName } from '../../../util/translations';
 
 export default ({ charge, onRequestClose }) => {
-	const { amount, currency, created_at_date } = charge;
+	const { amount, currency, created_at_date, payment_intent } = charge;
 
 	const renderStatusTag = () => {
 		if (charge?.fully_refunded) {
@@ -100,6 +101,130 @@ export default ({ charge, onRequestClose }) => {
 					<span slot="description">{__('Status', 'surecart')}</span>
 					{renderStatusTag()}
 				</ScLineItem>
+				{payment_intent?.platform_fee && (
+					<>
+						<ScDivider style={{ '--spacing': '1em' }} />
+						<h3
+							css={css`
+								font-size: 13px;
+								margin: 0;
+							`}
+						>
+							{__('Platform Fee', 'surecart')}
+						</h3>
+						<ScLineItem>
+							<span slot="description">
+								{__('Base Amount', 'surecart')}
+							</span>
+							<ScFormatNumber
+								slot="price"
+								type="currency"
+								currency={currency}
+								value={
+									payment_intent?.platform_fee?.base_amount
+								}
+							/>
+							<span slot="price-description">
+								{`(${payment_intent.platform_fee.base_rate}%)`}
+							</span>
+						</ScLineItem>
+						<ScLineItem>
+							<span slot="description">
+								{__('Features', 'surecart')}
+							</span>
+						</ScLineItem>
+						{payment_intent?.platform_fee?.features_breakdown &&
+							Object.entries(
+								payment_intent.platform_fee.features_breakdown
+							).map(([key, feature]) => {
+								return (
+									<ScLineItem
+										key={key}
+										style={{
+											'margin-left': '1em',
+										}}
+									>
+										<span slot="description">
+											{key.charAt(0).toUpperCase() +
+												key.slice(1).toLowerCase()}
+										</span>
+										<ScFormatNumber
+											slot="price"
+											type="currency"
+											currency={currency}
+											value={feature?.amount}
+										/>
+										<span slot="price-description">
+											{`(${feature?.effective_rate}%)`}
+										</span>
+									</ScLineItem>
+								);
+							})}
+						<ScDivider style={{ '--spacing': '0.5em' }} />
+						<ScLineItem>
+							<span
+								slot="description"
+								style={{
+									'font-weight':
+										'var(--sc-font-weight-semibold)',
+									color: 'var(--sc-color-gray-800)',
+								}}
+							>
+								{__('Total Amount', 'surecart')}
+							</span>
+							<ScFormatNumber
+								slot="price"
+								type="currency"
+								currency={currency}
+								value={payment_intent?.platform_fee?.amount}
+								style={{
+									'font-weight':
+										'var(--sc-font-weight-semibold)',
+									color: 'var(--sc-color-gray-800)',
+								}}
+							/>
+						</ScLineItem>
+					</>
+				)}
+				{payment_intent?.service_fee && (
+					<>
+						<ScDivider style={{ '--spacing': '1em' }} />
+						<h3
+							css={css`
+								font-size: 13px;
+								margin: 0;
+							`}
+						>
+							{__('Service Fee', 'surecart')}
+						</h3>
+						<ScLineItem>
+							<span
+								slot="description"
+								style={{
+									'font-weight':
+										'var(--sc-font-weight-semibold)',
+									color: 'var(--sc-color-gray-800)',
+								}}
+							>
+								{__('Total Amount', 'surecart')}
+							</span>
+							<ScFormatNumber
+								slot="price"
+								type="currency"
+								currency={currency}
+								value={payment_intent?.service_fee?.amount}
+								style={{
+									'font-weight':
+										'var(--sc-font-weight-semibold)',
+									color: 'var(--sc-color-gray-800)',
+								}}
+							/>
+							<span slot="price-description">
+								{`(${payment_intent?.service_fee?.rate}%)`}
+							</span>
+						</ScLineItem>
+					</>
+				)}
 			</div>
 		</ScDrawer>
 	);
