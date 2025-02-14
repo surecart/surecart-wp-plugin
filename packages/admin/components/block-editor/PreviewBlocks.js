c502ca5f29d2f867fb0f6bfb3e4d4aa206289b05
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-02-14 20:54:02 +0600

    Added Disabled comonent to Preview blocks

--- packages/admin/components/block-editor/PreviewBlocks.js
+++ packages/admin/components/block-editor/PreviewBlocks.js
@@ -2,19 +2,22 @@
  * External dependencies.
  */
 import { BlockEditorProvider, BlockList } from '@wordpress/block-editor';
+import { Disabled } from '@wordpress/components';
 
 export default function ({ blocks }) {
 	return (
-		<BlockEditorProvider
-			value={blocks}
-			settings={surecartBlockEditorSettings}
-		>
-			<div className="editor-styles-wrapper">
-				<BlockList
-					renderAppender={() => null}
-					className="surecart-block-editor__block-list"
-				/>
-			</div>
-		</BlockEditorProvider>
+		<Disabled>
+			<BlockEditorProvider
+				value={blocks}
+				settings={surecartBlockEditorSettings}
+			>
+				<div className="editor-styles-wrapper">
+					<BlockList
+						renderAppender={() => null}
+						className="surecart-block-editor__block-list"
+					/>
+				</div>
+			</BlockEditorProvider>
+		</Disabled>
 	);
 }
