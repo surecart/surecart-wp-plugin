24bc3ca5f337573fe63e45766b3bb03c52656455
Andre Gagnon <ajgagnon@uwalumni.com>
2025-02-18 14:56:37 -0600

    Remove unused stylesheet

--- packages/components/src/components/controllers/checkout-form/sc-swap/sc-swap.tsx
+++ packages/components/src/components/controllers/checkout-form/sc-swap/sc-swap.tsx
@@ -7,7 +7,6 @@ import { updateFormState } from '@store/form/mutations';
 
 @Component({
   tag: 'sc-swap',
-  styleUrl: 'sc-swap.scss',
   shadow: true,
 })
 export class ScSwap {
@@ -16,27 +15,26 @@ export class ScSwap {
 
   async onSwapToggleChange(e) {
     try {
-        updateFormState('FETCH');
-          checkoutState.checkout = await toggleSwap({ id: this.lineItem?.id, action: e.target.checked ? 'swap' : 'unswap' });
-        updateFormState('RESOLVE');
+      updateFormState('FETCH');
+      checkoutState.checkout = await toggleSwap({ id: this.lineItem?.id, action: e.target.checked ? 'swap' : 'unswap' });
+      updateFormState('RESOLVE');
     } catch (e) {
-        updateFormState('REJECT');
-        console.error(e);
+      updateFormState('REJECT');
+      console.error(e);
     }
-
   }
 
   render() {
-    if(!this?.lineItem?.price?.current_swap && !this?.lineItem?.swap) {
-        return null;
+    if (!this?.lineItem?.price?.current_swap && !this?.lineItem?.swap) {
+      return null;
     }
 
     const swap = this?.lineItem?.swap || this?.lineItem?.price?.current_swap;
 
     return (
-        <sc-switch checked={!!this?.lineItem?.swap} onScChange={(e) => this.onSwapToggleChange(e)}>
-            {swap?.description}
-        </sc-switch>
+      <sc-switch checked={!!this?.lineItem?.swap} onScChange={e => this.onSwapToggleChange(e)}>
+        {swap?.description}
+      </sc-switch>
     );
   }
 }
