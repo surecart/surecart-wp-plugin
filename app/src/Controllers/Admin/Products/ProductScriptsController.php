494c6b0555a8e9832e766c1e319d9a9b148981bb
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-02-21 11:11:51 +0600

    Move scripts to ProductScriptsController

--- app/src/Controllers/Admin/Products/ProductScriptsController.php
+++ app/src/Controllers/Admin/Products/ProductScriptsController.php
@@ -46,18 +46,125 @@ class ProductScriptsController extends AdminModelEditController {
 	}
 
 	/**
-	 * Override the enqueueComponents method to add the product block editor styles.
+	 * Enqueue needed scripts.
 	 *
 	 * @return void
 	 */
-	public function enqueueComponents() {
-		parent::enqueueComponents();
+	public function enqueueScriptDependencies() {
+		parent::enqueueScriptDependencies();
 
-		// Set current screen as block editor screen.
 		$current_screen = get_current_screen();
 		$current_screen->is_block_editor( true );
 
+		// Editor & media.
+		wp_enqueue_style( 'wp-edit-blocks' );
+		wp_enqueue_editor();
+
+		// Format library.
+		wp_enqueue_style( 'wp-format-library' );
+		wp_enqueue_script( 'wp-format-library' );
+
+		global $editor_styles;
+		wp_add_inline_script(
+			'wp-blocks',
+			'wp.blocks && wp.blocks.unstable__bootstrapServerSideBlockDefinitions && wp.blocks.unstable__bootstrapServerSideBlockDefinitions(' . wp_json_encode( get_block_editor_server_block_settings() ) . ');'
+		);
+
+		$block_editor_context = new \WP_Block_Editor_Context( array( 'name' => 'surecart/block-editor' ) );
+
+		// Get block editor settings.
+		$editor_settings = get_block_editor_settings( [], $block_editor_context );
+
+		wp_add_inline_script(
+			'surecart-components',
+			sprintf( 'wp.blocks.setCategories( %s );', wp_json_encode( $editor_settings['blockCategories'] ) ),
+			'before'
+		);
+
+		wp_localize_script(
+			'surecart-components',
+			'surecartBlockEditorSettings',
+			$editor_settings
+		);
+
+		$active_global_styles_id = \WP_Theme_JSON_Resolver::get_user_global_styles_post_id();
+		$active_theme            = get_stylesheet();
+
+		$navigation_rest_route = rest_get_route_for_post_type_items(
+			'wp_navigation'
+		);
+
+		$preload_paths = array(
+			array( '/wp/v2/media', 'OPTIONS' ),
+			'/wp/v2/types?context=view',
+			'/wp/v2/types/wp_template?context=edit',
+			'/wp/v2/types/wp_template-part?context=edit',
+			'/wp/v2/templates?context=edit&per_page=-1',
+			'/wp/v2/template-parts?context=edit&per_page=-1',
+			'/wp/v2/themes?context=edit&status=active',
+			'/wp/v2/global-styles/' . $active_global_styles_id . '?context=edit',
+			'/wp/v2/global-styles/' . $active_global_styles_id,
+			'/wp/v2/global-styles/themes/' . $active_theme,
+			array( $navigation_rest_route, 'OPTIONS' ),
+			array(
+				add_query_arg(
+					array(
+						'context'   => 'edit',
+						'per_page'  => 100,
+						'order'     => 'desc',
+						'orderby'   => 'date',
+						// array indices are required to avoid query being encoded and not matching in cache .
+						'status[0]' => 'publish',
+						'status[1]' => 'draft',
+					),
+					$navigation_rest_route
+				),
+				'GET',
+			),
+		);
+
+		block_editor_rest_api_preload( $preload_paths, $block_editor_context );
+
+		wp_add_inline_script(
+			'wp-edit-site',
+			sprintf(
+				'wp.domReady( function() { wp.editSite.initializeEditor( "site-editor", %s ); } );',
+				wp_json_encode( $editor_settings )
+			)
+		);
+
+		// Preload server-registered block schemas.
+		wp_add_inline_script(
+			'wp-blocks',
+			'wp.blocks.unstable__bootstrapServerSideBlockDefinitions(' . wp_json_encode( get_block_editor_server_block_settings() ) . ');'
+		);
+
+		wp_add_inline_script(
+			'wp-blocks',
+			sprintf( 'wp.blocks.setCategories( %s );', wp_json_encode( isset( $editor_settings['blockCategories'] ) ? $editor_settings['blockCategories'] : array() ) ),
+			'after'
+		);
+
+		if (
+			current_theme_supports( 'wp-block-styles' ) &&
+			( ! is_array( $editor_styles ) || count( $editor_styles ) === 0 )
+		) {
+			wp_enqueue_style( 'wp-block-library-theme' );
+		}
+
+		// Global styles.
+		wp_register_style( 'sc-global-presets', false ); // phpcs:ignore
+		wp_add_inline_style( 'sc-global-presets', wp_get_global_stylesheet( array( 'presets' ) ) );
+		wp_enqueue_style( 'sc-global-presets' );
+
+		/**
+		 * Filters the arguments used to register a block type.
+		 */
 		add_filter( 'register_block_type_args', array( $this, 'registerMetadataAttribute' ) );
+
+		/**
+		 * Fires after block editor assets have been enqueued.
+		 */
 		do_action( 'enqueue_block_editor_assets' );
 	}
 
