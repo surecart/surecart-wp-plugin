a55b32bd0ee5e06a4ba29a6d2f364ad3b8b6c45d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-02-28 19:10:23 +0600

    Add width control and removed alignment control

--- app/src/Integrations/Elementor/Widgets/CartMenuIcon.php
+++ app/src/Integrations/Elementor/Widgets/CartMenuIcon.php
@@ -92,10 +92,7 @@ class CartMenuIcon extends \Elementor\Widget_Base {
 				'type'        => \Elementor\Controls_Manager::ICONS,
 				'skin'        => 'inline',
 				'label_block' => false,
-				'default'     => [
-					'value'   => 'fas fa-shopping-cart',
-					'library' => 'fa-solid',
-				],
+				'default'     => null,
 				'recommended' => [
 					'fa-solid' => [
 						'cart-arrow-down',
@@ -114,7 +111,7 @@ class CartMenuIcon extends \Elementor\Widget_Base {
 				'label_on'     => esc_html__( 'Yes', 'surecart' ),
 				'label_off'    => esc_html__( 'No', 'surecart' ),
 				'return_value' => 'yes',
-				'default'      => 'no',
+				'default'      => 'yes',
 			)
 		);
 
@@ -128,6 +125,205 @@ class CartMenuIcon extends \Elementor\Widget_Base {
 	 * @return void
 	 */
 	private function register_style_settings() {
+		$this->start_controls_section(
+			'section_style_icon',
+			[
+				'label' => esc_html__( 'Icon', 'surecart' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->start_controls_tabs( 'icon_colors' );
+
+		$this->start_controls_tab(
+			'icon_colors_normal',
+			[
+				'label' => esc_html__( 'Normal', 'surecart' ),
+			]
+		);
+
+		$this->add_control(
+			'primary_color',
+			[
+				'label'     => esc_html__( 'Primary Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}} .sc-cart-icon'     => 'color: {{VALUE}};',
+					'{{WRAPPER}} .sc-cart-icon svg' => 'fill: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->start_controls_tab(
+			'icon_colors_hover',
+			[
+				'label' => esc_html__( 'Hover', 'surecart' ),
+			]
+		);
+
+		$this->add_control(
+			'hover_primary_color',
+			[
+				'label'     => esc_html__( 'Primary Color', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '',
+				'selectors' => [
+					'{{WRAPPER}} .sc-cart-icon:hover'     => 'color: {{VALUE}};',
+					'{{WRAPPER}} .sc-cart-icon:hover svg' => 'fill: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'hover_animation',
+			[
+				'label' => esc_html__( 'Hover Animation', 'surecart' ),
+				'type'  => \Elementor\Controls_Manager::HOVER_ANIMATION,
+			]
+		);
+
+		$this->end_controls_tab();
+
+		$this->end_controls_tabs();
+
+		$this->add_responsive_control(
+			'size',
+			[
+				'label'      => esc_html__( 'Size', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+				'range'      => [
+					'px' => [
+						'min' => 6,
+						'max' => 300,
+					],
+				],
+				'default'    => [
+					'size' => 16,
+					'unit' => 'px',
+				],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-cart-icon'     => 'font-size: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .sc-cart-icon svg' => 'height: {{SIZE}}{{UNIT}};',
+				],
+				'separator'  => 'before',
+			]
+		);
+
+		$this->add_responsive_control(
+			'width',
+			[
+				'label'      => esc_html__( 'Width', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+				'range'      => [
+					'px' => [
+						'min' => 6,
+						'max' => 300,
+					],
+				],
+				'default'    => [
+					'size' => 16,
+					'unit' => 'px',
+				],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-cart-icon' => 'width: {{SIZE}}{{UNIT}};',
+				],
+				'separator'  => 'before',
+			]
+		);
+
+		$this->add_control(
+			'fit_to_size',
+			[
+				'label'       => esc_html__( 'Fit to Size', 'surecart' ),
+				'type'        => \Elementor\Controls_Manager::SWITCHER,
+				'description' => 'Avoid gaps around icons when width and height aren\'t equal',
+				'label_off'   => esc_html__( 'Off', 'surecart' ),
+				'label_on'    => esc_html__( 'On', 'surecart' ),
+				'condition'   => [
+					'cart_icon[library]' => 'svg',
+				],
+				'selectors'   => [
+					'{{WRAPPER}} .sc-cart-icon svg' => 'width: auto;',
+				],
+			]
+		);
+
+		$this->add_control(
+			'icon_padding',
+			[
+				'label'      => esc_html__( 'Padding', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-cart-icon' => 'padding: {{SIZE}}{{UNIT}};',
+				],
+				'range'      => [
+					'px'  => [
+						'max' => 50,
+					],
+					'em'  => [
+						'min' => 0,
+						'max' => 5,
+					],
+					'rem' => [
+						'min' => 0,
+						'max' => 5,
+					],
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'rotate',
+			[
+				'label'          => esc_html__( 'Rotate', 'surecart' ),
+				'type'           => \Elementor\Controls_Manager::SLIDER,
+				'size_units'     => [ 'deg', 'grad', 'rad', 'turn', 'custom' ],
+				'default'        => [
+					'unit' => 'deg',
+				],
+				'tablet_default' => [
+					'unit' => 'deg',
+				],
+				'mobile_default' => [
+					'unit' => 'deg',
+				],
+				'selectors'      => [
+					'{{WRAPPER}} .sc-cart-icon i, {{WRAPPER}} .sc-cart-icon svg' => 'transform: rotate({{SIZE}}{{UNIT}});',
+				],
+			]
+		);
+
+		$this->add_control(
+			'border_width',
+			[
+				'label'      => esc_html__( 'Border Width', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'vw', 'custom' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-cart-icon' => 'border-width: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_responsive_control(
+			'border_radius',
+			[
+				'label'      => esc_html__( 'Border Radius', 'surecart' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em', 'rem', 'custom' ],
+				'selectors'  => [
+					'{{WRAPPER}} .sc-cart-icon' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
 	}
 
 	/**
@@ -162,7 +358,7 @@ class CartMenuIcon extends \Elementor\Widget_Base {
 		if ( \Elementor\Plugin::instance()->editor->is_edit_mode() ) {
 			$this->cart_icon = $this->cart_icon ?? \SureCart::svg()->get( 'shopping-bag' ); // Fall back to default icon for editor.
 			?>
-			<div class="sc-cart-icon" style="font-size: var(--sc-cart-icon-size, 1.1em); cursor: pointer; position: relative;" aria-label="<?php echo esc_attr__( 'Open cart', 'surecart' ); ?>">
+			<div class="sc-cart-icon elementor-animation-<?php echo esc_attr( $settings['hover_animation'] ); ?>" style="font-size: var(--sc-cart-icon-size, 1.1em); cursor: pointer; position: relative;" aria-label="<?php echo esc_attr__( 'Open cart', 'surecart' ); ?>">
 				<?php echo $this->cart_icon; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				<span class="sc-cart-count" style="box-sizing: border-box; position: absolute; inset: -12px -16px auto auto; text-align: center; font-size: 10px; font-weight: bold; border-radius: var(--sc-cart-icon-counter-border-radius, 9999px); color: var(--sc-cart-icon-counter-color, var(--sc-color-primary-text, var(--sc-color-white))); background: var(--sc-cart-icon-counter-background, var(--sc-color-primary-500)); box-shadow: var(--sc-cart-icon-box-shadow, var(--sc-shadow-x-large)); padding: 2px 6px; line-height: 14px; min-width: 14px; z-index: 1;">2</span>
 			</div>
