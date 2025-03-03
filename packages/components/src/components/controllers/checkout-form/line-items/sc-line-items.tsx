8951561a092b526bfb7aa5ffbff27bc27ccacd74
Andre Gagnon <ajgagnon@uwalumni.com>
2025-03-03 16:47:25 -0600

    Styling

--- packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
+++ packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
@@ -1,4 +1,4 @@
-import { Component, Fragment, h, Prop } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 import { __ } from '@wordpress/i18n';
 
 import { state as checkoutState } from '@store/checkout';
@@ -79,35 +79,32 @@ export class ScLineItems {
 
     return (
       <div class="line-items" part="base" tabindex="0">
-        {sortedItems.map((item, index) => {
+        {sortedItems.map(item => {
           const max = getMaxStockQuantity(item?.price?.product as Product, item?.variant as Variant);
           return (
-            <Fragment>
-              {index > 0 && <sc-divider></sc-divider>}
-              <div class={`line-item ${item?.price?.current_swap || item?.swap ? 'line-item--has-swap' : ''}`}>
-                <sc-product-line-item
-                  key={item.id}
-                  image={item?.image}
-                  name={(item?.price?.product as Product)?.name}
-                  price={item?.price?.name}
-                  variant={item?.variant_display_options}
-                  fees={item?.fees?.data}
-                  amount={item.ad_hoc_display_amount ? item.ad_hoc_display_amount : item.subtotal_display_amount}
-                  scratch={!item.ad_hoc_display_amount && item?.scratch_display_amount}
-                  trial={item?.price?.trial_text}
-                  interval={`${item?.price?.short_interval_text} ${item?.price?.short_interval_count_text}`}
-                  quantity={item.quantity}
-                  purchasableStatus={item?.purchasable_status_display}
-                  {...(max ? { max } : {})}
-                  editable={this.isEditable(item)}
-                  removable={!item?.locked && this.removable}
-                  onScUpdateQuantity={e => updateCheckoutLineItem({ id: item.id, data: { quantity: e.detail } })}
-                  onScRemove={() => removeCheckoutLineItem(item?.id)}
-                  exportparts="base:line-item, product-line-item, image:line-item__image, text:line-item__text, title:line-item__title, suffix:line-item__suffix, price:line-item__price, price__amount:line-item__price-amount, price__description:line-item__price-description, price__scratch:line-item__price-scratch, static-quantity:line-item__static-quantity, remove-icon__base:line-item__remove-icon, quantity:line-item__quantity, quantity__minus:line-item__quantity-minus, quantity__minus-icon:line-item__quantity-minus-icon, quantity__plus:line-item__quantity-plus, quantity__plus-icon:line-item__quantity-plus-icon, quantity__input:line-item__quantity-input, line-item__price-description:line-item__price-description"
-                />
-                <sc-swap lineItem={item} />
-              </div>
-            </Fragment>
+            <div class={`line-item ${item?.price?.current_swap || item?.swap ? 'line-item--has-swap' : ''}`}>
+              <sc-product-line-item
+                key={item.id}
+                image={item?.image}
+                name={(item?.price?.product as Product)?.name}
+                price={item?.price?.name}
+                variant={item?.variant_display_options}
+                fees={item?.fees?.data}
+                amount={item.ad_hoc_display_amount ? item.ad_hoc_display_amount : item.subtotal_display_amount}
+                scratch={!item.ad_hoc_display_amount && item?.scratch_display_amount}
+                trial={item?.price?.trial_text}
+                interval={`${item?.price?.short_interval_text} ${item?.price?.short_interval_count_text}`}
+                quantity={item.quantity}
+                purchasableStatus={item?.purchasable_status_display}
+                {...(max ? { max } : {})}
+                editable={this.isEditable(item)}
+                removable={!item?.locked && this.removable}
+                onScUpdateQuantity={e => updateCheckoutLineItem({ id: item.id, data: { quantity: e.detail } })}
+                onScRemove={() => removeCheckoutLineItem(item?.id)}
+                exportparts="base:line-item, product-line-item, image:line-item__image, text:line-item__text, title:line-item__title, suffix:line-item__suffix, price:line-item__price, price__amount:line-item__price-amount, price__description:line-item__price-description, price__scratch:line-item__price-scratch, static-quantity:line-item__static-quantity, remove-icon__base:line-item__remove-icon, quantity:line-item__quantity, quantity__minus:line-item__quantity-minus, quantity__minus-icon:line-item__quantity-minus-icon, quantity__plus:line-item__quantity-plus, quantity__plus-icon:line-item__quantity-plus-icon, quantity__input:line-item__quantity-input, line-item__price-description:line-item__price-description"
+              />
+              <sc-swap lineItem={item} />
+            </div>
           );
         })}
       </div>
