9991c3831b9671ea2908fbb7742c5a17931b0705
Andre Gagnon <ajgagnon@uwalumni.com>
2025-03-05 15:46:21 -0600

    Add tooltip, cursor.

--- packages/admin/components/block-editor/PreviewBlocks.js
+++ packages/admin/components/block-editor/PreviewBlocks.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import { __ } from '@wordpress/i18n';
 
 /**
  * External dependencies.
@@ -10,7 +11,7 @@ import {
 	__unstableIframe as Iframe,
 	__unstableEditorStyles as EditorStyles,
 } from '@wordpress/block-editor';
-import { Disabled } from '@wordpress/components';
+import { Disabled, Tooltip } from '@wordpress/components';
 
 export default function ({ blocks }) {
 	return (
@@ -26,30 +27,36 @@ export default function ({ blocks }) {
 					}
 				`}
 			>
-				<Iframe
-					name="editor-canvas"
-					className="edit-site-visual-editor__editor-canvas"
-					css={css`
-						width: 100% !important;
-						height: 100vh !important;
-						max-height: 400px !important;
-						padding: 5px;
-						zoom: 0.7;
-						cursor: pointer;
-					`}
+				<Tooltip
+					text={__('Click to edit', 'surecart')}
+					placement="bottom"
 				>
-					<EditorStyles
-						styles={surecartBlockEditorSettings?.styles}
-					/>
-					<Disabled style={{ cursor: 'pointer' }}>
-						<div style={{ padding: '10px 20px' }}>
-							<BlockList
-								renderAppender={() => null}
-								className="surecart-block-editor__block-list"
-							/>
+					<Iframe
+						name="editor-canvas"
+						className="edit-site-visual-editor__editor-canvas"
+						css={css`
+							width: 100% !important;
+							height: 100vh !important;
+							max-height: 400px !important;
+							padding: 5px;
+							zoom: 0.7;
+						`}
+					>
+						<EditorStyles
+							styles={surecartBlockEditorSettings?.styles}
+						/>
+						<div style={{ cursor: 'pointer' }}>
+							<Disabled>
+								<div style={{ padding: '10px 20px' }}>
+									<BlockList
+										renderAppender={() => null}
+										className="surecart-block-editor__block-list"
+									/>
+								</div>
+							</Disabled>
 						</div>
-					</Disabled>
-				</Iframe>
+					</Iframe>
+				</Tooltip>
 			</div>
 		</BlockEditorProvider>
 	);
