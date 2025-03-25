2a2ea48db88c3b5fc16e376ae577291acbccd4e2
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-03-25 17:05:48 +0530

    Updated comments

--- packages/blocks-next/src/blocks/product-quick-view/view.php
+++ packages/blocks-next/src/blocks/product-quick-view/view.php
@@ -16,11 +16,11 @@ use SureCart\Models\Blocks\ProductPageBlock;
 		while ( $query->have_posts() ) :
 			$query->the_post();
 
-			// Get an instance of the current Post Template block.
+			// Get an instance of the current Product Quick view block.
 			$block_instance = $block->parsed_block;
 
 			// Set the block name to one that does not correspond to an existing registered block.
-			// This ensures that for the inner instances of the Post Template block, we do not render any block supports.
+			// This ensures that for the inner instances of the Product Quick view block, we do not render any block supports.
 			$block_instance['blockName'] = 'core/null';
 
 			$product_post_id      = get_the_ID();
@@ -34,7 +34,7 @@ use SureCart\Models\Blocks\ProductPageBlock;
 				// Use an early priority to so that other 'render_block_context' filters have access to the values.
 				add_filter( 'render_block_context', $filter_block_context, 1 );
 
-				// Render the inner blocks of the Post Template block with `dynamic` set to `false` to prevent calling
+				// Render the inner blocks of the Product Quick view block with `dynamic` set to `false` to prevent calling
 				// `render_callback` and ensure that no wrapper markup is included.
 				$block_content = ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
 				remove_filter( 'render_block_context', $filter_block_context, 1 );
