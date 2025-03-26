77ca2ff5d1f851e04e42532e5f95de20b6fde82b
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-03-26 19:55:06 +0600

    Show product details in frontend if product id is set

--- packages/blocks-next/src/blocks/product-page/controller.php
+++ packages/blocks-next/src/blocks/product-page/controller.php
@@ -4,6 +4,20 @@
 use SureCart\Models\Blocks\ProductPageBlock;
 
 $product = sc_get_product();
+global $post;
+
+if ( ! empty( $attributes['product_id'] ) ) {
+	$product = \SureCart\Models\Product::with( [ 'image', 'prices', 'product_medias', 'product_media.media', 'variants', 'variant_options' ] )->find( $attributes['product_id'] );
+
+
+	if ( ! empty( $product->post->ID ) ) {
+		$current_post = get_post( $product->post->ID );
+		if ( $current_post instanceof \WP_Post ) {
+			$post = $current_post; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+			setup_postdata( $post ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		}
+	}
+}
 
 $controller = new ProductPageBlock();
 $state      = $controller->state();
