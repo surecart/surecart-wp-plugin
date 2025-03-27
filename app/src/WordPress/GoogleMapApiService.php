b50ebff4bb5fb174db224be67b21c4b9990234b6
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-03-27 15:46:35 +0600

    Removed Google map validation logic from backend

--- app/src/WordPress/GoogleMapApiService.php
+++ app/src/WordPress/GoogleMapApiService.php
@@ -29,20 +29,15 @@ class GoogleMapApiService {
 	/**
 	 * Encrypt the Google Map API key before saving it to the database.
 	 *
-	 * @param mixed  $value    New value.
+	 * @param mixed  $value     New value.
 	 * @param mixed  $old_value Old value.
-	 * @param string $option   Option name.
+	 * @param string $option    Option name.
 	 *
 	 * @return string|\WP_Error
 	 */
 	public function encryptSettings( $value, $old_value, $option ) {
 		if ( empty( $option ) ) {
 			return $value;
-		}
-
-		$validated = $this->validate( $value );
-		if ( is_wp_error( $validated ) ) {
-			return new \WP_Error( 'invalid_google_map_api_key', $validated->get_error_message() );
 		}
 
 		return Encryption::encrypt( $value );
@@ -90,41 +85,5 @@ class GoogleMapApiService {
 		}
 
 		return get_option( self::API_KEY_OPTION_NAME, '' );
-	}
-
-	/**
-	 * Get Google Map api request.
-	 *
-	 * @param string $api_key google map api key.
-	 * @return object
-	 */
-	public function makeRequest( $api_key ) {
-		$google_map_url      = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' . $api_key;
-		$google_map_response = wp_remote_get( $google_map_url );
-		$google_map_body     = wp_remote_retrieve_body( $google_map_response );
-		return json_decode( $google_map_body );
-	}
-
-	/**
-	 * Validate the Google Map API key.
-	 *
-	 * @param string $api_key Google Map API key.
-	 *
-	 * @return true|\WP_Error
-	 */
-	public function validate( $api_key ) {
-		// If the API key is empty, just pass it as valid.
-		if ( empty( $api_key ) ) {
-			return true;
-		}
-
-		$response = $this->makeRequest( $api_key );
-
-		// Check if the response is valid.
-		if ( isset( $response->status ) && 'OK' !== $response->status ) {
-			return new \WP_Error( 'google_map_api_key_invalid', $response->error_message ?? __( 'Invalid Google Map API key.', 'surecart' ) );
-		}
-
-		return true;
 	}
 }
