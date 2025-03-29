086051fcfa06fb7161cb1529bff17adde7074b6e
Andre Gagnon <ajgagnon@uwalumni.com>
2025-03-29 08:59:15 -0500

    Don't show tooltip on back button

--- packages/pages/admin/ProductPageBackButton.js
+++ packages/pages/admin/ProductPageBackButton.js
@@ -24,6 +24,7 @@ export default () => {
 			<FullscreenModeClose
 				icon={wordpress}
 				label={__('Edit Product', 'surecart')}
+				showTooltip={false}
 				href={addQueryArgs('/wp-admin/admin.php', {
 					page: 'sc-products',
 					action: 'edit',
