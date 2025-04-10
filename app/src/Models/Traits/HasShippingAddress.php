27959f4efc63a6e43f7f772e34ee857974a1b8c5
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-04-10 19:49:40 +0530

    SUR-3124 - Fix: Fix Shipping Currency to Reflect Selected Multi-Currency Option During Checkout.

--- app/src/Models/Traits/HasShippingAddress.php
+++ app/src/Models/Traits/HasShippingAddress.php
@@ -2,6 +2,9 @@
 
 namespace SureCart\Models\Traits;
 
+use SureCart\Support\Currency;
+
+
 trait HasShippingAddress {
 	/**
 	 * Always set discount as object.
@@ -12,6 +15,28 @@ trait HasShippingAddress {
 	protected function setShippingAddressAttribute( $value ) {
 		// force either string or object.
 		$this->attributes['shipping_address'] = is_string( $value ) ? $value : (object) $value;
+		return $this;
+	}
+
+	/**
+	 * Set the shipping choices display amount.
+	 *
+	 * @param array|object $value Value to set.
+	 * @return $this
+	 */
+	protected function setShippingChoicesAttribute( $value ) {
+		if ( empty( $value ) || ! isset( $value->data ) || ! is_array( $value->data ) ) {
+			return $this;
+		}
+
+		foreach ( $value->data as $key => $choice ) {
+			if ( ! empty( $choice->amount ) ) {
+				$currency               = $choice->currency ?? 'usd';
+				$choice->display_amount = Currency::format( $choice->amount, $currency );
+			}
+		}
+
+		$this->attributes['shipping_choices'] = $value;
 		return $this;
 	}
 }
