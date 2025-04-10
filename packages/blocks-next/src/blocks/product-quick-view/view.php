dd987b0c70c59922fe6b1ae691ea5b0a453b7425
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-04-10 18:44:49 +0530

    Added view product page sticky footer in the quick view popup

--- packages/blocks-next/src/blocks/product-quick-view/view.php
+++ packages/blocks-next/src/blocks/product-quick-view/view.php
@@ -24,65 +24,80 @@ use SureCart\Models\Blocks\ProductPageBlock;
 		data-wp-class--show-closing-animation="state.showClosingAnimation"
 		data-wp-on--keydown="actions.handleKeyDown"
 	>
+		<div
+			class="sc-product-quick-view-dialog__content"
+		>
+			<a 
+				class="sc-product-quick-view-dialog__close-button"
+				data-wp-on--click="actions.navigate"
+				data-wp-on--keydown="actions.navigate"
+				data-wp-on--mouseenter="actions.prefetch"
+				role="button"
+				tabindex="0"
+				aria-label="<?php esc_attr_e( 'Close quick view', 'surecart' ); ?>"
+				href="<?php echo esc_url( $close_url ); ?>"
+			>
+				<?php echo wp_kses( SureCart::svg()->get( 'x' ), sc_allowed_svg_html() ); ?>
+			</a>
+			<?php
+			while ( $query->have_posts() ) :
+				$query->the_post();
+
+				// Get an instance of the current Product Quick view block.
+				$block_instance = $block->parsed_block;
+
+				// Set the block name to one that does not correspond to an existing registered block.
+				// This ensures that for the inner instances of the Product Quick view block, we do not render any block supports.
+				$block_instance['blockName'] = 'core/null';
+
+				$product_post_id      = get_the_ID();
+				$product_post_type    = get_post_type();
+				$filter_block_context = static function ( $context ) use ( $product_post_id, $product_post_type ) {
+					$context['postType'] = $product_post_type;
+					$context['postId']   = $product_post_id;
+					return $context;
+				};
+
+					// Use an early priority to so that other 'render_block_context' filters have access to the values.
+					add_filter( 'render_block_context', $filter_block_context, 1 );
+
+					// Render the inner blocks of the Product Quick view block with `dynamic` set to `false` to prevent calling
+					// `render_callback` and ensure that no wrapper markup is included.
+					$block_content = ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
+					remove_filter( 'render_block_context', $filter_block_context, 1 );
+
+					// Wrap the render inner blocks in a `li` element with the appropriate post classes.
+					$post_classes = implode( ' ', get_post_class( 'wp-block-post' ) );
+
+					$controller = new ProductPageBlock();
+					$state      = $controller->state();
+					$context    = $controller->context();
+
+					wp_interactivity_state( 'surecart/product-page', $state );
+				?>
+					<form 
+						<?php
+						echo wp_kses_data( wp_interactivity_data_wp_context( $context ) );
+						?>
+						data-wp-interactive='{ "namespace": "surecart/product-page" }'
+						data-wp-on--submit="callbacks.handleSubmit"
+						data-wp-init="callbacks.init"
+					>
+						<?php echo $block_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</form>
+			<?php endwhile; ?>
+		</div>
 		<a 
-			class="sc-product-quick-view-dialog__close-button"
-			data-wp-on--click="actions.navigate"
-			data-wp-on--keydown="actions.navigate"
-			data-wp-on--mouseenter="actions.prefetch"
+			class="sc-view-product-page-sticky"
+			data-wp-class--hidden="state.loading"
 			role="button"
 			tabindex="0"
-			aria-label="<?php esc_attr_e( 'Close quick view', 'surecart' ); ?>"
-			href="<?php echo esc_url( $close_url ); ?>"
+			aria-label="<?php esc_attr_e( 'View product page', 'surecart' ); ?>"
+			href="<?php echo esc_url( get_the_permalink( $product_id ) ); ?>"
 		>
-			<?php echo wp_kses( SureCart::svg()->get( 'x' ), sc_allowed_svg_html() ); ?>
+			<?php esc_html_e( 'View product page', 'surecart' ); ?>
+			<?php echo wp_kses( SureCart::svg()->get( 'arrow-right' ), sc_allowed_svg_html() ); ?>
 		</a>
-		<?php
-		while ( $query->have_posts() ) :
-			$query->the_post();
-
-			// Get an instance of the current Product Quick view block.
-			$block_instance = $block->parsed_block;
-
-			// Set the block name to one that does not correspond to an existing registered block.
-			// This ensures that for the inner instances of the Product Quick view block, we do not render any block supports.
-			$block_instance['blockName'] = 'core/null';
-
-			$product_post_id      = get_the_ID();
-			$product_post_type    = get_post_type();
-			$filter_block_context = static function ( $context ) use ( $product_post_id, $product_post_type ) {
-				$context['postType'] = $product_post_type;
-				$context['postId']   = $product_post_id;
-				return $context;
-			};
-
-				// Use an early priority to so that other 'render_block_context' filters have access to the values.
-				add_filter( 'render_block_context', $filter_block_context, 1 );
-
-				// Render the inner blocks of the Product Quick view block with `dynamic` set to `false` to prevent calling
-				// `render_callback` and ensure that no wrapper markup is included.
-				$block_content = ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
-				remove_filter( 'render_block_context', $filter_block_context, 1 );
-
-				// Wrap the render inner blocks in a `li` element with the appropriate post classes.
-				$post_classes = implode( ' ', get_post_class( 'wp-block-post' ) );
-
-				$controller = new ProductPageBlock();
-				$state      = $controller->state();
-				$context    = $controller->context();
-
-				wp_interactivity_state( 'surecart/product-page', $state );
-			?>
-				<form 
-					<?php
-					echo wp_kses_data( wp_interactivity_data_wp_context( $context ) );
-					?>
-					data-wp-interactive='{ "namespace": "surecart/product-page" }'
-					data-wp-on--submit="callbacks.handleSubmit"
-					data-wp-init="callbacks.init"
-				>
-					<?php echo $block_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-				</form>
-		<?php endwhile; ?>
 		<div class="sc-block-ui" style="--sc-block-ui-opacity:1" data-wp-bind--hidden="!state.loading" hidden>
 			<span class="sc-spinner" data-wp-bind--hidden="!state.loading"></span>
 		</div>
