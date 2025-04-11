a756aeaa895970954ecc4bdd2000b86f13d7e7f5
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-04-11 16:18:16 +0530

    Added ShippingChoice Model & required traits.

--- app/src/Models/Traits/HasShippingAddress.php
+++ app/src/Models/Traits/HasShippingAddress.php
@@ -17,26 +17,4 @@ trait HasShippingAddress {
 		$this->attributes['shipping_address'] = is_string( $value ) ? $value : (object) $value;
 		return $this;
 	}
-
-	/**
-	 * Set the shipping choices display amount.
-	 *
-	 * @param array|object $value Value to set.
-	 * @return $this
-	 */
-	protected function setShippingChoicesAttribute( $value ) {
-		if ( empty( $value ) || ! isset( $value->data ) || ! is_array( $value->data ) ) {
-			return $this;
-		}
-
-		foreach ( $value->data as $key => $choice ) {
-			if ( ! empty( $choice->amount ) ) {
-				$currency               = $choice->currency ?? 'usd';
-				$choice->display_amount = Currency::format( $choice->amount, $currency );
-			}
-		}
-
-		$this->attributes['shipping_choices'] = $value;
-		return $this;
-	}
 }
