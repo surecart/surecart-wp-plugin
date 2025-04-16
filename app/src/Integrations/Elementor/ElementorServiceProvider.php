0ef0aea44f7420de62304349c376646c9443d0ae
Andre Gagnon <ajgagnon@uwalumni.com>
2025-04-16 14:08:57 -0500

    Refactor

--- app/src/Integrations/Elementor/ElementorServiceProvider.php
+++ app/src/Integrations/Elementor/ElementorServiceProvider.php
@@ -1,9 +1,6 @@
 <?php
 namespace SureCart\Integrations\Elementor;
 
-use SureCart\Integrations\Elementor\Conditions\Conditions;
-use SureCart\Integrations\Elementor\Documents\ProductDocument;
-use SureCart\Models\Product;
 use SureCartCore\ServiceProviders\ServiceProviderInterface;
 
 /**
@@ -23,6 +20,10 @@ class ElementorServiceProvider implements ServiceProviderInterface {
 
 		$container['surecart.elementor.widgets'] = function () {
 			return new ElementorWidgetsService();
+		};
+
+		$container['surecart.elementor.documents'] = function () {
+			return new ElementorDocumentsService();
 		};
 
 		$container['surecart.elementor.dynamic_tags'] = function () {
@@ -60,91 +61,7 @@ class ElementorServiceProvider implements ServiceProviderInterface {
 
 		if ( defined( 'ELEMENTOR_PRO_VERSION' ) ) {
 			$container['elementor.block.adapter.service']->bootstrap();
+			$container['surecart.elementor.documents']->bootstrap();
 		}
-
-		// Register product theme condition.
-		if ( defined( 'ELEMENTOR_PRO_VERSION' ) ) {
-			add_action( 'elementor/documents/register', [ $this, 'register_document' ] );
-			add_action( 'elementor/theme/register_conditions', [ $this, 'product_theme_conditions' ] );
-			add_filter( 'elementor/query/get_autocomplete/surecart-product', [ $this, 'get_autocomplete' ], 10, 2 );
-			add_filter( 'elementor/query/get_value_titles/surecart-product', [ $this, 'get_titles' ], 10, 2 );
-		}
-	}
-
-	/**
-	 * Get the titles for the query control
-	 * This is important as it shows the previously selected items when the conditions load in.
-	 *
-	 * @param array $results The results.
-	 * @param array $request The request.
-	 *
-	 * @return array
-	 */
-	public function get_titles( $results, $request ) {
-		if ( 'surecart-product' !== $request['get_titles']['object'] || empty( $request['id'] ) ) {
-			return $results;
-		}
-
-		$products = Product::where(
-			[
-				'ids' => [ $request['id'] ],
-			]
-		)->get();
-
-		foreach ( $products as $product ) {
-			$results[ $product->id ] = $product->name;
-		}
-		return $results;
-	}
-
-	/**
-	 * Get autocomplete
-	 *
-	 * @param array $results The results.
-	 * @param array $data Request data.
-	 *
-	 * @return array
-	 */
-	public function get_autocomplete( $results, $data ) {
-		if ( 'surecart-product' !== $data['autocomplete']['object'] ) {
-			return $results;
-		}
-
-		$products = Product::where(
-			[
-				'query'    => $data['q'],
-				'archived' => false,
-			]
-		)->get();
-
-		foreach ( $products as $product ) {
-			$results[] = [
-				'id'   => $product->id,
-				'text' => $product->name,
-			];
-		}
-		return $results;
-	}
-
-	/**
-	 * Add product theme condition.
-	 *
-	 * @param \ElementorPro\Modules\ThemeBuilder\Classes\Documents_Manager $documents_manager The documents manager.
-	 *
-	 * @return void
-	 */
-	public function register_document( $documents_manager ) {
-		$documents_manager->register_document_type( 'surecart-product', ProductDocument::get_class_full_name() );
-	}
-
-	/**
-	 * Add product theme condition.
-	 *
-	 * @param \ElementorPro\Modules\ThemeBuilder\Classes\Conditions_Manager $conditions_manager The conditions manager.
-	 *
-	 * @return void
-	 */
-	public function product_theme_conditions( $conditions_manager ) {
-		$conditions_manager->register_condition_instance( new Conditions() );
 	}
 }
