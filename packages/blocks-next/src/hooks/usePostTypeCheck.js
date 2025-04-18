af7338e8552328839f3d9bcf67f1f9c8e9d44c9f
Andre Gagnon <ajgagnon@uwalumni.com>
2025-04-18 15:03:13 -0500

    Simplify

--- packages/blocks-next/src/hooks/usePostTypeCheck.js
+++ packages/blocks-next/src/hooks/usePostTypeCheck.js
@@ -1,24 +1,21 @@
 /**
  * External dependencies.
  */
-import { useSelect } from "@wordpress/data";
+import { useSelect } from '@wordpress/data';
 
 export function usePostTypeCheck(postTypes, postIdIncludes) {
-	const { shouldRender } = useSelect(
+	return useSelect(
 		(select) => {
 			const { getCurrentPostType, getCurrentPostId } =
 				select('core/editor') || {};
 			const postType = getCurrentPostType?.() || null;
 			const postId = getCurrentPostId?.();
 
-			return {
-				shouldRender:
-					postTypes.includes(postType) &&
-					postIdIncludes.some((id) => postId?.includes(id)),
-			};
+			return (
+				postTypes.includes(postType) &&
+				postIdIncludes.some((id) => postId?.includes(id))
+			);
 		},
 		[postTypes, postIdIncludes]
 	);
-
-	return shouldRender;
 }
