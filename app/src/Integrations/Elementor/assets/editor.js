83df5a2821fccbedceef9980e18c4777e54c0563
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-04-26 00:49:05 +0600

    Fixed Product and Product Card widget to remove empty container and better conditionals for surecart button

--- app/src/Integrations/Elementor/assets/editor.js
+++ app/src/Integrations/Elementor/assets/editor.js
@@ -28,100 +28,165 @@ jQuery(window).ready(function () {
 		win.focus();
 	});
 
+	/**
+	 * When adding a SureCart Product Widget, replace the default block with the product element.
+	 * This addresses issues with nested widgets in Elementor, ensuring state preservation on reload.
+	 */
+	elementor.hooks.addAction(
+		'panel/open_editor/widget/surecart-product',
+		function (panel, model, view) {
+			// Remove the default SureCart block by clearing the model.
+			model.destroy();
+
+			// Clear the preview container if it's empty.
+			maybeClearElementorPreview();
+
+			insertSureCartTemplate(
+				window?.scElementorData?.sc_product_template
+			);
+		}
+	);
+
+	// SureCart Product Card Widget.
+	elementor.hooks.addAction(
+		'panel/open_editor/widget/surecart-product-card',
+		function (panel, model, view) {
+			// Remove the default SureCart block by clearing the model.
+			model.destroy();
+
+			// Clear the preview container if it's empty.
+			maybeClearElementorPreview();
+
+			insertSureCartTemplate(
+				window?.scElementorData?.sc_product_card_template
+			);
+		}
+	);
+
 	// SureCart Product Pricing Widget.
 	elementor.hooks.addAction(
 		'panel/open_editor/widget/surecart-product-pricing',
 		function (panel, model, view) {
 			model.destroy();
 
-			insertSureCartTemplates(
+			insertSureCartTemplate(
 				window?.scElementorData?.sc_product_pricing_template
 			);
 		}
 	);
 
-	const templateAddSection = jQuery('#tmpl-elementor-add-section');
-	if (0 < templateAddSection.length) {
-		var oldTemplateButton = templateAddSection.html();
-		oldTemplateButton = oldTemplateButton.replace(
-			'<div class="elementor-add-section-drag-title',
-			'<div class="elementor-add-section-area-button elementor-surecart-template-button" title="SureCart"></div><div class="elementor-add-section-drag-title'
-		);
-		templateAddSection.html(oldTemplateButton);
+	/**
+	 * Clears the preview container if its first container element and which is empty.
+	 */
+	function maybeClearElementorPreview() {
+		const firstContainer =
+			elementor.getPreviewContainer().document?.container?.children?.[0];
+
+		if (
+			0 === firstContainer?.children?.length &&
+			firstContainer?.type === 'container'
+		) {
+			$e.run('document/elements/empty', {
+				force: true,
+			});
+		}
 	}
 
-	elementor.on('preview:loaded', function () {
-		jQuery(elementor.$previewContents[0].body).on(
-			'click',
-			'.elementor-surecart-template-button',
-			function (event) {
-				event.preventDefault();
-				const modal = jQuery('#sc-elementor-modal-dialog');
+	/**
+	 * Sets up the SureCart template button in the Elementor editor.
+	 */
+	function setupSureCartTemplateButton() {
+		const documentType = elementor.config.document.type;
 
-				// Reset and show the modal
-				modal.removeClass('show').hide();
-				modal.addClass('show').show();
+		const showSureCartIcon =
+			documentType === 'surecart-product' || documentType === 'loop-item';
+
+		if (!showSureCartIcon) return;
+
+		const templateAddSection = jQuery('#tmpl-elementor-add-section');
+		console.log('templateAddSection', templateAddSection);
+		if (templateAddSection?.length > 0) {
+			let oldTemplateButton = templateAddSection.html();
+			console.log('oldTemplateButton', oldTemplateButton);
+			if (
+				!oldTemplateButton.includes(
+					'elementor-surecart-template-button'
+				)
+			) {
+				oldTemplateButton = oldTemplateButton.replace(
+					'<div class="elementor-add-section-drag-title',
+					'<div class="elementor-add-section-area-button elementor-surecart-template-button" title="SureCart"></div><div class="elementor-add-section-drag-title'
+				);
+				templateAddSection.html(oldTemplateButton);
 			}
-		);
-	});
-
-	// Close dialog on close button click
-	jQuery(document).on('click', '#sc-elementor-modal-close', function () {
-		const modal = jQuery('#sc-elementor-modal-dialog');
-		modal.removeClass('show').hide();
-	});
-
-	// Handle card click events
-	jQuery(document).on('click', '.sc-elementor-modal-card', function () {
-		const modal = jQuery('#sc-elementor-modal-dialog');
-		modal.removeClass('show').fadeOut();
-
-		const selectedOption = jQuery(this).attr('id');
-		const templateName =
-			selectedOption === 'sc-elementor-single-product-template'
-				? window?.scElementorData?.sc_product_template
-				: window?.scElementorData?.sc_product_card_template;
-		insertSureCartTemplates(templateName);
-	});
-
-	function insertSureCartTemplates(template) {
-		// Get the active container - either the one being dragged into or the selected one
-		const container =
-			elementor.channels.data.request('container:active') ||
-			elementor.getPreviewView().getContainer();
-
-		// Get the insertion index based on the drop location or selected element
-		const dropLocation = elementor.channels.data.request('drop-location');
-		const selectedElement = elementor.selection.getElements()[0];
-
-		let at;
-		if (dropLocation) {
-			// If we have a drop location, use that
-			at = dropLocation.index;
-		} else if (selectedElement) {
-			// If we have a selected element, insert after it
-			at = container.view.collection.indexOf(selectedElement.model);
-		} else {
-			// Default to beginning of container
-			at = 0;
 		}
 
-		// Insert the product element content into the editor.
-		template.content.forEach((contentElement) => {
-			$e.run('document/elements/create', {
-				container,
-				model: contentElement,
-				options: { at },
-			});
-
-			// Increment the index to ensure elements are added in the correct order.
-			at++;
+		elementor.on('preview:loaded', function () {
+			jQuery(elementor.$previewContents[0].body).on(
+				'click',
+				'.elementor-surecart-template-button',
+				function (event) {
+					event.preventDefault();
+					const modal = jQuery('#sc-elementor-modal-dialog');
+					modal.removeClass('show').hide().addClass('show').show();
+				}
+			);
 		});
 
-		// Apply the settings to the newly created elements.
-		$e.run('document/elements/settings', {
-			container,
-			settings: template.page_settings,
+		jQuery(document).on('click', '#sc-elementor-modal-close', function () {
+			jQuery('#sc-elementor-modal-dialog').removeClass('show').hide();
+		});
+
+		jQuery(document).on('click', '.sc-elementor-modal-card', function () {
+			jQuery('#sc-elementor-modal-dialog').removeClass('show').fadeOut();
+
+			const selectedOption = jQuery(this).attr('id');
+			const templateName =
+				selectedOption === 'sc-elementor-single-product-template'
+					? window?.scElementorData?.sc_product_template
+					: window?.scElementorData?.sc_product_card_template;
+
+			insertSureCartTemplate(templateName);
+		});
+	}
+
+	// Run initially to set up the SureCart template button.
+	setupSureCartTemplateButton();
+
+	// Re-run every time Elementor document type changes to ensure the button is set up correctly.
+	elementor.on('document:loaded', setupSureCartTemplateButton);
+	elementor.channels.editor.on(
+		'change:document:type',
+		setupSureCartTemplateButton
+	);
+
+	function generateUniqueIds(element) {
+		element.id = elementorCommon.helpers.getUniqueId();
+		if (element?.elements) {
+			// Recursively generate IDs for child elements.
+			element.elements.forEach((childElement) => {
+				generateUniqueIds(childElement);
+			});
+		}
+		return element;
+	}
+
+	function insertSureCartTemplate(template) {
+		// Generate elements with unique IDs.
+		const elements = [];
+		for (let contentElement of template.elements) {
+			contentElement = generateUniqueIds(contentElement);
+			elements.push(contentElement);
+		}
+
+		$e.run('document/elements/create', {
+			container: elementor.getPreviewContainer(),
+			model: {
+				id: elementorCommon.helpers.getUniqueId(),
+				elType: 'container',
+				settings: template?.settings || {},
+				elements,
+			},
 			options: {
 				external: true,
 			},
