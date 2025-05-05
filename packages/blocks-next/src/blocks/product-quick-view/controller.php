76875d2c9e864239699872393eae88f783c25f15
Andre Gagnon <ajgagnon@uwalumni.com>
2025-05-05 17:24:31 -0500

    Simplify

--- packages/blocks-next/src/blocks/product-quick-view/controller.php
+++ packages/blocks-next/src/blocks/product-quick-view/controller.php
@@ -1,22 +1,23 @@
 <?php
-$url         = \SureCart::block()->urlParams( 'product-quick-view' );
-$show_footer = $attributes['showViewProductPageStickyFooter'] ?? true;
-$product_id  = $url->getProductQuickViewArg();
-$args        = [
+$product_id = (int) $_GET['product-quick-view'] ?? null;
+$close_url  = remove_query_arg( 'product-quick-view' );
+$query      = new WP_Query( [
 	'post__in'  => ! empty( $product_id ) ? [ $product_id ] : [ 0 ], // Ensures no posts are found if null.
 	'post_type' => 'sc_product',
-];
-$query      = new WP_Query( $args );
-$close_url  = $url->removeProductQuickViewArg();
+] );
 
 // Set the interactivity state for the quick view.
 wp_interactivity_state(
-	'@surecart/product-quick-view',
+	'surecart/product-quick-view',
 	array(
-		'open'                 => false,
-		'showClosingAnimation' => false,
-		'showOpeningAnimation' => false,
+		'open' => $query->have_posts(),
 	)
 );
+
+// We need to enqueue these assets as we do not know if the quick view item will need lightbox or slider functionality.
+wp_enqueue_style( 'surecart-lightbox' );
+wp_enqueue_script_module( 'surecart/lightbox' );
+wp_enqueue_style( 'surecart-image-slider' );
+wp_enqueue_script_module( '@surecart/image-slider' );
 
 return 'file:./view.php';
