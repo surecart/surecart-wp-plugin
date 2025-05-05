4cedb6cdac9243d595804e198b2dafea378aa10e
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-05-05 19:34:00 +0530

    Updated check

--- packages/components/src/components/ui/address/sc-address.tsx
+++ packages/components/src/components/ui/address/sc-address.tsx
@@ -148,8 +148,10 @@ export class ScAddress {
         value: region[1],
         label: this.decodeHtmlEntities(region[0]),
       }));
-      
-      this.regions = window?.wp?.hooks?.applyFilters('surecart_address_regions', this.regions, this.address.country) as Array<{ value: string; label: string }>;
+
+      if ( window?.wp?.hooks?.applyFilters ) {
+        this.regions = window.wp.hooks.applyFilters('surecart_address_regions', this.regions, this.address.country) as Array<{ value: string; label: string }>;
+      }
     });
   }
 
