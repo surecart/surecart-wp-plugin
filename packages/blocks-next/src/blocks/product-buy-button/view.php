02986587fb673b604825797a306e5ffcd9baaf55
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-06-18 12:23:23 +0600

    Removed duplicate interactivity call

--- packages/blocks-next/src/blocks/product-buy-button/view.php
+++ packages/blocks-next/src/blocks/product-buy-button/view.php
@@ -103,7 +103,6 @@ if ( isset( $attributes['show_sticky_purchase_button'] ) && $attributes['show_st
 				<div
 					data-wp-interactive='{ "namespace": "surecart/product-page" }'
 					<?php echo wp_kses_data( wp_interactivity_data_wp_context( $context ) ); ?>
-					data-wp-interactive='{ "namespace": "surecart/product-page" }'
 					data-wp-on--submit="callbacks.handleSubmit"
 					data-wp-init="callbacks.init"
 				>
