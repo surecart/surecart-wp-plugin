75b9bba72ab4fea5574fe9e4470e8e92a6b8ee5f
Andre Gagnon <ajgagnon@uwalumni.com>
2025-07-01 15:08:44 -0500

    Refactor cart subtotal amount view for cleaner code by consolidating attributes in a single line.

--- packages/blocks-next/src/blocks/cart-subtotal-amount/view.php
+++ packages/blocks-next/src/blocks/cart-subtotal-amount/view.php
@@ -1,7 +1,1 @@
-<span <?php echo wp_kses_data(
-	get_block_wrapper_attributes(
-		array(
-			'data-wp-text' => 'state.checkout.total_scratch_display_amount',
-		)
-	)
-); ?>></span>
+<span <?php echo wp_kses_data( get_block_wrapper_attributes( array( 'data-wp-text' => 'state.checkout.total_scratch_display_amount' ) ) ); ?>></span>
