9290d497c1d5d3afdc8de62cebd15437355356a7
Andre Gagnon <ajgagnon@uwalumni.com>
2025-07-31 16:32:24 -0500

    Refactor getInitialLineItems method in ProductDonation block to access active_ad_hoc_prices as a property instead of a method, ensuring consistency in data retrieval.

--- packages/blocks/Blocks/ProductDonation/Block.php
+++ packages/blocks/Blocks/ProductDonation/Block.php
@@ -88,14 +88,14 @@ class Block extends BaseBlock {
 	 * @return array
 	 */
 	public function getInitialLineItems( $product, $amounts ) {
-		if ( empty( $product->active_ad_hoc_prices()[0] ) ) {
+		if ( empty( $product->active_ad_hoc_prices[0] ) ) {
 			return array();
 		}
 
 		return array(
 			array(
-				'price'         => $product->active_ad_hoc_prices()[0]->id,
-				'ad_hoc_amount' => $product->active_ad_hoc_prices()[0]->amount,
+				'price'         => $product->active_ad_hoc_prices[0]->id,
+				'ad_hoc_amount' => $product->active_ad_hoc_prices[0]->amount,
 				'quantity'      => 1,
 			),
 		);
