849e5363cc474d37e2349bb8d57020753fbb237d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-08-01 17:46:47 +0600

    Removed unused imports and fixed props passing

--- packages/admin/components/affiliates/Metadata.js
+++ packages/admin/components/affiliates/Metadata.js
@@ -1,11 +1,19 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+
+/**
+ * External dependencies.
+ */
 import { ScText } from '@surecart/components-react';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies.
+ */
 import Box from '../../ui/Box';
 
 export default ({ metadata, title, loading }) => {
-	// Return null if no metadata
+	// Return null if no metadata.
 	if (!metadata || !Object.keys(metadata).length) {
 		return null;
 	}
@@ -32,7 +40,8 @@ export default ({ metadata, title, loading }) => {
 						>
 							{key.replaceAll('_', ' ')}
 						</ScText>
-						<div>{`${metadata[key]}`}</div>
+
+						<div>{metadata[key]}</div>
 					</div>
 				))}
 			</div>
