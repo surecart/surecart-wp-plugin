3385fd83d0c90d7690750475b8d96bf21b393aa2
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-08-01 17:18:21 +0530

    Fix issue where page params clear when we close quick view popup on esc or outside popup click.

--- packages/blocks-next/src/blocks/product-quick-view/view.php
+++ packages/blocks-next/src/blocks/product-quick-view/view.php
@@ -17,59 +17,59 @@ use SureCart\Models\Blocks\ProductPageBlock;
 	);
 	?>
 	>
-	<div 
-		class="sc-product-quick-view-dialog"
-		tabindex="-1"
-		data-wp-on--keydown="callbacks.handleKeyDown"
-		<?php
-		echo wp_kses_data(
-			wp_interactivity_data_wp_context(
-				[
-					'url' => $close_url,
-				]
-			)
-		);
-		?>
-	>
 	<?php
 	while ( $query->have_posts() ) :
 		$query->the_post();
 		?>
-			<div class="sc-product-quick-view-dialog__content">
+			<div 
+				class="sc-product-quick-view-dialog"
+				tabindex="-1"
+				data-wp-on--keydown="callbacks.handleKeyDown"
 				<?php
+				echo wp_kses_data(
+					wp_interactivity_data_wp_context(
+						[
+							'url' => $close_url,
+						]
+					)
+				);
+				?>
+			>
+				<div class="sc-product-quick-view-dialog__content">
+					<?php
 
-				// Get an instance of the current Product Quick view block.
-				$block_instance = $block->parsed_block;
+					// Get an instance of the current Product Quick view block.
+					$block_instance = $block->parsed_block;
 
-				// Set the block name to one that does not correspond to an existing registered block.
-				// This ensures that for the inner instances of the Product Quick view block, we do not render any block supports.
-				$block_instance['blockName'] = 'core/null';
+					// Set the block name to one that does not correspond to an existing registered block.
+					// This ensures that for the inner instances of the Product Quick view block, we do not render any block supports.
+					$block_instance['blockName'] = 'core/null';
 
-				$product_post_id      = get_the_ID();
-				$product_post_type    = get_post_type();
-				$filter_block_context = static function ( $context ) use ( $product_post_id, $product_post_type ) {
-					$context['postType'] = $product_post_type;
-					$context['postId']   = $product_post_id;
-					return $context;
-				};
+					$product_post_id      = get_the_ID();
+					$product_post_type    = get_post_type();
+					$filter_block_context = static function ( $context ) use ( $product_post_id, $product_post_type ) {
+						$context['postType'] = $product_post_type;
+						$context['postId']   = $product_post_id;
+						return $context;
+					};
 
-				// Use an early priority to so that other 'render_block_context' filters have access to the values.
-				add_filter( 'render_block_context', $filter_block_context, 1 );
+					// Use an early priority to so that other 'render_block_context' filters have access to the values.
+					add_filter( 'render_block_context', $filter_block_context, 1 );
 
-				// Render the inner blocks of the Product Quick view block with `dynamic` set to `false` to prevent calling
-				// `render_callback` and ensure that no wrapper markup is included.
-				$block_content = ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
-				remove_filter( 'render_block_context', $filter_block_context, 1 );
+					// Render the inner blocks of the Product Quick view block with `dynamic` set to `false` to prevent calling
+					// `render_callback` and ensure that no wrapper markup is included.
+					$block_content = ( new WP_Block( $block_instance ) )->render( array( 'dynamic' => false ) );
+					remove_filter( 'render_block_context', $filter_block_context, 1 );
 
-				// Wrap the render inner blocks in a `li` element with the appropriate post classes.
-				$post_classes = implode( ' ', get_post_class( 'wp-block-post' ) );
+					// Wrap the render inner blocks in a `li` element with the appropriate post classes.
+					$post_classes = implode( ' ', get_post_class( 'wp-block-post' ) );
 
-				$controller = new ProductPageBlock();
-				$state      = $controller->state();
-				$context    = $controller->context();
+					$controller = new ProductPageBlock();
+					$state      = $controller->state();
+					$context    = $controller->context();
 
-				wp_interactivity_state( 'surecart/product-page', $state );
-		?>
+					wp_interactivity_state( 'surecart/product-page', $state );
+			?>
 				<form
 					<?php
 					echo wp_kses_data( wp_interactivity_data_wp_context( $context ) );
@@ -80,8 +80,8 @@ use SureCart\Models\Blocks\ProductPageBlock;
 					<?php echo $block_content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				</form>
 			</div>
+		</div>
 	<?php endwhile; ?>
-	</div>
 	<div class="sc-product-quick-view-overlay" data-wp-on--click="actions.close" data-wp-bind--hidden="surecart/product-list::state.loading"
 		<?php
 		echo wp_kses_data(
