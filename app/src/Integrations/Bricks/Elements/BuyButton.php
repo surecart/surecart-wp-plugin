7d604c412bb8bd7df1a8e9afe7674e31533d2f77
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-08-04 19:12:21 +0600

    For Elementor and Bricks manage sticky php from single file

--- app/src/Integrations/Bricks/Elements/BuyButton.php
+++ app/src/Integrations/Bricks/Elements/BuyButton.php
@@ -287,32 +287,6 @@ class BuyButton extends \Bricks\Element {
 
 		echo $output; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
-		// Add the sticky purchase button if enabled.
-		if ( ! $show_sticky_purchase ) {
-			return;
-		}
-
-		// Get the sticky purchase template.
-		$template = get_block_template( 'surecart/surecart//sticky-purchase', 'wp_template_part' );
-
-		// If the template is empty, don't render it.
-		if ( empty( $template ) || empty( $template->content ) ) {
-			return;
-		}
-
-		// Add the show sticky purchase button context to the template.
-		$filter_block_context = static function ( $context ) use ( $settings ) {
-			$context['showStickyPurchaseButton'] = $settings['show_sticky_purchase_button'];
-			return $context;
-		};
-
-		// Add the filter to the template.
-		add_filter( 'render_block_context', $filter_block_context, 1 );
-
-		// Render the template.
-		echo do_blocks( $template->content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-		// Remove the filter from the template.
-		remove_filter( 'render_block_context', $filter_block_context, 1 );
+		require SURECART_PLUGIN_DIR . '/views/blocks/sticky-purchase.php';
 	}
 }
