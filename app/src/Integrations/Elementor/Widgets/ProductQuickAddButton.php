c75f53dd68818de58a3977307f6517c635ec405d
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-08-12 20:53:11 +0530

    Added Editor Preview, Added Border Style & Text Align Style settings.

--- app/src/Integrations/Elementor/Widgets/ProductQuickAddButton.php
+++ app/src/Integrations/Elementor/Widgets/ProductQuickAddButton.php
@@ -196,6 +196,32 @@ class ProductQuickAddButton extends \Elementor\Widget_Base {
 		);
 
 		$this->add_control(
+			'content_align',
+			[
+				'label'     => esc_html__( 'Content Alignment', 'surecart' ),
+				'type'      => \Elementor\Controls_Manager::CHOOSE,
+				'options'   => [
+					'flex-start' => [
+						'title' => esc_html__( 'Start', 'surecart' ),
+						'icon'  => 'eicon-h-align-left',
+					],
+					'center'     => [
+						'title' => esc_html__( 'Center', 'surecart' ),
+						'icon'  => 'eicon-h-align-center',
+					],
+					'flex-end'   => [
+						'title' => esc_html__( 'End', 'surecart' ),
+						'icon'  => 'eicon-h-align-right',
+					],
+				],
+				'default'   => 'flex-start',
+				'selectors' => [
+					$button_selector => 'justify-content: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
 			'button_text_color',
 			[
 				'label'     => esc_html__( 'Text Color', 'surecart' ),
@@ -215,6 +241,14 @@ class ProductQuickAddButton extends \Elementor\Widget_Base {
 				'selectors' => [
 					$button_selector => 'background-color: {{VALUE}}',
 				],
+			]
+		);
+
+		$this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name'     => 'border',
+				'selector' => $button_selector,
 			]
 		);
 
@@ -247,11 +281,38 @@ class ProductQuickAddButton extends \Elementor\Widget_Base {
 			'width'                  => $settings['width'] ?? null,
 			'show_loading_indicator' => true,
 		);
-		ob_start();
+		$width_class    = ! empty( $attributes['width'] ) ? 'has-custom-width wp-block-button__width-' . $attributes['width'] : '';
+		$show_icon      = in_array( $attributes['quick_view_button_type'], [ 'icon', 'both' ], true );
+		$show_text      = in_array( $attributes['quick_view_button_type'], [ 'text', 'both' ], true );
+
+		if ( \Elementor\Plugin::$instance->editor->is_edit_mode() ) {
+			?>
+				<div class="wp-block-buttons">
+					<div class="wp-block-button <?php echo esc_attr( $width_class ); ?>">
+						<div class="wp-block-button__link sc-button__link  wp-block-surecart-product-quick-view-button" aria-label="Quick Add Product" role="button">
+							<?php if ( $show_icon && 'before' === $attributes['icon_position'] ) : ?>
+								<svg class="wp-block-surecart-product-quick-view-button__icon sc-button__link-text" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+									<line x1="12" y1="5" x2="12" y2="19"></line>
+									<line x1="5" y1="12" x2="19" y2="12"></line>
+								</svg>
+							<?php endif; ?>
+							<?php if ( $show_text ) : ?>
+								<span class="sc-button__link-text"><?php echo esc_html( $attributes['label'] ); ?></span>
+							<?php endif; ?>
+							<?php if ( $show_icon && 'after' === $attributes['icon_position'] ) : ?>
+								<svg class="wp-block-surecart-product-quick-view-button__icon sc-button__link-text" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+									<line x1="12" y1="5" x2="12" y2="19"></line>
+									<line x1="5" y1="12" x2="19" y2="12"></line>
+								</svg>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			<?php
+		}
+
 		?>
 		<!-- wp:surecart/product-quick-view-button <?php echo wp_json_encode( $attributes ); ?> /-->
 		<?php
-		$output = ob_get_clean();
-		echo $output; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
