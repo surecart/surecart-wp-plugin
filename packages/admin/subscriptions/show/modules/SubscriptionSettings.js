c470259a53d0b77a084c16d0865fe1939ef90b76
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-08-19 01:11:23 +0600

    doc: added doc link

--- packages/admin/subscriptions/show/modules/SubscriptionSettings.js
+++ packages/admin/subscriptions/show/modules/SubscriptionSettings.js
@@ -1,5 +1,7 @@
 import { ScSwitch } from '@surecart/components-react';
 import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { help } from '@wordpress/icons';
 import Box from '../../../ui/Box';
 
 export default ({ subscription, updateSubscription, loading }) => {
@@ -12,7 +14,28 @@ export default ({ subscription, updateSubscription, loading }) => {
 	}
 
 	return (
-		<Box loading={loading} title={__('Subscription Settings', 'surecart')}>
+		<Box
+			loading={loading}
+			title={__('Subscription Settings', 'surecart')}
+			header_action={
+				<div
+					style={{
+						margin: '-10px',
+						opacity: 0.5,
+					}}
+				>
+					<Button
+						href="https://surecart.com/docs/managing-subscriptions/#restarting-installments-automatically"
+						size="compact"
+						icon={help}
+						target="_blank"
+						rel="noopener noreferrer"
+						showTooltip={true}
+						label={__('Learn More', 'surecart')}
+					/>
+				</div>
+			}
+		>
 			<ScSwitch
 				checked={subscription?.restart_on_completed}
 				onScChange={(e) =>
