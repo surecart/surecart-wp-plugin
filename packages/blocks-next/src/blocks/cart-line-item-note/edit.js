ec125a281018fcaa03ba8686a186ee5104724bf8
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-01 16:36:36 -0500

    Update class attribute in cart line item note block to use JSX syntax

--- packages/blocks-next/src/blocks/cart-line-item-note/edit.js
+++ packages/blocks-next/src/blocks/cart-line-item-note/edit.js
@@ -8,7 +8,7 @@ export default () => {
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
-			<div class="line-item-note__text">
+			<div className="line-item-note__text">
 				{__('Note: Your note here...', 'surecart')}
 			</div>
 		</div>
