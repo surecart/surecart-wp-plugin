bb174b6ff08f66c548ad119c817711d375fe1974
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-12 13:01:22 -0500

    Use video attributes.

--- packages/blocks-next/src/blocks/product-media/video.php
+++ packages/blocks-next/src/blocks/product-media/video.php
@@ -1,15 +1,17 @@
 <?php
-$video  = $media->video_attributes( 'large' );
-$poster = $media->attributes( 'large' );
+// get the video and poster attributes.
+$video = $media->video_attributes( 'large' );
 
-\SureCart::render(
-	'media/video',
+// render the video html.
+$html = SureCart::view( 'media/video' )->with(
 	[
-		'poster'  => $poster->src,
-		'src'     => $video->src,
-		'alt'     => $video->alt,
-		'title'   => $video->title,
-		'style'   => ! empty( $media->getMetadata( 'aspect_ratio' ) ) ? 'aspect-ratio: ' . esc_attr( $media->getMetadata( 'aspect_ratio' ) ) . ';' : '',
-		'loading' => $loading ?? 'eager',
+		'poster' => $video->poster,
+		'src'    => $video->src,
+		'alt'    => $video->alt,
+		'title'  => $video->title ?? get_the_title(),
+		'style'  => ! empty( $media->getMetadata( 'aspect_ratio' ) ) ? 'aspect-ratio: ' . esc_attr( $media->getMetadata( 'aspect_ratio' ) ) . ';' : '',
 	]
-);
+)->toString();
+
+// filter the video html.
+echo wp_kses( apply_filters( 'surecart_video_html', $html, $video->src, $video->poster ), sc_allowed_svg_html() );
