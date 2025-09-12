4cb77d6271334a7424d370d2300bf15dc7bc30bf
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-12 12:31:32 -0500

    Remove thumbnail

--- app/src/Support/Contracts/GalleryItem.php
+++ app/src/Support/Contracts/GalleryItem.php
@@ -14,16 +14,6 @@ interface GalleryItem {
 	public function html( $size = 'full', $attr = [] ): string;
 
 	/**
-	 * Get the thumbnail attribute markup.
-	 *
-	 * @param string $size The size of the image.
-	 * @param array  $attr The attributes for the tag.
-	 *
-	 * @return string
-	 */
-	public function thumbnail( $size = 'full', $attr = [] ): string;
-
-	/**
 	 * Get the page descriptoin attribute
 	 *
 	 * @param string $size The size of the image.
