c50e8d38f285f69e9e6418872819bfcb37cd9826
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-09-15 19:03:54 +0530

    SUR-2875 - Enhancement:  Add Pagination for the Download Files for Product in the Customer Dashboard.

--- packages/components/src/components/controllers/dashboard/sc-downloads-list/sc-downloads-list.tsx
+++ packages/components/src/components/controllers/dashboard/sc-downloads-list/sc-downloads-list.tsx
@@ -1,9 +1,10 @@
-import { Component, h, Prop, State } from '@stencil/core';
+import { Component, h, Prop, State, Element } from '@stencil/core';
 import apiFetch from '../../../../functions/fetch';
 import { __ } from '@wordpress/i18n';
 import { addQueryArgs } from '@wordpress/url';
 
 import { Download, Media } from '../../../../types';
+import { onFirstVisible } from '../../../../functions/lazy';
 
 @Component({
   tag: 'sc-downloads-list',
@@ -11,11 +12,91 @@ import { Download, Media } from '../../../../types';
   shadow: true,
 })
 export class ScDownloadsList {
-  @Prop() downloads: Download[];
+  @Element() el: HTMLScDownloadsListElement;
   @Prop() customerId: string;
+  @Prop() productId: string;
   @Prop() heading: string;
-  @State() busy: string;
+  @State() downloads: Download[];
+  @State() downloading: string;
+  @State() busy: boolean;
+  @State() loading: boolean;
   @State() error: string;
+  @State() pagination: {
+    total: number;
+    total_pages: number;
+  } = {
+    total: 0,
+    total_pages: 0,
+  };
+  @Prop({ mutable: true }) query: any = {
+    page: 1,
+    per_page: 20,
+  };
+
+  componentWillLoad() {
+    onFirstVisible(this.el, () => {
+      this.initialFetch();
+    });
+  }
+
+  async initialFetch() {
+    if (!this.productId || !this.customerId) {
+      return;
+    }
+    try {
+      this.loading = true;
+      await this.getItems();
+    } catch (e) {
+      console.error(this.error);
+      this.error = e?.message || __('Something went wrong', 'surecart');
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async fetchItems() {
+    if (!this.productId || !this.customerId) {
+      return;
+    }
+    try {
+      this.busy = true;
+      await this.getItems();
+    } catch (e) {
+      console.error(this.error);
+      this.error = e?.message || __('Something went wrong', 'surecart');
+    } finally {
+      this.busy = false;
+    }
+  }
+
+  /** Get all subscriptions */
+  async getItems() {
+    const response = (await await apiFetch({
+      path: addQueryArgs(`surecart/v1/downloads/`, {
+        product_ids: [this.productId],
+        customer_ids: [this.customerId],
+        downloadable: true,
+        ...this.query,
+      }),
+      parse: false,
+    })) as Response;
+    this.pagination = {
+      total: parseInt(response.headers.get('X-WP-Total')),
+      total_pages: parseInt(response.headers.get('X-WP-TotalPages')),
+    };
+    this.downloads = (await response.json()) as Download[];
+    return this.downloads;
+  }
+
+  nextPage() {
+    this.query.page = this.query.page + 1;
+    this.fetchItems();
+  }
+
+  prevPage() {
+    this.query.page = this.query.page - 1;
+    this.fetchItems();
+  }
 
   async downloadItem(download) {
     if (download?.url) {
@@ -27,7 +108,7 @@ export class ScDownloadsList {
     if (!mediaId) return;
 
     try {
-      this.busy = mediaId;
+      this.downloading = mediaId;
       const media = (await apiFetch({
         path: addQueryArgs(`surecart/v1/customers/${this.customerId}/expose/${mediaId}`, {
           expose_for: 60,
@@ -43,7 +124,7 @@ export class ScDownloadsList {
       console.error(e);
       this.error = e?.message || __('Something went wrong', 'surecart');
     } finally {
-      this.busy = null;
+      this.downloading = null;
     }
   }
 
@@ -83,58 +164,107 @@ export class ScDownloadsList {
     return <sc-icon name="file" />;
   };
 
-  render() {
+  renderList() {
+    if (this?.loading) {
+      return this.renderLoading();
+    }
+    if (this?.downloads?.length === 0) {
+      return this.renderEmpty();
+    }
     const downloads = this.downloads || [];
+    
+    return (
+        <sc-card no-padding>
+        <sc-stacked-list>
+          {downloads.map(download => {
+            const media = download?.media as Media;
+            return (
+              <sc-stacked-list-row style={{ '--columns': '1' }}>
+                <sc-flex class="single-download" justifyContent="flex-start" alignItems="center">
+                  <div class="single-download__preview">{this.renderFileExt(download)}</div>
+                  <div>
+                    <div>
+                      <strong>{media?.filename ?? download?.name ?? ''}</strong>
+                    </div>
+
+                    <sc-flex justifyContent="flex-start" alignItems="center" style={{ gap: '0.5em' }}>
+                      {media?.byte_size && <sc-format-bytes value={media.byte_size}></sc-format-bytes>}
+
+                      {!!media?.release_json?.version && (
+                        <sc-tag
+                          type="primary"
+                          size="small"
+                          style={{
+                            '--sc-tag-primary-background-color': '#f3e8ff',
+                            '--sc-tag-primary-color': '#6b21a8',
+                          }}
+                        >
+                          v{media?.release_json?.version}
+                        </sc-tag>
+                      )}
+                    </sc-flex>
+                  </div>
+                </sc-flex>
+                <sc-button
+                  size="small"
+                  slot="suffix"
+                  onClick={() => this.downloadItem(download)}
+                  busy={media?.id ? this.downloading == media?.id : false}
+                  disabled={media?.id ? this.downloading == media?.id : false}
+                >
+                  {__('Download', 'surecart')}
+                </sc-button>
+              </sc-stacked-list-row>
+            );
+          })}
+        </sc-stacked-list>
+      </sc-card>
+    );
+  }
+
+  renderLoading() {
+    return (
+      <sc-card no-padding style={{ '--overflow': 'hidden' }}>
+        <sc-stacked-list>
+          <sc-stacked-list-row style={{ '--columns': '2' }} mobile-size={0}>
+            <div style={{ padding: '0.5em' }}>
+              <sc-skeleton style={{ width: '30%', marginBottom: '0.75em' }}></sc-skeleton>
+              <sc-skeleton style={{ width: '20%' }}></sc-skeleton>
+            </div>
+          </sc-stacked-list-row>
+        </sc-stacked-list>
+      </sc-card>
+    );
+  }
+
+  renderEmpty() {
+    return (
+      <div>
+        <sc-divider style={{ '--spacing': '0' }}></sc-divider>
+        <slot name="empty">
+          <sc-empty icon="download">{__("You don't have any downloads.", 'surecart')}</sc-empty>
+        </slot>
+      </div>
+    );
+  }
+
+  render() {
     return (
       <sc-dashboard-module class="purchase" part="base" heading={__('Downloads', 'surecart')}>
         <span slot="heading">
           <slot name="heading">{this.heading || __('Downloads', 'surecart')}</slot>
         </span>
-        <sc-card no-padding>
-          <sc-stacked-list>
-            {downloads.map(download => {
-              const media = download?.media as Media;
-              return (
-                <sc-stacked-list-row style={{ '--columns': '1' }}>
-                  <sc-flex class="single-download" justifyContent="flex-start" alignItems="center">
-                    <div class="single-download__preview">{this.renderFileExt(download)}</div>
-                    <div>
-                      <div>
-                        <strong>{media?.filename ?? download?.name ?? ''}</strong>
-                      </div>
-
-                      <sc-flex justifyContent="flex-start" alignItems="center" style={{ gap: '0.5em' }}>
-                        {media?.byte_size && <sc-format-bytes value={media.byte_size}></sc-format-bytes>}
-
-                        {!!media?.release_json?.version && (
-                          <sc-tag
-                            type="primary"
-                            size="small"
-                            style={{
-                              '--sc-tag-primary-background-color': '#f3e8ff',
-                              '--sc-tag-primary-color': '#6b21a8',
-                            }}
-                          >
-                            v{media?.release_json?.version}
-                          </sc-tag>
-                        )}
-                      </sc-flex>
-                    </div>
-                  </sc-flex>
-                  <sc-button
-                    size="small"
-                    slot="suffix"
-                    onClick={() => this.downloadItem(download)}
-                    busy={media?.id ? this.busy == media?.id : false}
-                    disabled={media?.id ? this.busy == media?.id : false}
-                  >
-                    {__('Download', 'surecart')}
-                  </sc-button>
-                </sc-stacked-list-row>
-              );
-            })}
-          </sc-stacked-list>
-        </sc-card>
+        {this.renderList()}
+        <sc-pagination
+          page={this.query.page}
+          perPage={this.query.per_page}
+          total={this.pagination.total}
+          totalPages={this.pagination.total_pages}
+          totalShowing={this?.downloads?.length}
+          onScNextPage={() => this.nextPage()}
+          onScPrevPage={() => this.prevPage()}
+        ></sc-pagination>
+         {this.busy && <sc-block-ui></sc-block-ui>}
       </sc-dashboard-module>
     );
   }
