0a3fc4b23e84e8402c7622c634eb97a2139a7e70
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-15 14:58:10 -0500

    Add tooltip

--- packages/admin/upsell-funnels/modules/Upsell/ReplacementBehavior.js
+++ packages/admin/upsell-funnels/modules/Upsell/ReplacementBehavior.js
@@ -1,27 +1,71 @@
 import { __ } from '@wordpress/i18n';
-import { ScSelect, ScSwitch } from '@surecart/components-react';
+import { ScFormControl, ScIcon, ScSelect } from '@surecart/components-react';
+import HelpTooltip from '../../../components/HelpTooltip';
+import { ExternalLink } from '@wordpress/components';
 
 export default function ReplacementBehavior({ upsell, onUpdate }) {
 	return (
-		<ScSelect
-			label={__('When accepted', 'surecart')}
-			choices={[
-				{
-					label: __('Replace the entire order', 'surecart'),
-					value: 'all',
-				},
-				{ label: __('Add to the order', 'surecart'), value: 'none' },
-			]}
-			value={upsell?.replacement_behavior || 'none'}
-			help={__(
-				'Choose the behavior of accepting this upsell',
-				'surecart'
-			)}
-			onScChange={(e) =>
-				onUpdate({
-					replacement_behavior: e.target.value,
-				})
-			}
-		/>
+		<div>
+			<ScFormControl label={__('When accepted', 'surecart')}>
+				<HelpTooltip
+					content={
+						<div>
+							<p style={{ marginTop: 0 }}>
+								<strong>
+									{__('Add to the order', 'surecart')}
+								</strong>
+								<br />
+								{__(
+									'The upsell is added in addition to the customer’s current order.',
+									'surecart'
+								)}
+							</p>
+							<p>
+								<strong>
+									{__('Replace the entire order', 'surecart')}
+								</strong>
+								<br />
+								{__(
+									'The upsell replaces the customer’s entire order, removing all existing items.',
+									'surecart'
+								)}
+							</p>
+							<ExternalLink
+								href="https://surecart.com/docs/price-boost/"
+								target="_blank"
+							>
+								{__('Learn More', 'surecart')}
+							</ExternalLink>
+						</div>
+					}
+					position="top left"
+					slot="label-end"
+				>
+					<ScIcon name="info" style={{ opacity: 0.5 }} />
+				</HelpTooltip>
+			</ScFormControl>
+			<ScSelect
+				choices={[
+					{
+						label: __('Replace the entire order', 'surecart'),
+						value: 'all',
+					},
+					{
+						label: __('Add to the order', 'surecart'),
+						value: 'none',
+					},
+				]}
+				value={upsell?.replacement_behavior || 'none'}
+				help={__(
+					'Choose the behavior of accepting this upsell',
+					'surecart'
+				)}
+				onScChange={(e) =>
+					onUpdate({
+						replacement_behavior: e.target.value,
+					})
+				}
+			/>
+		</div>
 	);
 }
