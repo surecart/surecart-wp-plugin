26b506fb13b1aa898003113f82958624ff8e2d70
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-18 16:18:30 -0500

    Add test for queuing content for syncing in ImportTest class

--- .dev/tests/php/unit/models/Import/ImportTest.php
+++ .dev/tests/php/unit/models/Import/ImportTest.php
@@ -16,6 +16,7 @@ class ImportTest extends SureCartUnitTestCase
         \SureCart::make()->bootstrap([
             'providers' => [
                 \SureCart\WordPress\PluginServiceProvider::class,
+                \SureCart\Sync\SyncServiceProvider::class,
             ]
         ], false);
     }
@@ -45,5 +46,31 @@ class ImportTest extends SureCartUnitTestCase
                 ]
             ]
         ]);
+    }
+
+    public function test_queues_content_for_syncing() {
+         // Create a mock for the RequestService
+         $requests = \Mockery::mock(RequestService::class)->makePartial();
+
+         // Set expectation that makeRequest should be called with specific arguments
+         $requests->shouldReceive('makeRequest')
+             ->once()
+             ->withSomeOfArgs('imports/products')
+             ->andReturn((object) [
+                 'id' => 'test_import',
+             ]);
+ 
+         // Mock the unAuthorizedRequest alias to use our mock
+         \SureCart::alias('request', function () use ($requests) {
+             return call_user_func_array([$requests, 'makeRequest'], func_get_args());
+         });
+ 
+         Import::queue('products', [
+             'products' => [
+                 [
+                     'name' => 'Product 1',
+                 ]
+             ]
+         ]);
     }
 }
