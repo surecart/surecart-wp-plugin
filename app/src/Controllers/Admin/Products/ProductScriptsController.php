7164602dc71653020c16658eb23e89260320b78d
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-09-18 17:58:53 +0600

    Added review_protocol supported with model edit controller

--- app/src/Controllers/Admin/Products/ProductScriptsController.php
+++ app/src/Controllers/Admin/Products/ProductScriptsController.php
@@ -13,7 +13,7 @@ class ProductScriptsController extends AdminModelEditController {
 	 *
 	 * @var array
 	 */
-	protected $with_data = [ 'currency', 'supported_currencies', 'tax_protocol', 'checkout_page_url' ];
+	protected $with_data = [ 'currency', 'supported_currencies', 'tax_protocol', 'checkout_page_url', 'review_protocol' ];
 
 	/**
 	 * Script handle.
