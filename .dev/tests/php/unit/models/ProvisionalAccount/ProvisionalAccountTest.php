7d0952ba764cadbc83c97330de29439fe1b96182
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-18 16:18:35 -0500

    Add ProvisionalAccountTest enhancements for syncing and transient validation

--- .dev/tests/php/unit/models/ProvisionalAccount/ProvisionalAccountTest.php
+++ .dev/tests/php/unit/models/ProvisionalAccount/ProvisionalAccountTest.php
@@ -16,6 +16,7 @@ class ProvisionalAccountTest extends SureCartUnitTestCase
         \SureCart::make()->bootstrap([
             'providers' => [
                 \SureCart\WordPress\PluginServiceProvider::class,
+                \SureCart\Sync\SyncServiceProvider::class,
             ]
         ], false);
     }
@@ -59,33 +60,53 @@ class ProvisionalAccountTest extends SureCartUnitTestCase
 
     public function test_seeds_account_products() {
         $provisional_account = \Mockery::mock(ProvisionalAccount::class)->shouldAllowMockingProtectedMethods()->makePartial();
-
-        // Create a mock for the RequestService
         $requests = \Mockery::mock(RequestService::class)->makePartial();
 
-        // Set expectation that makeRequest should be called with specific arguments
         $requests->shouldReceive('makeRequest')
-          ->once()
-          ->andReturn((object) [
-              'id' => 'test_provisional_account',
-              'account_name' => 'Test Account',
-          ]);
+            ->once()
+            ->andReturn((object) [
+                'id' => 'test_provisional_account',
+                'account_name' => 'Test Account',
+            ]);
+        $requests->shouldReceive('makeRequest')
+            ->once()
+            ->withSomeOfArgs('imports/products')
+            ->andReturn((object) [
+                'id' => 'test_import',
+            ]);
 
         // Mock the unAuthorizedRequest alias to use our mock
-        \SureCart::alias('unAuthorizedRequest', function () use ($requests) {
-            return call_user_func_array([$requests, 'makeRequest'], func_get_args());
-        });
-
-        $provisional_account->shouldReceive('seed')->once()->andReturn(true);
+        \SureCart::alias('unAuthorizedRequest', fn ()  => call_user_func_array([$requests, 'makeRequest'], func_get_args()));
+        \SureCart::alias('request', fn ()  => call_user_func_array([$requests, 'makeRequest'], func_get_args()));
 
        $provisional_account::create(
             [
                 'products' => [
                     [
                         'name' => 'Product 1',
+                        'content' => 'Product 1 content',
                     ]
                 ]
             ]
+        );
+
+        // Get all transients
+        $transients = $this->get_transients();
+
+        // assert the transient is set.
+        $this->assertSame('Product 1 content', $transients[0]->option_value);
+    }
+
+    public function get_transients() {
+        // get all transients.
+        global $wpdb;
+
+        // Get all transients
+        return $wpdb->get_results(
+            "SELECT option_name, option_value 
+            FROM {$wpdb->options} 
+            WHERE option_name LIKE '_transient_%' 
+            AND option_name NOT LIKE '_transient_timeout_%'"
         );
     }
 }
