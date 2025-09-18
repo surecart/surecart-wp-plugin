1ad330c48f7f0d471a5009166471691d70b3a574
Andre Gagnon <ajgagnon@uwalumni.com>
2025-09-18 15:11:35 -0500

    Add ImportTest

---
+++ .dev/tests/php/unit/models/Import/ImportTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace SureCart\Tests\Models\Import;
+
+use SureCart\Models\Import;
+use SureCart\Request\RequestService;
+use SureCart\Tests\SureCartUnitTestCase;
+
+class ImportTest extends SureCartUnitTestCase
+{
+    use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    public function setUp() : void
+    {
+        parent::setUp();
+        \SureCart::make()->bootstrap([
+            'providers' => [
+                \SureCart\WordPress\PluginServiceProvider::class,
+            ]
+        ], false);
+    }
+
+    public function test_imports_products()
+    {
+        // Create a mock for the RequestService
+        $requests = \Mockery::mock(RequestService::class)->makePartial();
+
+        // Set expectation that makeRequest should be called with specific arguments
+        $requests->shouldReceive('makeRequest')
+            ->once()
+            ->withSomeOfArgs('imports/products')
+            ->andReturn((object) [
+                'id' => 'test_import',
+            ]);
+
+        // Mock the unAuthorizedRequest alias to use our mock
+        \SureCart::alias('request', function () use ($requests) {
+            return call_user_func_array([$requests, 'makeRequest'], func_get_args());
+        });
+
+        Import::queue('products', [
+            'products' => [
+                [
+                    'name' => 'Product 1',
+                ]
+            ]
+        ]);
+    }
+}
