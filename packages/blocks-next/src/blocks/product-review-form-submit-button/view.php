25ea79365f49d7cda42752e1726a01bc7a3560cb
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-10-13 14:00:56 +0600

    review form confirmation loading

--- packages/blocks-next/src/blocks/product-review-form-submit-button/view.php
+++ packages/blocks-next/src/blocks/product-review-form-submit-button/view.php
@@ -1,4 +1,5 @@
-<div <?php
+<div
+	<?php
 	echo wp_kses_data(
 		get_block_wrapper_attributes(
 			array(
@@ -7,18 +8,15 @@
 		)
 	);
 	?>
+>
+	<button
+		class="wp-block-button__link wp-element-button sc-button__link <?php echo ! empty( $styles['classnames'] ) ? esc_attr( $styles['classnames'] ) : ''; ?>"
+		data-wp-bind--disabled="context.busy"
+		data-wp-class--sc-button__link--busy="context.busy"
+		style="<?php echo ! empty( $styles['css'] ) ? esc_attr( $styles['css'] ) : ''; ?>"
+		type="submit"
 	>
-		<button
-			class="wp-block-button__link wp-element-button sc-button__link <?php echo ! empty( $styles['classnames'] ) ? esc_attr( $styles['classnames'] ) : ''; ?>"
-			style="<?php echo ! empty( $styles['css'] ) ? esc_attr( $styles['css'] ) : ''; ?>"
-			data-wp-bind--disabled="context.busy"
-			type="submit"
-		>
-			<?php echo wp_kses_post( $attributes['text'] ?? __( 'Submit Review', 'surecart' ) ); ?>
-			<span
-				class="sc-spinner"
-				aria-hidden="true"
-				data-wp-bind--hidden="!context.busy"
-			></span>
-		</button>
+		<span class="sc-spinner" aria-hidden="true" data-wp-bind--hidden="!context.busy"></span>
+		<span class="sc-button__link-text"><?php echo wp_kses_post( $attributes['text'] ?? __( 'Submit Review', 'surecart' ) ); ?></span>
+	</button>
 </div>
