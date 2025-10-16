ea257839f907c6ba94e258fa00783b84ea5e01fa
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2025-10-16 23:37:56 +0600

    Remove Email from customer name block and refactorred

--- packages/blocks-next/src/blocks/review-reviewer-name/controller.php
+++ packages/blocks-next/src/blocks/review-reviewer-name/controller.php
@@ -1,36 +1,38 @@
 <?php
+
 if ( empty( $block->context['review'] ) ) {
 	return;
 }
 
 $customer = $block->context['review']->customer ?? null;
-$format   = $attributes['format'] ?? 'display_name';
+if ( empty( $customer ) ) {
+	return '';
+}
 
+$format         = $attributes['format'] ?? 'display_name';
 $formatted_name = '';
 
 switch ( $format ) {
 	case 'first_name':
-		$formatted_name = $customer->first_name ?? $customer->name ?? $customer->email ?? '';
+		$formatted_name = $customer->first_name;
 		break;
 
 	case 'last_name':
-		$formatted_name = $customer->last_name ?? $customer->name ?? $customer->email ?? '';
+		$formatted_name = $customer->last_name;
 		break;
 
 	case 'first_last':
-		$first_name = $customer->first_name ?? '';
-		$last_name  = $customer->last_name ?? '';
-		if ( ! empty( $first_name ) && ! empty( $last_name ) ) {
-			$formatted_name = trim( $first_name . ' ' . $last_name );
-		} else {
-			$formatted_name = $customer->name ?? $customer->email ?? '';
-		}
+		$formatted_name = trim( implode( ' ', array_filter( [ $customer->first_name ?? '', $customer->last_name ?? '' ] ) ) );
 		break;
 
 	case 'display_name':
 	default:
-		$formatted_name = $customer->name ?? $customer->email ?? '';
+		$formatted_name = $customer->name;
 		break;
 }
+
+// Fallback name.
+$anonymous_label = apply_filters( 'sc_anonymous_reviewer_name', __( 'Anonymous', 'surecart' ) );
+$formatted_name  = $formatted_name ?? $customer->name ?? $anonymous_label;
 
 return 'file:./view.php';
