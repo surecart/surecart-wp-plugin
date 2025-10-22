052822e0b73ce7236253def1371e43313e5ded75
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-10-22 17:30:03 +0530

    Moved the Dynamic Pricing menu to Products Menu.

--- app/src/WordPress/Admin/Menus/AdminMenuPageService.php
+++ app/src/WordPress/Admin/Menus/AdminMenuPageService.php
@@ -307,7 +307,7 @@ class AdminMenuPageService {
 		// Product submenu pages.
 		$taxonomies = array_diff( get_object_taxonomies( 'sc_product' ), array( 'sc_account', 'sc_collection' ) );
 		sort( $taxonomies, SORT_STRING ); // Sort the taxonomies alphabetically.
-		$is_product_menu_opened = in_array( $_GET['page'] ?? '', array( 'sc-products', 'sc-product-groups', 'sc-bumps', 'sc-upsell-funnels', 'sc-product-collections' ), true ) || in_array( $_GET['taxonomy'] ?? '', array_merge( $taxonomies, array( 'sc_collection' ) ), true );
+		$is_product_menu_opened = in_array( $_GET['page'] ?? '', array( 'sc-products', 'sc-product-groups', 'sc-bumps', 'sc-upsell-funnels', 'sc-product-collections', 'sc-auto-fees' ), true ) || in_array( $_GET['taxonomy'] ?? '', array_merge( $taxonomies, array( 'sc_collection' ) ), true );
 		if ( $is_product_menu_opened ) {
 			$this->pages += array(
 				'product-collections' => \add_submenu_page( $this->slug, __( 'Product Collections', 'surecart' ), '↳ ' . __( 'Collections', 'surecart' ), 'edit_sc_products', 'sc-product-collections', '__return_false' ),
@@ -328,6 +328,7 @@ class AdminMenuPageService {
 			$this->pages += array(
 				'bumps'          => \add_submenu_page( $this->slug, __( 'Order Bumps', 'surecart' ), '↳ ' . __( 'Order Bumps', 'surecart' ), 'edit_sc_products', 'sc-bumps', '__return_false' ),
 				'upsells'        => \add_submenu_page( $this->slug, __( 'Upsells', 'surecart' ), '↳ ' . __( 'Upsells', 'surecart' ), 'edit_sc_products', 'sc-upsell-funnels', '__return_false' ),
+				'auto_fee'       => \add_submenu_page( $this->slug, __( 'Dynamic Pricing', 'surecart' ), '↳ ' . __( 'Dynamic Pricing', 'surecart' ), 'edit_sc_products', 'sc-auto-fees', '__return_false' ),
 				'product-groups' => \add_submenu_page( $this->slug, __( 'Upgrade Groups', 'surecart' ), '↳ ' . __( 'Upgrade Groups', 'surecart' ), 'edit_sc_products', 'sc-product-groups', '__return_false' ),
 			);
 		}
@@ -338,13 +339,6 @@ class AdminMenuPageService {
 		$this->pages += array(
 			'coupons'  => \add_submenu_page( $this->slug, __( 'Coupons', 'surecart' ), __( 'Coupons', 'surecart' ), 'edit_sc_coupons', 'sc-coupons', '__return_false' ),
 			'licenses' => \add_submenu_page( $this->slug, __( 'Licenses', 'surecart' ), __( 'Licenses', 'surecart' ), 'edit_sc_products', 'sc-licenses', '__return_false' ),
-		);
-
-		/**
-		 * Dynamic Pricing.
-		 */
-		$this->pages += array(
-			'auto_fee'                 => \add_submenu_page( $this->slug, __( 'Dynamic Pricing', 'surecart' ), __( 'Dynamic Pricing', 'surecart' ), 'edit_sc_products', 'sc-auto-fees', '__return_false' ),
 		);
 
 		/**
