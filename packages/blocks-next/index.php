4a8794599f2102f8cda49d4d0a25ddcca56562dc
Andre Gagnon <ajgagnon@uwalumni.com>
2025-11-20 15:30:22 -0600

    Just combine unique product list id.

--- packages/blocks-next/index.php
+++ packages/blocks-next/index.php
@@ -85,17 +85,10 @@ add_filter(
 		}
 
 		// pass a unique id to each product list block.
-		if ( 'surecart/product-list' === $parsed_block['blockName'] ) {
+		if ( in_array( $parsed_block['blockName'], [ 'surecart/product-list', 'surecart/product-list-related' ], true ) ) {
 			// we use our own counter to ensure uniqueness so that product page urls don't have ids.
 			global $sc_query_id;
 			$sc_query_id = sc_unique_product_list_id();
-		}
-
-		// pass a unique id to each related product list block.
-		if ( 'surecart/product-list-related' === $parsed_block['blockName'] ) {
-			// we use our own counter to ensure uniqueness so that product page urls don't have ids.
-			global $sc_query_id;
-			$sc_query_id = sc_unique_related_product_list_id( 'related-' );
 		}
 
 		// pass a unique id to each product list block.
@@ -104,6 +97,7 @@ add_filter(
 			global $sc_query_id;
 			$sc_query_id = sc_unique_product_page_id();
 		}
+
 		return $context;
 	},
 	10,
