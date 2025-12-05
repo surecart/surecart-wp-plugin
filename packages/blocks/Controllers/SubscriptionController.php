1a291a52797ca0cd4ca4dcbc99caf26c3a652400
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2025-12-05 22:23:47 +0530

    Moved the filter to the component level

--- packages/blocks/Controllers/SubscriptionController.php
+++ packages/blocks/Controllers/SubscriptionController.php
@@ -201,7 +201,7 @@ class SubscriptionController extends BaseController {
 							)
 						),
 					]
-				)->render( apply_filters( 'surecart/dashboard/subscription/inner_html', '', $subscription ) )
+				)->render()
 			);
 			?>
 		<?php do_action( 'surecart/dashboard/subscription/after_current_plan', $subscription ); ?>
