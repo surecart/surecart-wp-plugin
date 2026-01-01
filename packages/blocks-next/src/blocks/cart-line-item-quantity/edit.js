2717658c8351ef099190df741479725653cc383f
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2026-01-01 20:13:09 +0600

    Fixed cart line item quality plus-minus icons

--- packages/blocks-next/src/blocks/cart-line-item-quantity/edit.js
+++ packages/blocks-next/src/blocks/cart-line-item-quantity/edit.js
@@ -1,6 +1,6 @@
 import { useBlockProps } from '@wordpress/block-editor';
-import { ScIcon } from '@surecart/components-react';
 import { __, sprintf } from '@wordpress/i18n';
+import ScIcon from '../../components/ScIcon';
 
 export default ({ context: { editable = true } }) => {
 	const blockProps = useBlockProps({
