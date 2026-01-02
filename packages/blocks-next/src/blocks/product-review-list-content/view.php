70d0c5be2c8030bf4b9adfb3733634caec2bf8bd
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-02 11:58:04 -0600

    Refactor

--- packages/blocks-next/src/blocks/product-review-list-content/view.php
+++ packages/blocks-next/src/blocks/product-review-list-content/view.php
@@ -1,6 +1,3 @@
-<?php
-$classes = ( isset( $attributes['style']['elements']['link']['color']['text'] ) ) ? 'has-link-color' : '';
-?>
-<div <?php echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => $classes ) ) ); ?>>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
 	<?php echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </div>
