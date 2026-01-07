c02024f9a7af849d78b85be80bdd3e4e8fb8d5b9
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2026-01-07 11:37:24 +0530

    SUR-3831 - Fix: SVG not rendering in sc-icon component in the editor.

--- packages/blocks/index.js
+++ packages/blocks/index.js
@@ -1,6 +1,12 @@
 import { defineCustomElements } from '@surecart/components/loader';
 defineCustomElements();
 
+if (window?.registerSureCartIconPath) {
+	window.registerSureCartIconPath(
+		window?.scData?.plugin_url + '/dist/icon-assets'
+	);
+}
+
 import '../admin/store/add-entities';
 import './general';
 import './cart';
--- packages/components/src/components/ui/icon/library.ts
+++ packages/components/src/components/ui/icon/library.ts
@@ -1,10 +1,7 @@
+import { IconLibrary } from '../../../types';
+
 export type IconLibraryResolver = (name: string) => string;
 export type IconLibraryMutator = (svg: SVGElement) => void;
-export interface IconLibrary {
-  name: string;
-  resolver: IconLibraryResolver;
-  mutator?: IconLibraryMutator;
-}
 
 let registry: IconLibrary[] = [];
 let watchedIcons: any[] = [];
@@ -18,7 +15,7 @@ export function unwatchIcon(icon: any) {
 }
 
 export function getIconLibrary(name?: string) {
-  return registry.filter(lib => lib.name === name)[0];
+  return window?.registry?.filter(lib => lib.name === name)[0];
 }
 
 export function registerIconLibrary(name: string, options: { resolver: IconLibraryResolver; mutator?: IconLibraryMutator }) {
@@ -28,6 +25,8 @@ export function registerIconLibrary(name: string, options: { resolver: IconLibra
     resolver: options.resolver,
     mutator: options.mutator,
   });
+  window.registry = registry;
+
   // Redraw watched icons
   watchedIcons.map(icon => {
     if (icon.library === name) {
@@ -37,7 +36,7 @@ export function registerIconLibrary(name: string, options: { resolver: IconLibra
 }
 
 export function unregisterIconLibrary(name: string) {
-  registry = registry.filter(lib => lib.name !== name);
+  window.registry = window?.registry?.filter(lib => lib.name !== name);
 }
 
 window.ceRegisterIconLibrary = registerIconLibrary;
