ab78a60a80c2adf6f8990da6a20a12818ddb0e9a
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2026-01-08 19:59:10 +0600

    remove styling from cart order bump amount

--- packages/blocks-next/src/blocks/cart-order-bump-amount/index.js
+++ packages/blocks-next/src/blocks/cart-order-bump-amount/index.js
@@ -1,15 +1,14 @@
 /**
- * External dependencies
+ * External dependencies.
  */
 import { registerBlockType } from '@wordpress/blocks';
 import { currencyDollar as icon } from '@wordpress/icons';
 
 /**
- * Internal dependencies
+ * Internal dependencies.
  */
 import edit from './edit';
 import metadata from './block.json';
-import './style.scss';
 
 /**
  * Every block starts by registering a new block type definition.
