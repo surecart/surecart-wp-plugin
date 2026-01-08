a8a2d930ec8ff5fa058139291e63b610a12b8cec
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2026-01-08 19:38:42 +0600

    bump discount badge styling refactored

--- packages/blocks-next/src/blocks/cart-order-bump-discount-badge/edit.js
+++ packages/blocks-next/src/blocks/cart-order-bump-discount-badge/edit.js
@@ -4,6 +4,9 @@
 import { useBlockProps } from '@wordpress/block-editor';
 
 export default () => {
-	const blockProps = useBlockProps();
+	const blockProps = useBlockProps({
+		className: 'sc-tag sc-tag--primary',
+	});
+
 	return <div {...blockProps}>-10%</div>;
 };
--- packages/blocks-next/src/blocks/cart-order-bump-discount-badge/view.php
+++ packages/blocks-next/src/blocks/cart-order-bump-discount-badge/view.php
@@ -1,4 +1,4 @@
-<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
+<div <?php echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => 'sc-tag sc-tag--primary' ) ) ); ?>
 	data-wp-text="state.bumpDiscountText"
 	data-wp-bind--hidden="!state.bumpHasDiscount"
 ></div>
