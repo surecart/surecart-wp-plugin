06d0ecff560436021d64302ddbf53c2554706a27
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-14 14:11:08 -0600

    Add 'nofollow' attribute support in icon block controller for enhanced link management.

--- packages/blocks-next/src/blocks/icon/controller.php
+++ packages/blocks-next/src/blocks/icon/controller.php
@@ -8,6 +8,7 @@ $alignment    = $attributes['alignment'] ?? '';
 $link_url     = $attributes['link_url'] ?? '';
 $link_target  = $link_url ? $attributes['link_target'] : '';
 $link_rel     = $link_url ? $attributes['link_rel'] : '';
+$nofollow     = $attributes['nofollow'] ?? false;
 $html_tag     = $link_url ? 'a' : 'div';
 
 // Return the view.
