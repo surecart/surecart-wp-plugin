32622afafe12a3de69df8201f19e77d981e49374
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-14 16:23:17 -0600

    Remove duplicate styles

--- packages/blocks-next/src/blocks/product-review-summary/index.js
+++ packages/blocks-next/src/blocks/product-review-summary/index.js
@@ -10,7 +10,6 @@ import { starFilled as icon } from '@wordpress/icons';
 import edit from './edit';
 import save from './save';
 import metadata from './block.json';
-import './style.scss';
 
 /**
  * Register block.
