46a69a79d09fe3ef41997dc38cd2899d59dd6a45
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-19 13:58:44 -0600

    Extra space

--- packages/blocks/Blocks/Payment/Block.php
+++ packages/blocks/Blocks/Payment/Block.php
@@ -63,13 +63,13 @@ class Block extends BaseBlock {
 			<?php else : ?>
 				<sc-stripe-element slot="stripe"></sc-stripe-element>
 			<?php endif; ?>
-			
+
 			<?php if ( 'test' === $mode ) : ?>
 				<?php if ( current_user_can( 'edit_posts' ) ) : ?>
 					<sc-test-mode-info slot="label-end">
 						<sc-tag tabindex="0" slot="trigger" type="warning" size="small" exportparts="base:test-badge__base, content:test-badge__content" aria-label="<?php echo esc_html__( 'Test Mode Info, Press Enter to open the Information Popover', 'surecart' ); ?>">
 							<?php echo esc_html__( 'Test Mode', 'surecart' ); ?>
-							<sc-icon slot="suffix" name="info"  />
+							<sc-icon slot="suffix" name="info" />
 						</sc-tag>
 					</sc-test-mode-info>
 				<?php else : ?>
