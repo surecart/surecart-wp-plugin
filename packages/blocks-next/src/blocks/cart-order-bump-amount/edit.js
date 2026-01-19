ae1ad6972606a3167e199ab52e6b84d2b6027023
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-19 17:05:26 -0600

    Fallback

--- packages/blocks-next/src/blocks/cart-order-bump-amount/edit.js
+++ packages/blocks-next/src/blocks/cart-order-bump-amount/edit.js
@@ -5,5 +5,5 @@ import { useBlockProps } from '@wordpress/block-editor';
 
 export default () => {
 	const blockProps = useBlockProps();
-	return <div {...blockProps}>{scData?.currency_symbol}27</div>;
+	return <div {...blockProps}>{scData?.currency_symbol || "$"}27</div>;
 };
