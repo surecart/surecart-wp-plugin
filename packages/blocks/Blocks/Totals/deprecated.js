8f9492cee1da8bbaa6cb41f133d17ff659a62649
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2026-01-21 19:27:00 +0530

    Fix the speak textx not announcing

--- packages/blocks/Blocks/Totals/deprecated.js
+++ packages/blocks/Blocks/Totals/deprecated.js
@@ -172,4 +172,47 @@ export default [
 			);
 		},
 	},
+	{
+		attributes: {
+			collapsible: {
+				type: 'boolean',
+			},
+			collapsedOnDesktop: {
+				type: 'boolean',
+			},
+			order_summary_text: {
+				type: 'string',
+				default: 'Summary',
+			},
+			invoice_summary_text: {
+				type: 'string',
+				default: 'Invoice Summary',
+			},
+			collapsedOnMobile: {
+				type: 'boolean',
+				default: false,
+			},
+		},
+		save({ attributes }) {
+			const {
+				collapsedOnDesktop,
+				collapsible,
+				order_summary_text,
+				invoice_summary_text,
+				collapsedOnMobile,
+			} = attributes;
+
+			return (
+				<sc-order-summary
+					collapsible={collapsible ? '1' : false}
+					collapsed-on-desktop={collapsedOnDesktop ? '1' : false}
+					order-summary-text={order_summary_text}
+					invoice-summary-text={invoice_summary_text}
+					collapsed-on-mobile={collapsedOnMobile ? '1' : false}
+				>
+					<InnerBlocks.Content />
+				</sc-order-summary>
+			);
+		},
+	},
 ];
