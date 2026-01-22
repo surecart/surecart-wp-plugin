38629c807bee96da3aaab37dd0e703c66941e201
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2026-01-22 17:52:58 +0530

    Add the speak region in the checkout block itself & removed from the order summary

--- packages/blocks/Blocks/CheckoutForm/Block.php
+++ packages/blocks/Blocks/CheckoutForm/Block.php
@@ -47,10 +47,24 @@ class Block extends BaseBlock {
 		$seen_forms[ $attributes['id'] ] = true;
 
 		global $sc_form_id;
-		$sc_form_id         = $attributes['id'];
-		$wrapper_attributes = get_block_wrapper_attributes( [ 'class' => $attributes['textalign'] ?? '' ] );
-		$result             = do_blocks( $form->post_content );
+		$sc_form_id = $attributes['id'];
 		unset( $seen_forms[ $attributes['id'] ] );
-		return '<div ' . $wrapper_attributes . '>' . $result . '</div>';
+		ob_start();
+		?>
+		<div 
+			<?php
+			echo wp_kses_data(
+				get_block_wrapper_attributes( [ 'class' => $attributes['textalign'] ?? '' ] )
+			)
+			?>
+		>
+			<?php echo do_blocks( $form->post_content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<!-- speak element -->
+			<p id="a11y-speak-intro-text" class="a11y-speak-intro-text" style="position: absolute;margin: -1px;padding: 0;height: 1px;width: 1px;overflow: hidden;clip: rect(1px, 1px, 1px, 1px);-webkit-clip-path: inset(50%);clip-path: inset(50%);border: 0;word-wrap: normal !important;"></p>
+			<div id="a11y-speak-assertive" class="a11y-speak-region" style="position: absolute;margin: -1px;padding: 0;height: 1px;width: 1px;overflow: hidden;clip: rect(1px, 1px, 1px, 1px);-webkit-clip-path: inset(50%);clip-path: inset(50%);border: 0;word-wrap: normal !important;" aria-live="assertive" aria-relevant="additions text" aria-atomic="true">&nbsp;</div>
+			<div id="a11y-speak-polite" class="a11y-speak-region" style="position: absolute;margin: -1px;padding: 0;height: 1px;width: 1px;overflow: hidden;clip: rect(1px, 1px, 1px, 1px);-webkit-clip-path: inset(50%);clip-path: inset(50%);border: 0;word-wrap: normal !important;" aria-live="polite" aria-relevant="additions text" aria-atomic="true"></div>
+		</div>
+		<?php
+		return ob_get_clean();
 	}
 }
