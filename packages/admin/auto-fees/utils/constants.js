c48bd7c574770b8b081034ef62847544f3c9c4a1
Andre Gagnon <ajgagnon@uwalumni.com>
2026-01-28 10:28:55 -0600

    Update text.

--- packages/admin/auto-fees/utils/constants.js
+++ packages/admin/auto-fees/utils/constants.js
@@ -134,8 +134,8 @@ export const operatorLabels = {
 };
 
 export const supportedValuesLabels = {
-	checkout: __('Checkout', 'surecart'),
-	subscription: __('Renewal', 'surecart'),
+	checkout: __('Initial Checkout', 'surecart'),
+	subscription: __('Renewal & Plan Changes', 'surecart'),
 };
 
 export const TYPE_CHOICES = [
@@ -170,7 +170,7 @@ export const TYPE_CHOICES = [
 
 export const APPLIES_WHILE_CHOICES = [
 	{
-		label: __('Initial Checkout', 'surecart'),
+		label: __('Initial checkout', 'surecart'),
 		value: 'initial',
 		description: __(
 			'Apply this dynamic price to the first purchase only.',
