c50a131a3cbcbc51bfc87c2bd7f6f29d1d756f40
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2026-02-17 21:49:16 +0530

    Only show the Import from Woo option when woocommerce plugin is active.

--- views/admin/products/import-products-button.php
+++ views/admin/products/import-products-button.php
@@ -2,14 +2,16 @@
 	<a href="<?php echo esc_attr( \SureCart::getUrl()->edit( 'product' ) ); ?>" class="button button-secondary" style="border-top-right-radius: 0; border-bottom-right-radius: 0;">
 		<?php esc_html_e( 'Add New', 'surecart' ); ?>
 	</a>
-	<sc-dropdown placement="bottom-end">
-		<button slot="trigger" class="button button-secondary" style="border-top-left-radius: 0; border-bottom-left-radius: 0;" aria-haspopup="true" aria-expanded="false" aria-label="<?php esc_attr_e( 'More Options', 'surecart' ); ?>">
-			<sc-icon name="chevron-down" style="vertical-align: middle; margin-top: -2px;"></sc-icon>
-		</button>
-		<sc-menu>
-			<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->import( 'products' ) ); ?>">
-				<?php esc_html_e( 'Import from Woo', 'surecart' ); ?>
-			</sc-menu-item>
-		</sc-menu>
-	</sc-dropdown>
+	<?php if ( class_exists( 'WooCommerce' ) ) : ?>
+		<sc-dropdown placement="bottom-end">
+			<button slot="trigger" class="button button-secondary" style="border-top-left-radius: 0; border-bottom-left-radius: 0;" aria-haspopup="true" aria-expanded="false" aria-label="<?php esc_attr_e( 'More Options', 'surecart' ); ?>">
+				<sc-icon name="chevron-down" style="vertical-align: middle; margin-top: -2px;"></sc-icon>
+			</button>
+			<sc-menu>
+				<sc-menu-item href="<?php echo esc_attr( \SureCart::getUrl()->import( 'products' ) ); ?>">
+					<?php esc_html_e( 'Import from Woo', 'surecart' ); ?>
+				</sc-menu-item>
+			</sc-menu>
+		</sc-dropdown>
+	<?php endif; ?>
 </div>
