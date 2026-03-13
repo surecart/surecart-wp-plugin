f301d551008fe8a5841cf2a887c58bfe8dda09f5
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2026-03-13 22:41:08 +0600

    added missing block deprecations for first name and last name

--- packages/blocks/Blocks/FirstName/deprecated.js
+++ packages/blocks/Blocks/FirstName/deprecated.js
@@ -1,45 +1,91 @@
-export default [
-	{
-		attributes: {
-			required: {
-				type: 'boolean',
-				default: false,
-			},
-			label: {
-				type: 'string',
-				default: 'First Name',
-			},
-			help: {
-				type: 'string',
-				default: '',
-			},
-			placeholder: {
-				type: 'string',
-			},
+// v2: apiVersion 2 -> 3 migration (className no longer auto-provided).
+const v2 = {
+	attributes: {
+		required: {
+			type: 'boolean',
+			default: false,
 		},
-		save({ attributes, className }) {
-			const {
-				label,
-				help,
-				autofocus,
-				placeholder,
-				showLabel,
-				size,
-				required,
-			} = attributes;
-
-			return (
-				<sc-customer-firstname
-					class={className || false}
-					label={label || false}
-					help={help || false}
-					autofocus={autofocus || false}
-					placeholder={placeholder || false}
-					showLabel={showLabel || false}
-					size={size || false}
-					required={required ? '1' : '0'}
-				></sc-customer-firstname>
-			);
+		label: {
+			type: 'string',
+			default: 'First Name',
+		},
+		help: {
+			type: 'string',
+			default: '',
+		},
+		placeholder: {
+			type: 'string',
 		},
 	},
-];
+	save({ className, attributes }) {
+		const {
+			label,
+			help,
+			autofocus,
+			placeholder,
+			showLabel,
+			size,
+			required,
+		} = attributes;
+
+		return (
+			<sc-customer-firstname
+				class={className || false}
+				label={label || false}
+				help={help || false}
+				autofocus={autofocus || false}
+				placeholder={placeholder || false}
+				showLabel={showLabel || false}
+				size={size || false}
+				required={required || false}
+			></sc-customer-firstname>
+		);
+	},
+};
+
+// v1: required attribute was stored as string '1'/'0' instead of boolean.
+const v1 = {
+	attributes: {
+		required: {
+			type: 'boolean',
+			default: false,
+		},
+		label: {
+			type: 'string',
+			default: 'First Name',
+		},
+		help: {
+			type: 'string',
+			default: '',
+		},
+		placeholder: {
+			type: 'string',
+		},
+	},
+	save({ attributes, className }) {
+		const {
+			label,
+			help,
+			autofocus,
+			placeholder,
+			showLabel,
+			size,
+			required,
+		} = attributes;
+
+		return (
+			<sc-customer-firstname
+				class={className || false}
+				label={label || false}
+				help={help || false}
+				autofocus={autofocus || false}
+				placeholder={placeholder || false}
+				showLabel={showLabel || false}
+				size={size || false}
+				required={required ? '1' : '0'}
+			></sc-customer-firstname>
+		);
+	},
+};
+
+export default [v2, v1];
