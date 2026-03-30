193f505a69c4fc5b3acf533e825f80e9957255c8
Rajkiran Gajanan Bagal <rajkiranbagal8585@gmail.com>
2026-03-30 13:32:59 +0530

    Revert switch_to_locale wrappers in TimeDate.php
    switch_to_locale(get_locale()) is a no-op on frontend and incorrectly
    overrides user locale preference in admin. Removing these wrappers
    restores the default WordPress locale behavior for date/time formatting.

--- app/src/Support/TimeDate.php
+++ app/src/Support/TimeDate.php
@@ -40,12 +40,7 @@ class TimeDate {
 	 * @return string
 	 */
 	public static function formatDate( $timestamp ) {
-		switch_to_locale( get_locale() );
-		try {
-			return wp_date( self::getDateFormat(), $timestamp );
-		} finally {
-			restore_previous_locale();
-		}
+		return wp_date( self::getDateFormat(), $timestamp );
 	}
 
 	/**
@@ -54,12 +49,7 @@ class TimeDate {
 	 * @return string
 	 */
 	public static function formatTime( $timestamp ) {
-		switch_to_locale( get_locale() );
-		try {
-			return wp_date( self::getTimeFormat(), $timestamp );
-		} finally {
-			restore_previous_locale();
-		}
+		return wp_date( self::getTimeFormat(), $timestamp );
 	}
 
 	/**
@@ -79,19 +69,14 @@ class TimeDate {
 	 * @return string
 	 */
 	public static function humanTimeDiff( $timestamp, $ignore_after = '1 day' ) {
-		switch_to_locale( get_locale() );
-		try {
-			if ( $timestamp > strtotime( "-$ignore_after", time() ) ) {
-				return sprintf(
-				/* translators: %s: human-readable time difference */
-					_x( '%s ago', '%s = human-readable time difference', 'surecart' ),
-					human_time_diff( $timestamp, time() )
-				);
-			} else {
-				return self::formatDate( $timestamp );
-			}
-		} finally {
-			restore_previous_locale();
+		if ( $timestamp > strtotime( "-$ignore_after", time() ) ) {
+			return sprintf(
+			/* translators: %s: human-readable time difference */
+				_x( '%s ago', '%s = human-readable time difference', 'surecart' ),
+				human_time_diff( $timestamp, time() )
+			);
+		} else {
+			return self::formatDate( $timestamp );
 		}
 	}
 
