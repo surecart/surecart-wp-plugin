4df0d16b23b8f9d2f999f02b5bb422146707acb5
Andre Gagnon <ajgagnon@uwalumni.com>
2026-04-08 14:08:39 -0500

    Fix pot memory issue.

--- translate.js
+++ translate.js
@@ -1,30 +1,31 @@
 var shell = require('shelljs');
 
+// Peast JS parsing can exceed PHP's default 128M; WP-CLI reads this in bin/wp.
+var potEnv = Object.assign({}, process.env, {
+	WP_CLI_PHP_ARGS: '-d memory_limit=512M',
+});
+
+function makePot(args) {
+	shell.exec('./vendor/bin/wp i18n make-pot ' + args, { env: potEnv });
+}
+
 // app directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./app languages/surecart.pot --ignore-domain --skip-audit'
-);
+makePot('./app languages/surecart.pot --ignore-domain --skip-audit');
 // views directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./views languages/surecart.pot --ignore-domain --merge --skip-audit'
-);
+makePot('./views languages/surecart.pot --ignore-domain --merge --skip-audit');
 // templates directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./templates languages/surecart.pot --ignore-domain --merge --skip-audit'
-);
+makePot('./templates languages/surecart.pot --ignore-domain --merge --skip-audit');
 // admin directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./packages/admin languages/surecart.pot --ignore-domain --merge --skip-audit'
-);
+makePot('./packages/admin languages/surecart.pot --ignore-domain --merge --skip-audit');
 // blocks directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./packages/blocks --exclude=node_modules,dist languages/surecart.pot --ignore-domain --merge --skip-audit'
+makePot(
+	'./packages/blocks --exclude=node_modules,dist languages/surecart.pot --ignore-domain --merge --skip-audit'
 );
 // blocks-next directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./packages/blocks-next --exclude=node_modules,dist,build languages/surecart.pot --ignore-domain --merge --skip-audit'
+makePot(
+	'./packages/blocks-next --exclude=node_modules,dist,build languages/surecart.pot --ignore-domain --merge --skip-audit'
 );
 // components directory
-shell.exec(
-	'./vendor/bin/wp i18n make-pot ./packages/components/dist/components --include=*.js languages/surecart.pot --ignore-domain --merge --skip-audit'
+makePot(
+	'./packages/components/dist/components --include=*.js languages/surecart.pot --ignore-domain --merge --skip-audit'
 );
