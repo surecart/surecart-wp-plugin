9172b6504fcc76e03ce27cee1303f8c7275e6eb7
mzakash <m.akash.cse@gmail.com>
2026-05-15 22:00:12 +0600

    remove bundle item price block

--- packages/blocks-next/src/blocks/bundle-item-price/controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-$bundle_item = $block->context['surecart/bundleItem'] ?? null;
-if ( empty( $bundle_item ) ) {
-	return '';
-}
-
-return 'file:./view.php';
--- packages/blocks-next/src/blocks/bundle-item-price/view.php
+++
@@ -1,3 +0,0 @@
-<span <?php echo wp_kses_data( get_block_wrapper_attributes( array( 'class' => 'sc-bundle-item__included' ) ) ); ?>>
-	<?php echo esc_html__( 'Included', 'surecart' ); ?>
-</span>
