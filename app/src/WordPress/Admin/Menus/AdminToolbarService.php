64f491eae3bce07b3444705f95059571d343698a
mzakash <m.akash.cse@gmail.com>
2026-06-01 19:17:40 +0600

    feat(admin/bundle): bundle-aware admin toolbar edit link
    Detect bundles in the front-end admin toolbar and switch the node
    titles (Edit Bundle / Bundle Details) and edit URL key accordingly.

--- app/src/WordPress/Admin/Menus/AdminToolbarService.php
+++ app/src/WordPress/Admin/Menus/AdminToolbarService.php
@@ -279,22 +279,25 @@ class AdminToolbarService {
 		</style>
 
 		<?php
-		$product = sc_get_product();
+		$product   = sc_get_product();
+		$is_bundle = ! empty( $product->bundle );
+		$url_key   = $is_bundle ? 'bundle' : 'product';
+
 		$wp_admin_bar->add_node(
 			[
 				'id'    => 'surecart-toolbar',
-				'title' => __( 'Edit Product', 'surecart' ),
+				'title' => $is_bundle ? __( 'Edit Bundle', 'surecart' ) : __( 'Edit Product', 'surecart' ),
 				'href'  => '#',
 			]
 		);
 
-		// Add product edit link.
+		// Add product/bundle edit link.
 		$wp_admin_bar->add_node(
 			[
 				'id'     => 'surecart-edit-product',
 				'parent' => 'surecart-toolbar',
-				'title'  => 'Product Details',
-				'href'   => \SureCart::getUrl()->edit( 'product', $product->id ),
+				'title'  => $is_bundle ? __( 'Bundle Details', 'surecart' ) : __( 'Product Details', 'surecart' ),
+				'href'   => \SureCart::getUrl()->edit( $url_key, $product->id ),
 			]
 		);
 
