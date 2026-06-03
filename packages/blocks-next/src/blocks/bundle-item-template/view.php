98bab3169812df9a50b76985e576f13c8cb0c5eb
Andre Gagnon <ajgagnon@uwalumni.com>
2026-06-03 11:43:49 -0500

    Change urls to use slug.

--- packages/blocks-next/src/blocks/bundle-item-template/view.php
+++ packages/blocks-next/src/blocks/bundle-item-template/view.php
@@ -63,6 +63,7 @@ $render_inner_blocks = static function ( $current_option = null ) use ( $block )
 $component_context = wp_interactivity_data_wp_context(
 	array(
 		'componentProductId'         => $component->id ?? null,
+		'componentProductSlug'       => ! empty( $component->slug ) ? $component->slug : ( $component->id ?? null ),
 		'componentVariants'          => $variants_payload,
 		'componentHasUnlimitedStock' => ! empty( $component->has_unlimited_stock ),
 		'componentOptionValues'      => $initial_option_values,
