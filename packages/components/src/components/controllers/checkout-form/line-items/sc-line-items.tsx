ea4ae018acf91f96a63c10ea243906fb7c5047d6
mzakash <m.akash.cse@gmail.com>
2026-06-11 19:52:21 +0600

    Export description and trial-fees parts on checkout line items

--- packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
+++ packages/components/src/components/controllers/checkout-form/line-items/sc-line-items.tsx
@@ -15,6 +15,8 @@ import { getMaxStockQuantity } from '../../../../functions/quantity';
  * @part line-item__text - The line item text
  * @part line-item__title - The line item title
  * @part line-item__suffix - The line item suffix
+ * @part line-item__description - The line item description (variant, price name, SKU)
+ * @part line-item__trial-fees - The line item trial and fees
  * @part line-item__price - The line item price
  * @part line-item__price-amount - The line item price amount
  * @part line-item__price-description - The line item price description
@@ -27,7 +29,6 @@ import { getMaxStockQuantity } from '../../../../functions/quantity';
  * @part line-item__quantity-plus - The line item quantity plus
  * @part line-item__quantity-plus-icon - The line item quantity plus icon
  * @part line-item__quantity-input - The line item quantity input
- * @part line-item__price-description - The line item price description
  */
 
 @Component({
@@ -102,7 +103,7 @@ export class ScLineItems {
                 removable={!item?.locked && this.removable}
                 onScUpdateQuantity={e => updateCheckoutLineItem({ id: item.id, data: { quantity: e.detail } })}
                 onScRemove={() => removeCheckoutLineItem(item?.id)}
-                exportparts="base:line-item, product-line-item, image:line-item__image, placeholder__image: line-item__placeholder-image, text:line-item__text, title:line-item__title, suffix:line-item__suffix, price:line-item__price, price__amount:line-item__price-amount, price__description:line-item__price-description, price__scratch:line-item__price-scratch, static-quantity:line-item__static-quantity, remove-icon__base:line-item__remove-icon, quantity:line-item__quantity, quantity__minus:line-item__quantity-minus, quantity__minus-icon:line-item__quantity-minus-icon, quantity__plus:line-item__quantity-plus, quantity__plus-icon:line-item__quantity-plus-icon, quantity__input:line-item__quantity-input, line-item__price-description:line-item__price-description"
+                exportparts="base:line-item, product-line-item, image:line-item__image, placeholder__image: line-item__placeholder-image, text:line-item__text, title:line-item__title, suffix:line-item__suffix, description:line-item__description, trial-fees:line-item__trial-fees, price:line-item__price, price__amount:line-item__price-amount, price__description:line-item__price-description, price__scratch:line-item__price-scratch, static-quantity:line-item__static-quantity, remove-icon__base:line-item__remove-icon, quantity:line-item__quantity, quantity__minus:line-item__quantity-minus, quantity__minus-icon:line-item__quantity-minus-icon, quantity__plus:line-item__quantity-plus, quantity__plus-icon:line-item__quantity-plus-icon, quantity__input:line-item__quantity-input"
               />
               <sc-swap lineItem={item} />
             </div>
